6d04e32535a0c0419ba55acbddb03c8f3132c388
Lance Paine <lance@lbpaine.com>
2016-09-14 12:34:11 +0100

    DRT-4381 refactor DeskRecsChart to make it readable.

--- client/src/main/scala/spatutorial/client/components/DeskRecsChart.scala
+++ client/src/main/scala/spatutorial/client/components/DeskRecsChart.scala
@@ -1,8 +1,10 @@
 package spatutorial.client.components
 
+import diode.ActionType
 import diode.data.Pot
 import diode.react.ModelProxy
 import japgolly.scalajs.react.{Callback, ReactComponentB}
+import org.scalajs.dom.svg.A
 import spatutorial.client.components.Bootstrap.{Button, CommonStyle, Panel}
 import spatutorial.client.services.{ChangeDeskUsage, UpdateCrunch, UpdateMotd}
 import spatutorial.shared.{CrunchResult, SimulationResult}
@@ -23,50 +25,67 @@ object DeskRecsChart {
   log.info("initialising deskrecschart")
 
   def DeskRecs(labels: IndexedSeq[String]) = ReactComponentB[ModelProxy[DeskRecsModel]]("CrunchResults")
-    .render_P { proxy =>
+    .render_P { proxy => {
+      val potCrunchResult: Pot[CrunchResult] = proxy().potCrunchResult
       Panel(Panel.Props("Desk Recommendations and Wait times"),
-        proxy().crunchProxy.renderPending(_ >= 500, _ => <.p("Waiting for data")),
-        proxy().crunchProxy.render(chartData =>
-          Chart(Chart.ChartProps("Desk Recs",
-            Chart.LineChart,
-            ChartData(takeEvery15th(labels), Seq(
-              ChartDataset(
-                takeEvery15th(chartData.recommendedDesks).map(_.toDouble), "Desk Recommendations")))
-          ))),
-        proxy().crunchProxy.render(chartData => {
-          val sampledWaitTimes = chartData.waitTimes.grouped(15).map(_.max).map(_.toDouble).toList
-          val sampledLabels = takeEvery15th(labels)
-          Chart(Chart.ChartProps("Wait Times",
-            Chart.LineChart,
-            ChartData(sampledLabels, Seq(ChartDataset(sampledWaitTimes, "Wait Times")))
-          ))
-        }),
-        proxy().crunchProxy.render(crunchResult => {
-          def inputChange(idx: Int)(e: ReactEventI) = {
-            val ev = e.target.value
-            log.info(s"direct call in callback ${idx} ${ev}")
-            Callback.log(s"callback from outside", ev, idx)
-            proxy.dispatch(ChangeDeskUsage(ev, idx))
-          }
-
-          val rds = takeEvery15th(crunchResult.recommendedDesks)
-          val skippedLabels = takeEvery15th(labels)
-          val zip: Seq[((String, Int), Int)] = skippedLabels.zip(rds).zipWithIndex
-          <.ul(zip.map { case (dr, idx) => {
-            <.li(<.span(dr._1.toString(),
-              <.input.number(^.value := dr._2,
-                ^.onChange ==> inputChange(idx))))
-          }
-          })
-        }),
+        potCrunchResult.renderPending(_ >= 500, _ => <.p("Waiting for data")),
+        deskRecsChart(labels, potCrunchResult),
+        waitTimesChart(labels, potCrunchResult),
+        deskSimulationInputs(labels, potCrunchResult, proxy.dispatch),
         Button(Button.Props(proxy.dispatch(UpdateCrunch()), CommonStyle.danger), Icon.refresh, "Update")
       )
+    }
     }.componentDidMount(scope =>
     Callback.log("Mounted DeskRecs")
   ).build
 
+  def deskSimulationInputs(labels: IndexedSeq[String], potCrunchResult: Pot[CrunchResult], dispatch: Any => Callback): ReactNode = {
+    def inputChange(idx: Int)(e: ReactEventI) = {
+      val ev = e.target.value
+      log.info(s"direct call in callback ${idx} ${ev}")
+      Callback.log(s"callback from outside", ev, idx)
+      dispatch(ChangeDeskUsage(ev, idx))
+    }
+
+    potCrunchResult.render(crunchResult => {
+      val rds = takeEvery15th(crunchResult.recommendedDesks)
+      val skippedLabels = takeEvery15th(labels)
+      val zip: Seq[((String, Int), Int)] = skippedLabels.zip(rds).zipWithIndex
+      <.ul(zip.map { case (dr, idx) => {
+        <.li(<.span(dr._1.toString(),
+          <.input.number(^.value := dr._2,
+            ^.onChange ==> inputChange(idx))))
+      }
+      })
+    })
+  }
+
+  def waitTimesChart(labels: IndexedSeq[String], potCrunchResult: Pot[CrunchResult]): ReactNode = {
+    potCrunchResult.render(chartData => {
+      val grouped: Iterator[Seq[Int]] = chartData.waitTimes.grouped(15)
+      val maxInEachGroup: Iterator[Int] = grouped.map(_.max)
+      val sampledWaitTimes = maxInEachGroup.map(_.toDouble).toList
+      val sampledLabels = takeEvery15th(labels)
+      Chart(Chart.ChartProps("Wait Times",
+        Chart.LineChart,
+        ChartData(sampledLabels, Seq(ChartDataset(sampledWaitTimes, "Wait Times")))
+      ))
+    })
+  }
+
+  def deskRecsChart(labels: IndexedSeq[String], potCrunchResult: Pot[CrunchResult]): ReactNode = {
+    potCrunchResult.render(chartData =>
+      Chart(Chart.ChartProps("Desk Recs",
+        Chart.LineChart,
+        ChartData(takeEvery15th(labels), Seq(
+          ChartDataset(
+            takeEvery15th(chartData.recommendedDesks).map(_.toDouble), "Desk Recommendations")))
+      )))
+  }
+
   def takeEvery15th[N](desks: IndexedSeq[N]) = desks.zipWithIndex.collect {
     case (n, i) if (i % 15 == 0) => n
   }
+
   def apply(labels: IndexedSeq[String], proxy: ModelProxy[DeskRecsModel]) = DeskRecs(labels)(proxy)
 }
