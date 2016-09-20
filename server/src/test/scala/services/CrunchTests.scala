3295af122bf5673d5187ca39c6682ccbb7dff851
Rich Birch <rich@freshcodelimited.com>
2016-09-20 11:58:27 +0100

    DRT-4392 Start failing test

--- server/src/test/scala/services/CrunchTests.scala
+++ server/src/test/scala/services/CrunchTests.scala
@@ -13,35 +13,21 @@ import scala.util.Random
 
 object CrunchTests extends TestSuite {
   def tests = TestSuite {
-//    'canCrunch - {
-//      val blockWidth = 15
-//      val workloads = Iterator.continually(Random.nextDouble() * 20).take(100 * blockWidth).toSeq
-//      val cr = TryRenjin.crunch(workloads)
-//      println(cr)
-//      assert(true)
+//    'canUseCsvForWorkloadInput - {
+//      val bufferedSource = scala.io.Source.fromFile("optimiser-LHR-T2-NON-EEA-2016-09-12_121059-in-out.csv")
+//      val recs: List[Array[String]] = bufferedSource.getLines.map { l =>
+//        l.split(",").map(_.trim)
+//      }.toList
+//
+//      val workloads = recs.map(_(0).toDouble)
+//      val minDesks = recs.map(_(1).toInt)
+//      val maxDesks = recs.map(_(2).toInt)
+//      val recDesks = recs.map(_(3).toInt)
+//
+//      val cr = TryRenjin.crunch(workloads, minDesks, maxDesks)
+//      println(cr.recommendedDesks.toString())
+//      println(recDesks.toVector)
 //    }
-//    'anotherOne - {
-//      val workload = (0 until 30 * 15) map (_ => 23.0d)
-//      val cr = TryRenjin.crunch(workload)
-//      val cr2 = TryRenjin.crunch(workload)
-//      val cr3 = TryRenjin.crunch(Nil)
-//      assert(true)
-//    }
-    'canUseCsvForWorkloadInput - {
-      val bufferedSource = scala.io.Source.fromFile("optimiser-LHR-T2-NON-EEA-2016-09-12_121059-in-out.csv")
-      val recs: List[Array[String]] = bufferedSource.getLines.map { l =>
-        l.split(",").map(_.trim)
-      }.toList
-
-      val workloads = recs.map(_(0).toDouble)
-      val minDesks = recs.map(_(1).toInt)
-      val maxDesks = recs.map(_(2).toInt)
-      val recDesks = recs.map(_(3).toInt)
-
-      val cr = TryRenjin.crunch(workloads, minDesks, maxDesks)
-      println(cr.recommendedDesks.toString())
-      println(recDesks.toVector)
-    }
   }
 }
 
--- server/src/test/scala/services/WorkloadCalculatorTests.scala
+++ server/src/test/scala/services/WorkloadCalculatorTests.scala
@@ -1,35 +1,103 @@
 package services
 
+import org.joda.time.DateTime
+import services.PassengerInfoRouterActor.{PaxTypeAndQueueCount, VoyagePaxSplits, VoyagesPaxSplits}
+import services.PassengerQueueTypes.{Desks, PaxTypes}
+import services.PaxLoad.PaxType
+import spatutorial.shared.ApiFlight
 import utest.{TestSuite, _}
 
-//object PaxLoadCalculator {
-//  val paxType = (PassengerQueueTypes.PaxTypes.EEAMACHINEREADABLE, PassengerQueueTypes.Desks.eeaDesk)
-//
-//  def calculateVoyagePaxLoadByDesk(voyagePaxSplits: VoyagePaxSplits, flowRate: => Int): Map[Symbol, Seq[PaxLoad]] = {
-//    val firstMinute = voyagePaxSplits.scheduledArrivalDateTime
-//    val groupedByDesk: Map[Symbol, Seq[PaxTypeAndQueueCount]] = voyagePaxSplits.paxSplits.groupBy(_.queueType)
-//    groupedByDesk.mapValues(
-//      (paxTypeAndCount: Seq[PaxTypeAndQueueCount]) => {
-//        val totalPax = paxTypeAndCount.map(_.paxCount).sum
-//        val headPaxType = paxTypeAndCount.head
-//        calcPaxLoad(firstMinute, totalPax, (headPaxType.passengerType, headPaxType.queueType), flowRate).reverse
-//      }
-//    )
-//  }
-//
-//  def calcPaxLoad(currMinute: DateTime, remaining: Int, paxType: PaxType, flowRate: Int): List[PaxLoad] = {
-//    if (remaining <= flowRate)
-//      PaxLoad(currMinute, remaining, paxType) :: Nil
-//    else
-//      calcPaxLoad(currMinute + 60000, remaining - flowRate, paxType, flowRate) :::
-//        PaxLoad(currMinute, flowRate, paxType) :: Nil
-//  }
-//}
+
+object PaxLoad {
+  type PaxType = (String, Symbol)
+}
+case class PaxLoad(time: DateTime, nbPax: Int, paxType: PaxType) {
+
+}
+
+object PaxLoadCalculator {
+  val paxType = (PassengerQueueTypes.PaxTypes.EEAMACHINEREADABLE, PassengerQueueTypes.Desks.eeaDesk)
+
+  def flightWithSplits(apiFlight: ApiFlight): VoyagePaxSplits = {
+    VoyagePaxSplits("STN", "RY", "1234", 1, DateTime.now, List(
+      PaxTypeAndQueueCount(PaxTypes.EEAMACHINEREADABLE, Desks.eeaDesk, 1)
+    ))
+  }
+
+  def calculateVoyagePaxLoadByDesk(voyagePaxSplits: VoyagePaxSplits, flowRate: => Int): Map[Symbol, Seq[PaxLoad]] = {
+    val firstMinute = voyagePaxSplits.scheduledArrivalDateTime
+    val groupedByDesk: Map[Symbol, Seq[PaxTypeAndQueueCount]] = voyagePaxSplits.paxSplits.groupBy(_.queueType)
+    groupedByDesk.mapValues(
+      (paxTypeAndCount: Seq[PaxTypeAndQueueCount]) => {
+        val totalPax = paxTypeAndCount.map(_.paxCount).sum
+        val headPaxType = paxTypeAndCount.head
+        calcPaxLoad(firstMinute, totalPax, (headPaxType.passengerType, headPaxType.queueType), flowRate).reverse
+      }
+    )
+  }
+
+  def calcPaxLoad(currMinute: DateTime, remaining: Int, paxType: PaxType, flowRate: Int): List[PaxLoad] = {
+    if (remaining <= flowRate)
+      PaxLoad(currMinute, remaining, paxType) :: Nil
+    else
+      calcPaxLoad(currMinute.plusMinutes(1) , remaining - flowRate, paxType, flowRate) :::
+        PaxLoad(currMinute, flowRate, paxType) :: Nil
+  }
+}
+
+object PassengerQueueTypes {
+  object Desks {
+    val eeaDesk = 'desk
+    val egate = 'egate
+    val nationalsDesk = 'nationalsDesk
+  }
+
+  object PaxTypes {
+    val EEANONMACHINEREADABLE = "eea-non-machine-readable"
+    val NATIONALVISA = "national-visa"
+    val EEAMACHINEREADABLE = "eea-machine-readable"
+    val NATIONALNONVISA = "national-non-visa"
+  }
+
+  val egatePercentage = 0.6
+
+  type QueueType = Symbol
+  //  type PaxTypeAndQueue = (PassengerType, QueueType)
+  case class PaxTypeAndQueueCount(passengerType: String, queueType: Symbol, paxCount: Int)
+  type PaxTypeAndQueueCounts = Seq[PaxTypeAndQueueCount]
+}
+
+object PassengerInfoRouterActor {
+  type PaxTypeAndQueueCounts = Seq[PaxTypeAndQueueCount]
+
+  case class VoyagePaxSplits(destinationPort: String, carrierCode: String,
+                             voyageNumber: String,
+                             totalPaxCount: Int,
+                             scheduledArrivalDateTime: DateTime,
+                             paxSplits: PaxTypeAndQueueCounts)
+  case class VoyagesPaxSplits(voyageSplits: List[VoyagePaxSplits])
+
+  case class PaxTypeAndQueueCount(passengerType: String, queueType: Symbol, paxCount: Int)
+}
 
 object WorkloadCalculatorTests extends TestSuite{
   def tests = TestSuite {
     'WorkloadCalculator - {
-      assert(false)
+
+      val flights = List(
+        ApiFlight(Operator = "BA", Status = "", EstDT = "", ActDT = "", EstChoxDT = "", ActChoxDT = "", Gate = "", Stand = "", MaxPax = 1, ActPax = 100, TranPax = 0, RunwayID = "", BaggageReclaimId = "", FlightID = 1, AirportID = "", Terminal = "", ICAO = "", IATA = "", Origin = "", SchDT = "2020-01-01 00:00:00")
+      )
+      val voysPaxSplits = VoyagesPaxSplits(List(
+        VoyagePaxSplits("STN", "RY", "1234", 1, DateTime.now, List(
+          PaxTypeAndQueueCount(PaxTypes.EEAMACHINEREADABLE, Desks.eeaDesk, 1)
+        )),
+        VoyagePaxSplits("STN", "RY", "1234", 1, DateTime.now, List(
+          PaxTypeAndQueueCount(PaxTypes.EEAMACHINEREADABLE, Desks.eeaDesk, 1)
+        ))
+      ))
+      val loads = voysPaxSplits.voyageSplits
+        .map(voyPaxSplits => PaxLoadCalculator.calculateVoyagePaxLoadByDesk(voyPaxSplits, 20))
+      assert(loads == Nil)
     }
   }
 }
