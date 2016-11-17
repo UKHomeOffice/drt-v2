ac4c732b569cec42668b092b06a7e0d715f2d77c
Ben Eppel <b.eppel@gmail.com>
2016-11-17 16:18:53 +0000

    DRT-4419 Add support to deploy multiple ports

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,7 +12,7 @@ while getopts ":a:b:h:p:" opt; do
       JVA_HOST=$OPTARG
       ;;
     p)
-      PORT_CODE=${OPTARG,,}
+      PORT_CODES=${OPTARG,,}
       ;;
     \?)
       echo "Invalid option: -$OPTARG" >&2
@@ -24,16 +24,20 @@ echo PIPELINE_BUILD_ID ${PIPELINE_BUILD_ID}
 echo PORT_CODE ${PORT_CODE}
 TARGET_NAME=drt-${PIPELINE_BUILD_ID}
 TARGET_NAME_ZIP="${TARGET_NAME}.zip"
-LOCAL_NAME_ZIP="$PORT_CODE-${TARGET_NAME}.zip"
 ARTIFACT_PATH=server/target/universal/$TARGET_NAME_ZIP
 
-#Download build artifact to target location
-curl ${ARTIFACT_REPO_URL}/${ARTIFACT_PATH} > $LOCAL_NAME_ZIP
+for PORT_CODE in $(echo $PORT_CODES | tr "," "\n")
+do
+    LOCAL_NAME_ZIP="$PORT_CODE-${TARGET_NAME}.zip"
 
-scp $LOCAL_NAME_ZIP ci-build@${JVA_HOST}:/home/ci-build/
-ssh ci-build@${JVA_HOST} "sudo unzip /home/ci-build/${LOCAL_NAME_ZIP} -d /usr/share/drt-v2/${PORT_CODE}/"
-ssh ci-build@${JVA_HOST} "sudo rm -f /usr/share/drt-v2/${PORT_CODE}/current && sudo ln -s /usr/share/drt-v2/${PORT_CODE}/${TARGET_NAME} /usr/share/drt-v2/${PORT_CODE}/current"
-ssh ci-build@${JVA_HOST} "sudo service drt-v2-${PORT_CODE} restart"
+    #Download build artifact to target location
+    curl ${ARTIFACT_REPO_URL}/${ARTIFACT_PATH} > $LOCAL_NAME_ZIP
 
-#clean up
-ssh ci-build@${JVA_HOST} "sudo rm /home/ci-build/${LOCAL_NAME_ZIP}"
+    scp $LOCAL_NAME_ZIP ci-build@${JVA_HOST}:/home/ci-build/
+    ssh ci-build@${JVA_HOST} "sudo unzip /home/ci-build/${LOCAL_NAME_ZIP} -d /usr/share/drt-v2/${PORT_CODE}/"
+    ssh ci-build@${JVA_HOST} "sudo rm -f /usr/share/drt-v2/${PORT_CODE}/current && sudo ln -s /usr/share/drt-v2/${PORT_CODE}/${TARGET_NAME} /usr/share/drt-v2/${PORT_CODE}/current"
+    ssh ci-build@${JVA_HOST} "sudo service drt-v2-${PORT_CODE} restart"
+
+    #clean up
+    ssh ci-build@${JVA_HOST} "sudo rm /home/ci-build/${LOCAL_NAME_ZIP}"
+done
