33dc118649c89a48632423848487a6f16aefd833
Rich Birch <rich@freshcodelimited.com>
2016-11-17 10:30:07 +0000

    Fix deplot script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,7 +12,7 @@ while getopts ":a:b:h:p:" opt; do
       JVA_HOST=$OPTARG
       ;;
     p)
-      PORT_CODE=$OPTARG
+      PORT_CODE=${OPTARG,,}
       ;;
     \?)
       echo "Invalid option: -$OPTARG" >&2
@@ -23,16 +23,17 @@ done
 echo PIPELINE_BUILD_ID ${PIPELINE_BUILD_ID}
 echo PORT_CODE ${PORT_CODE}
 TARGET_NAME=drt-${PIPELINE_BUILD_ID}
-TARGET_NAME_ZIP="${PORT_CODE}-${TARGET_NAME}.zip"
+TARGET_NAME_ZIP="${TARGET_NAME}.zip"
+LOCAL_NAME_ZIP="$PORT_CODE-${TARGET_NAME}.zip"
 ARTIFACT_PATH=server/target/universal/$TARGET_NAME_ZIP
 
 #Download build artifact to target location
-curl ${ARTIFACT_REPO_URL}/${ARTIFACT_PATH} > $TARGET_NAME_ZIP
+curl ${ARTIFACT_REPO_URL}/${ARTIFACT_PATH} > $LOCAL_NAME_ZIP
 
-scp $TARGET_NAME_ZIP ci-build@${JVA_HOST}:/home/ci-build/
-ssh ci-build@${JVA_HOST} "sudo unzip /home/ci-build/${TARGET_NAME_ZIP} -d /usr/share/drt-v2/${PORT_CODE}/"
+scp $LOCAL_NAME_ZIP ci-build@${JVA_HOST}:/home/ci-build/
+ssh ci-build@${JVA_HOST} "sudo unzip /home/ci-build/${LOCAL_NAME_ZIP} -d /usr/share/drt-v2/${PORT_CODE}/"
 ssh ci-build@${JVA_HOST} "sudo rm -f /usr/share/drt-v2/${PORT_CODE}/current && sudo ln -s /usr/share/drt-v2/${PORT_CODE}/${TARGET_NAME} /usr/share/drt-v2/${PORT_CODE}/current"
 ssh ci-build@${JVA_HOST} "sudo service drt-v2-${PORT_CODE} restart"
 
 #clean up
-ssh ci-build@${JVA_HOST} "sudo rm /home/ci-build/${TARGET_NAME_ZIP}"
+ssh ci-build@${JVA_HOST} "sudo rm /home/ci-build/${LOCAL_NAME_ZIP}"
