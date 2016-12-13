3a1ecf812a62368b09090f63ac8fd97ea6ebbc84
Rich Birch <rich@freshcodelimited.com>
2016-12-13 10:59:47 +0000

    DRT-4440 Tidy up dead code

--- server/src/main/scala/services/ApiService.scala
+++ server/src/main/scala/services/ApiService.scala
@@ -18,9 +18,6 @@ import scala.io.Codec
 import scala.util.{Success, Try}
 import spatutorial.shared.HasAirportConfig
 
-//  case class Row(id: Int, city: String, city2: String, country: String, code1: String, code2: String, loc1: Double,
-//                 loc2: Double, elevation: Double,dkDouble: Double, dk: String, tz: String)
-
 
 trait AirportToCountryLike {
   lazy val airportInfo: Map[String, AirportInfo] = {
@@ -63,21 +60,6 @@ object AirportToCountry extends AirportToCountryLike {
 
 }
 
-//trait LegacyCrunchOnDemand {
-//
-//  private val log = LoggerFactory.getLogger(getClass)
-//
-//  def tryCrunch(terminalName: TerminalName, queueName: String, workloads: List[Double], airportConfig: AirportConfig): Try[CrunchResult] = {
-//    log.info(s"Crunch requested for $terminalName, $queueName, Workloads: ${workloads.take(15).mkString("(", ",", ")")}...")
-//    val repeat = List.fill[Int](workloads.length) _
-//    val optimizerConfig = OptimizerConfig(airportConfig.slaByQueue(queueName))
-//    //todo take the maximum desks from some durable store
-//    val minimumDesks: List[Int] = repeat(2)
-//    val maximumDesks: List[Int] = repeat(25)
-//    TryRenjin.crunch(workloads, minimumDesks, maximumDesks, optimizerConfig)
-//  }
-//}
-
 abstract class ApiService(airportConfig: AirportConfig)
   extends Api
     with WorkloadsCalculator
@@ -107,9 +89,13 @@ abstract class ApiService(airportConfig: AirportConfig)
   }
 
   override def processWork(terminalName: TerminalName, queueName: QueueName, workloads: List[Double], desks: List[Int]): SimulationResult = {
-    val fulldesks: List[Int] = desks.flatMap(x => List.fill(15)(x))
+    val fulldesks: List[Int] = if (queueName == "eGate")
+      desks.flatMap(x => List.fill(15)(x * 5))
+    else
+      desks.flatMap(x => List.fill(15)(x))
 
     val optimizerConfig = OptimizerConfig(airportConfig.slaByQueue(queueName))
+
     TryRenjin.processWork(workloads, fulldesks, optimizerConfig)
   }
 
@@ -156,46 +142,3 @@ trait ActorBackedCrunchService {
     }
   }
 }
-
-//abstract class ApiService
-//  extends Api
-//    with WorkloadsService
-//    with CrunchCalculator
-//    with ActorBackedCrunchService
-//    with FlightsService
-//    with AirportToCountryLike
-//    with AirportConfig
-//    with CrunchResultProvider {
-//  config: HasAirportConfig =>
-//
-//
-//  //  val log: LoggingAdapter = Logging.getLogger(this)
-//  ////  var todos: List[DeskRecTimeslot] = Nil
-//
-//  def crunch(terminalName: TerminalName, queueName: QueueName, workloads: List[Double]) = {
-//    Future.fromTry(tryCrunch(terminalName, queueName, workloads))
-//  }
-//
-//  def getLatestCrunch(terminalName: TerminalName, queueName: QueueName): Future[CrunchResult] = {
-//    tryCrunch(terminalName, queueName)
-//  }
-//
-//  override def welcomeMsg(name: String): String = {
-//    println("welcomeMsg")
-//    s"Welcome to SPA, $name! Time is now ${
-//      new Date
-//    }"
-//  }
-//
-//
-//  override def processWork(terminalName: TerminalName, queueName: QueueName, workloads: List[Double], desks: List[Int]): SimulationResult = {
-//    val fulldesks: List[Int] = desks.flatMap(x => List.fill(15)(x))
-//
-//    val optimizerConfig = OptimizerConfig(airportConfig.slaByQueue(queueName))
-//    TryRenjin.processWork(workloads, fulldesks, optimizerConfig)
-//  }
-//
-//  def airportConfiguration: AirportConfig = {
-//    config.airportConfig
-//  }
-//airportConfig}
