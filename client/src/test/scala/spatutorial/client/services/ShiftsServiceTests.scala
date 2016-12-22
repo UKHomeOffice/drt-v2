5cec8f1c376e5ea5a29ec49b0d55fb6d7051303f
Lance Paine <lance@lbpaine.com>
2016-12-22 12:00:38 +0000

    DRT-4457 add staff movements, and js test timer fn

--- client/src/test/scala/spatutorial/client/services/ShiftsServiceTests.scala
+++ client/src/test/scala/spatutorial/client/services/ShiftsServiceTests.scala
@@ -1,6 +1,7 @@
 package spatutorial.client.services
 
 import spatutorial.client.services.JSDateConversions.SDate
+import spatutorial.shared.FlightsApi.QueueName
 import spatutorial.shared.{WorkloadsHelpers, MilliDate}
 import utest._
 
@@ -66,101 +67,102 @@ object ShiftsServiceTests extends TestSuite {
         }
 
         "Given all the shifts, how fast is it?" - {
-          val shiftsRawTsv = """
-            |Alpha 1 ODM	01/12/16	06:30	15:18
-            |Alpha 1with ODM	01/12/16	06:30	15:18
-            |Alpha 2 /Customs from 0845
-            |"Alpha + Immigration, Assurance"	01/12/16	08:00	16:48
-            |"Alpha + Immigration, Assurance"	01/12/16	08:00	16:48
-            |
-            |0700-1100	01/12/16	07:00	11:00
-            |0700-1100	01/12/16	07:00	11:00
-            |0700-1100	01/12/16	07:00	11:00
-            |0700-1100	01/12/16	07:00	11:00
-            |0700-1100	01/12/16	07:00	11:00
-            |0700-1100	01/12/16	07:00	11:00
-            |0700-1400	01/12/16	07:00	14:00
-            |0700-1400	01/12/16	07:00	14:00
-            |0700-1600	01/12/16	07:00	16:00
-            |0700-1600	01/12/16	07:00	16:00
-            |0800-1700	01/12/16	08:00	17:00
-            |0800-1700	01/12/16	08:00	17:00
-            |
-            |Alpha/FGY	01/12/16	06:00	14:48
-            |Alpha/Casework	01/12/16	07:00	14:24
-            |Alpha (D)	01/12/16	07:00	14:54
-            |Alpha (DETECTION)	01/12/16	07:00	15:48
-            |Alpha (D)	01/12/16	07:00	15:48
-            |Alpha/SEA	01/12/16	07:00	15:48
-            |Alpha	01/12/16	07:00	15:48
-            |Alpha from 0845 with Crime Team/SB	01/12/16	07:00	15:48
-            |Alpha	01/12/16	07:00	15:48
-            |Alpha	01/12/16	07:00	15:48
-            |Alpha - R	01/12/16	07:00	15:48
-            |Alpha/OSO/SAT	01/12/16	07:00	15:48
-            |Alpha from 0845 with Crime Team/SB	01/12/16	07:00	15:48
-            |Alpha	01/12/16	07:00	15:48
-            |Alpha (DETECTION)	01/12/16	07:00	17:00
-            |
-            |Training	01/12/16	08:00	17:24
-            |Stats	01/12/16	09:00	16:24
-            |WI	01/12/16	09:30	14:30
-            |09:30-16:54	01/12/16	09:30	16:54
-            |1030-1915	01/12/16	10:30	19:15
-            |Bravo	01/12/16	11:00	18:24
-            |Bravo	01/12/16	11:00	18:54
-            |Bravo 1430-1700 CT training	01/12/16	11:00	19:48
-            |Bravo - R	01/12/16	11:00	19:48
-            |1200-2000	01/12/16	12:00	20:00
-            |
-            |Late Duty SO
-            |Charlie - ODM	01/12/16	14:30	23:18
-            |Charlie/SEA with OTO Joanne Clark	01/12/16	14:30	23:18
-            |Charlie/SEA/SAT OTO for Alison Burbeary	01/12/16	14:30	23:18
-            |Charlie	01/12/16	14:30	23:18
-            |Charlie 1430-1700 CT Training (D)	01/12/16	14:30	23:18
-            |Charlie - R	01/12/16	14:30	23:18
-            |Charlie/SEA	01/12/16	14:30	23:18
-            |
-            |Delta/OSO (D)	01/12/16	16:00	00:48
-            |Delta - R	01/12/16	16:00	00:48
-            |Delta/Fgy	01/12/16	16:00	00:48
-            |Delta/SEA with OTO M Elmhassani	01/12/16	16:00	00:48
-            |Delta	01/12/16	16:00	00:48
-            |Delta	01/12/16	16:00	00:48
-            |Delta/CWK	01/12/16	16:00	00:48
-            |Delta/SEA	01/12/16	16:00	00:48
-            |1600-2000	01/12/16	16:00	20:00
-            |1600-2000	01/12/16	16:00	20:00
-            |"Delta 1 Immigration, Assurance"	01/12/16	16:36	01:24
-            |
-            |Echo/SEA OTO for K Hopkins	01/12/16	17:00	01:48
-            |Echo	01/12/16	17:00	01:48
-            |
-            |1800-0100	01/12/16	18:00	01:00
-            |1800-0100	01/12/16	18:00	01:00
-            |1800-0100	01/12/16	18:00	01:00
-            |1800-0100	01/12/16	18:00	01:00
-            |1800-0100	01/12/16	18:00	01:00
-            |1800-0100	01/12/16	18:00	01:00
-            |1800-2200	01/12/16	18:00	22:00
-            |
-            |Night	01/12/16	22:00	08:00
-            |Night - ODM	01/12/16	22:30	07:18
-            |Night	01/12/16	22:30	07:18
-            |Night	01/12/16	22:30	07:18
-            |Night	01/12/16	22:30	07:18
-            |Night	01/12/16	22:30	07:18
-            |Night	01/12/16	22:30	07:18
-            |Night	01/12/16	22:30	07:18
-            |Night	01/12/16	22:30	07:18
-            |Night	01/12/16	22:30	07:18
-          """.stripMargin
+          val shiftsRawTsv =
+            """
+              |Alpha 1 ODM	01/12/16	06:30	15:18
+              |Alpha 1with ODM	01/12/16	06:30	15:18
+              |Alpha 2 /Customs from 0845
+              |"Alpha + Immigration, Assurance"	01/12/16	08:00	16:48
+              |"Alpha + Immigration, Assurance"	01/12/16	08:00	16:48
+              |
+              |0700-1100	01/12/16	07:00	11:00
+              |0700-1100	01/12/16	07:00	11:00
+              |0700-1100	01/12/16	07:00	11:00
+              |0700-1100	01/12/16	07:00	11:00
+              |0700-1100	01/12/16	07:00	11:00
+              |0700-1100	01/12/16	07:00	11:00
+              |0700-1400	01/12/16	07:00	14:00
+              |0700-1400	01/12/16	07:00	14:00
+              |0700-1600	01/12/16	07:00	16:00
+              |0700-1600	01/12/16	07:00	16:00
+              |0800-1700	01/12/16	08:00	17:00
+              |0800-1700	01/12/16	08:00	17:00
+              |
+              |Alpha/FGY	01/12/16	06:00	14:48
+              |Alpha/Casework	01/12/16	07:00	14:24
+              |Alpha (D)	01/12/16	07:00	14:54
+              |Alpha (DETECTION)	01/12/16	07:00	15:48
+              |Alpha (D)	01/12/16	07:00	15:48
+              |Alpha/SEA	01/12/16	07:00	15:48
+              |Alpha	01/12/16	07:00	15:48
+              |Alpha from 0845 with Crime Team/SB	01/12/16	07:00	15:48
+              |Alpha	01/12/16	07:00	15:48
+              |Alpha	01/12/16	07:00	15:48
+              |Alpha - R	01/12/16	07:00	15:48
+              |Alpha/OSO/SAT	01/12/16	07:00	15:48
+              |Alpha from 0845 with Crime Team/SB	01/12/16	07:00	15:48
+              |Alpha	01/12/16	07:00	15:48
+              |Alpha (DETECTION)	01/12/16	07:00	17:00
+              |
+              |Training	01/12/16	08:00	17:24
+              |Stats	01/12/16	09:00	16:24
+              |WI	01/12/16	09:30	14:30
+              |09:30-16:54	01/12/16	09:30	16:54
+              |1030-1915	01/12/16	10:30	19:15
+              |Bravo	01/12/16	11:00	18:24
+              |Bravo	01/12/16	11:00	18:54
+              |Bravo 1430-1700 CT training	01/12/16	11:00	19:48
+              |Bravo - R	01/12/16	11:00	19:48
+              |1200-2000	01/12/16	12:00	20:00
+              |
+              |Late Duty SO
+              |Charlie - ODM	01/12/16	14:30	23:18
+              |Charlie/SEA with OTO Joanne Clark	01/12/16	14:30	23:18
+              |Charlie/SEA/SAT OTO for Alison Burbeary	01/12/16	14:30	23:18
+              |Charlie	01/12/16	14:30	23:18
+              |Charlie 1430-1700 CT Training (D)	01/12/16	14:30	23:18
+              |Charlie - R	01/12/16	14:30	23:18
+              |Charlie/SEA	01/12/16	14:30	23:18
+              |
+              |Delta/OSO (D)	01/12/16	16:00	00:48
+              |Delta - R	01/12/16	16:00	00:48
+              |Delta/Fgy	01/12/16	16:00	00:48
+              |Delta/SEA with OTO M Elmhassani	01/12/16	16:00	00:48
+              |Delta	01/12/16	16:00	00:48
+              |Delta	01/12/16	16:00	00:48
+              |Delta/CWK	01/12/16	16:00	00:48
+              |Delta/SEA	01/12/16	16:00	00:48
+              |1600-2000	01/12/16	16:00	20:00
+              |1600-2000	01/12/16	16:00	20:00
+              |"Delta 1 Immigration, Assurance"	01/12/16	16:36	01:24
+              |
+              |Echo/SEA OTO for K Hopkins	01/12/16	17:00	01:48
+              |Echo	01/12/16	17:00	01:48
+              |
+              |1800-0100	01/12/16	18:00	01:00
+              |1800-0100	01/12/16	18:00	01:00
+              |1800-0100	01/12/16	18:00	01:00
+              |1800-0100	01/12/16	18:00	01:00
+              |1800-0100	01/12/16	18:00	01:00
+              |1800-0100	01/12/16	18:00	01:00
+              |1800-2200	01/12/16	18:00	22:00
+              |
+              |Night	01/12/16	22:00	08:00
+              |Night - ODM	01/12/16	22:30	07:18
+              |Night	01/12/16	22:30	07:18
+              |Night	01/12/16	22:30	07:18
+              |Night	01/12/16	22:30	07:18
+              |Night	01/12/16	22:30	07:18
+              |Night	01/12/16	22:30	07:18
+              |Night	01/12/16	22:30	07:18
+              |Night	01/12/16	22:30	07:18
+              |Night	01/12/16	22:30	07:18
+            """.stripMargin
 
           val lines = shiftsRawTsv.split("\n")
           val parsedShifts = lines.map(l => l.split("\t"))
-              .filter(_.length == 4)
-              .map(pl => Shift(pl(0), pl(1), pl(2), pl(3)))
+            .filter(_.length == 4)
+            .map(pl => Shift(pl(0), pl(1), pl(2), pl(3)))
 
 
           println(parsedShifts.mkString("\n"))
@@ -172,13 +174,28 @@ object ShiftsServiceTests extends TestSuite {
             val timeMinPlusOneDay: Long = startOfDay + WorkloadsHelpers.oneMinute * 60 * 36
             val daysWorthOf15Minutes = startOfDay until timeMinPlusOneDay by (WorkloadsHelpers.oneMinute * 15)
 
-            val staffAtTIme = daysWorthOf15Minutes.map {
-
-              time => (time) -> shiftService.staffAt(time)
+            TestTimer.timeIt(50) {
+              val staffAtTIme = daysWorthOf15Minutes.map {
+                time => (time) -> shiftService.staffAt(time)
+              }
             }
+//            println(staffAtTIme.mkString("\n"))
 
-            println(staffAtTIme.mkString("\n"))
+          }
 
+          "Staff movements" - {
+            import StaffMovements._
+            val shiftService = ShiftService(parsedShifts.toList)
+            val movements = (StaffMovement("IS81", SDate(2016, 12, 10, 10, 0), -2) :: Nil).sortBy(_.time)
+
+            "Shifts can be represented as staff movements" - {
+              val sDate: MilliDate = SDate(2016, 12, 10, 10, 0)
+              assert(staffAt(shiftService)(movements)(sDate) == shiftService.staffAt(sDate) - 2)
+            }
+            "Movements from after the asked for date are not included" - {
+              val sDate: MilliDate = SDate(2016, 12, 10, 9, 0)
+              assert(staffAt(shiftService)(movements)(sDate) == shiftService.staffAt(sDate))
+            }
           }
         }
       }
@@ -199,14 +216,45 @@ object JSDateConversions {
 
 }
 
+object StaffMovements {
+
+  case class StaffMovement(reason: String, time: MilliDate, delta: Int, queue: Option[QueueName] = None)
+
+  def adjustmentsAt(movements: Seq[StaffMovement])(dateTime: MilliDate) = movements.takeWhile(_.time <= dateTime).map(_.delta).sum
+
+  def staffAt(shiftService: ShiftService)(movements: Seq[StaffMovement])(dateTime: MilliDate) = {
+    val baseStaff = shiftService.staffAt(dateTime)
+    baseStaff + adjustmentsAt(movements)(dateTime)
+  }
+}
+
+
+object TestTimer {
+  def timeIt(times: Int)(f:  => Unit) = {
+    val start = new Date()
+    println(s"Starting timer at ${start}")
+    (1 to times).foreach(n => {
+      println(n)
+      f
+    })
+    val end = new Date()
+    println(s"Trial done at ${end}")
+    val timeTaken = (end.getTime() - start.getTime())
+    println(s"Time taken in ${times} runs ${timeTaken}ms, ${timeTaken.toDouble/times} per run")
+  }
+}
+
 case class Shift(name: String, startDt: MilliDate, endDt: MilliDate, numberOfStaff: Int)
 
+
 object Shift {
+
   import JSDateConversions._
+
   def apply(name: String, startDate: String, startTime: String, endTime: String): Shift = {
     val ymd = startDate.split("/").toVector
 
-    val (d,m, y) = (ymd(0).toInt, ymd(1).toInt, ymd(2).toInt + 2000)
+    val (d, m, y) = (ymd(0).toInt, ymd(1).toInt, ymd(2).toInt + 2000)
 
     val startT = startTime.split(":").toVector
     val (startHour, startMinute) = (startT(0).toInt, startT(1).toInt)
@@ -219,6 +267,7 @@ object Shift {
     Shift(name, startDt, endDt, 1)
   }
 }
+
 case class ShiftService(shifts: List[Shift]) {
   def staffAt(date: MilliDate): Int = shifts.filter(shift =>
     (shift.startDt <= date && date <= shift.endDt)).map(_.numberOfStaff).sum
