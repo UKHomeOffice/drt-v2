4735696f086bc34b37d7abb6e45c6ce030b0aa63
Rich Birch <rich@freshcodelimited.com>
2017-01-09 17:22:17 +0000

    DRT-4428 Shift examples now have todays date for ease of copying and pasting

--- client/src/main/scala/spatutorial/client/components/Staffing.scala
+++ client/src/main/scala/spatutorial/client/components/Staffing.scala
@@ -24,31 +24,37 @@ object Staffing {
         val startOfDay: Long = SDate(today.getFullYear, today.getMonth, today.getDate, 0, 0)
         val timeMinPlusOneDay: Long = startOfDay + WorkloadsHelpers.oneMinute * 60 * 24
         val daysWorthOf15Minutes = startOfDay until timeMinPlusOneDay by (WorkloadsHelpers.oneMinute * 15)
+        val shiftExamples = Seq(
+          s"Morning shift,${today.getDate}/${today.getMonth + 1}/${today.getFullYear - 2000},00:00,07:59,5",
+          s"Day shift,${today.getDate}/${today.getMonth + 1}/${today.getFullYear - 2000},08:00,15:59,20",
+          s"Evening shift,${today.getDate}/${today.getMonth + 1}/${today.getFullYear - 2000},16:00,23:59,15"
+        )
         <.div(
           <.h1("Staffing"),
           <.h2("Shifts"),
           <.div("One shift per line with values separated by commas, e.g.:"),
-          <.div("Morning A,20/01/17,00:00,08:00,5"),
+          <.div(shiftExamples.map(<.p(_))),
           <.textarea(^.value := rawShifts,
             ^.className := "staffing-editor",
             ^.onChange ==> ((e: ReactEventI) => shiftsMP.dispatch(UpdateShifts(e.target.value)))),
           <.h2("Staff over the day"),
           <.table(
             <.tr(
-                daysWorthOf15Minutes.map((t: Long) => {
-                  val d = new Date(t)
-                  val display = s"${d.getHours}:${d.getMinutes}"
-                  <.td(display)
-                })
+              daysWorthOf15Minutes.map((t: Long) => {
+                val d = new Date(t)
+                val display = s"${d.getHours}:${d.getMinutes}"
+                <.td(display)
+              })
             ),
             <.tr(
-                daysWorthOf15Minutes.map(t => <.td(s"${StaffMovements.staffAt(ss)(Nil)(t)}"))
+              daysWorthOf15Minutes.map(t => <.td(s"${StaffMovements.staffAt(ss)(Nil)(t)}"))
             )
           )
         )
       })
     }
   }
+
   def apply(): ReactElement =
     component(Props())
 
