6707dc3afa5cc6f8bf569ec8980d741f4e88f7f7
Ben Eppel <b.eppel@gmail.com>
2017-03-01 17:05:38 +0000

    DRT-4497 Add serializer changes

--- server/src/main/scala/actors/serializers/StaffMovementProtoBufSerializer.scala
+++ server/src/main/scala/actors/serializers/StaffMovementProtoBufSerializer.scala
@@ -1,37 +1,25 @@
 package actors.serializers
 
-import java.util.UUID
-
-import actors.StaffMovements
 import akka.serialization.SerializerWithStringManifest
-import server.protobuf.messages.StaffMovementMessages.{StaffMovementMessage, StaffMovementsMessage}
-import spatutorial.shared.{MilliDate, StaffMovement}
+import server.protobuf.messages.StaffMovementMessages.StaffMovementsMessage
 
 class StaffMovementProtoBufSerializer extends SerializerWithStringManifest {
 
   println("We are making the serializer")
 
-  override def identifier: Int = 1
+  override def identifier: Int = 9001
 
-  override def manifest(o: AnyRef): String = o.getClass.getName
+  override def manifest(o: AnyRef): String = {
+    println(s"The manifest is called ${o.getClass.getName}")
+    o.getClass.getName
+  }
 
   final val StaffMovementsManifest = classOf[StaffMovementsMessage].getName
 
   override def toBinary(o: AnyRef): Array[Byte] = {
     println("We are inside toBinary")
     o match {
-      case sms: StaffMovements =>
-        println(s"We are making binary $sms")
-        StaffMovementsMessage(sms.staffMovements.map(sm =>
-          StaffMovementMessage(
-            Some(sm.terminalName),
-            Some(sm.reason),
-            Some(sm.time.millisSinceEpoch),
-            Some(sm.delta),
-            Some(sm.uUID.toString),
-            sm.queue
-          )
-        )).toByteArray
+      case sms: StaffMovementsMessage => sms.toByteArray
       case error =>
         println(s"(serializing) We failed to match against $error")
         throw new Exception("oh dear")
@@ -42,19 +30,7 @@ class StaffMovementProtoBufSerializer extends SerializerWithStringManifest {
     println("We are inside fromBinary")
 
     manifest match {
-      case StaffMovementsManifest =>
-
-        val smm = StaffMovementsMessage.parseFrom(bytes)
-        println(s"We are deserializing binary $smm")
-
-        StaffMovements(smm.staffMovements.map(sm => StaffMovement(
-          sm.terminalName.getOrElse(""),
-          sm.reason.getOrElse(""),
-          MilliDate(sm.time.getOrElse(0)),
-          sm.delta.getOrElse(0),
-          UUID.fromString(sm.uUID.getOrElse("")),
-          sm.queueName
-        )).toList)
+      case StaffMovementsManifest => StaffMovementsMessage.parseFrom(bytes)
       case error =>
         println(s"(deserializing) We failed to match against $error for $bytes")
         throw new Exception("oh dear")
