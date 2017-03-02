14118e705b5cd35fd0ed3fa299aa6ffe3041523c
Ben Eppel <b.eppel@gmail.com>
2017-03-02 09:32:42 +0000

    DRT-4497 remove logging

--- server/src/main/scala/actors/serializers/StaffMovementProtoBufSerializer.scala
+++ server/src/main/scala/actors/serializers/StaffMovementProtoBufSerializer.scala
@@ -5,35 +5,23 @@ import server.protobuf.messages.StaffMovementMessages.StaffMovementsMessage
 
 class StaffMovementProtoBufSerializer extends SerializerWithStringManifest {
 
-  println("We are making the serializer")
-
   override def identifier: Int = 9001
 
   override def manifest(o: AnyRef): String = {
-    println(s"The manifest is called ${o.getClass.getName}")
     o.getClass.getName
   }
 
   final val StaffMovementsManifest = classOf[StaffMovementsMessage].getName
 
   override def toBinary(o: AnyRef): Array[Byte] = {
-    println("We are inside toBinary")
     o match {
       case sms: StaffMovementsMessage => sms.toByteArray
-      case error =>
-        println(s"(serializing) We failed to match against $error")
-        throw new Exception("oh dear")
     }
   }
 
   override def fromBinary(bytes: Array[Byte], manifest: String): AnyRef = {
-    println("We are inside fromBinary")
-
     manifest match {
       case StaffMovementsManifest => StaffMovementsMessage.parseFrom(bytes)
-      case error =>
-        println(s"(deserializing) We failed to match against $error for $bytes")
-        throw new Exception("oh dear")
     }
   }
 }
