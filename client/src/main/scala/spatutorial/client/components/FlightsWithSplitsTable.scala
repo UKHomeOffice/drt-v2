somanythings/drt-scalajs-spa-exploration

DRT-4508 consolidate PaxType and Queues from the PaxLoadCalculator with those from ApiSplits

client/src/main/scala/spatutorial/client/components/FlightsWithSplitsTable.scala
@@ -4,8 +4,7 @@ import diode.data.{Pot, Ready}
 import japgolly.scalajs.react.ReactComponentB
 import japgolly.scalajs.react.vdom.prefix_<^.{<, TagMod, ^}
 import spatutorial.client.modules.{FlightsWithSplitsView, GriddleComponentWrapper, ViewTools}
-import spatutorial.shared.AirportInfo
-import spatutorial.shared.FlightsApi.FlightsWithSplits
+import spatutorial.shared.{AirportInfo, PaxTypeAndQueue, PaxTypesAndQueues}
 import chandu0101.scalajs.react.components.Spinner
 import diode.data.{Pot, Ready}
 import japgolly.scalajs.react.{ReactComponentB, _}
@@ -13,7 +12,7 @@ import japgolly.scalajs.react.vdom.all.{ReactAttr => _, TagMod => _, _react_attr
 import japgolly.scalajs.react.vdom.prefix_<^._
 import spatutorial.client.logger
 import spatutorial.client.modules.{GriddleComponentWrapper, ViewTools}
-import spatutorial.shared.AirportInfo
+import spatutorial.shared.FlightsApi.FlightsWithSplits
 
 import scala.scalajs.js
 import scala.scalajs.js.Object
@@ -34,26 +33,30 @@ object FlightsWithSplitsTable {
     flights.flights.map(flightAndSplit => {
       val f = flightAndSplit.apiFlight
       val literal = js.Dynamic.literal
-      val splitsTuples: Map[String, Int] = flightAndSplit.splits
-        .splits.groupBy(split => split.queueType + " " + split.passengerType).map(x => (x._1, x._2.map(_.paxCount).sum))
+      val splitsTuples: Map[PaxTypeAndQueue, Int] = flightAndSplit.splits
+        .splits.groupBy(split => {
+        PaxTypeAndQueue(split.passengerType, split.queueType)
+      }
+      ).map(x => (x._1, x._2.map(_.paxCount).sum))
+      logger.log.debug("flightAndSplit:" + splitsTuples)
 
       import spatutorial.shared.DeskAndPaxTypeCombinations._
 
       val total = "advPaxInfo total"
 
-      def splitsField(fieldName: String): (String, scalajs.js.Any) = {
-        "Splits " + fieldName -> (splitsTuples.get(fieldName) match {
+      def splitsField(fieldName: String, ptQ: PaxTypeAndQueue): (String, scalajs.js.Any) = {
+        "Splits " + fieldName -> (splitsTuples.get(ptQ) match {
           case Some(v: Int) => Int.box(v)
           case None => ""
         })
       }
 
       literal(
-        splitsField(deskEeaNonMachineReadable),
-        splitsField(nationalsDeskVisa),
-        splitsField(nationalsDeskNonVisa),
-        splitsField(egate),
-        splitsField(deskEea),
+        splitsField(deskEeaNonMachineReadable, PaxTypesAndQueues.eeaNonMachineReadableToDesk),
+        splitsField(nationalsDeskVisa, PaxTypesAndQueues.visaNationalToDesk),
+        splitsField(nationalsDeskNonVisa, PaxTypesAndQueues.nonVisaNationalToDesk),
+        splitsField(egate, PaxTypesAndQueues.eeaMachineReadableToEGate),
+        splitsField(deskEea, PaxTypesAndQueues.eeaMachineReadableToDesk),
         "Splits " + total -> splitsTuples.values.sum,
         "Operator" -> f.Operator,
         "Status" -> f.Status,

server/src/main/scala/passengersplits/core/PassengerTypeCalculator.scala
@@ -1,38 +1,36 @@
 package passengersplits.core
 import passengersplits.parsing.PassengerInfoParser.PassengerInfoJson
-import spatutorial.shared.PassengerQueueTypes
+import spatutorial.shared.{PassengerQueueTypes, PaxType}
 import spatutorial.shared.PassengerQueueTypes.PaxTypeAndQueueCounts
 import spatutorial.shared.PassengerSplits.PaxTypeAndQueueCount
+import spatutorial.shared.PaxTypes.{EeaMachineReadable, EeaNonMachineReadable, NonVisaNational, VisaNational}
 
 import scala.collection.immutable.Iterable
 import scala.collection.immutable.Seq
 
 trait PassengerQueueCalculator {
-  import spatutorial.shared.PassengerQueueTypes.Desks._
-  import spatutorial.shared.PassengerQueueTypes.PaxTypes._
+  import spatutorial.shared.Queues._
 
-  def calculateQueuePaxCounts(paxTypeCounts: Map[PassengerType, Int]): PaxTypeAndQueueCounts = {
+  def calculateQueuePaxCounts(paxTypeCounts: Map[PaxType, Int]): PaxTypeAndQueueCounts = {
     val queues: Iterable[PaxTypeAndQueueCount] = paxTypeCounts flatMap calculateQueuesFromPaxTypes
-    queues.toList
+    queues.toList.sortBy(_.passengerType.toString)
   }
 
-  def calculateQueuesFromPaxTypes(paxTypeAndCount: (PassengerType, Int)): Seq[PaxTypeAndQueueCount] = {
+  def calculateQueuesFromPaxTypes(paxTypeAndCount: (PaxType, Int)): Seq[PaxTypeAndQueueCount] = {
     paxTypeAndCount match {
-      case (EEANONMACHINEREADABLE, c) =>
-        Seq(PaxTypeAndQueueCount(EEANONMACHINEREADABLE, eeaDesk, c))
-      case (EEAMACHINEREADABLE, paxCount) =>
+      case (EeaNonMachineReadable, paxCount) =>
+        Seq(PaxTypeAndQueueCount(EeaNonMachineReadable, EeaDesk, paxCount))
+      case (EeaMachineReadable, paxCount) =>
         val egatePaxCount = (PassengerQueueTypes.egatePercentage * paxCount).toInt
         Seq(
-          PaxTypeAndQueueCount(EEAMACHINEREADABLE, eeaDesk,  (paxCount - egatePaxCount)),
-          PaxTypeAndQueueCount(EEAMACHINEREADABLE, egate, egatePaxCount)
+          PaxTypeAndQueueCount(EeaMachineReadable, EeaDesk,  (paxCount - egatePaxCount)),
+          PaxTypeAndQueueCount(EeaMachineReadable, EGate, egatePaxCount)
         )
-      case (otherPaxType, c) => Seq(PaxTypeAndQueueCount(otherPaxType, nationalsDesk, c))
+      case (otherPaxType, c) => Seq(PaxTypeAndQueueCount(otherPaxType, NonEeaDesk, c))
     }
   }
 
-  type PassengerType = String
-
-  def countPassengerTypes(passengerTypes: Seq[PassengerType]): Map[PassengerType, Int] = {
+  def countPassengerTypes(passengerTypes: Seq[PaxType]): Map[PaxType, Int] = {
     passengerTypes.groupBy((x) => x).mapValues(_.length)
   }
 
@@ -49,7 +47,7 @@ object PassengerQueueCalculator extends PassengerQueueCalculator {
 
 
 object PassengerTypeCalculator {
-  type PassengerType = String
+  type PassengerType = PaxType
 
   def paxTypes(passengerInfos: Seq[PassengerInfoJson]) = {
     passengerInfos.map {
@@ -116,11 +114,11 @@ object PassengerTypeCalculator {
 
   def paxType(eeaFlag: String, documentCountry: String, documentType: Option[String]): PassengerType = {
     (eeaFlag, documentCountry, documentType) match {
-      case (EEA, country, _) if nonMachineReadableCountries contains (country) => "eea-non-machine-readable"
-      case (EEA, country, _) if (EEACountries contains country) => "eea-machine-readable"
-      case ("", country, Some(DocType.Visa)) if !(EEACountries contains country) => "national-visa"
-      case ("", country, Some(DocType.Passport)) if !(EEACountries contains country) => "national-non-visa"
-      case _ => "national-visa"
+      case (EEA, country, _) if nonMachineReadableCountries contains (country) => EeaNonMachineReadable
+      case (EEA, country, _) if (EEACountries contains country) => EeaMachineReadable
+      case ("", country, Some(DocType.Visa)) if !(EEACountries contains country) => VisaNational
+      case ("", country, Some(DocType.Passport)) if !(EEACountries contains country) => NonVisaNational
+      case _ => VisaNational
     }
   }
 }

server/src/test/scala/passengersplits/CanFindASplitForAnApiFlightSpec.scala
@@ -11,7 +11,10 @@ import passengersplits.parsing.PassengerInfoParser.{EventCodes, PassengerInfoJso
 import services.SDate
 import services.SDate.implicits._
 import spatutorial.shared.PassengerSplits.{FlightNotFound, PaxTypeAndQueueCount, VoyagePaxSplits}
+import spatutorial.shared.PaxTypes._
+import spatutorial.shared.Queues._
 import spatutorial.shared.{ApiFlight, PassengerQueueTypes, SDateLike}
+
 
 class CanFindASplitForAnApiFlightSpec extends
   TestKit(ActorSystem("CanFindASplitForAnApiFlightSpec", ConfigFactory.empty())) with AfterAll with SpecificationLike with ImplicitSender with CoreActors with Core {
@@ -39,8 +42,8 @@ class CanFindASplitForAnApiFlightSpec extends
         val flightScheduledDateTime = SDate(2017, 4, 2, 15, 33)
         flightPassengerReporter ! ReportVoyagePaxSplit("LGW", "EZ", "12345", flightScheduledDateTime)
         val expectedPaxSplits = List(
-          PaxTypeAndQueueCount(PassengerQueueTypes.PaxTypes.EEAMACHINEREADABLE, PassengerQueueTypes.Desks.eeaDesk, 1),
-          PaxTypeAndQueueCount(PassengerQueueTypes.PaxTypes.EEAMACHINEREADABLE, PassengerQueueTypes.Desks.egate, 0)
+          PaxTypeAndQueueCount(EeaMachineReadable, EeaDesk, 1),
+          PaxTypeAndQueueCount(EeaMachineReadable, EGate, 0)
         )
         expectMsg(VoyagePaxSplits("LGW", "EZ", "12345", 1, flightScheduledDateTime, expectedPaxSplits))
         success
@@ -58,9 +61,9 @@ class CanFindASplitForAnApiFlightSpec extends
         flightPassengerReporter ! ReportVoyagePaxSplit("STN", "EZ", "789", scheduleArrivalTime)
 
         val expectedPaxSplits = List(
-          PaxTypeAndQueueCount(PassengerQueueTypes.PaxTypes.EEAMACHINEREADABLE, PassengerQueueTypes.Desks.eeaDesk, 1),
-          PaxTypeAndQueueCount(PassengerQueueTypes.PaxTypes.EEAMACHINEREADABLE, PassengerQueueTypes.Desks.egate, 0),
-          PaxTypeAndQueueCount(PassengerQueueTypes.PaxTypes.NATIONALNONVISA, PassengerQueueTypes.Desks.nationalsDesk, 1)
+          PaxTypeAndQueueCount(EeaMachineReadable, EeaDesk, 1),
+          PaxTypeAndQueueCount(EeaMachineReadable, EGate, 0),
+          PaxTypeAndQueueCount(NonVisaNational, NonEeaDesk, 1)
         )
         expectMsg(VoyagePaxSplits("STN", "EZ", "789", 2, scheduleArrivalTime, expectedPaxSplits))
         success
@@ -75,9 +78,9 @@ class CanFindASplitForAnApiFlightSpec extends
         val scheduleArrivalSDate: SDateLike = SDate(2015, 7, 12, 10, 22)
         flightPassengerReporter ! ReportVoyagePaxSplit("STN", "BA", "978", scheduleArrivalSDate)
         expectMsg(VoyagePaxSplits("STN", "BA", "978", 100, scheduleArrivalSDate, List(
-          PaxTypeAndQueueCount(PassengerQueueTypes.PaxTypes.EEAMACHINEREADABLE, PassengerQueueTypes.Desks.eeaDesk, 32),
-          PaxTypeAndQueueCount(PassengerQueueTypes.PaxTypes.EEAMACHINEREADABLE, PassengerQueueTypes.Desks.egate, 48),
-          PaxTypeAndQueueCount(PassengerQueueTypes.PaxTypes.NATIONALNONVISA, PassengerQueueTypes.Desks.nationalsDesk, 20)
+          PaxTypeAndQueueCount(EeaMachineReadable, EeaDesk, 32),
+          PaxTypeAndQueueCount(EeaMachineReadable, EGate, 48),
+          PaxTypeAndQueueCount(NonVisaNational, NonEeaDesk, 20)
         )))
         success
       }

UKHomeOffice/drt-v2

client/src/main/scala/spatutorial/client/components/FlightsWithSplitsTable.scala
@@ -4,16 +4,18 @@ import diode.data.{Pot, Ready}
 import japgolly.scalajs.react.ReactComponentB
 import japgolly.scalajs.react.vdom.prefix_<^.{<, TagMod, ^}
 import drt.client.modules.{FlightsWithSplitsView, GriddleComponentWrapper, ViewTools}
-import drt.shared.AirportInfo
-import drt.shared.FlightsApi.FlightsWithSplits
+import drt.shared.{AirportInfo, PaxTypeAndQueue, PaxTypesAndQueues}
+import drt.client.modules.{FlightsWithSplitsView, GriddleComponentWrapper, ViewTools}
+import drt.shared.{AirportInfo, PaxTypeAndQueue, PaxTypesAndQueues}
 import chandu0101.scalajs.react.components.Spinner
 import diode.data.{Pot, Ready}
 import japgolly.scalajs.react.{ReactComponentB, _}
 import japgolly.scalajs.react.vdom.all.{ReactAttr => _, TagMod => _, _react_attrString => _, _react_autoRender => _, _react_fragReactNode => _}
 import japgolly.scalajs.react.vdom.prefix_<^._
+import drt.client.modules.{GriddleComponentWrapper, ViewTools}
 import drt.client.logger
 import drt.client.modules.{GriddleComponentWrapper, ViewTools}
-import drt.shared.AirportInfo
+import drt.shared.FlightsApi.FlightsWithSplits
 
 import scala.scalajs.js
 import scala.scalajs.js.Object
@@ -34,26 +36,30 @@ object FlightsWithSplitsTable {
     flights.flights.map(flightAndSplit => {
       val f = flightAndSplit.apiFlight
       val literal = js.Dynamic.literal
-      val splitsTuples: Map[String, Int] = flightAndSplit.splits
-        .splits.groupBy(split => split.queueType + " " + split.passengerType).map(x => (x._1, x._2.map(_.paxCount).sum))
+      val splitsTuples: Map[PaxTypeAndQueue, Int] = flightAndSplit.splits
+        .splits.groupBy(split => {
+        PaxTypeAndQueue(split.passengerType, split.queueType)
+      }
+      ).map(x => (x._1, x._2.map(_.paxCount).sum))
+      logger.log.debug("flightAndSplit:" + splitsTuples)
 
       import drt.shared.DeskAndPaxTypeCombinations._
 
       val total = "advPaxInfo total"
 
-      def splitsField(fieldName: String): (String, scalajs.js.Any) = {
-        "Splits " + fieldName -> (splitsTuples.get(fieldName) match {
+      def splitsField(fieldName: String, ptQ: PaxTypeAndQueue): (String, scalajs.js.Any) = {
+        "Splits " + fieldName -> (splitsTuples.get(ptQ) match {
           case Some(v: Int) => Int.box(v)
           case None => ""
         })
       }
 
       literal(
-        splitsField(deskEeaNonMachineReadable),
-        splitsField(nationalsDeskVisa),
-        splitsField(nationalsDeskNonVisa),
-        splitsField(egate),
-        splitsField(deskEea),
+        splitsField(deskEeaNonMachineReadable, PaxTypesAndQueues.eeaNonMachineReadableToDesk),
+        splitsField(nationalsDeskVisa, PaxTypesAndQueues.visaNationalToDesk),
+        splitsField(nationalsDeskNonVisa, PaxTypesAndQueues.nonVisaNationalToDesk),
+        splitsField(egate, PaxTypesAndQueues.eeaMachineReadableToEGate),
+        splitsField(deskEea, PaxTypesAndQueues.eeaMachineReadableToDesk),
         "Splits " + total -> splitsTuples.values.sum,
         "Operator" -> f.Operator,
         "Status" -> f.Status,

server/src/main/scala/passengersplits/core/PassengerTypeCalculator.scala
@@ -1,38 +1,37 @@
 package passengersplits.core
 import passengersplits.parsing.PassengerInfoParser.PassengerInfoJson
-import drt.shared.PassengerQueueTypes
+import drt.shared.{PassengerQueueTypes, PaxType}
 import drt.shared.PassengerQueueTypes.PaxTypeAndQueueCounts
 import drt.shared.PassengerSplits.PaxTypeAndQueueCount
+import drt.shared.PaxTypes.{EeaMachineReadable, EeaNonMachineReadable, NonVisaNational, VisaNational}
 
 import scala.collection.immutable.Iterable
 import scala.collection.immutable.Seq
 
 trait PassengerQueueCalculator {
-  import drt.shared.PassengerQueueTypes.Desks._
-  import drt.shared.PassengerQueueTypes.PaxTypes._
+  import drt.shared.Queues._
+  import drt.shared.PaxType
 
-  def calculateQueuePaxCounts(paxTypeCounts: Map[PassengerType, Int]): PaxTypeAndQueueCounts = {
+  def calculateQueuePaxCounts(paxTypeCounts: Map[PaxType, Int]): PaxTypeAndQueueCounts = {
     val queues: Iterable[PaxTypeAndQueueCount] = paxTypeCounts flatMap calculateQueuesFromPaxTypes
-    queues.toList
+    queues.toList.sortBy(_.passengerType.toString)
   }
 
-  def calculateQueuesFromPaxTypes(paxTypeAndCount: (PassengerType, Int)): Seq[PaxTypeAndQueueCount] = {
+  def calculateQueuesFromPaxTypes(paxTypeAndCount: (PaxType, Int)): Seq[PaxTypeAndQueueCount] = {
     paxTypeAndCount match {
-      case (EEANONMACHINEREADABLE, c) =>
-        Seq(PaxTypeAndQueueCount(EEANONMACHINEREADABLE, eeaDesk, c))
-      case (EEAMACHINEREADABLE, paxCount) =>
+      case (EeaNonMachineReadable, paxCount) =>
+        Seq(PaxTypeAndQueueCount(EeaNonMachineReadable, EeaDesk, paxCount))
+      case (EeaMachineReadable, paxCount) =>
         val egatePaxCount = (PassengerQueueTypes.egatePercentage * paxCount).toInt
         Seq(
-          PaxTypeAndQueueCount(EEAMACHINEREADABLE, eeaDesk,  (paxCount - egatePaxCount)),
-          PaxTypeAndQueueCount(EEAMACHINEREADABLE, egate, egatePaxCount)
+          PaxTypeAndQueueCount(EeaMachineReadable, EeaDesk,  (paxCount - egatePaxCount)),
+          PaxTypeAndQueueCount(EeaMachineReadable, EGate, egatePaxCount)
         )
-      case (otherPaxType, c) => Seq(PaxTypeAndQueueCount(otherPaxType, nationalsDesk, c))
+      case (otherPaxType, c) => Seq(PaxTypeAndQueueCount(otherPaxType, NonEeaDesk, c))
     }
   }
 
-  type PassengerType = String
-
-  def countPassengerTypes(passengerTypes: Seq[PassengerType]): Map[PassengerType, Int] = {
+  def countPassengerTypes(passengerTypes: Seq[PaxType]): Map[PaxType, Int] = {
     passengerTypes.groupBy((x) => x).mapValues(_.length)
   }
 
@@ -49,7 +48,7 @@ object PassengerQueueCalculator extends PassengerQueueCalculator {
 
 
 object PassengerTypeCalculator {
-  type PassengerType = String
+  type PassengerType = PaxType
 
   def paxTypes(passengerInfos: Seq[PassengerInfoJson]) = {
     passengerInfos.map {
@@ -116,11 +115,11 @@ object PassengerTypeCalculator {
 
   def paxType(eeaFlag: String, documentCountry: String, documentType: Option[String]): PassengerType = {
     (eeaFlag, documentCountry, documentType) match {
-      case (EEA, country, _) if nonMachineReadableCountries contains (country) => "eea-non-machine-readable"
-      case (EEA, country, _) if (EEACountries contains country) => "eea-machine-readable"
-      case ("", country, Some(DocType.Visa)) if !(EEACountries contains country) => "national-visa"
-      case ("", country, Some(DocType.Passport)) if !(EEACountries contains country) => "national-non-visa"
-      case _ => "national-visa"
+      case (EEA, country, _) if nonMachineReadableCountries contains (country) => EeaNonMachineReadable
+      case (EEA, country, _) if (EEACountries contains country) => EeaMachineReadable
+      case ("", country, Some(DocType.Visa)) if !(EEACountries contains country) => VisaNational
+      case ("", country, Some(DocType.Passport)) if !(EEACountries contains country) => NonVisaNational
+      case _ => VisaNational
     }
   }
 }

server/src/test/scala/passengersplits/CanFindASplitForAnApiFlightSpec.scala
@@ -11,7 +11,10 @@ import passengersplits.parsing.PassengerInfoParser.{EventCodes, PassengerInfoJso
 import services.SDate
 import services.SDate.implicits._
 import drt.shared.PassengerSplits.{FlightNotFound, PaxTypeAndQueueCount, VoyagePaxSplits}
+import drt.shared.PaxTypes._
+import drt.shared.Queues._
 import drt.shared.{ApiFlight, PassengerQueueTypes, SDateLike}
+
 
 class CanFindASplitForAnApiFlightSpec extends
   TestKit(ActorSystem("CanFindASplitForAnApiFlightSpec", ConfigFactory.empty())) with AfterAll with SpecificationLike with ImplicitSender with CoreActors with Core {
@@ -39,8 +42,8 @@ class CanFindASplitForAnApiFlightSpec extends
         val flightScheduledDateTime = SDate(2017, 4, 2, 15, 33)
         flightPassengerReporter ! ReportVoyagePaxSplit("LGW", "EZ", "12345", flightScheduledDateTime)
         val expectedPaxSplits = List(
-          PaxTypeAndQueueCount(PassengerQueueTypes.PaxTypes.EEAMACHINEREADABLE, PassengerQueueTypes.Desks.eeaDesk, 1),
-          PaxTypeAndQueueCount(PassengerQueueTypes.PaxTypes.EEAMACHINEREADABLE, PassengerQueueTypes.Desks.egate, 0)
+          PaxTypeAndQueueCount(EeaMachineReadable, EeaDesk, 1),
+          PaxTypeAndQueueCount(EeaMachineReadable, EGate, 0)
         )
         expectMsg(VoyagePaxSplits("LGW", "EZ", "12345", 1, flightScheduledDateTime, expectedPaxSplits))
         success
@@ -58,9 +61,9 @@ class CanFindASplitForAnApiFlightSpec extends
         flightPassengerReporter ! ReportVoyagePaxSplit("STN", "EZ", "789", scheduleArrivalTime)
 
         val expectedPaxSplits = List(
-          PaxTypeAndQueueCount(PassengerQueueTypes.PaxTypes.EEAMACHINEREADABLE, PassengerQueueTypes.Desks.eeaDesk, 1),
-          PaxTypeAndQueueCount(PassengerQueueTypes.PaxTypes.EEAMACHINEREADABLE, PassengerQueueTypes.Desks.egate, 0),
-          PaxTypeAndQueueCount(PassengerQueueTypes.PaxTypes.NATIONALNONVISA, PassengerQueueTypes.Desks.nationalsDesk, 1)
+          PaxTypeAndQueueCount(EeaMachineReadable, EeaDesk, 1),
+          PaxTypeAndQueueCount(EeaMachineReadable, EGate, 0),
+          PaxTypeAndQueueCount(NonVisaNational, NonEeaDesk, 1)
         )
         expectMsg(VoyagePaxSplits("STN", "EZ", "789", 2, scheduleArrivalTime, expectedPaxSplits))
         success
@@ -94,9 +97,9 @@ class CanFindASplitForAnApiFlightSpec extends
         val scheduleArrivalSDate: SDateLike = SDate(2015, 7, 12, 10, 22)
         flightPassengerReporter ! ReportVoyagePaxSplit("STN", "BA", "978", scheduleArrivalSDate)
         expectMsg(VoyagePaxSplits("STN", "BA", "978", 100, scheduleArrivalSDate, List(
-          PaxTypeAndQueueCount(PassengerQueueTypes.PaxTypes.EEAMACHINEREADABLE, PassengerQueueTypes.Desks.eeaDesk, 32),
-          PaxTypeAndQueueCount(PassengerQueueTypes.PaxTypes.EEAMACHINEREADABLE, PassengerQueueTypes.Desks.egate, 48),
-          PaxTypeAndQueueCount(PassengerQueueTypes.PaxTypes.NATIONALNONVISA, PassengerQueueTypes.Desks.nationalsDesk, 20)
+          PaxTypeAndQueueCount(EeaMachineReadable, EeaDesk, 32),
+          PaxTypeAndQueueCount(EeaMachineReadable, EGate, 48),
+          PaxTypeAndQueueCount(NonVisaNational, NonEeaDesk, 20)
         )))
         success
       }