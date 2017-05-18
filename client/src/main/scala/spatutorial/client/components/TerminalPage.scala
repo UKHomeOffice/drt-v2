582ac24f461ddd5eaeb8ab597d8aa79cd29d435f
lance paine <lance@lbpaine.com>
2017-05-18 15:22:46 +0100

    drt-4567 fix airport info issue - don't .value on a modelproxy

--- client/src/main/scala/spatutorial/client/components/TerminalPage.scala
+++ client/src/main/scala/spatutorial/client/components/TerminalPage.scala
@@ -68,7 +68,7 @@ object TerminalPage {
 
                 def originMapper(portCode: String): VdomElement = {
                   Try {
-                    vdomElementFromTag(airportWrapper(portCode) { (proxy: ModelProxy[Pot[AirportInfo]]) =>
+                    vdomElementFromComponent(airportWrapper(portCode) { (proxy: ModelProxy[Pot[AirportInfo]]) =>
                       <.span(
                         proxy().render(ai => <.span(^.title := s"${ai.airportName}, ${ai.city}, ${ai.country}", portCode)),
                         proxy().renderEmpty(<.span(portCode)),
