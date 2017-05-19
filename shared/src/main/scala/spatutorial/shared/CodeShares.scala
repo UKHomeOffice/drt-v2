ad1f003b06c8dbd9df682235130af4c1ae98de2e
Rich Birch <rich@freshcodelimited.com>
2017-05-19 17:51:28 +0100

    DRT-4566 Rename for clarity

--- shared/src/main/scala/spatutorial/shared/CodeShares.scala
+++ shared/src/main/scala/spatutorial/shared/CodeShares.scala
@@ -1,15 +1,15 @@
 package drt.shared
 
 object CodeShares {
-  def uniqueArrivalsWithCodeshares[GenFlight](toFlight: (GenFlight) => ApiFlight)(flights: Seq[GenFlight]): List[(GenFlight, Set[ApiFlight])] = {
-    val grouped: Map[(String, String, String), Seq[GenFlight]] = flights.groupBy(f => (toFlight(f).SchDT, toFlight(f).Terminal, toFlight(f).Origin))
+  def uniqueArrivalsWithCodeshares[GenFlight](apiFlight: (GenFlight) => ApiFlight)(flights: Seq[GenFlight]): List[(GenFlight, Set[ApiFlight])] = {
+    val grouped: Map[(String, String, String), Seq[GenFlight]] = flights.groupBy(f => (apiFlight(f).SchDT, apiFlight(f).Terminal, apiFlight(f).Origin))
     grouped.map {
       case (_, flights) =>
-        val mainFlight: GenFlight = flights.sortBy(f => toFlight(f).ActPax).reverse.head
+        val mainFlight: GenFlight = flights.sortBy(f => apiFlight(f).ActPax).reverse.head
         val shares: Set[ApiFlight] = flights
           .filter(_ != mainFlight)
           .toSet
-          .map(toFlight)
+          .map(apiFlight)
 
         (mainFlight, shares)
     }.toList
