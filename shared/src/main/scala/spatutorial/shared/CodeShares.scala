0d113f6e16467adff01726708cdefe6fdf04345f
Ben Eppel <b.eppel@gmail.com>
2017-05-23 17:06:05 +0100

    DRT-4566 Improve code readability

--- shared/src/main/scala/spatutorial/shared/CodeShares.scala
+++ shared/src/main/scala/spatutorial/shared/CodeShares.scala
@@ -1,17 +1,16 @@
 package drt.shared
 
 object CodeShares {
-  def uniqueArrivalsWithCodeshares[GenFlight](apiFlight: (GenFlight) => ApiFlight)(flights: Seq[GenFlight]): List[(GenFlight, Set[ApiFlight])] = {
-    val grouped: Map[(String, String, String), Seq[GenFlight]] = flights.groupBy(f => (apiFlight(f).SchDT, apiFlight(f).Terminal, apiFlight(f).Origin))
-    grouped.map {
-      case (_, flights) =>
-        val mainFlight: GenFlight = flights.sortBy(f => apiFlight(f).ActPax).reverse.head
+  def uniqueArrivalsWithCodeshares[GenFlight](apiFlightFromGenFlight: (GenFlight) => ApiFlight)(flights: Seq[GenFlight]): List[(GenFlight, Set[ApiFlight])] = {
+    val grouped: Map[(String, String, String), Seq[GenFlight]] = flights.groupBy(f => (apiFlightFromGenFlight(f).SchDT, apiFlightFromGenFlight(f).Terminal, apiFlightFromGenFlight(f).Origin))
+    grouped.values.map(flights => {
+        val mainFlight: GenFlight = flights.sortBy(f => apiFlightFromGenFlight(f).ActPax).reverse.head
         val shares: Set[ApiFlight] = flights
           .filter(_ != mainFlight)
           .toSet
-          .map(apiFlight)
+          .map(apiFlightFromGenFlight)
 
         (mainFlight, shares)
-    }.toList
+    }).toList
   }
 }
