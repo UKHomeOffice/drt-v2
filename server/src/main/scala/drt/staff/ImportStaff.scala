1330be41bf2b59d6e56652e8567f9b5eda1a479e
Rich Birch <rich@freshcodelimited.com>
2017-07-11 14:30:40 +0100

    DRT-4639 Correct imported shifts to be 15 minutes rather than 16

--- server/src/main/scala/drt/staff/ImportStaff.scala
+++ server/src/main/scala/drt/staff/ImportStaff.scala
@@ -22,7 +22,7 @@ object ImportStaff {
             //The client deals in local time, and these shifts are sent to the client as strings with no timezone for now.
             //TODO: store shifts not as strings.
             val shiftStartDate = new DateTime(shift.shift_start).withZone(DateTimeZone.forID("Europe/London"))
-            val shiftsEndDate = shiftStartDate.addMinutes(15)
+            val shiftsEndDate = shiftStartDate.addMinutes(14)
 
             f"shift$index, ${shift.terminal}, ${shiftStartDate.ddMMyyString}, ${shiftStartDate.getHours()}%02d:${shiftStartDate.getMinutes()}%02d, ${shiftsEndDate.getHours()}%02d:${shiftsEndDate.getMinutes()}%02d, ${shift.staff}"
         }.mkString("\n")
