a35d5f8326b1bc5c6d6482378a930a6b94fdea73
Ben Eppel <b.eppel@gmail.com>
2017-07-21 13:34:48 +0100

    DRT-4659 Restore debug, add tests for filtering movements by day

---
+++ client/src/test/scala/drt/client/components/TerminalStaffingTests.scala
@@ -0,0 +1,35 @@
+package drt.client.components
+
+
+import java.util.UUID
+
+import drt.client.services.JSDateConversions._
+import drt.shared.StaffMovement
+import utest.{TestSuite, _}
+import TerminalStaffing.todaysMovements
+import scala.collection.immutable._
+
+
+object TerminalStaffingTests extends TestSuite {
+
+  def tests = TestSuite {
+    "Staff Movements" - {
+      "should only display movements within the date range provided" - {
+        val uid = UUID.randomUUID()
+        val yesterday = StaffMovement("T1", "reason", SDate(2017, 7, 20, 12, 0), 1, uid, None)
+        val today =     StaffMovement("T1", "reason", SDate(2017, 7, 21, 12, 0), 1, uid, None)
+        val sm = Seq(
+          yesterday,
+          today
+        )
+
+        val expected =  Seq(today)
+        val result = todaysMovements(sm, SDate(2017, 7, 21, 0, 0), SDate(2017, 7, 22, 0, 0))
+
+        assert(expected == result)
+      }
+    }
+  }
+
+
+}
