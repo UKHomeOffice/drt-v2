d50d25f40cb950def6e0fe508bfa8392d788595f
GMason <gary.mason18@homeoffice.gsi.gov.uk>
2017-07-26 17:26:53 +0100

    DRT-4673 & DRT-4649 Removed Terminal name from the titles of heatmaps and amended pax number hover over to display in same way as API

--- client/src/main/scala/drt/client/components/FlightComponents.scala
+++ client/src/main/scala/drt/client/components/FlightComponents.scala
@@ -59,10 +59,11 @@ object FlightComponents {
     val port: String = if (flight.ActPax > 0) flight.ActPax.toString else "n/a"
     val last: String = flight.LastKnownPax.getOrElse("n/a").toString
     val max: String = if (flight.MaxPax > 0) flight.MaxPax.toString else "n/a"
+    val TotalPortPax: Int ={flight.ActPax}+{flight.TranPax}
 
     s"""
        |API: ${api} = (${apiIncTrans} - ${apiIncTrans - apiPax} transfer)
-       |Port: ${port} (Port transfer: ${flight.TranPax})
+       |Port: ${port} ($TotalPortPax - ${flight.TranPax} transfer)
        |Previous: ${last}
        |Max: ${max}
                   """.stripMargin
