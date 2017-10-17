d08964a2888728ada691ef3db49e248213879895
Rich Birch <rich@freshcodelimited.com>
2017-10-17 12:42:05 +0100

    DRT-4689 Test for arrivals not updating bug - caused by a noop update not triggering a pull

--- server/src/test/scala/services/crunch/FlightUpdatesTriggerNewCrunchStateSpec.scala
+++ server/src/test/scala/services/crunch/FlightUpdatesTriggerNewCrunchStateSpec.scala
@@ -40,9 +40,44 @@ class FlightUpdatesTriggerNewCrunchStateSpec extends CrunchTestLike {
     )
 
     crunch.liveArrivalsInput.offer(inputFlightsBefore)
-
     crunch.liveTestProbe.expectMsgAnyClassOf(classOf[PortState])
 
+    crunch.liveArrivalsInput.offer(inputFlightsAfter)
+    val flightsAfterUpdate = crunch.liveTestProbe.expectMsgAnyClassOf(classOf[PortState]) match {
+      case PortState(flights, _) => flights.values.map(_.copy(lastUpdated = None))
+    }
+
+    val expectedFlights = Set(ApiFlightWithSplits(
+      updatedArrival,
+      Set(ApiSplits(Set(ApiPaxTypeAndQueueCount(EeaMachineReadable, Queues.EeaDesk, 100.0)), TerminalAverage, None, Percentage))))
+
+    flightsAfterUpdate === expectedFlights
+  }
+
+  "Given a noop update to an existing flight followed by a real update " +
+    "When I expect a CrunchState " +
+    "Then I should see one containing the updated flight" >> {
+
+    val scheduled = "2017-01-01T00:00Z"
+
+    val flight = ArrivalGenerator.apiFlight(flightId = 1, schDt = scheduled, iata = "BA0001", terminal = "T1", actPax = 21)
+    val inputFlightsBefore = Flights(List(flight))
+    val updatedArrival = flight.copy(ActPax = 50)
+    val inputFlightsAfter = Flights(List(updatedArrival))
+    val crunch = runCrunchGraph(
+      procTimes = Map(
+        eeaMachineReadableToDesk -> 25d / 60,
+        eeaMachineReadableToEGate -> 25d / 60
+      ),
+      queues = Map("T1" -> Seq(EeaDesk, EGate)),
+      crunchStartDateProvider = (_) => SDate(scheduled),
+      crunchEndDateProvider = (_) => SDate(scheduled).addMinutes(30)
+    )
+
+    crunch.liveArrivalsInput.offer(inputFlightsBefore)
+    crunch.liveTestProbe.expectMsgAnyClassOf(classOf[PortState])
+
+    crunch.liveArrivalsInput.offer(inputFlightsBefore)
     crunch.liveArrivalsInput.offer(inputFlightsAfter)
 
     val flightsAfterUpdate = crunch.liveTestProbe.expectMsgAnyClassOf(classOf[PortState]) match {
