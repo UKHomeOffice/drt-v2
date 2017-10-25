61578a5f6b1163c891807db8f16cff9bc1ad8fab
Ben Eppel <b.eppel@gmail.com>
2017-10-25 16:01:19 +0100

    DRT-4687 Add function to get headline figures out

---
+++ server/src/test/scala/services/ForecastHeadlineFiguresSpec.scala
@@ -0,0 +1,86 @@
+package services
+
+import controllers.Forecast
+import drt.shared.CrunchApi.CrunchMinute
+import drt.shared.Queues
+import org.specs2.mutable.Specification
+
+class ForecastHeadlineFiguresSpec extends Specification {
+
+  "Given one Crunch Minutes for EeaDesk with 1 passenger at 2017-01-02T00:00Z" >> {
+    "And I ask for headline figures for week beginning 2017-01-02T00:00Z for T1 " +
+      "Then I should see Total Pax of 1 and EEA Pax of 1 and total workload of 2 on 2017-01-02" >> {
+      val startMinute = SDate("2017-01-02T00:00Z").millisSinceEpoch
+      val cms = Set(CrunchMinute("T1", Queues.EeaDesk, startMinute, 1, 2, 0, 0))
+      val result = Forecast.headLineFigures(cms, "T1")
+
+      val expected = Map(startMinute -> Map(
+        Queues.EeaDesk -> Tuple2(1, 2)
+      ))
+      result === expected
+    }
+  }
+  
+  "Given one Crunch Minutes for EeaDesk with 1 passenger at 2017-01-02T00:00Z for T1 and another for T2" >> {
+    "And I ask for headline figures for week beginning 2017-01-02T00:00Z for T1 " +
+      "Then I should only see the values from T1" >> {
+      val startMinute = SDate("2017-01-02T00:00Z").millisSinceEpoch
+      val cms = Set(
+        CrunchMinute("T1", Queues.EeaDesk, startMinute, 1, 2, 0, 0),
+        CrunchMinute("T2", Queues.EeaDesk, startMinute, 1, 2, 0, 0)
+      )
+      val result = Forecast.headLineFigures(cms, "T1")
+
+      val expected = Map(startMinute -> Map(
+        Queues.EeaDesk -> Tuple2(1, 2)
+      ))
+      result === expected
+    }
+  }
+
+  "Given two Crunch Minutes for EeaDesk and EGate with 1 passenger at 2017-01-02T00:00Z" >> {
+    "And I ask for headline figures for week beginning 2017-01-02T00:00Z for T1 " +
+      "Then I should see Total Pax of 1 and EEA Pax of 1 and total workload of 2 on 2017-01-02" >> {
+      val startMinute = SDate("2017-01-02T00:00Z").millisSinceEpoch
+      val cms = Set(
+        CrunchMinute("T1", Queues.EeaDesk, startMinute, 1, 2, 0, 0),
+        CrunchMinute("T1", Queues.EGate, startMinute, 1, 2, 0, 0)
+      )
+      val result = Forecast.headLineFigures(cms, "T1")
+
+      val expected = Map(startMinute -> Map(
+        Queues.EeaDesk -> Tuple2(1, 2),
+        Queues.EGate -> Tuple2(1, 2)
+      ))
+      result === expected
+    }
+  }
+
+  "Given two Crunch Minutes for EeaDesk and EGate with 1 passenger at 2017-01-02T00:00Z and at 2017-01-03T00:00Z" >> {
+    "And I ask for headline figures for week beginning 2017-01-02T00:00Z for T1 " +
+      "Then I should see Total Pax of 1 and EEA Pax of 1 and total workload of 2 on 2017-01-02 and 2017-01-03" >> {
+
+      val day1StartMinute = SDate("2017-01-02T00:00Z").millisSinceEpoch
+      val day2StartMinute = SDate("2017-01-03T00:00Z").millisSinceEpoch
+      val cms = Set(
+        CrunchMinute("T1", Queues.EeaDesk, day1StartMinute, 1, 2, 0, 0),
+        CrunchMinute("T1", Queues.EGate, day1StartMinute, 1, 2, 0, 0),
+        CrunchMinute("T1", Queues.EeaDesk, day2StartMinute, 1, 2, 0, 0),
+        CrunchMinute("T1", Queues.EGate, day2StartMinute, 1, 2, 0, 0)
+      )
+      val result = Forecast.headLineFigures(cms, "T1")
+
+      val expected = Map(
+        day1StartMinute -> Map(
+          Queues.EeaDesk -> Tuple2(1, 2),
+          Queues.EGate -> Tuple2(1, 2)
+        ),
+        day2StartMinute -> Map(
+          Queues.EeaDesk -> Tuple2(1, 2),
+          Queues.EGate -> Tuple2(1, 2)
+        )
+      )
+      result === expected
+    }
+  }
+}
