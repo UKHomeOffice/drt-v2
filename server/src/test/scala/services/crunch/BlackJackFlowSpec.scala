d805d79fbb5017e97281773084dfa5ba4a6b2164
Rich Birch <rich@freshcodelimited.com>
2017-10-27 17:31:30 +0100

    DRT-4788 Tidying

--- server/src/test/scala/services/crunch/BlackJackFlowSpec.scala
+++ server/src/test/scala/services/crunch/BlackJackFlowSpec.scala
@@ -50,7 +50,6 @@ class BlackJackFlowSpec extends CrunchTestLike {
       case PortState(_, c, _) => c
     }
     val actDesks = crunchMinutes.values.toList.sortBy(_.minute).map(cm => {
-      println(s"${SDate(cm.minute).toLocalDateTimeString()} - ${cm.actDesks} ${cm.actWait}")
       (cm.actDesks, cm.actWait)
     }).take(30)
 
