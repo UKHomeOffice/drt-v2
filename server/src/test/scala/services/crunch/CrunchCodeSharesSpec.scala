8ec86ceba7ba92e8bbddd1be2709c0bba164e58d
Ben Eppel <b.eppel@gmail.com>
2017-12-14 16:01:35 +0000

    DRT-4890 Exclude cancelled flights from crunch

--- server/src/test/scala/services/crunch/CrunchCodeSharesSpec.scala
+++ server/src/test/scala/services/crunch/CrunchCodeSharesSpec.scala
@@ -5,7 +5,6 @@ import drt.shared.CrunchApi.PortState
 import drt.shared.FlightsApi.Flights
 import drt.shared.PaxTypesAndQueues._
 import drt.shared._
-import passengersplits.parsing.VoyageManifestParser.VoyageManifests
 import services.SDate
 import services.graphstages.Crunch._
 
@@ -77,41 +76,6 @@ class CrunchCodeSharesSpec extends CrunchTestLike {
         "T2" -> Map(Queues.EeaDesk -> Seq(
           0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
           12.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0)))
-
-      resultSummary === expected
-    }
-
-    "Given two flights, one with an invalid terminal " +
-      "When I ask for a crunch " +
-      "I should only see crunch results for the flight with a valid terminal" >> {
-      val scheduled00 = "2017-01-01T00:00Z"
-
-      val scheduled = "2017-01-01T00:00Z"
-
-      val flights = Flights(List(
-        ArrivalGenerator.apiFlight(flightId = 1, schDt = scheduled00, iata = "BA0001", terminal = "T1", actPax = 15),
-        ArrivalGenerator.apiFlight(flightId = 2, schDt = scheduled00, iata = "FR8819", terminal = "XXX", actPax = 10)
-      ))
-
-      val fiveMinutes = 600d / 60
-
-      val crunch = runCrunchGraph(
-        now = () => SDate(scheduled),
-        airportConfig = airportConfig.copy(defaultProcessingTimes = Map("T1" -> Map(eeaMachineReadableToDesk -> fiveMinutes))),
-        minutesToCrunch = 120,
-        crunchStartDateProvider = (_) => getLocalLastMidnight(SDate(scheduled)),
-        crunchEndDateProvider = (_) => getLocalLastMidnight(SDate(scheduled)).addMinutes(120)
-      )
-
-      crunch.liveArrivalsInput.offer(flights)
-
-      val result = crunch.liveTestProbe.expectMsgAnyClassOf(classOf[PortState])
-      val resultSummary = paxLoadsFromPortState(result, 30)
-
-      val expected = Map(
-        "T1" -> Map(Queues.EeaDesk -> Seq(
-          15.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-          0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0)))
 
       resultSummary === expected
     }
