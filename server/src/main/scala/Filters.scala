f64c16d8dafb6cafbafb1e41ce81f434d8af82aa
Rich Birch <rich@freshcodelimited.com>
2017-12-22 14:18:36 +0000

    DRT-4898 Remove debug log

--- server/src/main/scala/Filters.scala
+++ server/src/main/scala/Filters.scala
@@ -1,15 +1,10 @@
 import com.google.inject.Inject
 import controllers.NoCacheFilter
-import org.slf4j.LoggerFactory
 import play.api.Environment
 import play.api.http.HttpFilters
 
 class Filters @Inject()(
                          env: Environment,
                          noCache: NoCacheFilter) extends HttpFilters {
-  val log = LoggerFactory.getLogger(getClass)
-  override val filters = {
-    log.info(s"getting filters")
-    Seq(noCache)
-  }
+  override val filters = Seq(noCache)
 }
