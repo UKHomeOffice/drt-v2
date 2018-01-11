58eaf3598233a4c9a25c9851ad17569fa65d2b87
Ben Eppel <b.eppel@gmail.com>
2018-01-11 09:57:09 +0000

    DRT-4908 Fix default value to for year select

--- client/src/main/scala/drt/client/components/SnapshotSelector.scala
+++ client/src/main/scala/drt/client/components/SnapshotSelector.scala
@@ -112,7 +112,7 @@ object SnapshotSelector {
             <.div(^.className := "col-sm-2 no-gutters", <.label("Snapshot as of", ^.className := "text")),
             <.div(^.className := "col-sm-1 no-gutters narrower", drawSelect(List.range(1, daysInMonth(state.month, state.year) + 1).map(_.toString), days.map(_.toString), state.day, (v: String) => (s: State) => s.copy(day = v.toInt))),
             <.div(^.className := "col-sm-2 no-gutters narrower", drawSelect(months.map(_._1.toString), months.map(_._2.toString), state.month, (v: String) => (s: State) => s.copy(month = v.toInt))),
-            <.div(^.className := "col-sm-1 no-gutters narrower", drawSelect(years.map(_.toString), years.map(_.toString), state.day, (v: String) => (s: State) => s.copy(year = v.toInt))),
+            <.div(^.className := "col-sm-1 no-gutters narrower", drawSelect(years.map(_.toString), years.map(_.toString), state.year, (v: String) => (s: State) => s.copy(year = v.toInt))),
             <.div(^.className := "col-sm-1 no-gutters", <.label("at", ^.className := "text center")),
             <.div(^.className := "col-sm-1 no-gutters narrower", drawSelect(hours.map(h => f"$h%02d"), hours.map(_.toString), state.hours, (v: String) => (s: State) => s.copy(hours = v.toInt))),
             <.div(^.className := "col-sm-1 no-gutters narrower", drawSelect(minutes.map(m => f"$m%02d"), minutes.map(_.toString), state.minutes, (v: String) => (s: State) => s.copy(minutes = v.toInt))),
