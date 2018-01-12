0fffeecf718cdf33a04fc3acd23b2b49e40fbe2c
Rich Birch <rich@freshcodelimited.com>
2018-01-12 15:17:06 +0000

    DRT-4899 Oops

--- project/Settings.scala
+++ project/Settings.scala
@@ -10,7 +10,7 @@ object Settings {
   val name = "scalajs-spa"
 
   /** The version of your application */
-  val version = sys.env.getOrElse("BUILD_ID", "dev-yeah")
+  val version = sys.env.getOrElse("BUILD_ID", "dev")
 
   /** Options for the scala compiler */
   val scalacOptions = Seq(
