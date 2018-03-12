f9b64a268dbd46419d1cfa1ab719a239bb56597f
Rich Birch <rich@freshcodelimited.com>
2018-03-12 11:30:14 +0000

    DRT-4891 Add new crunch stage tests

--- server/src/main/scala/services/graphstages/CrunchLoadGraphStage.scala
+++ server/src/main/scala/services/graphstages/CrunchLoadGraphStage.scala
@@ -2,7 +2,7 @@ package services.graphstages
 
 import akka.stream._
 import akka.stream.stage.{GraphStage, GraphStageLogic, InHandler, OutHandler}
-import drt.shared.CrunchApi.{CrunchMinute, MillisSinceEpoch}
+import drt.shared.CrunchApi.{CrunchMinute, CrunchMinutes, MillisSinceEpoch}
 import drt.shared.FlightsApi.{QueueName, TerminalName}
 import drt.shared._
 import org.slf4j.{Logger, LoggerFactory}
@@ -11,18 +11,18 @@ import services.{OptimizerConfig, OptimizerCrunchResult, SDate}
 
 import scala.collection.immutable.Map
 import scala.language.postfixOps
-import scala.util.{Success, Try}
+import scala.util.{Failure, Success, Try}
 
 
-class CrunchLoadGraphStage(optionalInitialCrunchMinutes: Option[Set[CrunchMinute]],
+class CrunchLoadGraphStage(optionalInitialCrunchMinutes: Option[CrunchMinutes],
                            airportConfig: AirportConfig,
                            expireAfterMillis: MillisSinceEpoch,
                            now: () => SDateLike,
                            crunch: (Seq[Double], Seq[Int], Seq[Int], OptimizerConfig) => Try[OptimizerCrunchResult])
-  extends GraphStage[FlowShape[Loads, Set[CrunchMinute]]] {
+  extends GraphStage[FlowShape[Loads, CrunchMinutes]] {
 
   val inLoads: Inlet[Loads] = Inlet[Loads]("inLoads.in")
-  val outCrunchMinutes: Outlet[Set[CrunchMinute]] = Outlet[Set[CrunchMinute]]("outCrunchMinutes.out")
+  val outCrunchMinutes: Outlet[CrunchMinutes] = Outlet[CrunchMinutes]("outCrunchMinutes.out")
 
   override val shape = new FlowShape(inLoads, outCrunchMinutes)
 
@@ -60,13 +60,14 @@ class CrunchLoadGraphStage(optionalInitialCrunchMinutes: Option[Set[CrunchMinute
          */
 
         val allMinuteMillis = incomingLoads.loadMinutes.map(_.minute)
-        val firstMinute = Crunch.getLocalLastMidnight(SDate(allMinuteMillis.min)).millisSinceEpoch
-        val lastMinute = Crunch.getLocalNextMidnight(SDate(allMinuteMillis.max)).millisSinceEpoch
+        val firstMinute = Crunch.getLocalLastMidnight(SDate(allMinuteMillis.min))
+        val lastMinute = Crunch.getLocalNextMidnight(SDate(allMinuteMillis.max))
+        log.info(s"Crunch ${firstMinute.toLocalDateTimeString()} - ${lastMinute.toLocalDateTimeString()}")
 
         val updatedLoads: Map[Int, LoadMinute] = mergeLoads(incomingLoads.loadMinutes, loadMinutes)
         loadMinutes = Crunch.purgeExpired(updatedLoads, (lm: LoadMinute) => lm.minute, now, expireAfterMillis)
 
-        val crunchMinutes: Set[CrunchMinute] = crunchLoads(firstMinute, lastMinute, loadMinutes.values.toSet)
+        val crunchMinutes: Set[CrunchMinute] = crunchLoads(firstMinute.millisSinceEpoch, lastMinute.millisSinceEpoch, loadMinutes.values.toSet)
         val crunchMinutesByKey = crunchMinutes.map(cm => (cm.key, cm)).toMap
 
         val diff = crunchMinutes -- existingCrunchMinutes.values.toSet
@@ -92,22 +93,27 @@ class CrunchLoadGraphStage(optionalInitialCrunchMinutes: Option[Set[CrunchMinute
             .groupBy(_.queueName)
             .flatMap {
               case (qn, qLms) =>
+                log.info(s"Crunching $tn $qn")
                 val sla = airportConfig.slaByQueue.getOrElse(qn, 15)
                 val sortedLms = qLms.toSeq.sortBy(_.minute)
                 val workMinutes: Map[MillisSinceEpoch, Double] = sortedLms.map(m => (m.minute, m.workLoad)).toMap
                 val paxMinutes: Map[MillisSinceEpoch, Double] = sortedLms.map(m => (m.minute, m.paxLoad)).toMap
                 val minuteMillis = firstMinute until lastMinute by 60000
-                val fullWorkloadMinutes = minuteMillis.map(m => workMinutes.getOrElse(m, 0d))
+                val fullWorkMinutes = minuteMillis.map(m => workMinutes.getOrElse(m, 0d))
+                val fullPaxMinutes = minuteMillis.map(m => paxMinutes.getOrElse(m, 0d))
                 val (minDesks, maxDesks) = minMaxDesksForQueue(minuteMillis, tn, qn)
-                val triedResult: Try[OptimizerCrunchResult] = crunch(fullWorkloadMinutes, minDesks, maxDesks, OptimizerConfig(sla))
+                val triedResult: Try[OptimizerCrunchResult] = crunch(fullWorkMinutes, minDesks, maxDesks, OptimizerConfig(sla))
                 triedResult match {
                   case Success(OptimizerCrunchResult(desks, waits)) =>
                     minuteMillis.zipWithIndex.map {
                       case (minute, idx) =>
-                        val wl = workMinutes(minute)
-                        val pl = paxMinutes(minute)
-                        CrunchMinute(tn, qn, minute, wl, pl, desks(idx), waits(idx))
+                        val wl = fullWorkMinutes(idx)
+                        val pl = fullPaxMinutes(idx)
+                        CrunchMinute(tn, qn, minute, pl, wl, desks(idx), waits(idx))
                     }
+                  case Failure(t) =>
+                    log.warn(s"failed to crunch: $t")
+                    Set()
                 }
             }
         }.toSet
@@ -160,7 +166,7 @@ class CrunchLoadGraphStage(optionalInitialCrunchMinutes: Option[Set[CrunchMinute
       if (crunchMinutesToPush.isEmpty) log.info(s"We have no crunch minutes. Nothing to push")
       else if (isAvailable(outCrunchMinutes)) {
         log.info(s"Pushing ${crunchMinutesToPush.size} crunch minutes")
-        push(outCrunchMinutes, crunchMinutesToPush.values.toSet)
+        push(outCrunchMinutes, CrunchMinutes(crunchMinutesToPush.values.toSet))
         crunchMinutesToPush = Map()
       } else log.info(s"outCrunchMinutes not available to push")
     }
