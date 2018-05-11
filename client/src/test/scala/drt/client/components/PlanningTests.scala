9296db1ec00cf78704bdae607c913c359bcb6fcd
Rich Birch <rich@freshcodelimited.com>
2018-05-11 16:05:28 +0100

    Fix tests to pass regardless of server timezone

--- client/src/test/scala/drt/client/components/PlanningTests.scala
+++ client/src/test/scala/drt/client/components/PlanningTests.scala
@@ -12,38 +12,38 @@ object PlanningTests extends TestSuite {
       "Given a Sunday then I should get Midnight same day back" - {
         val start = SDate("2017-10-22T18:15:00")
 
-        val result = getLastSunday(start)
+        val result = getLastSunday(start).millisSinceEpoch
 
-        val expected = SDate("2017-10-22T02:00:00")
+        val expected = SDate("2017-10-22T02:00:00").millisSinceEpoch
 
-        assert(result.toISOString() == expected.toISOString())
+        assert(result == expected)
       }
       "Given a Monday then I should get Midnight previous Sunday back" - {
         val start = SDate("2017-10-23T18:15:00")
 
-        val result = getLastSunday(start)
+        val result = getLastSunday(start).millisSinceEpoch
 
-        val expected = SDate("2017-10-22T02:00:00")
+        val expected = SDate("2017-10-22T02:00:00").millisSinceEpoch
 
-        assert(result.toISOString() == expected.toISOString())
+        assert(result == expected)
       }
       "Given a Friday then I should get Midnight last Sunday back" - {
         val start = SDate("2017-10-27T18:15:00")
 
-        val result = getLastSunday(start)
+        val result = getLastSunday(start).millisSinceEpoch
 
-        val expected = SDate("2017-10-22T02:00:00")
+        val expected = SDate("2017-10-22T02:00:00").millisSinceEpoch
 
-        assert(result.toISOString() == expected.toISOString())
+        assert(result == expected)
       }
       "Given a Saturday then I should get Midnight last Sunday back" - {
         val start = SDate("2017-10-28T18:15:00")
 
-        val result = getLastSunday(start)
+        val result = getLastSunday(start).millisSinceEpoch
 
-        val expected = SDate("2017-10-22T02:00:00")
+        val expected = SDate("2017-10-22T02:00:00").millisSinceEpoch
 
-        assert(result.toISOString() == expected.toISOString())
+        assert(result == expected)
       }
     }
   }
