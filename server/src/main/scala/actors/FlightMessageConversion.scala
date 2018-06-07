0bebecdd10836cf5e0aeaa0c67d3df3f75fc7439
Ryan Da Costa <Ryan.daCosta@digital.homeoffice.gov.uk>
2018-06-07 16:06:06 +0100

    [NIJ] - better formatting and use a clearer filter on strings

--- server/src/main/scala/actors/FlightMessageConversion.scala
+++ server/src/main/scala/actors/FlightMessageConversion.scala
@@ -5,7 +5,6 @@ import org.apache.commons.lang3.StringUtils
 import server.protobuf.messages.CrunchState.{FlightWithSplitsMessage, PaxTypeAndQueueCountMessage, SplitMessage}
 import server.protobuf.messages.FlightsMessage.{FlightMessage, FlightStateSnapshotMessage}
 import services.{ArrivalsState, SDate}
-
 import scala.util.{Success, Try}
 
 object FlightMessageConversion {
@@ -47,11 +46,11 @@ object FlightMessageConversion {
       stand = apiFlight.Stand.filter(StringUtils.isNotBlank(_)),
       status = Option(StringUtils.trimToNull(apiFlight.Status)),
       maxPax = apiFlight.MaxPax.filter(_ != 0),
-      actPax = apiFlight.ActPax.filter(_!= 0),
+      actPax = apiFlight.ActPax.filter(_ != 0),
       tranPax = apiFlight.TranPax,
       runwayID = apiFlight.RunwayID.filter(StringUtils.isNotBlank(_)),
       baggageReclaimId = apiFlight.BaggageReclaimId.filter(StringUtils.isNotBlank(_)),
-      flightID = apiFlight.FlightID.filter(_!= 0),
+      flightID = apiFlight.FlightID.filter(_ != 0),
       airportID = Option(StringUtils.trimToNull(apiFlight.AirportID)),
       terminal = Option(StringUtils.trimToNull(apiFlight.Terminal)),
       iCAO = Option(StringUtils.trimToNull(apiFlight.rawICAO)),
@@ -59,11 +58,11 @@ object FlightMessageConversion {
       origin = Option(StringUtils.trimToNull(apiFlight.Origin)),
       pcpTime = apiFlight.PcpTime.filter(_ != 0),
 
-      scheduled = Option(apiFlight.Scheduled).filter(_!= 0),
-      estimated = apiFlight.Estimated.filter(_!= 0),
-      touchdown = apiFlight.Actual.filter(_!= 0),
-      estimatedChox = apiFlight.EstimatedChox.filter(_!= 0),
-      actualChox = apiFlight.ActualChox.filter(_!= 0)
+      scheduled = Option(apiFlight.Scheduled).filter(_ != 0),
+      estimated = apiFlight.Estimated.filter(_ != 0),
+      touchdown = apiFlight.Actual.filter(_ != 0),
+      estimatedChox = apiFlight.EstimatedChox.filter(_ != 0),
+      actualChox = apiFlight.ActualChox.filter(_ != 0)
     )
   }
 
@@ -80,20 +79,20 @@ object FlightMessageConversion {
 
   def flightMessageToApiFlight(flightMessage: FlightMessage): Arrival = {
     Arrival(
-      Operator = flightMessage.operator.filter(_ != ""),
+      Operator = flightMessage.operator.filter(StringUtils.isNotBlank(_)),
       Status = flightMessage.status.getOrElse(""),
-      Estimated = flightMessage.estimated.filter(_!= 0),
-      Actual = flightMessage.touchdown.filter(_!=0),
-      EstimatedChox = flightMessage.estimatedChox.filterNot(_!=0),
-      ActualChox = flightMessage.actualChox.filter(_!=0),
-      Gate = flightMessage.gate.filter(_ != ""),
-      Stand = flightMessage.stand.filter(_ != ""),
+      Estimated = flightMessage.estimated.filter(_ != 0),
+      Actual = flightMessage.touchdown.filter(_ != 0),
+      EstimatedChox = flightMessage.estimatedChox.filterNot(_ != 0),
+      ActualChox = flightMessage.actualChox.filter(_ != 0),
+      Gate = flightMessage.gate.filter(StringUtils.isNotBlank(_)),
+      Stand = flightMessage.stand.filter(StringUtils.isNotBlank(_)),
       MaxPax = flightMessage.maxPax.filter(_ != 0),
       ActPax = flightMessage.actPax.filter(_ != 0),
       TranPax = flightMessage.tranPax,
-      RunwayID = flightMessage.runwayID.filter(_ != ""),
-      BaggageReclaimId = flightMessage.baggageReclaimId.filter(_ != ""),
-      FlightID = flightMessage.flightID.filter(_ != ""),
+      RunwayID = flightMessage.runwayID.filter(StringUtils.isNotBlank(_)),
+      BaggageReclaimId = flightMessage.baggageReclaimId.filter(StringUtils.isNotBlank(_)),
+      FlightID = flightMessage.flightID.filter(StringUtils.isNotBlank(_)),
       AirportID = flightMessage.airportID.getOrElse(""),
       Terminal = flightMessage.terminal.getOrElse(""),
       rawICAO = flightMessage.iCAO.getOrElse(""),
