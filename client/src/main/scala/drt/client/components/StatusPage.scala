814b3249227ca7e13847230639c78b2d266aac9c
Rich Birch <ringo@albumsnaps.com>
2018-07-10 18:42:39 +0100

    DRT-4958 Tasty styling

--- client/src/main/scala/drt/client/components/StatusPage.scala
+++ client/src/main/scala/drt/client/components/StatusPage.scala
@@ -3,6 +3,7 @@ package drt.client.components
 import drt.client.logger.{Logger, LoggerFactory}
 import drt.client.services.JSDateConversions.SDate
 import drt.client.services.SPACircuit
+import drt.shared.CrunchApi.MillisSinceEpoch
 import drt.shared.{FeedStatusFailure, FeedStatusSuccess, FeedStatuses}
 import japgolly.scalajs.react.ScalaComponent
 import japgolly.scalajs.react.vdom.html_<^._
@@ -26,11 +27,19 @@ object StatusPage {
               val rag = if (feed.lastSuccessAt.getOrElse(0L) > feed.lastFailureAt.getOrElse(0L)) "green" else "red"
               <.div(^.className := s"feed-status $rag",
                 <.h3(feed.name),
-                <.ul(
-                  <.li(s"Last successful connection: ${feed.lastSuccessAt.map(lu => SDate(lu).prettyDateTime()).getOrElse("n/a")}"),
-                  <.li(s"Last updates: ${feed.lastUpdatesAt.map(lu => SDate(lu).prettyDateTime()).getOrElse("n/a")}"),
-                  <.li(s"Last failed connection: ${feed.lastFailureAt.map(lu => SDate(lu).prettyDateTime()).getOrElse("n/a")}")
-                ),
+                {
+                  val times = Seq(
+                    ("Checked", feed.lastSuccessAt),
+                    ("Updated", feed.lastUpdatesAt),
+                    ("Failed", feed.lastFailureAt)
+                  )
+                  <.ul(^.className := "times-summary", times.zipWithIndex.map {
+                    case ((label, maybeSDate), idx) =>
+                      val className = if (idx == times.length - 1) "last" else ""
+                      <.li(^.className := className, <.div(^.className := "vert-align", <.div(<.div(label), <.div(s"${maybeSDate.map(lu => s"${minutesAgo(lu)} mins").getOrElse("n/a")}"))))
+                  }.toVdomArray)
+                },
+                <.div(^.className := "clear"),
                 <.h4("Recent connections"),
                 <.ul(
                   feed.statuses.sortBy(_.date).reverse.map {
@@ -45,6 +54,10 @@ object StatusPage {
       }
     })
     .build
+
+  def minutesAgo(millisToCheck: MillisSinceEpoch) = {
+    (SDate.now().millisSinceEpoch - millisToCheck) / 60000
+  }
 
   def apply(): VdomElement = component(Props())
 }
