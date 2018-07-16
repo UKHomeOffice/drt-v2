9e0b8f2716b65c36ff991bf0f96da678cf8061fd
MassimoDanieli <massimo@massimodanieli.com>
2018-07-16 11:16:39 +0100

    added k8s deployment files

---
+++ bin/deploy.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+
+# default values
+export DRONE_DEPLOY_TO=${DRONE_DEPLOY_TO:?'[error] Please specify which cluster to deploy to.'}
+export KUBE_NAMESPACE=${KUBE_NAMESPACE=drt-dev}
+
+export NAME="lhr"
+
+case ${DRONE_DEPLOY_TO} in
+  'acp-notprod')
+    export KUBE_SERVER="https://kube-api-notprod.notprod.acp.homeoffice.gov.uk"
+    export KUBE_TOKEN=${KUBE_TOKEN_ACP_NOTPROD}
+    ;;
+esac
+
+echo "--- kube api url: ${KUBE_SERVER}"
+echo "--- namespace: ${KUBE_NAMESPACE}"
+
+echo "--- downloading ca for kube api"
+if ! curl --silent --fail --retry 5 \
+    https://raw.githubusercontent.com/UKHomeOffice/acp-ca/master/${DRONE_DEPLOY_TO}.crt -o /tmp/ca.crt; then
+  echo "[error] failed to download ca for kube api"
+  exit 1
+fi
+
+echo "--- deploying drt-LHR application"
+if ! kd --certificate-authority=/tmp/ca.crt \
+  --namespace=${KUBE_NAMESPACE} \
+  --timeout=5m \
+  -f kube/networkpolicy.yaml \
+  -f kube/service.yaml \
+  -f kube/deployment.yaml \
+  -f kube/ingress.yaml; then
+  echo "[error] failed to deploy drt-LHR application"
+  exit 1
+fi
