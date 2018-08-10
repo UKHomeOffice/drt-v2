2fc4f5cb5821e86e98edbc6b3c5283afc6be30d2
Rich Birch <rich@freshcodelimited.com>
2018-08-10 14:40:07 +0100

    DRT-5145 Better test coverage  - Test no snapshot taken when under the threshold

--- server/src/test/scala/services/persistence/SnapshottingSpec.scala
+++ server/src/test/scala/services/persistence/SnapshottingSpec.scala
@@ -8,6 +8,8 @@ import org.slf4j.{Logger, LoggerFactory}
 import server.protobuf.messages.FlightsMessage.{FlightMessage, FlightsDiffMessage}
 import services.crunch.CrunchTestLike
 
+import scala.concurrent.duration._
+
 class TestSnapshottingActor(probe: ActorRef, snapshotMessage: GeneratedMessage, override val snapshotBytesThreshold: Int = 0, override val maybeSnapshotInterval: Option[Int] = None) extends RecoveryActorLike {
   override val log: Logger = LoggerFactory.getLogger(getClass)
 
@@ -46,6 +48,41 @@ class SnapshottingSpec extends CrunchTestLike {
     val received = probe.receiveN(1)
 
     received === Seq(snapshotMessage)
+  }
+
+  "Given an actor extending the RecoveryActorLike trait " +
+    "When I send a message to be persisted and it's under the bytes threshold " +
+    "Then I should not see the a snapshot being saved" >> {
+    val probe = TestProbe("snapshotprobe")
+
+    val snapshotMessage = new FlightsDiffMessage(None, Seq(), Seq())
+
+    val snapshotBytesThreshold = 10
+    val testActor = system.actorOf(Props(classOf[TestSnapshottingActor], probe.ref, snapshotMessage, snapshotBytesThreshold, None))
+
+    testActor ! new FlightMessage(iATA = Option("BA1010"))
+
+    probe.expectNoMessage(2 seconds)
+
+    true
+  }
+
+  "Given an actor extending the RecoveryActorLike trait and a snapshot interval of 10 " +
+    "When I send 9 messages to be persisted " +
+    "Then I should not see a snapshot being saved" >> {
+    val probe = TestProbe("snapshotprobe")
+
+    val snapshotMessage = new FlightsDiffMessage(None, Seq(), Seq())
+
+    val snapshotInterval = Option(10)
+    val snapshotBytesThreshold = 1024
+    val testActor = system.actorOf(Props(classOf[TestSnapshottingActor], probe.ref, snapshotMessage, snapshotBytesThreshold, snapshotInterval))
+
+    1 to 9 foreach (_ => testActor ! new FlightMessage(iATA = Option("BA1010")))
+
+    probe.expectNoMessage(2 seconds)
+
+    true
   }
 
   "Given an actor extending the RecoveryActorLike trait and a snapshot interval of 10 " +
