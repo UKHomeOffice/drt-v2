38f83bcd66911c9b02afcceae7f318b7cdf99668
Massimo Danieli <massimo@massimodanieli.com>
2018-08-20 16:50:54 +0100

    cyrepss moved in root

---
+++ cypress/integration/arrival.js
@@ -0,0 +1,146 @@
+describe('Arrivals page', () => {
+
+  const currentDateTime = new Date();
+  const schDT = currentDateTime.toISOString().split("T")[0];
+  const currentMillis = currentDateTime.getTime();
+
+  function addFlight() {
+    cy.request('POST',
+      '/v2/test/live/test/arrival',
+      {
+        "Operator": "TestAir",
+        "Status": "On Chocks",
+        "EstDT": schDT + "T00:55:00Z",
+        "ActDT": schDT + "T00:55:00Z",
+        "EstChoxDT": schDT + "T01:01:00Z",
+        "ActChoxDT": schDT + "T01:05:00Z",
+        "Gate": "46",
+        "Stand": "44R",
+        "MaxPax": 78,
+        "ActPax": 51,
+        "TranPax": 0,
+        "RunwayID": "05L",
+        "BaggageReclaimId": "05",
+        "FlightID": 14710007,
+        "AirportID": "MAN",
+        "Terminal": "T1",
+        "ICAO": "TS123",
+        "IATA": "TS123",
+        "Origin": "AMS",
+        "SchDT": schDT + "T00:15:00Z"
+      });
+  }
+
+  function loadManifestFixture() {
+    cy.request('POST', '/v2/test/live/test/manifest', manifest);
+    cy.request('GET', '/v2/test/live/export/arrivals/' + currentMillis + '/T1?startHour=0&endHour=24')
+    cy.get('.pax-api');
+  }
+
+  function setRoles(roles) {
+    cy.request("POST", 'v2/test/live/test/mock-roles', { "roles": roles})
+  }
+
+  before(() => {
+    cy.request('DELETE', '/v2/test/live/test/data');
+  });
+
+  const manifest = {
+    "EventCode": "DC",
+    "DeparturePortCode": "AMS",
+    "VoyageNumberTrailingLetter": "",
+    "ArrivalPortCode": "STN",
+    "DeparturePortCountryCode": "MAR",
+    "VoyageNumber": "0123",
+    "VoyageKey": "key",
+    "ScheduledDateOfDeparture": schDT,
+    "ScheduledDateOfArrival": schDT,
+    "CarrierType": "AIR",
+    "CarrierCode": "TS",
+    "ScheduledTimeOfDeparture": "06:30:00",
+    "PassengerList": [
+      {
+        "DocumentIssuingCountryCode": "GBR",
+        "PersonType": "P",
+        "DocumentLevel": "Primary",
+        "Age": "30",
+        "DisembarkationPortCode": "",
+        "InTransitFlag": "N",
+        "DisembarkationPortCountryCode": "",
+        "NationalityCountryEEAFlag": "EEA",
+        "PassengerIdentifier": "",
+        "DocumentType": "P",
+        "PoavKey": "1",
+        "NationalityCountryCode": "GBR"
+      },
+      {
+        "DocumentIssuingCountryCode": "ZWE",
+        "PersonType": "P",
+        "DocumentLevel": "Primary",
+        "Age": "30",
+        "DisembarkationPortCode": "",
+        "InTransitFlag": "N",
+        "DisembarkationPortCountryCode": "",
+        "NationalityCountryEEAFlag": "",
+        "PassengerIdentifier": "",
+        "DocumentType": "P",
+        "PoavKey": "2",
+        "NationalityCountryCode": "ZWE"
+      },
+      {
+        "DocumentIssuingCountryCode": "AUS",
+        "PersonType": "P",
+        "DocumentLevel": "Primary",
+        "Age": "30",
+        "DisembarkationPortCode": "",
+        "InTransitFlag": "N",
+        "DisembarkationPortCountryCode": "",
+        "NationalityCountryEEAFlag": "",
+        "PassengerIdentifier": "",
+        "DocumentType": "P",
+        "PoavKey": "3",
+        "NationalityCountryCode": "AUS"
+      }
+    ],
+    "ScheduledTimeOfArrival": "00:15:00",
+    "FileId": "fileID"
+  };
+
+  const csvWithNoApiSplits = "IATA,ICAO,Origin,Gate/Stand,Status,Scheduled Date,Scheduled Time,Est Arrival,Act Arrival,Est " +
+    "Chox,Act Chox,Est PCP,Total Pax,PCP Pax,API e-Gates,API EEA,API Non-EEA,API Fast Track,Historical e-Gates,Historical " +
+    "EEA,Historical Non-EEA,Historical Fast Track,Terminal Average e-Gates,Terminal Average EEA,Terminal " +
+    "Average Non-EEA,Terminal Average Fast Track" + "\n" +
+    "TS0123,TS0123,AMS,46/44R,On Chocks," + schDT + ",01:15,01:55,01:55,02:01,02:05,02:18,51,51,17,0,34,,,,,,13,37,1,";
+
+  const csvWithhAPISplits = "IATA,ICAO,Origin,Gate/Stand,Status,Scheduled Date,Scheduled Time,Est Arrival,Act Arrival," +
+    "Est Chox,Act Chox,Est PCP,Total Pax,PCP Pax,API e-Gates,API EEA,API Non-EEA,API Fast Track,Historical e-Gates," +
+    "Historical EEA,Historical Non-EEA,Historical Fast Track,Terminal Average e-Gates,Terminal Average EEA,Terminal " +
+    "Average Non-EEA,Terminal Average Fast Track,API Actual - EEA (Machine Readable),API Actual - Non EEA (Non Visa)," +
+    "API Actual - Non EEA (Visa),API Actual - eGates" + "\n" +
+    "TS0123,TS0123,AMS,46/44R,On Chocks," + schDT + ",01:15,01:55,01:55,02:01,02:05,02:18,51,51,17,0,34,,,,,,13,37,1,,0.0,1.0,1.0,1.0";
+
+  it('Displays a flight after it has been ingested via the live feed', () => {
+    addFlight();
+    cy.visit('/v2/test/live#terminal/T1/current/arrivals//0/24');
+    cy.get("#arrivals").contains("TS0123")
+  });
+
+  it('Does not show API splits in the flights export for regular users', () => {
+    loadManifestFixture();
+    cy.request('POST', '/v2/test/live/test/manifest', manifest);
+    cy.request('GET', '/v2/test/live/export/arrivals/' + currentMillis + '/T1?startHour=0&endHour=24').then((resp) => {
+      expect(resp.body).to.equal(csvWithNoApiSplits)
+    });
+  });
+
+  it('Allows you to view API splits in the flights export for super users', () => {
+    loadManifestFixture();
+    setRoles(["drt:team"]);
+    cy.request({
+      method: 'GET',
+      url: '/v2/test/live/export/arrivals/' + currentMillis + '/T1?startHour=0&endHour=24',
+    }).then((resp) => {
+      expect(resp.body).to.equal(csvWithhAPISplits)
+    })
+  });
+});
---
+++ cypress/integration/staff-movements.js
@@ -0,0 +1,68 @@
+describe('Staff movements', function () {
+  before(function () {
+    cy.request('DELETE', '/v2/test/live/test/data');
+    var schDT = new Date().toISOString().split("T")[0];
+    cy.request('POST',
+      '/v2/test/live/test/arrival',
+      {
+        "Operator": "Flybe",
+        "Status": "On Chocks",
+        "EstDT": schDT + "T00:55:00Z",
+        "ActDT": schDT + "T00:55:00Z",
+        "EstChoxDT": schDT + "T01:01:00Z",
+        "ActChoxDT": schDT + "T01:05:00Z",
+        "Gate": "46",
+        "Stand": "44R",
+        "MaxPax": 78,
+        "ActPax": 51,
+        "TranPax": 0,
+        "RunwayID": "05L",
+        "BaggageReclaimId": "05",
+        "FlightID": 14710007,
+        "AirportID": "MAN",
+        "Terminal": "T1",
+        "ICAO": "SA123",
+        "IATA": "SA123",
+        "Origin": "AMS",
+        "SchDT": schDT + "T00:15:00Z"
+      });
+  });
+  function addMovementFor1Hour() {
+    cy.get('#sticky-body > :nth-child(1)').contains("+").click();
+    cy.contains("Save").click();
+  }
+
+  describe('When adding staff movements on the desks and queues page', function () {
+    it("Should update the available staff when 1 staff member is added for 1 hour", function () {
+      cy.visit('/v2/test/live#terminal/T1/current/desksAndQueues//0/24');
+      addMovementFor1Hour();
+      var staffDeployedSelector = '#sticky-body > :nth-child(1) > :nth-child(14)';
+      cy.get(staffDeployedSelector).contains("1");
+      cy.contains("Staff Movements").click();
+      cy.get('tbody > :nth-child(2) > td').eq(0).contains("1");
+      cy.get('tbody > :nth-child(2) > td').eq(1).contains("1");
+      cy.get('tbody > :nth-child(2) > td').eq(2).contains("1");
+      cy.get('tbody > :nth-child(2) > td').eq(3).contains("1");
+      cy.get('tbody > :nth-child(2) > td').eq(4).contains("0");
+      cy.get('.fa-remove').click()
+    });
+    it("Should update the available staff when 1 staff member is added for 1 hour twice", function () {
+      cy.visit('/v2/test/live#terminal/T1/current/desksAndQueues//0/24');
+      addMovementFor1Hour();
+      addMovementFor1Hour();
+      var staffDeployedSelector = '#sticky-body > :nth-child(1) > :nth-child(14)';
+      cy.get(staffDeployedSelector).contains("1");
+      cy.contains("Staff Movements").click();
+      cy.get('tbody > :nth-child(2) > td').eq(0).contains("2");
+      cy.get('tbody > :nth-child(2) > td').eq(1).contains("2");
+      cy.get('tbody > :nth-child(2) > td').eq(2).contains("2");
+      cy.get('tbody > :nth-child(2) > td').eq(3).contains("2");
+      cy.get('tbody > :nth-child(2) > td').eq(4).contains("0");
+      cy.get('.fa-remove').each(function (el) {
+        el.click()
+      })
+
+    });
+  });
+
+});
