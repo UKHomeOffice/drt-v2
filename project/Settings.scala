cd250f64e0dd1a1ad7a93be206df7bd3bcc9ac1f
Massimo Danieli <massimo@massimodanieli.com>
2018-09-06 15:21:16 +0100

    typo in build.sbt

--- project/Settings.scala
+++ project/Settings.scala
@@ -100,7 +100,7 @@ object Settings {
     "com.hierynomus" % "sshj" % sshJ,
     "com.lihaoyi" %% "pprint" % pprint,
     "com.lihaoyi" %%% "utest" % uTest % Test,
-    "com.mfglabs" %% "commons-aws-s3" % awsCommons,
+    "com.mfglabs" %% "commons-aws" % awsCommons,
 
     "javax.mail" % "mail" % "1.4.7",
     "info.folone" %% "poi-scala" % "0.18",
