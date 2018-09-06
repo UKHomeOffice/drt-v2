006c6f1afd6f901f22645a9dd04dc8d4ff58d13c
Rich Birch <rich@freshcodelimited.com>
2018-09-06 12:46:06 +0100

    DRT-5033 Fix tests

--- server/src/test/scala/services/staffing/FixedPointsActorSpec.scala
+++ server/src/test/scala/services/staffing/FixedPointsActorSpec.scala
@@ -5,7 +5,7 @@ import akka.actor.{ActorRef, Props}
 import akka.pattern.AskableActorRef
 import akka.testkit.TestProbe
 import akka.util.Timeout
-import drt.shared.{MilliDate, StaffAssignment}
+import drt.shared.{MilliDate, StaffAssignment, StaffAssignments}
 import services.SDate
 import services.crunch.CrunchTestLike
 import services.graphstages.StaffAssignmentHelper
@@ -16,7 +16,7 @@ import scala.concurrent.duration._
 class TestableFixedPointsActor(testProbe: ActorRef) extends FixedPointsActor {
   def sendAck(): Unit = testProbe ! MsgAck
 
-  override def onUpdateState(data: Seq[StaffAssignment]): Unit = {
+  override def onUpdateState(data: StaffAssignments): Unit = {
     super.onUpdateState(data)
     sendAck()
   }
@@ -29,22 +29,23 @@ class FixedPointsActorSpec extends CrunchTestLike {
   "Given some fixed points and a fixed points actor " +
     "When I send the fixed points as a string to the actor and then query the actor's state " +
     "Then I should get back the same fixed points I previously sent it" >> {
-    val assignment = StaffAssignment("Roving officer", "T1", MilliDate(SDate("2018-01-01T00:00").millisSinceEpoch), MilliDate(SDate("2018-01-01T00:14").millisSinceEpoch), 1)
-    val fixedPoints = StaffAssignmentHelper.toCsv(assignment)
+    val staffAssignments = StaffAssignments(
+      Seq(StaffAssignment("Roving officer", "T1", MilliDate(SDate("2018-01-01T00:00").millisSinceEpoch), MilliDate(SDate("2018-01-01T00:14").millisSinceEpoch), 1, None))
+    )
 
     val probe = TestProbe()
 
     val fixedPointsActor = system.actorOf(Props(classOf[TestableFixedPointsActor], probe.ref))
     val askableFixedPointsActor: AskableActorRef = fixedPointsActor
 
-    fixedPointsActor ! fixedPoints
+    fixedPointsActor ! staffAssignments
 
     probe.expectMsgAnyClassOf(MsgAck.getClass)
 
-    val storedFixedPointsWithSpacing = Await.result(askableFixedPointsActor ? GetState, 1 second).asInstanceOf[String]
-    val storedFixedPointsWithoutSpacing = storedFixedPointsWithSpacing.replace(", ", ",")
+    val storedFixedPoints = Await.result(askableFixedPointsActor ? GetState, 1 second).asInstanceOf[StaffAssignments]
+    val expected = staffAssignments
 
-    storedFixedPointsWithoutSpacing === fixedPoints
+    storedFixedPoints === expected
   }
 }
 
