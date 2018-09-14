8d05dbbfa3e77a9ec51b463310401d3684f3c155
Rich Birch <rich@freshcodelimited.com>
2018-09-14 10:40:14 +0100

    DRT-5033 Fix initialisation of TestFixedPointActor

--- server/src/main/scala/test/TestDrtSystem.scala
+++ server/src/main/scala/test/TestDrtSystem.scala
@@ -26,7 +26,7 @@ class TestDrtSystem(override val actorSystem: ActorSystem, override val config: 
   override lazy val forecastCrunchStateActor: ActorRef = system.actorOf(testForecastCrunchStateProps, name = "crunch-forecast-state-actor")
   override lazy val voyageManifestsActor: ActorRef = system.actorOf(Props(classOf[TestVoyageManifestsActor], now, expireAfterMillis, snapshotIntervalVm), name = "voyage-manifests-actor")
   override lazy val shiftsActor: ActorRef = system.actorOf(Props(classOf[TestShiftsActor], now))
-  override lazy val fixedPointsActor: ActorRef = system.actorOf(Props(classOf[TestFixedPointsActor]))
+  override lazy val fixedPointsActor: ActorRef = system.actorOf(Props(classOf[TestFixedPointsActor], now))
   override lazy val staffMovementsActor: ActorRef = system.actorOf(Props(classOf[TestStaffMovementsActor]))
 
   system.log.warning(s"Using test System")
