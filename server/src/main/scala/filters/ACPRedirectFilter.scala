b694e1c238e9ed8c21894342ab4881aa9ff47471
Rich Birch <rich@freshcodelimited.com>
2018-12-07 13:52:36 +0000

    Update ACP url for redirect

--- server/src/main/scala/filters/ACPRedirectFilter.scala
+++ server/src/main/scala/filters/ACPRedirectFilter.scala
@@ -15,7 +15,7 @@ class ACPRedirectFilter @Inject()(
   val log: Logger = LoggerFactory.getLogger(getClass)
   val acpRedirectFlagIsSet: Boolean = config.get[Boolean]("feature-flags.acp-redirect")
   val portCode: String = config.get[String]("portcode")
-  val redirectUrl = s"https://$portCode.drt.homeoffice.gov.uk/v2/$portCode/live"
+  val redirectUrl = s"https://$portCode.drt.homeoffice.gov.uk"
 
   val redirectCheckUriRegEx = ".+/data/should-reload".r
   override def apply(next: RequestHeader => Future[Result])
