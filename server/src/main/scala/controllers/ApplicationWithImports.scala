c1891e41025062fc52f0f7f005f4b518e66e6453
Rich Birch <rich@freshcodelimited.com>
2019-01-22 11:42:08 +0000

    DRT-5277 Send json response and appropriate status code

--- server/src/main/scala/controllers/ApplicationWithImports.scala
+++ server/src/main/scala/controllers/ApplicationWithImports.scala
@@ -3,15 +3,18 @@ package controllers
 import java.nio.file.Paths
 import java.util.UUID
 
+import api.ApiResponseBody
 import drt.server.feeds.lhr.LHRForecastFeed
 import drt.server.feeds.lhr.forecast.LHRForecastXLSExtractor
 import drt.shared.FlightsApi.Flights
 import play.api.libs.Files
+import play.api.libs.json.Json._
 import play.api.mvc.{Action, Request}
 import server.feeds.StoreFeedImportArrivals
 
 import scala.language.postfixOps
 import scala.util.Success
+
 
 trait ApplicationWithImports {
   self: Application =>
@@ -30,7 +33,7 @@ trait ApplicationWithImports {
     if (arrivals.nonEmpty) {
       log.info(s"Import found ${arrivals.length} arrivals")
       ctrl.arrivalsImportActor ! StoreFeedImportArrivals(Flights(arrivals))
-      Ok(s"Arrivals have been queued for processing")
-    } else Ok("No arrivals found")
+      Accepted(toJson(ApiResponseBody("Arrivals have been queued for processing")))
+    } else BadRequest(toJson(ApiResponseBody("No arrivals found")))
   }
 }
