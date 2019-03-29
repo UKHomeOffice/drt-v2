c3869b3d8a59053eb30b00442087a8f51b3370d1
Rich Birch <rich@freshcodelimited.com>
2019-03-29 14:10:12 +0000

    DRT-5509 Implement chaining in all tests to ensure correct order

--- e2e/cypress/integration/monthly-staffing.js
+++ e2e/cypress/integration/monthly-staffing.js
@@ -4,6 +4,11 @@ moment.locale("en-gb");
 
 describe('Monthly Staffing', function () {
 
+  beforeEach(function () {
+    cy.deleteData()
+      .setRoles(["test"]);
+  });
+
   function firstMidnightOfThisMonth() {
     return moment().tz('Europe/London').startOf('month');
   }
@@ -12,22 +17,25 @@ describe('Monthly Staffing', function () {
     return firstMidnightOfThisMonth().add(1, 'M');
   }
 
-  function saveShifts() {
-    cy.request("POST", "/data/staff", {
-      "shifts": [
-        {"port_code": "test", "terminal": "T1", "staff": "1", "shift_start": firstMidnightOfThisMonth().toISOString()},
-        {"port_code": "test", "terminal": "T1", "staff": "2", "shift_start": firstMidnightOfNextMonth().toISOString()}
-      ]
-    });
-  }
-  function resetShifts() {
-    cy.request("POST", "/data/staff", {
-      "shifts": [
-        {"port_code": "test", "terminal": "T1", "staff": "0", "shift_start": firstMidnightOfThisMonth().toISOString()},
-        {"port_code": "test", "terminal": "T1", "staff": "0", "shift_start": firstMidnightOfNextMonth().toISOString()}
-      ]
-    });
-  }
+  Cypress.Commands.add('saveShifts', ()=> {
+    cy
+      .request("POST", "/data/staff", {
+        "shifts": [
+          {"port_code": "test", "terminal": "T1", "staff": "1", "shift_start": firstMidnightOfThisMonth().toISOString()},
+          {"port_code": "test", "terminal": "T1", "staff": "2", "shift_start": firstMidnightOfNextMonth().toISOString()}
+        ]
+      });
+  });
+
+  Cypress.Commands.add('resetShifts', () => {
+    cy
+      .request("POST", "/data/staff", {
+        "shifts": [
+          {"port_code": "test", "terminal": "T1", "staff": "0", "shift_start": firstMidnightOfThisMonth().toISOString()},
+          {"port_code": "test", "terminal": "T1", "staff": "0", "shift_start": firstMidnightOfNextMonth().toISOString()}
+        ]
+      });
+  });
 
   function thisMonthDateString() {
     return moment().toISOString().split("T")[0];
@@ -41,19 +49,16 @@ describe('Monthly Staffing', function () {
 
     let cellToTest = ".htCore tbody :nth-child(1) :nth-child(2)";
     it("If I enter staff for the current month those staff should still be visible if I change months and change back", function () {
-      saveShifts();
-
-      cy.setRoles(["staff:edit", "test"]);
-
-      cy.visit('#terminal/T1/staffing/15/');
-      cy.get(cellToTest).contains("1");
-
-      cy.visit('#terminal/T1/staffing/15/?date=' + nextMonthDateString());
-      cy.get(cellToTest).contains("2");
-      cy.visit('#terminal/T1/staffing/15/?date=' + thisMonthDateString());
-      cy.get(cellToTest).contains("1");
-
-      resetShifts();
+      cy
+        .saveShifts()
+        .setRoles(["staff:edit", "test"])
+        .visit('#terminal/T1/staffing/15/')
+        .get(cellToTest).contains("1")
+        .visit('#terminal/T1/staffing/15/?date=' + nextMonthDateString())
+        .get(cellToTest).contains("2")
+        .visit('#terminal/T1/staffing/15/?date=' + thisMonthDateString())
+        .get(cellToTest).contains("1")
+        .resetShifts();
     });
   });
 });
--- e2e/cypress/integration/restrict-access-by-port.js
+++ e2e/cypress/integration/restrict-access-by-port.js
@@ -1,47 +1,48 @@
 describe('Restrict access by port', function () {
 
-  function navigateToHome() {
-    cy.visit('').then(() => {
-      cy.contains('.navbar-drt', 'DRT TEST').end();
-    }).end();
-  }
-
-  function navigateToHomeAccessRestricted() {
-    cy.visit('/').then(() => {
-      cy.get('#access-restricted').should('exist').end();
-      cy.get('#email-for-access').should('exist').end();
-    }).end();
-  }
+  Cypress.Commands.add('assertAccessRestricted', () => {
+    cy.get('#access-restricted').should('exist')
+      .get('#email-for-access').should('exist');
+  });
 
   describe('Restrict access by port', function () {
 
     it("When I have the correct permission to view the port I see the app", function () {
-      cy.setRoles(["test"]);
-      navigateToHome();
+      cy
+        .setRoles(["test"])
+        .navigateHome().then(() => cy.contains('.navbar-drt', 'DRT TEST'));
     });
 
     it("When I do not have any permission to view any ports I see access restricted page", function () {
-      cy.setRoles(["bogus"]);
-      navigateToHomeAccessRestricted();
-      cy.get('#alternate-ports').should('not.exist');
+      cy
+        .setRoles(["bogus"])
+        .navigateHome().then(() => {
+          cy.assertAccessRestricted()
+            .get('#alternate-ports').should('not.exist');
+        });
     });
 
     it("When I only have permission to view LHR I see access restricted page with a link to LHR only", function () {
-      cy.setRoles(["LHR"]);
-      navigateToHomeAccessRestricted();
-      cy.get('#alternate-ports').should('exist');
-      cy.contains('#LHR-link', 'LHR');
-      cy.get('#LGW-link').should('not.exist');
+      cy
+        .setRoles(["LHR"])
+        .navigateHome().then(() => {
+          cy.assertAccessRestricted()
+            .get('#alternate-ports').should('exist')
+            .contains('#LHR-link', 'LHR')
+            .get('#LGW-link').should('not.exist')
+        });
     });
 
     it("When I have permission to view LHR and LGW I see access restricted page with a link to both ports", function () {
-      cy.setRoles(["LHR", "LGW"]);
-      navigateToHomeAccessRestricted();
-      cy.get('#alternate-ports').should('exist');
-      cy.contains('#LHR-link', 'LHR');
-      cy.contains('#LGW-link', 'LGW');
+      cy
+        .setRoles(["LHR", "LGW"])
+        .navigateHome().then(() => {
+          cy.assertAccessRestricted()
+            .get('#alternate-ports').should('exist').then(() => {
+              cy.contains('#LHR-link', 'LHR')
+              cy.contains('#LGW-link', 'LGW');
+            });
+        });
     });
-
   });
-
 });
