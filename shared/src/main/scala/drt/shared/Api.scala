7c0a27e0d75944b60b2421a3f6dedc54ed96d348
Rich Birch <rich@freshcodelimited.com>
2019-04-08 18:46:33 +0100

    DRT-5554 Remove unused code

--- shared/src/main/scala/drt/shared/Api.scala
+++ shared/src/main/scala/drt/shared/Api.scala
@@ -407,12 +407,6 @@ object CrunchApi {
   case class CrunchState(flights: Set[ApiFlightWithSplits],
                          crunchMinutes: Set[CrunchMinute],
                          staffMinutes: Set[StaffMinute]) {
-    def window(start: SDateLike, end: SDateLike): CrunchState = {
-      val windowedFlights = flights.filter(f => f.apiFlight.hasPcpDuring(start, end))
-      val windowedCrunchMinutes = crunchMinutes.filter(cm => start.millisSinceEpoch <= cm.minute && cm.minute <= end.millisSinceEpoch)
-      val windowsStaffMinutes = staffMinutes.filter(sm => start.millisSinceEpoch <= sm.minute && sm.minute <= end.millisSinceEpoch)
-      CrunchState(windowedFlights, windowedCrunchMinutes, windowsStaffMinutes)
-    }
 
     def window(start: SDateLike, end: SDateLike, terminalName: TerminalName): CrunchState = {
       val windowedFlights = flights.filter(f => f.apiFlight.hasPcpDuring(start, end) && f.apiFlight.Terminal == terminalName)
@@ -420,33 +414,6 @@ object CrunchApi {
       val windowsStaffMinutes = staffMinutes.filter(sm => start.millisSinceEpoch <= sm.minute && sm.minute <= end.millisSinceEpoch && sm.terminalName == terminalName)
       CrunchState(windowedFlights, windowedCrunchMinutes, windowsStaffMinutes)
     }
-
-//    def extractCrunchMinutes(windowRange: Array[MillisSinceEpoch], portQueues: Map[TerminalName, Seq[QueueName]]): Map[TQM, CrunchMinute] = {
-//      val maybeCms = for {
-//        terminal <- portQueues.keys
-//        queue <- portQueues(terminal)
-//        minute <- windowRange
-//      } yield {
-//        val tqm = TQM(terminal, queue, minute)
-//        crunchMinutes.get(tqm).map(cm => (tqm, cm))
-//      }
-//      maybeCms.collect {
-//        case Some(tup) => tup
-//      }.toMap
-//    }
-//
-//    def extractStaffMinutes(windowRange: Array[MillisSinceEpoch], terminals: Seq[TerminalName]): Map[TM, StaffMinute] = {
-//      val maybeSms = for {
-//        terminal <- terminals
-//        minute <- windowRange
-//      } yield {
-//        val tm = TM(terminal, minute)
-//        staffMinutes.get(tm).map(cm => (tm, cm))
-//      }
-//      maybeSms.collect {
-//        case Some(tup) => tup
-//      }.toMap
-//    }
 
     def isEmpty: Boolean = flights.isEmpty && crunchMinutes.isEmpty && staffMinutes.isEmpty
   }
