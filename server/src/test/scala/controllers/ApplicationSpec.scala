c181b6c8e2064280155f6e16bd64ca6fee44314e
Rich Birch <rich@freshcodelimited.com>
2019-04-16 20:58:47 +0100

    DRT-5566 Formatting

--- server/src/test/scala/controllers/ApplicationSpec.scala
+++ server/src/test/scala/controllers/ApplicationSpec.scala
@@ -13,7 +13,12 @@ class ApplicationSpec extends CrunchTestLike {
 
   trait Context extends Scope {
     implicit val mat: Materializer = ActorMaterializer()
-    implicit val config: Configuration = play.api.Configuration.from(Map("portCode" -> "test", "dq.s3.bucket" -> "bucket", "googleTrackingCode"-> "", "virus-scanner-url" -> "", "crunch.forecast.max_days" -> "3"))
+    implicit val config: Configuration = play.api.Configuration.from(Map(
+      "portCode" -> "test",
+      "dq.s3.bucket" -> "bucket",
+      "googleTrackingCode"-> "",
+      "virus-scanner-url" -> "",
+      "crunch.forecast.max_days" -> "3"))
 
     val application = new Application()(config = config, mat = mat, env = Environment.simple(), system = system, ec = global)
   }
