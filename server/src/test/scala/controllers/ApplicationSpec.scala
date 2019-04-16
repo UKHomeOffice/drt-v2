d414d8b2e54e92c7b388ad5238bdb82ba0d44484
Rich Birch <rich@freshcodelimited.com>
2019-04-16 17:43:15 +0100

    DRT-5566 Change Int config value to String

--- server/src/test/scala/controllers/ApplicationSpec.scala
+++ server/src/test/scala/controllers/ApplicationSpec.scala
@@ -3,7 +3,7 @@ package controllers
 import akka.stream.{ActorMaterializer, Materializer}
 import org.joda.time.DateTime
 import org.specs2.matcher.Scope
-import play.api.Environment
+import play.api.{Configuration, Environment}
 import services.SDate
 import services.crunch.CrunchTestLike
 
@@ -13,7 +13,7 @@ class ApplicationSpec extends CrunchTestLike {
 
   trait Context extends Scope {
     implicit val mat: Materializer = ActorMaterializer()
-    implicit val config = play.api.Configuration.from(Map("portCode" -> "test", "dq.s3.bucket" -> "bucket", "googleTrackingCode"-> "", "virus-scanner-url" -> "", "crunch.forecast.max_days" -> 3))
+    implicit val config: Configuration = play.api.Configuration.from(Map("portCode" -> "test", "dq.s3.bucket" -> "bucket", "googleTrackingCode"-> "", "virus-scanner-url" -> "", "crunch.forecast.max_days" -> "3"))
 
     val application = new Application()(config = config, mat = mat, env = Environment.simple(), system = system, ec = global)
   }
