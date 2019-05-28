0a360a5d762710232d20723257e534cc450db4f9
Ben Eppel <ben@beneppelsoftware.com>
2019-05-28 13:00:54 +0100

    DRT-5504 Improve semantics of role test cases

--- e2e/cypress/integration/api-role-access.js
+++ e2e/cypress/integration/api-role-access.js
@@ -12,242 +12,242 @@ describe('Restrict access to endpoint by role', function () {
       roles: ["test"],
       endpoint: "/airport-config",
       method: "GET",
-      responseCode: 200
+      shouldBeGranted: true
     },
     {
       roles: ["test", "arrivals-and-splits:view"],
       endpoint: "/airport-info",
       method: "GET",
-      responseCode: 200
+      shouldBeGranted: true
     },
     {
       roles: ["test"],
       endpoint: "/airport-info",
       method: "GET",
-      responseCode: 401
+      shouldBeGranted: false
     },
     {
       roles: ["test"],
       endpoint: "/alerts/" + millis,
       method: "GET",
-      responseCode: 200
+      shouldBeGranted: true
     },
     {
       roles: ["test"],
       endpoint: "/version",
       method: "GET",
-      responseCode: 200
+      shouldBeGranted: true
     },
     {
       roles: ["test"],
       endpoint: "/feed-statuses",
       method: "GET",
-      responseCode: 200
+      shouldBeGranted: true
     },
     {
       roles: ["test"],
       endpoint: "/logged-in",
       method: "GET",
-      responseCode: 200
+      shouldBeGranted: true
     },
     {
       roles: [],
       endpoint: "/data/login",
       method: "POST",
-      responseCode: 400
+      shouldBeGranted: true
     },
     {
       roles: ["test"],
       endpoint: "/data/user",
       method: "GET",
-      responseCode: 200
+      shouldBeGranted: true
     },
     {
       roles: ["test"],
       endpoint: "/logging",
       method: "POST",
-      responseCode: 200
+      shouldBeGranted: true
     },
     {
       roles: ["test"],
       endpoint: "/data/staff",
       method: "POST",
-      responseCode: 401
+      shouldBeGranted: false
     },
     {
       roles: ["test", "port-operator-staff"],
       endpoint: "/data/feed/live/lhr",
       method: "POST",
-      responseCode: 400
+      shouldBeGranted: true
     },
     {
       roles: ["test"],
       endpoint: "/data/feed/live/lhr",
       method: "POST",
-      responseCode: 401
+      shouldBeGranted: false
     },
     {
       roles: [],
       endpoint: "/logging",
       method: "POST",
-      responseCode: 401
+      shouldBeGranted: false
     },
     {
       roles: ["test"],
       endpoint: "/data/user/has-port-access",
       method: "GET",
-      responseCode: 200
+      shouldBeGranted: true
     },
     {
       roles: [],
       endpoint: "/data/user/has-port-access",
       method: "GET",
-      responseCode: 401
+      shouldBeGranted: false
     },
     {
       roles: ["test"],
       endpoint: "/alerts",
       method: "POST",
-      responseCode: 401
+      shouldBeGranted: false
     },
     {
       roles: ["test"],
       endpoint: "/alerts",
       method: "DELETE",
-      responseCode: 401
+      shouldBeGranted: false
     },
     {
       roles: ["test", "fixed-points:view"],
       endpoint: "/fixed-points",
       method: "GET",
-      responseCode: 200
+      shouldBeGranted: true
     },
     {
       roles: ["test"],
       endpoint: "/fixed-points",
       method: "GET",
-      responseCode: 401
+      shouldBeGranted: false
     },
     {
       roles: ["test", "fixed-points:edit"],
       endpoint: "/fixed-points",
       method: "POST",
-      responseCode: 400
+      shouldBeGranted: true
     },
     {
       roles: ["test", "desks-and-queues:view"],
       endpoint: "/crunch-state",
       method: "GET",
-      responseCode: 200
+      shouldBeGranted: true
     },
     {
       roles: ["test"],
       endpoint: "/crunch-state",
       method: "GET",
-      responseCode: 401
+      shouldBeGranted: false
     },
     {
       roles: ["test", "desks-and-queues:view"],
       endpoint: "/export/desks/"+millis+"/"+millis+"/T1",
       method: "GET",
-      responseCode: 200
+      shouldBeGranted: true
     },
     {
       roles: ["test"],
       endpoint: "/export/desks/"+millis+"/"+millis+"/T1",
       method: "GET",
-      responseCode: 401
+      shouldBeGranted: false
     },
     {
       roles: ["test", "api:view-port-arrivals"],
       endpoint: "/export/arrivals/" + millis + "/T1",
       method: "GET",
-      responseCode: 200
+      shouldBeGranted: true
     },
     {
       roles: ["test", "api:view-port-arrivals"],
       endpoint: "/export/api/T1/2019/5/1",
       method: "GET",
-      responseCode: 404
+      shouldBeGranted: true
     },
     {
       roles: ["test"],
       endpoint: "/export/api/T1/2019/5/1",
       method: "GET",
-      responseCode: 401
+      shouldBeGranted: false
     },
     {
       roles: ["test"],
       endpoint: "/export/arrivals/" + millis + "/T1",
       method: "GET",
-      responseCode: 401
+      shouldBeGranted: false
     },
     {
       roles: ["test", "arrivals-and-splits:view"],
       endpoint: "/export/arrivals/" + millis + "/" + millis + "/T1",
       method: "GET",
-      responseCode: 200
+      shouldBeGranted: true
     },
     {
       roles: ["test"],
       endpoint: "/export/arrivals/" + millis + "/" + millis + "/T1",
       method: "GET",
-      responseCode: 401
+      shouldBeGranted: false
     },
     {
       roles: ["test"],
       endpoint: "/export/arrivals/" + millis + "/T1",
       method: "GET",
-      responseCode: 401
+      shouldBeGranted: false
     },
     {
       roles: ["test", "forecast:view"],
       endpoint: "/export/planning/" + millis + "/T1",
       method: "GET",
-      responseCode: 200
+      shouldBeGranted: true
     },
     {
       roles: ["test"],
       endpoint: "/export/planning/" + millis + "/T1",
       method: "GET",
-      responseCode: 401
+      shouldBeGranted: false
     },
     {
       roles: ["test", "forecast:view"],
       endpoint: "/export/headlines/" + millis + "/T1",
       method: "GET",
-      responseCode: 200
+      shouldBeGranted: true
     },
     {
       roles: ["test"],
       endpoint: "/export/headlines/" + millis + "/T1",
       method: "GET",
-      responseCode: 401
+      shouldBeGranted: false
     },
     {
       roles: ["test"],
       endpoint: "/export/users",
       method: "GET",
-      responseCode: 401
+      shouldBeGranted: false
     },
     {
       roles: ["test"],
       endpoint: "/api/drt/shared/Api/getShowAlertModalDialog",
       method: "POST",
-      responseCode: 401
+      shouldBeGranted: false
     },
     {
       roles: [],
       endpoint: "/",
       method: "GET",
-      responseCode: 200
+      shouldBeGranted: true
     }
   ]
 
   describe('Restrict access by role', function () {
 
     testCases.map(testCase => {
-      it("Expects " + testCase.responseCode + " for a "+ testCase.method + " request to " + testCase.endpoint + " with roles: [" + testCase.roles.join(", ") + "]", function () {
+      it("Expects " + (testCase.shouldBeGranted ? "access ALLOWED" : "access DENIED") + " for a "+ testCase.method + " request to " + testCase.endpoint + " with roles: [" + testCase.roles.join(", ") + "]", function () {
 
         cy.setRoles(testCase.roles)
           .request({
@@ -255,10 +255,12 @@ describe('Restrict access to endpoint by role', function () {
             url: testCase.endpoint,
             failOnStatusCode: false
           })
-          .then(resp => expect(resp.status)
+          .then(resp => {
+            const accessGranted = resp.status != 401
+            expect(accessGranted)
             .to
-            .eq(testCase.responseCode, "wrong status code")
-          );
+            .eq(testCase.shouldBeGranted, "wrong status code")
+          });
       });
     });
   });
