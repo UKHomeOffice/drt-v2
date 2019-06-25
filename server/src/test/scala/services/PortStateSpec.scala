56e272c12bdcaa4b543822ea3f13876c1e08bcb8
Rich Birch <rich@freshcodelimited.com>
2019-06-25 17:12:42 +0100

    DRT-5658 Add further test coverage for purge function

--- server/src/test/scala/services/PortStateSpec.scala
+++ server/src/test/scala/services/PortStateSpec.scala
@@ -6,8 +6,60 @@ import drt.shared._
 import org.specs2.mutable.Specification
 
 class PortStateSpec extends Specification {
-  "Given a PortState " +
-    "When I purge entries older than a certain date " +
+  "Given a PortState with no purgable entries " +
+    "When I purge " +
+    "Then I should still see all the entries" >> {
+    val newerTime1 = SDate("2019-01-01T00:30Z")
+    val newerTime2 = SDate("2019-01-02T00:30Z")
+    val oldArrival = ArrivalGenerator.apiFlight(iata = "BA0001", schDt = newerTime1.toISOString())
+    val newerArrival = ArrivalGenerator.apiFlight(iata = "BA0002", schDt = newerTime2.toISOString())
+    val flights = arrivalsToFlightsWithSplits(List(oldArrival, newerArrival))
+
+    val newerCrunchMinute1 = CrunchMinute("T1", Queues.EeaDesk, newerTime1.millisSinceEpoch, 0, 0, 0, 0)
+    val newerCrunchMinute2 = CrunchMinute("T1", Queues.EeaDesk, newerTime2.millisSinceEpoch, 0, 0, 0, 0)
+    val crunchMinutes = List(newerCrunchMinute1, newerCrunchMinute2)
+
+    val newerStaffMinute1 = StaffMinute("T1", newerTime1.millisSinceEpoch, 0, 0, 0)
+    val newerStaffMinute2 = StaffMinute("T1", newerTime2.millisSinceEpoch, 0, 0, 0)
+    val staffMinutes = List(newerStaffMinute1, newerStaffMinute2)
+
+    val portState = PortState(flights, crunchMinutes, staffMinutes)
+
+    val result = portState.purgeOlderThanDate(newerTime1)
+
+    val expected = PortState(flights, crunchMinutes, staffMinutes)
+
+    result === expected
+  }
+
+  "Given a PortState with only older entries " +
+    "When I purge older entries " +
+    "Then I should not see any entries survive" >> {
+    val oldTime1 = SDate("2019-01-01T00:30Z")
+    val oldTime2 = SDate("2019-01-02T00:30Z")
+    val oldArrival1 = ArrivalGenerator.apiFlight(iata = "BA0001", schDt = oldTime1.toISOString())
+    val oldArrival2 = ArrivalGenerator.apiFlight(iata = "BA0002", schDt = oldTime2.toISOString())
+    val flights = arrivalsToFlightsWithSplits(List(oldArrival1, oldArrival2))
+
+    val oldCrunchMinute1 = CrunchMinute("T1", Queues.EeaDesk, oldTime1.millisSinceEpoch, 0, 0, 0, 0)
+    val oldCrunchMinute2 = CrunchMinute("T1", Queues.EeaDesk, oldTime2.millisSinceEpoch, 0, 0, 0, 0)
+    val crunchMinutes = List(oldCrunchMinute1, oldCrunchMinute2)
+
+    val oldStaffMinute1 = StaffMinute("T1", oldTime1.millisSinceEpoch, 0, 0, 0)
+    val oldStaffMinute2 = StaffMinute("T1", oldTime2.millisSinceEpoch, 0, 0, 0)
+    val staffMinutes = List(oldStaffMinute1, oldStaffMinute2)
+
+    val portState = PortState(flights, crunchMinutes, staffMinutes)
+
+    val result = portState.purgeOlderThanDate(oldTime2.addMinutes(1))
+
+    val expected = PortState(arrivalsToFlightsWithSplits(List()), List(), List())
+
+    result === expected
+  }
+
+  "Given a PortState with older and newer entries " +
+    "When I purge older entries " +
     "Then I should only see the newer entries survive" >> {
     val oldTime = SDate("2019-01-01T00:30Z")
     val newerTime = SDate("2019-01-02T00:30Z")
