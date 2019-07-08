89f4854bc168fef9317cc5c1e0419ddd65fcdc53
Rich Birch <rich@freshcodelimited.com>
2019-07-08 11:36:57 +0100

    DRT-5401 Remove ACP redirect filter

--- server/src/main/scala/Filters.scala
+++ server/src/main/scala/Filters.scala
@@ -1,11 +1,11 @@
 import javax.inject.{Inject, Singleton}
-import filters.{ACPRedirectFilter, NoCacheFilter, SecurityHeadersFilter}
+import filters.{NoCacheFilter, SecurityHeadersFilter}
 import play.api.Environment
 import play.api.http.HttpFilters
 
 @Singleton
 class Filters @Inject()(
                          env: Environment,
-                         acpRedirectFilter : ACPRedirectFilter, noCache: NoCacheFilter, securityHeaders: SecurityHeadersFilter) extends HttpFilters {
-  override val filters = Seq(acpRedirectFilter, noCache, securityHeaders)
+                         noCache: NoCacheFilter, securityHeaders: SecurityHeadersFilter) extends HttpFilters {
+  override val filters = Seq(noCache, securityHeaders)
 }
