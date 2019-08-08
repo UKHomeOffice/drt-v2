c614d5ea3c630b34a596b9c91b8aeb41614184bb
Ben Eppel <ben@beneppelsoftware.com>
2019-08-08 17:07:24 +0100

    DRT-5708 update security policy and remove duplicate headers

--- server/src/main/scala/Filters.scala
+++ server/src/main/scala/Filters.scala
@@ -2,11 +2,16 @@ import filters.NoCacheFilter
 import javax.inject.{Inject, Singleton}
 import play.api.Environment
 import play.api.http.HttpFilters
-import play.filters.headers.SecurityHeadersFilter
+import play.filters.headers.{SecurityHeadersConfig, SecurityHeadersFilter}
 
 @Singleton
 class Filters @Inject()(
                          env: Environment,
                          noCache: NoCacheFilter) extends HttpFilters {
-  override val filters = Seq(noCache, SecurityHeadersFilter())
+  override val filters = Seq(noCache, SecurityHeadersFilter(SecurityHeadersConfig(
+    frameOptions = None,
+    xssProtection = None,
+    contentSecurityPolicy = Option("default-src 'self'; script-src 'self' www.google-analytics.com ajax.googleapis.com; style-src 'self' cdnjs.cloudflare.com; font-src 'self' cdnjs.cloudflare.com"),
+    contentTypeOptions = None
+  )))
 }
