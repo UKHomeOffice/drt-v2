3e8ce6d55d3790d63cac03b0b0d44fbcfd5ecea7
Ben Eppel <ben@beneppelsoftware.com>
2019-08-09 12:16:25 +0100

    DRT-5708 whitelist google analytics for security policy

--- server/src/main/scala/Filters.scala
+++ server/src/main/scala/Filters.scala
@@ -8,10 +8,21 @@ import play.filters.headers.{SecurityHeadersConfig, SecurityHeadersFilter}
 class Filters @Inject()(
                          env: Environment,
                          noCache: NoCacheFilter) extends HttpFilters {
-  override val filters = Seq(noCache, SecurityHeadersFilter(SecurityHeadersConfig(
+
+  override val filters = Seq(noCache, SecurityHeaders.filter)
+}
+
+object SecurityHeaders {
+  val default = "default-src 'self'"
+  val javaScript = "script-src 'self' www.google-analytics.com ajax.googleapis.com 'sha256-g7xvSeFvOZB+aVUYeI3wlm6J3HMikqnm32sVJHUX9f8='"
+  val styles = "style-src 'self' cdnjs.cloudflare.com 'unsafe-inline'"
+  val fonts = "font-src 'self' cdnjs.cloudflare.com"
+  val images = "img-src 'self' https://www.google-analytics.com www.google-analytics.com"
+
+  val filter = SecurityHeadersFilter(SecurityHeadersConfig(
     frameOptions = None,
     xssProtection = None,
-    contentSecurityPolicy = Option("default-src 'self'; script-src 'self' www.google-analytics.com ajax.googleapis.com; style-src 'self' cdnjs.cloudflare.com 'unsafe-inline'; font-src 'self' cdnjs.cloudflare.com"),
+    contentSecurityPolicy = Option(default + "; " + javaScript + "; " + styles + "; " + fonts + "; " + images),
     contentTypeOptions = None
-  )))
+  ))
 }
