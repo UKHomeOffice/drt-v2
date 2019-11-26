00ab11feabc11d9f7d312cb8762b330aef360b51
Rich Birch <rich@freshcodelimited.com>
2019-11-26 13:06:16 +0000

    DRT-6009 Add tests around ordered TQMs

--- shared/src/main/scala/drt/shared/Api.scala
+++ shared/src/main/scala/drt/shared/Api.scala
@@ -158,58 +158,6 @@ trait WithTimeAccessor {
   def timeValue: MillisSinceEpoch
 }
 
-case class TQM(terminal: Terminal, queue: Queue, minute: MillisSinceEpoch)
-  extends Ordered[TQM]
-    with WithTimeAccessor
-    with WithTerminal[TQM] {
-  override def equals(o: scala.Any): Boolean = o match {
-    case TQM(t, q, m) => t == terminal && q == queue && m == minute
-    case _ => false
-  }
-
-  lazy val comparisonVal: MillisSinceEpoch = minute + 1024L * queue.orderingVal + terminal.orderingVal
-
-  override def compare(that: TQM): Int = this.comparisonVal.compareTo(that.comparisonVal)
-
-  override def timeValue: MillisSinceEpoch = minute
-}
-
-object TQM {
-  implicit val rw: ReadWriter[TQM] = macroRW
-
-  def apply(crunchMinute: CrunchMinute): TQM = TQM(crunchMinute.terminal, crunchMinute.queue, crunchMinute.minute)
-
-  def apply(terminalName: String, queueName: String, minute: MillisSinceEpoch): TQM = TQM(Terminal(terminalName), Queue(queueName), minute)
-
-  def atTime: MillisSinceEpoch => TQM = (time: MillisSinceEpoch) => TQM("", "", time)
-}
-
-case class TM(terminal: Terminal, minute: MillisSinceEpoch)
-  extends Ordered[TM]
-    with WithTimeAccessor
-    with WithTerminal[TM] {
-  override def equals(o: scala.Any): Boolean = o match {
-    case TM(t, m) => t == terminal && m == minute
-    case _ => false
-  }
-
-  lazy val comparisonVal: MillisSinceEpoch = minute + terminal.orderingVal
-
-  override def compare(that: TM): Int = this.comparisonVal.compareTo(that.comparisonVal)
-
-  override def timeValue: MillisSinceEpoch = minute
-}
-
-object TM {
-  implicit val rw: ReadWriter[TM] = macroRW
-
-  def apply(staffMinute: StaffMinute): TM = TM(staffMinute.terminal, staffMinute.minute)
-
-  def apply(terminalName: String, minute: MillisSinceEpoch): TM = TM(Terminal(terminalName), minute)
-
-  def atTime: MillisSinceEpoch => TM = (time: MillisSinceEpoch) => TM("", time)
-}
-
 trait WithLegacyUniqueId[LI, I] extends Ordered[I] {
   def uniqueId: LI
 }
---
+++ shared/src/main/scala/drt/shared/TQM.scala
@@ -0,0 +1,32 @@
+package drt.shared
+
+import drt.shared.CrunchApi.{CrunchMinute, MillisSinceEpoch}
+import drt.shared.Queues.Queue
+import drt.shared.Terminals.Terminal
+import upickle.default.{macroRW, ReadWriter}
+
+case class TQM(terminal: Terminal, queue: Queue, minute: MillisSinceEpoch)
+  extends Ordered[TQM]
+    with WithTimeAccessor
+    with WithTerminal[TQM] {
+  override def equals(o: scala.Any): Boolean = o match {
+    case TQM(t, q, m) => t == terminal && q == queue && m == minute
+    case _ => false
+  }
+
+  lazy val comparisonVal: MillisSinceEpoch = minute + 1024L * queue.orderingVal + terminal.orderingVal
+
+  override def compare(that: TQM): Int = this.comparisonVal.compareTo(that.comparisonVal)
+
+  override def timeValue: MillisSinceEpoch = minute
+}
+
+object TQM {
+  implicit val rw: ReadWriter[TQM] = macroRW
+
+  def apply(crunchMinute: CrunchMinute): TQM = TQM(crunchMinute.terminal, crunchMinute.queue, crunchMinute.minute)
+
+  def apply(terminalName: String, queueName: String, minute: MillisSinceEpoch): TQM = TQM(Terminal(terminalName), Queue(queueName), minute)
+
+  def atTime: MillisSinceEpoch => TQM = (time: MillisSinceEpoch) => TQM("", "", time)
+}
