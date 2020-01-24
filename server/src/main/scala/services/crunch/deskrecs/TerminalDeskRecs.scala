4c65e2d1660489f0cd58b4c3bc7b33d887e1153b
Rich Birch <rich@freshcodelimited.com>
2020-01-24 10:35:41 +0000

    DRTII-250 Parameter tidying

--- server/src/main/scala/services/crunch/deskrecs/TerminalDeskRecs.scala
+++ server/src/main/scala/services/crunch/deskrecs/TerminalDeskRecs.scala
@@ -21,13 +21,12 @@ trait TerminalDeskRecsProviderLike {
 
   def desksAndWaits(loads: Map[Queue, Seq[Double]],
                     minDesks: Map[Queue, List[Int]],
-                    maxDesks: Map[Queue, List[Int]],
-                    slas: Map[Queue, Int]): Map[Queue, (List[Int], List[Int])] = loads
+                    maxDesks: Map[Queue, List[Int]]): Map[Queue, (List[Int], List[Int])] = loads
     .map { case (queueProcessing, loadsForQueue) =>
       val min = minDesks(queueProcessing)
       val max = maxDesks(queueProcessing)
       val sla = slas(queueProcessing)
-      cruncher(adjustedWork(queueProcessing, loadsForQueue, bankSize), min, max, OptimizerConfig(sla)) match {
+      cruncher(adjustedWork(queueProcessing, loadsForQueue), min, max, OptimizerConfig(sla)) match {
         case Success(OptimizerCrunchResult(desks, waits)) => Option(queueProcessing -> ((desks.toList, waits.toList)))
         case Failure(_) => None
       }
@@ -35,12 +34,12 @@ trait TerminalDeskRecsProviderLike {
     .collect { case Some(result) => result }
     .toMap
 
-  def adjustedWork(queue: Queue, work: Seq[Double], bankSize: Int): Seq[Double] = queue match {
+  def adjustedWork(queue: Queue, work: Seq[Double]): Seq[Double] = queue match {
     case EGate => work.map(_ / bankSize)
     case _ => work
   }
 
-  def minMaxDesksForQueue(deskRecMinutes: Iterable[MillisSinceEpoch], tn: Terminal, qn: Queue, minMaxDesks: Map[Terminal, Map[Queue, (List[Int], List[Int])]]): (List[Int], List[Int]) = {
+  def minMaxDesksForQueue(deskRecMinutes: Iterable[MillisSinceEpoch], tn: Terminal, qn: Queue): (List[Int], List[Int]) = {
     val defaultMinMaxDesks = (List.fill(24)(0), List.fill(24)(10))
     val queueMinMaxDesks = minMaxDesks.getOrElse(tn, Map()).getOrElse(qn, defaultMinMaxDesks)
     val minDesks = deskRecMinutes.map(desksForHourOfDayInUKLocalTime(_, queueMinMaxDesks._1))
@@ -54,12 +53,12 @@ trait TerminalDeskRecsProviderLike {
                              terminalWork: Map[Queue, Seq[Double]],
                              terminalRecs: TerminalDeskRecsProviderLike): Iterable[DeskRecMinute] = {
     val terminalMinMaxDesks = queuesByTerminal(terminal).map { queue =>
-      (queue, minMaxDesksForQueue(minuteMillis, terminal, queue, minMaxDesks))
+      (queue, minMaxDesksForQueue(minuteMillis, terminal, queue))
     }.toMap
     val minDesks = terminalMinMaxDesks.mapValues(_._1)
     val maxDesks = terminalMinMaxDesks.mapValues(_._2)
 
-    val queueDesksAndWaits = terminalRecs.desksAndWaits(terminalWork, minDesks, maxDesks, slas)
+    val queueDesksAndWaits = terminalRecs.desksAndWaits(terminalWork, minDesks, maxDesks)
 
     queueDesksAndWaits.flatMap {
       case (queue, (desks, waits)) =>
@@ -75,33 +74,30 @@ case class StaticTerminalDeskRecsProvider(queuesByTerminal: SortedMap[Terminal, 
 case class FlexedTerminalDeskRecsProvider(queuesByTerminal: SortedMap[Terminal, Seq[Queue]], minMaxDesks: Map[Terminal, Map[Queue, (List[Int], List[Int])]], slas: Map[Queue, Int], terminalDesks: Int, flexedQueuesPriority: List[Queue], cruncher: TryCrunch, bankSize: Int) extends TerminalDeskRecsProviderLike {
   override def desksAndWaits(loads: Map[Queue, Seq[Double]],
                              minDesks: Map[Queue, List[Int]],
-                             maxDesks: Map[Queue, List[Int]],
-                             slas: Map[Queue, Int]): Map[Queue, (List[Int], List[Int])] = {
+                             maxDesks: Map[Queue, List[Int]]): Map[Queue, (List[Int], List[Int])] = {
     val queuesToOptimise: Set[Queue] = loads.keys.toSet
     val flexedQueuesToOptimise = queuesToOptimise.filter(q => flexedQueuesPriority.contains(q))
     val staticQueuesToOptimise = queuesToOptimise.filter(q => !flexedQueuesPriority.contains(q))
 
-    val flexedRecs = flexedDesksAndWaits(flexedQueuesToOptimise, loads, minDesks, slas)
+    val flexedRecs = flexedDesksAndWaits(flexedQueuesToOptimise, loads, minDesks)
 
-    val staticRecs = super.desksAndWaits(loads.filterKeys(staticQueuesToOptimise), minDesks, maxDesks, slas)
+    val staticRecs = super.desksAndWaits(loads.filterKeys(staticQueuesToOptimise), minDesks, maxDesks)
 
     flexedRecs ++ staticRecs
   }
 
   def flexedDesksAndWaits(flexedQueuesToOptimise: Set[Queue],
                           loads: Map[Queue, Seq[Double]],
-                          minDesks: Map[Queue, List[Int]],
-                          slas: Map[Queue, Int]): Map[Queue, (List[Int], List[Int])] = flexedQueuesPriority
+                          minDesks: Map[Queue, List[Int]]): Map[Queue, (List[Int], List[Int])] = flexedQueuesPriority
     .filter(flexedQueued => flexedQueuesToOptimise.toList.contains(flexedQueued))
     .foldLeft(Map[Queue, (List[Int], List[Int])]()) {
       case (queueRecsSoFar, queueProcessing) =>
-        flexedQueueDesksAndWaits(terminalDesks, loads, minDesks, slas, flexedQueuesToOptimise, queueRecsSoFar, queueProcessing)
+        flexedQueueDesksAndWaits(terminalDesks, loads, minDesks, flexedQueuesToOptimise, queueRecsSoFar, queueProcessing)
     }
 
   def flexedQueueDesksAndWaits(terminalDesks: Int,
                                loads: Map[Queue, Seq[Double]],
                                minDesks: Map[Queue, List[Int]],
-                               slas: Map[Queue, Int],
                                flexedQueuesToOptimise: Set[Queue],
                                queueRecsSoFar: Map[Queue, (List[Int], List[Int])],
                                queueProcessing: Queue): Map[Queue, (List[Int], List[Int])] = {
@@ -114,7 +110,7 @@ case class FlexedTerminalDeskRecsProvider(queuesByTerminal: SortedMap[Terminal, 
       .foldLeft(availableMinusRemainingMinimums) {
         case (availableSoFar, (recs, _)) => availableSoFar.zip(recs).map { case (a, b) => a - b }
       }
-    cruncher(adjustedWork(queueProcessing, loads(queueProcessing), bankSize), minDesks(queueProcessing), actualAvailable, OptimizerConfig(slas(queueProcessing))) match {
+    cruncher(adjustedWork(queueProcessing, loads(queueProcessing)), minDesks(queueProcessing), actualAvailable, OptimizerConfig(slas(queueProcessing))) match {
       case Success(OptimizerCrunchResult(desks, waits)) => queueRecsSoFar + (queueProcessing -> ((desks.toList, waits.toList)))
       case Failure(t) =>
         log.error(s"Crunch failed for $queueProcessing", t)
