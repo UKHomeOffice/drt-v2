cfc703f9399dfadc8e2bf510012e22e4213cb908
Rich Birch <rich@freshcodelimited.com>
2020-04-07 11:39:49 +0100

    DRTII-371 Take pax adjustment average over x of the last known flights

--- server/src/main/scala/actors/daily/PassengersActor.scala
+++ server/src/main/scala/actors/daily/PassengersActor.scala
@@ -1,19 +1,13 @@
 package actors.daily
 
-import akka.actor.{ActorRef, PoisonPill}
-import akka.pattern.AskableActorRef
+import akka.actor.ActorRef
 import akka.persistence._
-import akka.stream.Materializer
-import akka.stream.scaladsl.{Sink, Source}
-import akka.util.Timeout
+import drt.shared.PortCode
 import drt.shared.Terminals.Terminal
-import drt.shared.{Arrival, PortCode, SDateLike}
 import org.slf4j.{Logger, LoggerFactory}
 import server.protobuf.messages.PaxMessage.{OriginTerminalPaxCountsMessage, PaxCountMessage}
-import services.SDate
+import services.{PaxDeltas, SDate}
 
-import scala.concurrent.duration._
-import scala.concurrent.{ExecutionContext, Future}
 import scala.language.postfixOps
 
 case class PointInTimeOriginTerminalDay(pointInTime: Long, origin: String, terminal: String, day: Long)
@@ -26,7 +20,7 @@ case class GetAverageAdjustment(originAndTerminal: OriginAndTerminal, numberOfDa
 
 case object Ack
 
-class PassengersActor(numDaysInAverage: Int) extends PersistentActor {
+class PassengersActor(maxDaysToConsider: Int, numDaysInAverage: Int) extends PersistentActor {
   override val persistenceId = s"daily-pax"
 
   val log: Logger = LoggerFactory.getLogger(persistenceId)
@@ -58,7 +52,7 @@ class PassengersActor(numDaysInAverage: Int) extends PersistentActor {
   private def setPortAverage(state: Map[OriginAndTerminal, Map[(Long, Long), Int]]): Unit = {
     val portAverageDeltas = state.values
       .map { originTerminalCounts =>
-        val maybeDeltas = PaxDeltas.maybePctDeltas(originTerminalCounts, numDaysInAverage, () => SDate.now())
+        val maybeDeltas = PaxDeltas.maybePctDeltas(originTerminalCounts, maxDaysToConsider, numDaysInAverage, () => SDate.now())
         PaxDeltas.maybeAveragePctDelta(maybeDeltas)
       }
       .collect { case Some(delta) => delta }
@@ -73,7 +67,8 @@ class PassengersActor(numDaysInAverage: Int) extends PersistentActor {
   }
 
   private def sendAverageDelta(gad: GetAverageAdjustment, replyTo: ActorRef): Unit = {
-    val maybeDeltas = PaxDeltas.maybePctDeltas(originTerminalPaxNosState.getOrElse(gad.originAndTerminal, Map()), gad.numberOfDays, () => SDate.now())
+    val originTerminalPaxNos = originTerminalPaxNosState.getOrElse(gad.originAndTerminal, Map())
+    val maybeDeltas = PaxDeltas.maybePctDeltas(originTerminalPaxNos, maxDaysToConsider, gad.numberOfDays, () => SDate.now())
     val maybeAverageDelta = PaxDeltas.maybeAveragePctDelta(maybeDeltas) match {
       case Some(average) => Option(average)
       case None => Option(portAverageDelta)
@@ -88,77 +83,3 @@ class PassengersActor(numDaysInAverage: Int) extends PersistentActor {
     .toMap
 }
 
-object PaxDeltas {
-  val log: Logger = LoggerFactory.getLogger(getClass)
-
-  def maybeAveragePctDelta(maybeDeltas: Seq[Option[Double]]): Option[Double] = {
-    val total = maybeDeltas.collect { case Some(diff) => diff }.sum
-    maybeDeltas.count(_.isDefined) match {
-      case 0 => None
-      case daysWithNumbers =>
-        val average = total / daysWithNumbers
-        Option(average)
-    }
-  }
-
-  def maybePctDeltas(dailyPaxNosByDay: Map[(Long, Long), Int],
-                     numberOfDays: Int,
-                     now: () => SDateLike): Seq[Option[Double]] = {
-    val startDay = now().addDays(-1).getLocalLastMidnight
-
-    (0 until numberOfDays).flatMap { dayOffset =>
-      val day = startDay.addDays(-1 * dayOffset)
-      val dayBefore = day.addDays(-1)
-      val maybeActualPax = dailyPaxNosByDay.get((day.millisSinceEpoch, day.millisSinceEpoch))
-      val maybeForecastPax = dailyPaxNosByDay.get((dayBefore.millisSinceEpoch, day.millisSinceEpoch))
-      for {
-        actual <- maybeActualPax
-        forecast <- maybeForecastPax
-      } yield {
-        if (forecast != 0) Option(1d - ((forecast - actual).toDouble / forecast))
-        else None
-      }
-    }
-  }
-
-  def applyAdjustmentsToArrivals(passengersActorProvider: () => AskableActorRef, numDaysInAverage: Int)
-                                (arrivals: List[Arrival])
-                                (implicit mat: Materializer, ec: ExecutionContext): Future[List[Arrival]] = {
-    val paxActor = passengersActorProvider()
-    val updatedArrivalsSource = Source(arrivals)
-      .mapAsync(1) { arrival =>
-        val request = GetAverageAdjustment(OriginAndTerminal(arrival.Origin, arrival.Terminal), numDaysInAverage)
-        lookupAndApplyAdjustment(paxActor, request, arrival)
-      }
-
-    val eventualUpdatedArrivals = updatedArrivalsSource
-      .runWith(Sink.seq)
-      .map(_.toList)
-
-    eventualUpdatedArrivals.onComplete(_ => paxActor.ask(PoisonPill)(new Timeout(1 second)))
-
-    eventualUpdatedArrivals
-  }
-
-  private def lookupAndApplyAdjustment(paxActor: AskableActorRef,
-                                       request: GetAverageAdjustment,
-                                       arrival: Arrival)(implicit ec: ExecutionContext): Future[Arrival] =
-    paxActor.ask(request)(new Timeout(15 second))
-      .asInstanceOf[Future[Option[Double]]]
-      .map {
-        case Some(adjustmentFactor) => applyAdjustment(arrival, adjustmentFactor)
-        case None => arrival
-      }
-      .recover { case t =>
-        log.error("Didn't get a passenger delta", t)
-        arrival
-      }
-
-  private def applyAdjustment(arrival: Arrival, delta: Double) = {
-    val updatedPax = arrival.ActPax.map(pax => (pax * delta).round.toInt) match {
-      case Some(positiveWithDelta) if positiveWithDelta > 0 => Option(positiveWithDelta)
-      case _ => Option(1)
-    }
-    arrival.copy(ActPax = updatedPax)
-  }
-}
