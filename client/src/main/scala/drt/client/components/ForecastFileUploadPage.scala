969c371fc901540310638b52e78b0982ea66eaa5
Nilesh Gupta <nilesh.nemichandgupta@digital.homeoffice.gov.uk>
2020-04-22 15:12:45 +0100

    DRTII-373 : Remove initialisation at RootModel class definition

--- client/src/main/scala/drt/client/components/ForecastFileUploadPage.scala
+++ client/src/main/scala/drt/client/components/ForecastFileUploadPage.scala
@@ -8,6 +8,7 @@ import japgolly.scalajs.react.component.Scala.Component
 import japgolly.scalajs.react.vdom.html_<^._
 import japgolly.scalajs.react.{Callback, CtorType, ScalaComponent, _}
 import org.scalajs.dom
+import org.scalajs.dom.html.Div
 import org.scalajs.dom.raw.{FormData, HTMLFormElement}
 
 
@@ -19,29 +20,37 @@ object ForecastFileUploadPage {
 
   case class Props()
 
+  val upload: VdomTagOf[Div] =
+    <.div(^.className := "fileUpload",
+      <.h3("Forecast Feed File Upload"),
+      <.form(<.input(^.`type` := "file", ^.name := "filename"),
+        <.br(),
+        <.input(^.`type` := "button", ^.value := "Upload", ^.onClick ==> onSubmit))
+    )
+
+  val uploadResult: String => VdomTagOf[Div] = (message: String) =>
+    <.div(
+      <.div(s"Upload status : ${message}"),
+      <.br(),
+      <.button(^.`type` := "button", "Upload another file", ^.onClick ==> onReset)
+    )
+
   val component: Component[Props, Unit, Unit, CtorType.Props] = ScalaComponent.builder[Props]("ForecastFileUpload")
     .render_P { _ =>
       val fileUploadStateRCP = SPACircuit.connect(m => FileUploadStateModel(m.fileUploadState))
       <.div(
         fileUploadStateRCP(fileUploadStateMP => {
-          <.div(fileUploadStateMP().fileUploadState.render(details => {
-            details.state match {
-              case "uploaded" | "error" =>
-                <.div(
-                  <.div(s"Upload status : ${details.message}"),
-                  <.br(),
-                  <.button(^.`type` := "button", "Reset", ^.onClick ==> onReset)
-                )
-              case _ =>
-                <.div(^.className := "fileUpload",
-                  <.h3("Forecast Feed File Upload"),
-                  <.form(<.input(^.`type` := "file", ^.name := "filename"),
-                    <.br(),
-                    <.input(^.`type` := "button", ^.value := "Upload", ^.onClick ==> onSubmit))
-                )
-            }
-          })
-          )
+          if (fileUploadStateMP().fileUploadState.isEmpty) {
+            upload
+          } else {
+            <.div(fileUploadStateMP().fileUploadState.render(details => {
+              details.state match {
+                case "uploaded" | "error" => uploadResult(details.message)
+                case _ => upload
+              }
+            })
+            )
+          }
         })
       )
     }.componentDidMount(_ => Callback {
