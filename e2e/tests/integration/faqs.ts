9e06796e08e266d6e9ea8a1fd1c1a021fed62c79
Nilesh Gupta <nilesh.nemichandgupta@digital.homeoffice.gov.uk>
2020-08-13 15:22:09 +0100

    DRTII-493 making reusable component for faq

--- e2e/tests/integration/faqs.ts
+++ e2e/tests/integration/faqs.ts
@@ -24,7 +24,9 @@ describe('Faqs page', () => {
               .get('a[href*="#faqs/DeskAndQueues"]')
               .click()
               .get('.faqs')
-              .contains("How are staff allocated if I select the ‘Available staff deployment’ radio button")
+              .then(() => {
+                cy.contains("How are staff allocated if I select the ‘Available staff deployment’ radio button")
+              });
     });
 
     it("Clicking on the Arrival faqs section , you should see Q & A", () => {
@@ -35,7 +37,9 @@ describe('Faqs page', () => {
               .get('a[href*="#faqs/arrivals"]')
               .click()
               .get('.faqs')
-              .contains("What do the RAG colours mean for each flight?")
+              .then(() => {
+                cy.contains("What do the RAG colours mean for each flight?")
+              });
     });
 
 
@@ -48,7 +52,10 @@ describe('Faqs page', () => {
               .get('a[href*="#faqs/portConfiguration"]')
               .click()
               .get('.faqs')
-              .contains("What are the processing times for each split")
+              .then(() => {
+                 cy.contains("What are the processing times for each split")
+              });
+
     });
 
     it("Clicking on staff-movements faqs section , you should see Q & A", () => {
@@ -59,7 +66,10 @@ describe('Faqs page', () => {
               .get('a[href*="#faqs/staff-movements"]')
               .click()
               .get('.faqs')
-              .contains("How do I can add fixed points/ Misc staff")
+              .then(() => {
+                cy.contains("How do I can add fixed points/ Misc staff")
+              });
+
     });
 
     it("Clicking on monthly-staffing faqs section , you should see Q & A", () => {
@@ -70,7 +80,9 @@ describe('Faqs page', () => {
               .get('a[href*="#faqs/monthly-staffing"]')
               .click()
               .get('.faqs')
-              .contains("How do I add my staff to the tool?")
+              .then(() => {
+                cy.contains("How do I add my staff to the tool?")
+              });
     });
 
 })
