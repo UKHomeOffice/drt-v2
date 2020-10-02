0973aca1174151be5299a79b7fe75e06fcb41f9a
Rich Birch <ringo@albumsnaps.com>
2020-10-02 11:29:14 +0100

    DRTII-469 Fix warning for negative max desks

--- server/src/main/scala/services/Optimiser.scala
+++ server/src/main/scala/services/Optimiser.scala
@@ -37,8 +37,7 @@ object Optimiser {
       fairMaxDesks.zip(maxDesks).map { case (fair, orig) => List(fair, orig).min }
     } else maxDesks.toIndexedSeq
 
-    indexedMinDesks.indices.foreach(i => if (indexedMinDesks(i) > bestMaxDesks(i)) println(s"${indexedMinDesks(i)} > ${bestMaxDesks(i)} @ $i"))
-    indexedMinDesks.indices.foreach(i => if (indexedMinDesks(i) < 0 || bestMaxDesks(i) < 0) println(s"${indexedMinDesks(i)} or ${bestMaxDesks(i)} is < 0 @ $i"))
+    if (bestMaxDesks.exists(_ < 0)) log.warn(s"Max desks contains some negative numbers")
 
     for {
       desks <- tryOptimiseWin(indexedWork, indexedMinDesks, bestMaxDesks, config.sla, weightChurn, weightPax, weightStaff, weightSla)
