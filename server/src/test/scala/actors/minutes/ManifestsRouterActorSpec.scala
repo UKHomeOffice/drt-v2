75546532d1ed6c4e31f340ab62eed1fcc3d7e129
Ben Eppel <ben@beneppelsoftware.com>
2020-11-17 14:12:15 +0000

    DRTII-623 fix race condition in tests

--- server/src/test/scala/actors/minutes/ManifestsRouterActorSpec.scala
+++ server/src/test/scala/actors/minutes/ManifestsRouterActorSpec.scala
@@ -1,7 +1,6 @@
 package actors.minutes
 
 import actors.PartitionedPortStateActor.{GetStateForDateRange, PointInTimeQuery}
-import actors.daily.RequestAndTerminate
 import actors.minutes.MinutesActorLike.{ManifestLookup, ManifestsUpdate}
 import actors.queues.{ApiFeedState, ManifestRouterActor}
 import actors.{GetFeedStatuses, GetState, ManifestLookupsLike}
@@ -41,31 +40,37 @@ class ManifestsRouterActorSpec extends CrunchTestLike {
     }
   }
 
-
   val noopUpdates: ManifestsUpdate = (_: UtcDate, _: VoyageManifests) => Future(Unit)
 
   "When sending an ApiFeedResponse" >> {
-
     "Given a Success response with 1 manifest" >> {
-
       val mockLookup = MockManifestsLookup()
       val manifestRouterActor: ActorRef = manifestRouterActorWithMock(mockLookup)
+      val creationDate = SDate("2020-11-20T12:00Z")
 
       val manifest = manifestForDate("2020-11-20")
 
-      val creationDate = SDate("2020-11-20T12:00Z")
-
       val manifestFeedSuccess = ManifestsFeedSuccess(DqManifests("lastSeen.zip", Set(manifest)), creationDate)
-
-      manifestRouterActor ! manifestFeedSuccess
 
       "Then it should be sent to the actor for the correct day" >> {
 
-        val expected =List((UtcDate(2020,11,20), VoyageManifests(Set(manifest))))
+        Await.result(manifestRouterActor.ask(manifestFeedSuccess), 1 second)
+        val expected = List((UtcDate(2020, 11, 20), VoyageManifests(Set(manifest))))
         mockLookup.paramsUpdate === expected
       }
+    }
+
+    "Given a Success response with 1 manifest" >> {
+      val mockLookup = MockManifestsLookup()
+      val manifestRouterActor: ActorRef = manifestRouterActorWithMock(mockLookup)
+      val creationDate = SDate("2020-11-20T12:00Z")
+
+      val manifest = manifestForDate("2020-11-20")
+
+      val manifestFeedSuccess = ManifestsFeedSuccess(DqManifests("lastSeen.zip", Set(manifest)), creationDate)
 
       "Then it should update the last seen zip file name" >> {
+        Await.result(manifestRouterActor.ask(manifestFeedSuccess), 1 second)
 
         val result: ApiFeedState = Await.result(manifestRouterActor.ask(GetState).mapTo[ApiFeedState], 1 second)
 
@@ -73,6 +78,7 @@ class ManifestsRouterActorSpec extends CrunchTestLike {
       }
 
       "Then it should record the successful response" >> {
+        Await.result(manifestRouterActor.ask(manifestFeedSuccess), 1 second)
 
         val result: ApiFeedState = Await.result(manifestRouterActor.ask(GetState).mapTo[ApiFeedState], 1 second)
 
@@ -98,11 +104,10 @@ class ManifestsRouterActorSpec extends CrunchTestLike {
 
       val manifestFeedFailure = ManifestsFeedFailure("Failed", creationDate)
 
-      manifestRouterActor ! manifestFeedFailure
-
       "Then it should record the failure response" >> {
+        Await.result(manifestRouterActor.ask(manifestFeedFailure), 1 second)
 
-        val result: ApiFeedState = Await.result(manifestRouterActor.ask(GetState).mapTo[ApiFeedState], 1 second)
+        val result: ApiFeedState = Await.result(manifestRouterActor.ask(GetState).mapTo[ApiFeedState], 2 seconds)
 
         result.maybeSourceStatuses === Option(
           FeedSourceStatuses(
@@ -117,6 +122,7 @@ class ManifestsRouterActorSpec extends CrunchTestLike {
         )
       }
       "Then it should respond with the feed statuses when asked" >> {
+        Await.result(manifestRouterActor.ask(manifestFeedFailure), 1 second)
 
         val result: Option[FeedSourceStatuses] = Await.result(manifestRouterActor.ask(GetFeedStatuses).mapTo[Option[FeedSourceStatuses]], 1 second)
 
@@ -135,83 +141,72 @@ class ManifestsRouterActorSpec extends CrunchTestLike {
     }
   }
 
-  "Given a manifest for a date " >> {
-    "When it is saved" >> {
-
+  "Given I request manifests for a date range" >> {
+    "Then each of the dates requested should be queried" >> {
       val testProbe = TestProbe()
-      val manifestRouterActor: ActorRef = manifestRouterActorWithTestProbe(testProbe)
+      val manifestRouterActor = manifestRouterActorWithTestProbe(testProbe)
 
-      val manifest = manifestForDate("2020-11-20")
+      val result = Await.result(manifestRouterActor.ask(
+        GetStateForDateRange(SDate("2020-11-01T00:00Z").millisSinceEpoch, SDate("2020-11-02T23:59Z").millisSinceEpoch)
+      ).mapTo[Source[VoyageManifests, NotUsed]], 1 second)
 
-      val manifests = VoyageManifests(Set(manifest))
+      result.runWith(Sink.seq)
 
-      "Given I request manifests for a date range" >> {
-        "Then each of the dates requested should be queried" >> {
-          val testProbe = TestProbe()
-          val manifestRouterActor = manifestRouterActorWithTestProbe(testProbe)
+      testProbe.expectMsgAllOf((UtcDate(2020, 11, 1), None), (UtcDate(2020, 11, 2), None))
 
-          val result = Await.result(manifestRouterActor.ask(
-            GetStateForDateRange(SDate("2020-11-01T00:00Z").millisSinceEpoch, SDate("2020-11-02T23:59Z").millisSinceEpoch)
-          ).mapTo[Source[VoyageManifests, NotUsed]], 1 second)
-
-          result.runWith(Sink.seq)
-
-          testProbe.expectMsgAllOf((UtcDate(2020, 11, 1), None), (UtcDate(2020, 11, 2), None))
-
-          success
-        }
-      }
-
-      "Given I request manifests for a date range at a point in time" >> {
-        "Then each of the dates requested should be queried with the correct point in time" >> {
-          val testProbe = TestProbe()
-          val manifestRouterActor = manifestRouterActorWithTestProbe(testProbe)
-
-          val pit = SDate("2020-11-05T00:00Z").millisSinceEpoch
-          val result = Await.result(manifestRouterActor.ask(
-            PointInTimeQuery(
-              pit,
-              GetStateForDateRange(SDate("2020-11-01T00:00Z").millisSinceEpoch, SDate("2020-11-02T23:59Z").millisSinceEpoch)
-            )
-
-          ).mapTo[Source[VoyageManifests, NotUsed]], 1 second)
-
-          result.runWith(Sink.seq)
-
-          testProbe.expectMsgAllOf((UtcDate(2020, 11, 1), Option(pit)), (UtcDate(2020, 11, 2), Option(pit)))
-
-          success
-        }
-      }
-
-      "Given I request manifests for a date range at a point in time" >> {
-        "Then manifests for all those dates should be returned in the stream" >> {
-
-          val manifest1 = manifestForDate("2020-11-01")
-          val manifest2 = manifestForDate("2020-11-02")
-          val manifest3 = manifestForDate("2020-11-03")
-          val manifestsLookup = MockManifestsLookup()
-          val testManifests = VoyageManifests(Set(manifest1, manifest2, manifest3))
-          val manifestRouterActor = system.actorOf(
-            ManifestRouterActor.props(manifestsLookup.lookup(testManifests), noopUpdates)
-          )
-
-          val resultSource: Future[Source[VoyageManifests, NotUsed]] = manifestRouterActor.ask(
-            GetStateForDateRange(
-              SDate("2020-11-01T00:00Z").millisSinceEpoch,
-              SDate("2020-11-02T23:59Z").millisSinceEpoch
-            )
-          ).mapTo[Source[VoyageManifests, NotUsed]]
-
-          val result = Await.result(ManifestRouterActor.runAndCombine(resultSource), 1 second)
-
-          val expected = VoyageManifests(Set(manifest1, manifest2))
-
-          result === expected
-        }
-      }
+      success
     }
   }
+
+  "Given I request manifests for a date range at a point in time" >> {
+    "Then each of the dates requested should be queried with the correct point in time" >> {
+      val testProbe = TestProbe()
+      val manifestRouterActor = manifestRouterActorWithTestProbe(testProbe)
+
+      val pit = SDate("2020-11-05T00:00Z").millisSinceEpoch
+      val result = Await.result(manifestRouterActor.ask(
+        PointInTimeQuery(
+          pit,
+          GetStateForDateRange(SDate("2020-11-01T00:00Z").millisSinceEpoch, SDate("2020-11-02T23:59Z").millisSinceEpoch)
+        )
+
+      ).mapTo[Source[VoyageManifests, NotUsed]], 1 second)
+
+      result.runWith(Sink.seq)
+
+      testProbe.expectMsgAllOf((UtcDate(2020, 11, 1), Option(pit)), (UtcDate(2020, 11, 2), Option(pit)))
+
+      success
+    }
+  }
+
+  "Given I request manifests for a date range at a point in time" >> {
+    "Then manifests for all those dates should be returned in the stream" >> {
+
+      val manifest1 = manifestForDate("2020-11-01")
+      val manifest2 = manifestForDate("2020-11-02")
+      val manifest3 = manifestForDate("2020-11-03")
+      val manifestsLookup = MockManifestsLookup()
+      val testManifests = VoyageManifests(Set(manifest1, manifest2, manifest3))
+      val manifestRouterActor = system.actorOf(
+        ManifestRouterActor.props(manifestsLookup.lookup(testManifests), noopUpdates)
+      )
+
+      val resultSource: Future[Source[VoyageManifests, NotUsed]] = manifestRouterActor.ask(
+        GetStateForDateRange(
+          SDate("2020-11-01T00:00Z").millisSinceEpoch,
+          SDate("2020-11-02T23:59Z").millisSinceEpoch
+        )
+      ).mapTo[Source[VoyageManifests, NotUsed]]
+
+      val result = Await.result(ManifestRouterActor.runAndCombine(resultSource), 1 second)
+
+      val expected = VoyageManifests(Set(manifest1, manifest2))
+
+      result === expected
+    }
+  }
+
 
   def manifestForDate(date: String) = {
     VoyageManifest(EventTypes.DC,
