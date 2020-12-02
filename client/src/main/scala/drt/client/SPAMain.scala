dd4ebe316b79add5638253a6aa97ac1da1e9aa28
Rich Birch <ringo@albumsnaps.com>
2020-12-02 11:47:49 +0000

    DRTII-635 Tidying

--- client/src/main/scala/drt/client/SPAMain.scala
+++ client/src/main/scala/drt/client/SPAMain.scala
@@ -14,7 +14,6 @@ import drt.shared.SDateLike
 import drt.shared.Terminals.Terminal
 import japgolly.scalajs.react.Callback
 import japgolly.scalajs.react.extra.router._
-import japgolly.scalajs.react.vdom.html_<^
 import org.scalajs.dom
 import org.scalajs.dom.console
 import scalacss.ProdDefaults._
@@ -193,8 +192,7 @@ object SPAMain {
 
       rule.notFound(redirectToPage(PortDashboardLoc(None))(Redirect.Replace))
     }
-//    .renderWith(Layout(_, _))
-    .renderWith(layout)
+    .renderWith(Layout(_, _))
     .onPostRender((maybePrevLoc, currentLoc) => {
       Callback(
         (maybePrevLoc, currentLoc) match {
@@ -332,15 +330,14 @@ object SPAMain {
       })
   }
 
-  def layout(c: RouterCtl[Loc], r: Resolution[Loc]): html_<^.VdomElement = Layout(c, r)
-
-  def pathToThisApp: String = dom.document.location.pathname
+  val pathToThisApp: String = dom.document.location.pathname
 
   val rootDomain: String = dom.document.location.host.split("\\.").drop(1).mkString(".")
 
   val useHttps: Boolean = dom.document.location.protocol == "https:"
 
   console.log(s"useHttps: '$useHttps'")
+
   val urls: Urls = Urls(rootDomain, useHttps)
 
   def absoluteUrl(relativeUrl: String): String = {
