14876e2dbb801a44bb42304ed5e128882c3d06c1
Nilesh Gupta <nilesh.nemichandgupta@digital.homeoffice.gov.uk>
2021-01-26 14:36:39 +0000

    DRTII-726 parsing valid data and ignore invalid data

--- server/src/main/scala/drt/server/feeds/lgw/LGWForecastXLSExtractor.scala
+++ server/src/main/scala/drt/server/feeds/lgw/LGWForecastXLSExtractor.scala
@@ -10,7 +10,7 @@ import org.apache.poi.ss.usermodel.{Cell, DateUtil, Sheet}
 import org.slf4j.{Logger, LoggerFactory}
 import services.SDate
 
-import scala.util.{Success, Try}
+import scala.util.{Failure, Success, Try}
 
 
 case class LGWForecastFlightRow(scheduledDate: SDateLike,
@@ -44,38 +44,45 @@ object LGWForecastXLSExtractor {
 
     val headingIndexByNameMap: Map[String, Int] = headingIndexByName(sheet.getRow(0))
 
-    val arrivalRows: Seq[LGWForecastFlightRow] = for {
+    val arrivalRowsTry: Seq[Try[LGWForecastFlightRow]] = for {
       rowNumber <- 1 to sheet.getLastRowNum
       row = sheet.getRow(rowNumber)
       if row.getCell(0) != null && row.getCell(1).getCellType != Cell.CELL_TYPE_BLANK
     } yield {
-      val terminalCell = "N"
-      val flightDateCell = numericCellOption(headingIndexByNameMap("Date"), row)
-      val flightTimeCell = numericCellOption(headingIndexByNameMap("Time (UTC)"), row)
-      val flightNumberCell = stringCellOption(headingIndexByNameMap("FlightNo"), row)
-      val airportCell = stringCellOption(headingIndexByNameMap("Airport"), row)
-      val serviceCell = stringCellOption(headingIndexByNameMap("Service"), row)
-      val arrivalOrDepCell = stringCellOption(headingIndexByNameMap("ArrDep"), row)
-      val internationalDomesticCell = stringCellOption(headingIndexByNameMap("Dom/Int"), row)
-      val totalCell = numericCellOption(headingIndexByNameMap("Forecast Pax"), row)
+      Try {
+        val terminalCell = "N"
+        val flightDateCell = numericCellOption(headingIndexByNameMap("Date"), row)
+        val flightTimeCell = numericCellOption(headingIndexByNameMap("Time (UTC)"), row)
+        val flightNumberCell = stringCellOption(headingIndexByNameMap("FlightNo"), row)
+        val airportCell = stringCellOption(headingIndexByNameMap("Airport"), row)
+        val serviceCell = stringCellOption(headingIndexByNameMap("Service"), row)
+        val arrivalOrDepCell = stringCellOption(headingIndexByNameMap("ArrDep"), row)
+        val internationalDomesticCell = stringCellOption(headingIndexByNameMap("Dom/Int"), row)
+        val totalCell = numericCellOption(headingIndexByNameMap("Forecast Pax"), row)
 
-      val scheduledCell = SDate(DateUtil.getJavaDate(flightDateCell.getOrElse(0.0) + flightTimeCell.getOrElse(0.0), TimeZone.getTimeZone("UTC")).getTime)
+        val scheduledCell = SDate(DateUtil.getJavaDate(flightDateCell.getOrElse(0.0) + flightTimeCell.getOrElse(0.0), TimeZone.getTimeZone("UTC")).getTime)
 
-      LGWForecastFlightRow(scheduledDate = scheduledCell,
-        flightCode = flightNumberCell.getOrElse(""),
-        origin = airportCell.getOrElse(""),
-        service = serviceCell.getOrElse(""),
-        arrivalOrDep = arrivalOrDepCell.getOrElse(""),
-        internationalDomestic = internationalDomesticCell.getOrElse(""),
-        totalPax = totalCell.map(_.toInt).getOrElse(0),
-        transferPax = 0,
-        terminalCell)
-
+        LGWForecastFlightRow(scheduledDate = scheduledCell,
+          flightCode = flightNumberCell.getOrElse(""),
+          origin = airportCell.getOrElse(""),
+          service = serviceCell.getOrElse(""),
+          arrivalOrDep = arrivalOrDepCell.getOrElse(""),
+          internationalDomestic = internationalDomesticCell.getOrElse(""),
+          totalPax = totalCell.map(_.toInt).getOrElse(0),
+          transferPax = 0,
+          terminalCell)
+      }
     }
 
-    log.info(s"Extracted ${arrivalRows.size} arrival rows from LGW XLS Workbook")
+    val arrivalRows = arrivalRowsTry.zipWithIndex.toList.flatMap {
+      case (Success(a), _) => Some(a)
+      case (Failure(e), i) => log.warn(s"Invalid data on row ${i + 2} ${e.getMessage}", e)
+        None
+    }.filter(_.service == "Passenger").filter(_.arrivalOrDep == "A").filter(_.internationalDomestic == "I")
 
-    arrivalRows.toList.filter(_.service == "Passenger").filter(_.arrivalOrDep == "A").filter(_.internationalDomestic == "I")
+    log.info(s"Extracted ${arrivalRows.size} arrival rows from LGW XLS Workbook")
+    arrivalRows
+
   }
 
 
