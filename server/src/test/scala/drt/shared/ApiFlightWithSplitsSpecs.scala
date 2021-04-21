bdd05c2ea09b056d817c29f4a91493d6dbf24cc7
Rich Birch <ringo@albumsnaps.com>
2021-04-21 18:45:34 +0100

    DRTII-812 Fix splits pax count and add more tests

--- server/src/test/scala/drt/shared/ApiFlightWithSplitsSpecs.scala
+++ server/src/test/scala/drt/shared/ApiFlightWithSplitsSpecs.scala
@@ -2,74 +2,68 @@ package drt.shared
 
 import controllers.ArrivalGenerator
 import drt.shared.PaxTypes._
-import drt.shared.SplitRatiosNs.SplitSources
-import drt.shared.SplitRatiosNs.SplitSources.{ApiSplitsWithHistoricalEGateAndFTPercentages, PredictedSplitsWithHistoricalEGateAndFTPercentages}
-import drt.shared.Terminals.T1
+import drt.shared.SplitRatiosNs.SplitSources.{ApiSplitsWithHistoricalEGateAndFTPercentages, Historical}
+import drt.shared.SplitRatiosNs.{SplitSource, SplitSources}
 import drt.shared.api.Arrival
 import org.specs2.mutable.Specification
 
 class ApiFlightWithSplitsSpecs extends Specification {
-
-  val flight: Arrival = ArrivalGenerator.arrival(
-    iata = "TST100",
-    actPax = Option(40),
-    tranPax = Option(0),
-    schDt = "2020-06-17T05:30:00Z",
-    terminal = T1,
-    airportId = PortCode("ID"),
-    status = ArrivalStatus("Scheduled"),
-    feedSources = Set(LiveFeedSource),
-    pcpDt = "2020-06-17T06:30:00Z"
-  )
-  val splitsWithinFivePercentageThreshold = Splits(Set(
-    ApiPaxTypeAndQueueCount(B5JPlusNational, Queues.EeaDesk, 2.0, None, None),
-    ApiPaxTypeAndQueueCount(B5JPlusNational, Queues.EGate, 4.0, None, None),
-    ApiPaxTypeAndQueueCount(EeaMachineReadable, Queues.EeaDesk, 3.0, None, None),
-    ApiPaxTypeAndQueueCount(EeaNonMachineReadable, Queues.EeaDesk, 4.0, None, None),
-    ApiPaxTypeAndQueueCount(NonVisaNational, Queues.NonEeaDesk, 5.0, None, None),
-    ApiPaxTypeAndQueueCount(VisaNational, Queues.NonEeaDesk, 6.0, None, None),
-    ApiPaxTypeAndQueueCount(Transit, Queues.Transfer, 7.0, None, None),
-    ApiPaxTypeAndQueueCount(EeaMachineReadable, Queues.EGate, 8.0, None, None)
-  ), ApiSplitsWithHistoricalEGateAndFTPercentages, Option(EventTypes.DC))
-
-  val splitsNotWithinFivePercentageThreshold = Splits(Set(
-    ApiPaxTypeAndQueueCount(B5JPlusNational, Queues.EeaDesk, 1.0, None, None),
-    ApiPaxTypeAndQueueCount(B5JPlusNational, Queues.EGate, 2.0, None, None),
-    ApiPaxTypeAndQueueCount(EeaMachineReadable, Queues.EeaDesk, 3.0, None, None),
-    ApiPaxTypeAndQueueCount(EeaNonMachineReadable, Queues.EeaDesk, 4.0, None, None),
-    ApiPaxTypeAndQueueCount(NonVisaNational, Queues.NonEeaDesk, 5.0, None, None),
-    ApiPaxTypeAndQueueCount(VisaNational, Queues.NonEeaDesk, 6.0, None, None),
-    ApiPaxTypeAndQueueCount(Transit, Queues.Transfer, 7.0, None, None),
-    ApiPaxTypeAndQueueCount(EeaMachineReadable, Queues.EGate, 8.0, None, None)
-  ), ApiSplitsWithHistoricalEGateAndFTPercentages, Option(EventTypes.DC))
-
-
   "flight Arrival" should {
-    val apiFlightWithSplits = new ApiFlightWithSplits(flight, Set(splitsWithinFivePercentageThreshold))
-
-    "has valid Api when api splits sum pax count is within 5% Threshold of LiveSourceFeed pax count" in {
-      val apiSplits = apiFlightWithSplits.splits.find(s => s.source == SplitSources.ApiSplitsWithHistoricalEGateAndFTPercentages).get
-      apiFlightWithSplits.isWithinThreshold(apiSplits) mustEqual true
-      apiFlightWithSplits.hasValidApi mustEqual true
+    "have valid Api when api splits pax count is within 5% Threshold of LiveSourceFeed pax count" in {
+      val flightWithSplits = flightWithPaxAndApiSplits(40, 0, 41, 0, Set(LiveFeedSource))
+      val apiSplits = flightWithSplits.splits.find(_.source == SplitSources.ApiSplitsWithHistoricalEGateAndFTPercentages).get
+      flightWithSplits.isWithinThreshold(apiSplits) mustEqual true
+      flightWithSplits.hasValidApi mustEqual true
     }
 
-    "has not valid Api when api splits sum pax count is not within 5% Threshold of LiveSourceFeed pax count" in {
-      val flightWithSplits = new ApiFlightWithSplits(flight, Set(splitsNotWithinFivePercentageThreshold))
+    "have valid Api when api splits pax count is within 5% Threshold of LiveSourceFeed pax count, after taking into account transfer pax" in {
+      val flightWithSplits = flightWithPaxAndApiSplits(40, 20, 21, 0, Set(LiveFeedSource))
+      val apiSplits = flightWithSplits.splits.find(_.source == SplitSources.ApiSplitsWithHistoricalEGateAndFTPercentages).get
+      flightWithSplits.isWithinThreshold(apiSplits) mustEqual true
+      flightWithSplits.hasValidApi mustEqual true
+    }
+
+    "have valid Api when api splits pax count is within 5% Threshold of LiveSourceFeed pax count, after taking into account transfer pax in both port feed and API" in {
+      val flightWithSplits = flightWithPaxAndApiSplits(40, 20, 21, 20, Set(LiveFeedSource))
+      val apiSplits = flightWithSplits.splits.find(_.source == SplitSources.ApiSplitsWithHistoricalEGateAndFTPercentages).get
+      flightWithSplits.isWithinThreshold(apiSplits) mustEqual true
+      flightWithSplits.hasValidApi mustEqual true
+    }
+
+    "not have valid Api when api splits pax count is not within 5% Threshold of LiveSourceFeed pax count" in {
+      val flightWithSplits = flightWithPaxAndApiSplits(40, 0, 50, 0, Set(LiveFeedSource))
       val apiSplits = flightWithSplits.splits.find(s => s.source == SplitSources.ApiSplitsWithHistoricalEGateAndFTPercentages).get
       flightWithSplits.isWithinThreshold(apiSplits) mustEqual false
       flightWithSplits.hasValidApi mustEqual false
     }
 
-    "has not valid Api splits when source is not ApiSplitsWithHistoricalEGateAndFTPercentages" in {
-      val flightWithSplits = new ApiFlightWithSplits(flight, Set(splitsNotWithinFivePercentageThreshold.copy(source = PredictedSplitsWithHistoricalEGateAndFTPercentages)))
+    "not have valid Api splits when source is not ApiSplitsWithHistoricalEGateAndFTPercentages" in {
+      val flightWithSplits = flightWithPaxAndHistoricSplits(40, 0, 41, 0, Set(LiveFeedSource))
       flightWithSplits.hasValidApi mustEqual false
     }
 
-    "has valid Api splits when source is not LiveFeedSource" in {
-      val flightWithSplits = new ApiFlightWithSplits(flight.copy(FeedSources=Set(ForecastFeedSource)), Set(splitsNotWithinFivePercentageThreshold))
+    "have valid Api splits when flight is missing LiveFeedSource, even if above threshold" in {
+      val flightWithSplits = flightWithPaxAndApiSplits(40, 0, 100, 0, Set())
       flightWithSplits.hasValidApi mustEqual true
     }
 
   }
+
+  private def flightWithPaxAndApiSplits(actPax: Int, transferPax: Int, splitsDirect: Int, splitsTransfer: Int, sources: Set[FeedSource]): ApiFlightWithSplits = {
+    val flight: Arrival = ArrivalGenerator.arrival(actPax = Option(actPax), tranPax = Option(transferPax), feedSources = sources)
+
+    ApiFlightWithSplits(flight, Set(splitsForPax(directPax = splitsDirect, transferPax = splitsTransfer, ApiSplitsWithHistoricalEGateAndFTPercentages)))
+  }
+
+  private def flightWithPaxAndHistoricSplits(actPax: Int, transferPax: Int, splitsDirect: Int, splitsTransfer: Int, sources: Set[FeedSource]): ApiFlightWithSplits = {
+    val flight: Arrival = ArrivalGenerator.arrival(actPax = Option(actPax), tranPax = Option(transferPax), feedSources = sources)
+
+    ApiFlightWithSplits(flight, Set(splitsForPax(directPax = splitsDirect, transferPax = splitsTransfer, Historical)))
+  }
+
+  def splitsForPax(directPax: Int, transferPax: Int, source: SplitSource): Splits = Splits(Set(
+    ApiPaxTypeAndQueueCount(VisaNational, Queues.NonEeaDesk, directPax, None, None),
+    ApiPaxTypeAndQueueCount(Transit, Queues.Transfer, transferPax, None, None),
+  ), source, Option(EventTypes.DC))
 
 }
