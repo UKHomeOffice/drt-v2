fd7b187a90d9e3b73ad4257f2351a17654452dc2
Nilesh Gupta <nilesh.nemichandgupta@digital.homeoffice.gov.uk>
2021-05-11 11:24:04 +0100

    DRTII-150 fixing e2e test

--- client/src/main/scala/drt/client/components/Layout.scala
+++ client/src/main/scala/drt/client/components/Layout.scala
@@ -60,7 +60,7 @@ object Layout {
 
   def buildFeedBackNavBar(user: LoggedInUser): VdomTagOf[UList] = {
     <.ul(^.className := "nav navbar-nav navbar-right",
-      <.li(<.div(<.span(^.className := "btn", "is this page useful ?", ^.disabled := true))),
+      <.li(<.div(<.span(^.className := "btn", "is this page useful?", ^.disabled := true))),
       <.li(PositiveFeedbackComponent(dom.window.location.toString, user)),
       <.li(NegativeFeedbackComponent(dom.window.location.toString, user))
     )
