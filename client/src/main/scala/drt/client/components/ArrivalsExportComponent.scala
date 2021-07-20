3a8455665bc14663b9573f7b6f10efa14a7a6dd6
Rich Birch <ringo@albumsnaps.com>
2021-07-20 12:21:28 +0100

    DRTII-852 Refactoring to remove code duplication

--- client/src/main/scala/drt/client/components/ArrivalsExportComponent.scala
+++ client/src/main/scala/drt/client/components/ArrivalsExportComponent.scala
@@ -5,22 +5,30 @@ import drt.client.components.TerminalContentComponent.exportLink
 import drt.client.components.styles.WithScalaCssImplicits
 import drt.client.logger.{Logger, LoggerFactory}
 import drt.client.services.JSDateConversions.SDate
-import drt.client.services.{ExportArrivalsCombinedTerminals, ExportArrivalsSingleTerminal, ExportArrivalsWithRedListDiversions, ExportArrivalsWithoutRedListDiversions, ViewMode}
-import drt.shared.SDateLike
+import drt.client.services.{ExportArrivals, ExportArrivalsCombinedTerminals, ExportArrivalsSingleTerminal, ExportArrivalsWithRedListDiversions, ExportArrivalsWithoutRedListDiversions, ExportType, ViewMode}
 import drt.shared.Terminals.Terminal
 import drt.shared.dates.LocalDate
+import drt.shared.{PortCode, SDateLike}
 import io.kinoplan.scalajs.react.material.ui.core.MuiGrid
 import japgolly.scalajs.react.component.Scala.Component
+import japgolly.scalajs.react.vdom.html_<^
 import japgolly.scalajs.react.vdom.html_<^._
 import japgolly.scalajs.react.{CtorType, Reusability, ScalaComponent}
 import uk.gov.homeoffice.drt.auth.LoggedInUser
 import uk.gov.homeoffice.drt.auth.Roles.ArrivalsAndSplitsView
 
-object BhxArrivalsExportComponent extends WithScalaCssImplicits {
+
+object ArrivalsExportComponent extends WithScalaCssImplicits {
   val today: SDateLike = SDate.now()
   val log: Logger = LoggerFactory.getLogger(getClass.getName)
 
-  case class Props(terminal: Terminal, selectedDate: SDateLike, loggedInUser: LoggedInUser, viewMode: ViewMode)
+  case class Props(terminal: Terminal,
+                   selectedDate: SDateLike,
+                   loggedInUser: LoggedInUser,
+                   viewMode: ViewMode,
+                   title: String,
+                   exports: List[ExportType],
+                  )
 
   case class State(showDialogue: Boolean = false)
 
@@ -32,9 +40,7 @@ object BhxArrivalsExportComponent extends WithScalaCssImplicits {
   val component: Component[Props, State, Unit, CtorType.Props] = ScalaComponent.builder[Props]("MultiDayExportComponent")
     .initialStateFromProps(p => State(false))
     .renderPS((scope, props, state) => {
-
       val showClass = if (state.showDialogue) "show" else "fade"
-
       <.div(
         <.a(
           Icon.download,
@@ -51,33 +57,12 @@ object BhxArrivalsExportComponent extends WithScalaCssImplicits {
             ^.role := "document",
             <.div(
               ^.className := "modal-content",
-              <.div(
-                ^.className := "modal-header",
-                <.h5(^.className := "modal-title", "Single terminal or both")
-              ),
+              <.div(^.className := "modal-header", <.h5(^.className := "modal-title", props.title)),
               <.div(
                 ^.className := "modal-body",
                 ^.id := "arrivals-export-modal-body",
                 if (props.loggedInUser.hasRole(ArrivalsAndSplitsView))
-                  MuiGrid(container = true)(
-                    MuiGrid(container = true, spacing = MuiGrid.Spacing.`16`)(
-                      MuiGrid(item = true, xs = 4)(
-                        exportLink(
-                          props.selectedDate,
-                          props.terminal.toString,
-                          ExportArrivalsSingleTerminal,
-                          SPAMain.exportUrl(ExportArrivalsSingleTerminal, props.viewMode, props.terminal)
-                        )
-                      ),
-                      MuiGrid(item = true, xs = 4)(
-                        exportLink(
-                          props.selectedDate,
-                          props.terminal.toString,
-                          ExportArrivalsCombinedTerminals,
-                          SPAMain.exportUrl(ExportArrivalsCombinedTerminals, props.viewMode, props.terminal)
-                        )
-                      )
-                    ))
+                  exportLinks(props.exports, props.selectedDate, props.terminal, props.viewMode)
                 else
                   EmptyVdom,
                 <.div(
@@ -96,5 +81,33 @@ object BhxArrivalsExportComponent extends WithScalaCssImplicits {
     .configure(Reusability.shouldComponentUpdate)
     .build
 
-  def apply: (Terminal, SDateLike, LoggedInUser, ViewMode) => VdomElement = (terminal: Terminal, selectedDate: SDateLike, loggedInUser: LoggedInUser, viewMode: ViewMode) =>
-    component(Props(terminal, selectedDate, loggedInUser, viewMode))}
+  private def exportLinks(exports: List[ExportType], date: SDateLike, terminal: Terminal, viewMode: ViewMode): html_<^.VdomElement =
+    MuiGrid(container = true)(
+      MuiGrid(container = true, spacing = MuiGrid.Spacing.`16`)(
+        exports.map(export =>
+          MuiGrid(item = true, xs = 4)(
+            exportLink(date, terminal.toString, export, SPAMain.exportUrl(export, viewMode, terminal))
+          )).toVdomArray
+      ))
+
+  def componentFactory(title: String, exports: List[ExportType]): (Terminal, SDateLike, LoggedInUser, ViewMode) => VdomElement =
+    (terminal: Terminal, selectedDate: SDateLike, loggedInUser: LoggedInUser, viewMode: ViewMode) =>
+      component(Props(terminal, selectedDate, loggedInUser, viewMode, title, exports))
+
+  def apply(portCode: PortCode): (Terminal, SDateLike, LoggedInUser, ViewMode) => VdomElement = portCode match {
+    case PortCode("LHR") => componentFactory(
+      "Reflect Red List Pax Diversions?",
+      List(ExportArrivalsWithRedListDiversions, ExportArrivalsWithoutRedListDiversions)
+    )
+    case PortCode("BHX") => componentFactory(
+      "Single terminal or both?",
+      List(ExportArrivalsSingleTerminal, ExportArrivalsCombinedTerminals))
+    case _ => (terminal, date, user, viewMode) =>
+      exportLink(
+        date,
+        terminal.toString,
+        ExportArrivals,
+        SPAMain.exportUrl(ExportArrivals, viewMode, terminal)
+      )
+  }
+}
