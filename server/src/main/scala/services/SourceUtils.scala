911ead6952c9105f135ed1da6931fd0de016bfcd
Rich Birch <ringo@albumsnaps.com>
2021-07-21 09:16:12 +0100

    DRTII-852 Remove dead code

--- server/src/main/scala/services/SourceUtils.scala
+++ server/src/main/scala/services/SourceUtils.scala
@@ -1,18 +1,8 @@
 package services
 
-import akka.NotUsed
-import akka.stream.scaladsl.Source
-
 import scala.concurrent.{ExecutionContext, Future}
 
 object SourceUtils {
-  def applyFutureIterablesReducer[I1, I2, T](iterable1: Iterable[I1],
-                                             eventualForIterables: I1 => I2 => Future[T],
-                                             futureReducer: (I2 => Future[T]) => Future[T])
-                                            (implicit ec: ExecutionContext): Source[T, NotUsed] =
-    Source(iterable1.toList)
-      .mapAsync(1)(d => futureReducer(eventualForIterables(d)))
-
   def reduceFutureIterables[T, I](iterable: Iterable[I], reducer: Iterable[T] => T)
                                  (implicit ec: ExecutionContext): (I => Future[T]) => Future[T] =
     (eventualForIterable: I => Future[T]) =>
