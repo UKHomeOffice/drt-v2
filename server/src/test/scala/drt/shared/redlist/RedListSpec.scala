2f7312accce2b5b335a1c33ab6bd25ff6bcf7903
Rich Birch <ringo@albumsnaps.com>
2021-08-06 08:15:10 +0100

    DRTII-872 Test for red list additions and removals

--- server/src/test/scala/drt/shared/redlist/RedListSpec.scala
+++ server/src/test/scala/drt/shared/redlist/RedListSpec.scala
@@ -8,18 +8,26 @@ import services.graphstages.Crunch
 class RedListSpec extends Specification{
   val date20210807: SDateLike = SDate("2021-08-07T00:00", Crunch.europeLondonTimeZone)
   val date20210808: SDateLike = SDate("2021-08-08T00:00", Crunch.europeLondonTimeZone)
+  val countriesRemovedFrom20210808 = Set("Bahrain", "India", "Qatar", "United Arab Emirates")
+  val countriesAddedFrom20210808 = Set("Georgia", "Mayotte", "Mexico", "Reunion")
 
   "Given a date just before the red list changes on 08/08/2021" >> {
     val redList20210807 = RedList.countryCodesByName(date20210807.millisSinceEpoch)
     "The red list should contain India" >> {
-      redList20210807.contains("India") === true
+      val expectedToExist = redList20210807.keys.toSet.intersect(countriesRemovedFrom20210808)
+      val expectedToNotExist = redList20210807.keys.toSet.intersect(countriesAddedFrom20210808)
+
+      expectedToExist === countriesRemovedFrom20210808 && expectedToNotExist === Set()
     }
   }
 
   "Given a date just before the red list changes on 08/08/2021" >> {
     val redList20210808 = RedList.countryCodesByName(date20210808.millisSinceEpoch)
     "The red list should not contain India" >> {
-      redList20210808.contains("India") === false
+      val expectedToNotExist = redList20210808.keys.toSet.intersect(countriesRemovedFrom20210808)
+      val expectedToExist = redList20210808.keys.toSet.intersect(countriesAddedFrom20210808)
+      
+      expectedToNotExist === Set() && expectedToExist == countriesAddedFrom20210808
     }
   }
 }
