8326914978aa7eb79511d96b79e189da12bb6d8f
Rich Birch <ringo@albumsnaps.com>
2021-10-15 12:54:43 +0100

    DRTII-912 Add acl.check-on-startup env flag

--- server/src/main/scala/actors/DrtSystemInterface.scala
+++ server/src/main/scala/actors/DrtSystemInterface.scala
@@ -289,11 +289,11 @@ trait DrtSystemInterface extends UserRoleProviderLike {
     case None => arrivalsNoOp
     case Some(aclFeed) =>
       val initialDelay =
-        if (config.get[Boolean]("check-acl-on-startup")) 0.minutes
+        if (config.get[Boolean]("acl.check-on-startup")) 0.minutes
         else AclFeed.delayUntilNextAclCheck(now(), 18) + (Math.random() * 60).minutes
 
       log.info(s"Daily ACL check. Initial delay: ${initialDelay.toMinutes} minutes")
-      
+
       Source.tick(initialDelay, 1.day, NotUsed).map { _ =>
         system.log.info(s"Requesting ACL feed")
         aclFeed.requestArrivals
