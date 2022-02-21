c5b056e0e692aae2d6215cb9e5f96dff416acd61
Rich Birch <ringo@albumsnaps.com>
2022-02-21 18:35:24 +0000

    DRTII-972 Fix e2e test to click on i

--- client/src/main/scala/drt/client/components/FlightChartComponent.scala
+++ client/src/main/scala/drt/client/components/FlightChartComponent.scala
@@ -5,7 +5,6 @@ import drt.client.components.ChartJSComponent.{ChartJsData, ChartJsOptions, Char
 import drt.client.logger.{Logger, LoggerFactory}
 import drt.client.services.SPACircuit
 import drt.shared.ArrivalKey
-import drt.shared.api.PassengerInfoSummary
 import io.kinoplan.scalajs.react.material.ui.core.MuiCircularProgress
 import japgolly.scalajs.react.vdom.html_<^._
 import japgolly.scalajs.react.{Callback, ReactEventFromInput, ScalaComponent}
@@ -21,76 +20,77 @@ object FlightChartComponent {
     .render_P { props =>
       val proxy = SPACircuit.connect(_.passengerInfoSummariesByArrival)
 
-      Tippy.interactiveInfo(
-        triggerCallback = Option((_: ReactEventFromInput) => {
-          Callback(SPACircuit.dispatch(GetPassengerInfoSummary(ArrivalKey(props.flightWithSplits.apiFlight))))
-        }),
-        content =
-          proxy { rcp =>
-            val infosPot = rcp()
-            <.div(^.cls := "container arrivals__table__flight__chart-box",
-              infosPot.render { infos =>
-                infos.get(ArrivalKey(props.flightWithSplits.apiFlight)) match {
-                  case Some(info) =>
-                    val sortedNats = summariseNationalities(info.nationalities, 10)
-                      .toList
-                      .sortBy {
-                        case (_, pax) => pax
-                      }
+      <.div(^.id := "charts-box",
+        Tippy.interactiveInfo(
+          triggerCallback = Option((_: ReactEventFromInput) => {
+            Callback(SPACircuit.dispatch(GetPassengerInfoSummary(ArrivalKey(props.flightWithSplits.apiFlight))))
+          }),
+          content =
+            proxy { rcp =>
+              val infosPot = rcp()
+              <.div(^.cls := "container arrivals__table__flight__chart-box",
+                infosPot.render { infos =>
+                  infos.get(ArrivalKey(props.flightWithSplits.apiFlight)) match {
+                    case Some(info) =>
+                      val sortedNats = summariseNationalities(info.nationalities, 10)
+                        .toList
+                        .sortBy {
+                          case (_, pax) => pax
+                        }
 
-                    val nationalityData = ChartJsData(sortedNats.map(_._1.code), sortedNats.map(_._2.toDouble), "Live API")
+                      val nationalityData = ChartJsData(sortedNats.map(_._1.code), sortedNats.map(_._2.toDouble), "Live API")
 
-                    val sortedAges = info.ageRanges.toList.sortBy(_._1.title)
-                    val ageData: ChartJsData = ChartJsData(sortedAges.map(_._1.title), sortedAges.map(_._2.toDouble), "Live API")
+                      val sortedAges = info.ageRanges.toList.sortBy(_._1.title)
+                      val ageData: ChartJsData = ChartJsData(sortedAges.map(_._1.title), sortedAges.map(_._2.toDouble), "Live API")
 
-                    val sortedPaxTypes = info.paxTypes.toList.sortBy(_._1.cleanName)
+                      val sortedPaxTypes = info.paxTypes.toList.sortBy(_._1.cleanName)
 
-                    val paxTypeData: ChartJsData = ChartJsData(sortedPaxTypes.map {
-                      case (pt, _) => PaxTypes.displayNameShort(pt)
-                    }, sortedPaxTypes.map(_._2.toDouble), "Live API")
-                    <.div(^.cls := "row",
-                      if (sortedNats.toMap.values.sum > 0)
-                        <.div(^.cls := "col-sm arrivals__table__flight__chart-box__chart nationality-chart",
-                          ChartJSComponent.Bar(
-                            ChartJsProps(
-                              data = nationalityData,
-                              300,
-                              300,
-                              options = ChartJsOptions.withSuggestedMax("Nationality breakdown", sortedNats.toMap.values.max + 5)
-                            )
-                          ))
-                      else
-                        EmptyVdom,
-                      if (sortedPaxTypes.toMap.values.sum > 0 && sortedAges.toMap.values.sum > 0)
-                        <.div(^.cls := "col-sm arrivals__table__flight__chart-box__chart passenger-type-chart",
-                          ChartJSComponent.Bar(
-                            ChartJsProps(
-                              data = paxTypeData,
-                              300,
-                              300,
-                              options = ChartJsOptions.withSuggestedMax("Passenger types", sortedPaxTypes.toMap.values.max + 5)
-                            )))
-                      else
-                        EmptyVdom,
-                      if (sortedAges.toMap.values.sum > 0)
-                        <.div(^.cls := "col-sm arrivals__table__flight__chart-box__chart age-breakdown-chart",
-                          ChartJSComponent.Bar(
-                            ChartJsProps(
-                              data = ageData,
-                              300,
-                              300,
-                              options = ChartJsOptions.withSuggestedMax("Age breakdown", sortedAges.toMap.values.max + 5)
+                      val paxTypeData: ChartJsData = ChartJsData(sortedPaxTypes.map {
+                        case (pt, _) => PaxTypes.displayNameShort(pt)
+                      }, sortedPaxTypes.map(_._2.toDouble), "Live API")
+                      <.div(^.cls := "row",
+                        if (sortedNats.toMap.values.sum > 0)
+                          <.div(^.cls := "col-sm arrivals__table__flight__chart-box__chart nationality-chart",
+                            ChartJSComponent.Bar(
+                              ChartJsProps(
+                                data = nationalityData,
+                                300,
+                                300,
+                                options = ChartJsOptions.withSuggestedMax("Nationality breakdown", sortedNats.toMap.values.max + 5)
+                              )
                             ))
-                        )
-                      else
-                        EmptyVdom
-                    )
-                  case None => <.div(MuiCircularProgress()(), ^.height := "282px", ^.display := "flex", ^.alignItems := "center", ^.justifyContent := "center")
+                        else
+                          EmptyVdom,
+                        if (sortedPaxTypes.toMap.values.sum > 0 && sortedAges.toMap.values.sum > 0)
+                          <.div(^.cls := "col-sm arrivals__table__flight__chart-box__chart passenger-type-chart",
+                            ChartJSComponent.Bar(
+                              ChartJsProps(
+                                data = paxTypeData,
+                                300,
+                                300,
+                                options = ChartJsOptions.withSuggestedMax("Passenger types", sortedPaxTypes.toMap.values.max + 5)
+                              )))
+                        else
+                          EmptyVdom,
+                        if (sortedAges.toMap.values.sum > 0)
+                          <.div(^.cls := "col-sm arrivals__table__flight__chart-box__chart age-breakdown-chart",
+                            ChartJSComponent.Bar(
+                              ChartJsProps(
+                                data = ageData,
+                                300,
+                                300,
+                                options = ChartJsOptions.withSuggestedMax("Age breakdown", sortedAges.toMap.values.max + 5)
+                              ))
+                          )
+                        else
+                          EmptyVdom
+                      )
+                    case None => <.div(MuiCircularProgress()(), ^.height := "282px", ^.display := "flex", ^.alignItems := "center", ^.justifyContent := "center")
+                  }
                 }
-              }
-            )
-          }
-      )
+              )
+            }
+        ))
     }
     .build
 
--- e2e/tests/integration/api-splits.ts
+++ e2e/tests/integration/api-splits.ts
@@ -7,166 +7,167 @@ moment.locale("en-gb");
 
 describe('API splits', () => {
 
-    beforeEach(() => {
-        cy.deleteData();
-    });
+  beforeEach(() => {
+    cy.deleteData();
+  });
 
-    const scheduledTime = todayAtUtc(14, 55);
+  const scheduledTime = todayAtUtc(14, 55);
 
-    const manifest = (passengerList): object => manifestForDateTime(
-        scheduledTime,
-        passengerList
-    )
+  const manifest = (passengerList): object => manifestForDateTime(
+    scheduledTime,
+    passengerList
+  )
 
-    const ofPassengerProfile = (passengerProfile, qty): object[] => {
-        return Array(qty).fill(passengerProfile);
-    }
+  const ofPassengerProfile = (passengerProfile, qty): object[] => {
+    return Array(qty).fill(passengerProfile);
+  }
 
-    it('should have 8 egates pax and 2 EEA queue pax when there are 10 UK Adults on board a flight', () => {
-        const apiManifest = manifest(ofPassengerProfile(passengerProfiles.ukPassport, 10));
-        cy
-            .addFlight(
-                {
-                    "ActPax": 10,
-                    "SchDT": scheduledTime.format()
-                }
-            )
-            .asABorderForceOfficer()
-            .waitForFlightToAppear("TS0123")
-            .addManifest(apiManifest)
-            .get('.egate-queue-pax')
-            .get('span')
-            .contains("8")
-            .get('.eeadesk-queue-pax')
-            .get('span')
-            .contains("2");
+  it('should have 8 egates pax and 2 EEA queue pax when there are 10 UK Adults on board a flight', () => {
+    const apiManifest = manifest(ofPassengerProfile(passengerProfiles.ukPassport, 10));
+    cy
+      .addFlight(
+        {
+          "ActPax": 10,
+          "SchDT": scheduledTime.format()
+        }
+      )
+      .asABorderForceOfficer()
+      .waitForFlightToAppear("TS0123")
+      .addManifest(apiManifest)
+      .get('.egate-queue-pax')
+      .get('span')
+      .contains("8")
+      .get('.eeadesk-queue-pax')
+      .get('span')
+      .contains("2");
 
-    });
-
-
-    it('should ignore the API splits if they are more than 5% different in passenger numbers to the live feed and flight charts option not exist', () => {
-        const apiManifest = manifest(ofPassengerProfile(passengerProfiles.ukPassport, 12));
-
-        cy
-            .addFlight(
-                {
-                    "ActPax": 10,
-                    "SchDT": scheduledTime.format()
-                }
-            )
-            .asABorderForceOfficer()
-            .waitForFlightToAppear("TS0123")
-            .addManifest(apiManifest)
-            .get('.notApiData',{ timeout: 5000 })
-            .contains("10")
-            .get(".arrivals__table__flight-code__info > .tooltip-trigger")
-            .should('not.exist')
-        ;
-    });
+  });
 
 
-    it('should count multiple entries with the same PassengerIdentifier as one passenger', () => {
-        const apiManifest = manifest(
-            ofPassengerProfile(ukAdultWithId("1"), 3).concat(
-                ofPassengerProfile(ukAdultWithId("2"), 3)
-            )
-        );
+  it('should ignore the API splits if they are more than 5% different in passenger numbers to the live feed and flight charts option not exist', () => {
+    const apiManifest = manifest(ofPassengerProfile(passengerProfiles.ukPassport, 12));
 
-        const summaryWith2Pax = [
-            {
-                "arrivalKey": {
-                    "origin": {"iata": "AMS"},
-                    "voyageNumber": {"$type": "uk.gov.homeoffice.drt.arrivals.VoyageNumber", "numeric": 123},
-                    "scheduled": scheduledTime.unix() * 1000
-                },
-                "ageRanges": [["25-49", 2]],
-                "nationalities": [[{"code": "GBR"}, 2]],
-                "paxTypes": [["EeaMachineReadable", 2]]
-            }]
+    cy
+      .addFlight(
+        {
+          "ActPax": 10,
+          "SchDT": scheduledTime.format()
+        }
+      )
+      .asABorderForceOfficer()
+      .waitForFlightToAppear("TS0123")
+      .addManifest(apiManifest)
+      .get('.notApiData', {timeout: 5000})
+      .contains("10")
+      .get(".arrivals__table__flight-code__info > .tooltip-trigger")
+      .should('not.exist')
+    ;
+  });
 
-        cy
-            .addFlight(
-                {
-                    "ActPax": 2,
-                    "SchDT": scheduledTime.format()
-                }
-            )
-            .asABorderForceOfficer()
-            .waitForFlightToAppear("TS0123")
-            .addManifest(apiManifest)
-            .get('.pax-api')
-            .request({
-                method: 'GET',
-                url: "/manifest/" + scheduledTime.format("YYYY-MM-DD") + "/summary",
-            }).then((resp) => {
-            expect(resp.body).to.equal(JSON.stringify(summaryWith2Pax), "Api splits incorrect for regular users")
-        })
-        ;
 
-    });
+  it('should count multiple entries with the same PassengerIdentifier as one passenger', () => {
+    const apiManifest = manifest(
+      ofPassengerProfile(ukAdultWithId("1"), 3).concat(
+        ofPassengerProfile(ukAdultWithId("2"), 3)
+      )
+    );
 
-    it('should have 7 egates pax and 4 EEA queue pax when there are 10 UK Adults and 1 uk child on board a flight', () => {
-        const ukAdults = ofPassengerProfile(passengerProfiles.ukPassport, 10);
-        const ukChildren = ofPassengerProfile(passengerProfiles.ukChild, 1);
-        const apiManifest = manifest(ukAdults.concat(ukChildren));
+    const summaryWith2Pax = [
+      {
+        "arrivalKey": {
+          "origin": {"iata": "AMS"},
+          "voyageNumber": {"$type": "uk.gov.homeoffice.drt.arrivals.VoyageNumber", "numeric": 123},
+          "scheduled": scheduledTime.unix() * 1000
+        },
+        "ageRanges": [["25-49", 2]],
+        "nationalities": [[{"code": "GBR"}, 2]],
+        "paxTypes": [["EeaMachineReadable", 2]]
+      }]
 
-        const expectedNationalitySummary = [
-            {
-                "arrivalKey": {
-                    "origin": {"iata": "AMS"},
-                    "voyageNumber": {"$type": "uk.gov.homeoffice.drt.arrivals.VoyageNumber", "numeric": 123},
-                    "scheduled": scheduledTime.unix() * 1000
-                },
+    cy
+      .addFlight(
+        {
+          "ActPax": 2,
+          "SchDT": scheduledTime.format()
+        }
+      )
+      .asABorderForceOfficer()
+      .waitForFlightToAppear("TS0123")
+      .addManifest(apiManifest)
+      .get('.pax-api')
+      .request({
+        method: 'GET',
+        url: "/manifest/" + scheduledTime.format("YYYY-MM-DD") + "/summary",
+      }).then((resp) => {
+      expect(resp.body).to.equal(JSON.stringify(summaryWith2Pax), "Api splits incorrect for regular users")
+    })
+    ;
 
-                "ageRanges": [
-                    ["25-49", 10],
-                    ["0-11", 1]
-                ],
+  });
 
-                "nationalities": [
-                    [{"code": "GBR"}, 11]
-                ],
+  it('should have 7 egates pax and 4 EEA queue pax when there are 10 UK Adults and 1 uk child on board a flight', () => {
+    const ukAdults = ofPassengerProfile(passengerProfiles.ukPassport, 10);
+    const ukChildren = ofPassengerProfile(passengerProfiles.ukChild, 1);
+    const apiManifest = manifest(ukAdults.concat(ukChildren));
 
-                "paxTypes": [
-                    ["EeaMachineReadable", 10], ["EeaBelowEGateAge", 1]
-                ]
-            }
+    const expectedNationalitySummary = [
+      {
+        "arrivalKey": {
+          "origin": {"iata": "AMS"},
+          "voyageNumber": {"$type": "uk.gov.homeoffice.drt.arrivals.VoyageNumber", "numeric": 123},
+          "scheduled": scheduledTime.unix() * 1000
+        },
+
+        "ageRanges": [
+          ["25-49", 10],
+          ["0-11", 1]
+        ],
+
+        "nationalities": [
+          [{"code": "GBR"}, 11]
+        ],
+
+        "paxTypes": [
+          ["EeaMachineReadable", 10], ["EeaBelowEGateAge", 1]
         ]
+      }
+    ]
 
-        cy
-            .addFlight(
-                {
-                    "ActPax": 11,
-                    "SchDT": scheduledTime.format()
-                }
-            )
-            .asABorderForceOfficer()
-            .waitForFlightToAppear("TS0123")
-            .addManifest(apiManifest)
-            .get('.pax-api',{timeout: 5000})
-            .get('.egate-queue-pax')
-            .get('span')
-            .contains("7")
-            .get('.eeadesk-queue-pax')
-            .get('span')
-            .contains("3")
-            .request({
-                method: 'GET',
-                url: "/manifest/" + scheduledTime.format("YYYY-MM-DD") + "/summary",
-            }).then((resp) => {
-            expect(resp.body).to.equal(JSON.stringify(expectedNationalitySummary), "Api splits incorrect for regular users")
-        })
-            .get(".arrivals__table__flight-code__info > .tooltip-trigger")
-            .trigger("focus")
-            .get(".nationality-chart")
-            .should("be.visible")
-            .get(".passenger-type-chart")
-            .should("be.visible")
-            .get(".age-breakdown-chart")
-            .should("be.visible")
-        ;
+    cy
+      .addFlight(
+        {
+          "ActPax": 11,
+          "SchDT": scheduledTime.format()
+        }
+      )
+      .asABorderForceOfficer()
+      .waitForFlightToAppear("TS0123")
+      .addManifest(apiManifest)
+      .get('.pax-api', {timeout: 5000})
+      .get('.egate-queue-pax')
+      .get('span')
+      .contains("7")
+      .get('.eeadesk-queue-pax')
+      .get('span')
+      .contains("3")
+      .request({
+        method: 'GET',
+        url: "/manifest/" + scheduledTime.format("YYYY-MM-DD") + "/summary",
+      }).then((resp) => {
+      expect(resp.body).to.equal(JSON.stringify(expectedNationalitySummary), "Api splits incorrect for regular users")
+    })
+      .get("#charts-box > .tooltip-trigger")
+      .trigger("focus")
+      .click()
+      .get(".nationality-chart")
+      .should("be.visible")
+      .get(".passenger-type-chart")
+      .should("be.visible")
+      .get(".age-breakdown-chart")
+      .should("be.visible")
+    ;
 
-    });
+  });
 
 });
 
