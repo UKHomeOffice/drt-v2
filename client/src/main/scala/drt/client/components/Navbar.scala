1af8df2db31430579c3d1897c6777bd7f1661d83
Nilesh Gupta <nilesh.nemichandgupta@digital.homeoffice.gov.uk>
2022-03-18 14:46:18 +0000

    DRTII-977 update css for responsive pages on mobile

--- client/src/main/scala/drt/client/components/Navbar.scala
+++ client/src/main/scala/drt/client/components/Navbar.scala
@@ -10,44 +10,59 @@ import io.kinoplan.scalajs.react.material.ui.core.MuiSnackbar
 import io.kinoplan.scalajs.react.material.ui.core.internal.Origin
 import japgolly.scalajs.react.extra.router.{BaseUrl, RouterCtl}
 import japgolly.scalajs.react.vdom.html_<^._
-import japgolly.scalajs.react.{Callback, ReactEvent}
-import org.scalajs.dom.html
+import japgolly.scalajs.react.{Callback, ReactEvent, ScalaComponent}
 import uk.gov.homeoffice.drt.auth.LoggedInUser
 import uk.gov.homeoffice.drt.ports.AirportConfig
+import org.scalajs.dom
 
 case class NavbarModel(feedStatuses: Pot[Seq[FeedSourceStatuses]], snackbarMessage: Pot[String])
 
+
 object Navbar {
-  def apply(ctl: RouterCtl[Loc],
-            page: Loc,
-            loggedInUser: LoggedInUser,
-            airportConfig: AirportConfig): VdomTagOf[html.Element] = {
-    val rcp = SPACircuit.connect(m => NavbarModel(m.feedStatuses, m.snackbarMessage))
 
-    def handleClose: (ReactEvent, String) => Callback = (_, _) => {
-      Callback(SPACircuit.dispatch(SetSnackbarMessage(Empty)))
-    }
+  case class Props(ctl: RouterCtl[Loc],
+                   page: Loc,
+                   loggedInUser: LoggedInUser,
+                   airportConfig: AirportConfig)
 
-    <.nav(^.className := "navbar navbar-default",
-      rcp(navbarModelProxy => {
-        val navbarModel = navbarModelProxy()
-        <.div(^.className := "navbar-drt",
-          navbarModel.snackbarMessage.renderReady { message =>
-            MuiSnackbar(anchorOrigin = Origin(vertical = "top", horizontal = "right"),
-              autoHideDuration = 5000,
-              message = <.div(^.className := "muiSnackBar", message),
-              open = true,
-              onClose = handleClose)()
-          },
-          <.span(^.className := "navbar-brand", s"DRT ${airportConfig.portCode}"),
-          <.div(^.className := "collapse navbar-collapse", MainMenu(ctl, page, navbarModel.feedStatuses.getOrElse(Seq()), airportConfig, loggedInUser),
-            <.ul(^.className := "nav navbar-nav navbar-right",
-              <.li(^.className := "contact-us-link", ctl.link(ContactUsLoc)(Icon.envelope, " ", "Contact Us")),
-              <.li(<.a(Icon.signOut, "Log Out", ^.href := "/oauth/logout?redirect=" + BaseUrl.until_#.value,
-                ^.onClick --> Callback(GoogleEventTracker.sendEvent(airportConfig.portCode.toString, "Log Out", loggedInUser.id))))
-            ))
-        )
-      }
-      ))
+  case class State(showDropDown: Boolean)
+
+  def handleClose: (ReactEvent, String) => Callback = (_, _) => {
+    Callback(SPACircuit.dispatch(SetSnackbarMessage(Empty)))
   }
+
+
+  val component = ScalaComponent.builder[Props]("NavBar")
+    .initialState(if (dom.window.innerWidth > 800) State(true) else State(false))
+    .renderPS((scope, props, state) => {
+      val rcp = SPACircuit.connect(m => NavbarModel(m.feedStatuses, m.snackbarMessage))
+      <.nav(^.className := "navbar navbar-default",
+        rcp(navbarModelProxy => {
+          val navbarModel: NavbarModel = navbarModelProxy()
+          <.div(^.className := "navbar-drt",
+            navbarModel.snackbarMessage.renderReady { message =>
+              MuiSnackbar(anchorOrigin = Origin(vertical = "top", horizontal = "right"),
+                autoHideDuration = 5000,
+                message = <.div(^.className := "muiSnackBar", message),
+                open = true,
+                onClose = handleClose)()
+            },
+            <.span(^.className := "navbar-brand",
+              <.a("", " ", s"DRT ${props.airportConfig.portCode}"),
+              ^.onClick --> scope.modState(_.copy(showDropDown = !state.showDropDown))),
+            <.div(^.className := "navbar-collapse",
+              if (state.showDropDown)
+                MainMenu(props.ctl, props.page, navbarModel.feedStatuses.getOrElse(Seq()), props.airportConfig, props.loggedInUser) else "",
+              <.ul(^.className := "nav navbar-nav navbar-right",
+                <.li(^.className := "contact-us-link", props.ctl.link(ContactUsLoc)(Icon.envelope, " ", "Contact Us")),
+                <.li(<.a(Icon.signOut, "Log Out", ^.href := "/oauth/logout?redirect=" + BaseUrl.until_#.value,
+                  ^.onClick --> Callback(GoogleEventTracker.sendEvent(props.airportConfig.portCode.toString, "Log Out", props.loggedInUser.id))))
+              ))
+          )
+        }
+        ))
+    }).build
+
+  def apply(props: Props) = component(props)
+
 }
