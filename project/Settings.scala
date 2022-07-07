686801b5806ac35d62c629eef8b2b001f1f98ded
Nilesh Gupta <nilesh.nemichandgupta@digital.homeoffice.gov.uk>
2022-07-07 15:43:15 +0100

    DRTII-988 update drt lib for fix test

--- project/Settings.scala
+++ project/Settings.scala
@@ -70,7 +70,7 @@ object Settings {
     val openSaml = "2.6.1"
     val drtBirminghamSchema = "1.2.0"
     val drtCirium = "186"
-    val drtLib = "v274"
+    val drtLib = "v276"
     val playJson = "2.6.0"
     val playIteratees = "2.6.1"
     val uPickle = "2.0.0"
