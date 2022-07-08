111b3e448199f3f4a367d359f612aa8bd6d641fd
Rich Birch <richbirch@users.noreply.github.com>
2022-07-08 10:10:44 +0100

    Latest drt-lib master build

--- project/Settings.scala
+++ project/Settings.scala
@@ -70,7 +70,7 @@ object Settings {
     val openSaml = "2.6.1"
     val drtBirminghamSchema = "1.2.0"
     val drtCirium = "186"
-    val drtLib = "v275"
+    val drtLib = "v277"
     val playJson = "2.6.0"
     val playIteratees = "2.6.1"
     val uPickle = "2.0.0"
