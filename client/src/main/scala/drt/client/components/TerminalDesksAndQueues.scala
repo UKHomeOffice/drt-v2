761d5b590f7ba2639ec9792816d92a9b3b09cb79
Rich Birch <ringo@albumsnaps.com>
2022-08-22 11:12:13 +0100

    DRTII-632 Refactor into chart component

--- client/src/main/scala/drt/client/components/TerminalDesksAndQueues.scala
+++ client/src/main/scala/drt/client/components/TerminalDesksAndQueues.scala
@@ -11,15 +11,16 @@ import drt.client.services.JSDateConversions.SDate
 import drt.client.services.ViewMode
 import drt.shared.CrunchApi.StaffMinute
 import drt.shared._
+import japgolly.scalajs.react.component.Scala.{Component, Unmounted}
 import japgolly.scalajs.react.extra.router.RouterCtl
 import japgolly.scalajs.react.vdom.html_<^._
-import japgolly.scalajs.react.{BackendScope, Callback, ReactEventFromInput, ScalaComponent}
+import japgolly.scalajs.react.{BackendScope, Callback, CtorType, ReactEventFromInput, ScalaComponent}
 import org.scalajs.dom
 import org.scalajs.dom.html.{Div, TableCell}
 import org.scalajs.dom.raw.Node
 import org.scalajs.dom.{DOMList, Element, Event, NodeListOf}
 import uk.gov.homeoffice.drt.auth.LoggedInUser
-import uk.gov.homeoffice.drt.ports.Queues.{EGate, EeaDesk, Queue, Transfer}
+import uk.gov.homeoffice.drt.ports.Queues.{EGate, Queue, Transfer}
 import uk.gov.homeoffice.drt.ports.{AirportConfig, Queues}
 import uk.gov.homeoffice.drt.time.SDateLike
 
@@ -208,73 +209,14 @@ object TerminalDesksAndQueues {
         }
         .mapValues(_.map(_._2).max)
 
-      def queueGraphic(queue: Queue) = {
-        val labels: Seq[String] = (0 until 96).map(m => SDate("2022-08-17T23:00").addMinutes(m * 15).toHoursAndMinutes)
-        val dayStart = SDate(props.viewStart.getLocalLastMidnight.millisSinceEpoch)
-        val sortedCrunchMinuteSummaries = props.portState.crunchSummary(dayStart, 96, 15, terminal, queues).toList.sortBy(_._1)
-        val paxInQueueSet: ChartJsDataSet = ChartJsDataSet.line(
-          label = "Pax in queue",
-          data = sortedCrunchMinuteSummaries.map {
-            case (_, queuesAndMinutes) => queuesAndMinutes(queue).maybePaxInQueue.getOrElse(0).toDouble
-          },
-          colour = RGBA.blue1,
-          backgroundColour = Option(RGBA.blue1.copy(alpha = 0.2)),
-          pointRadius = Option(0),
-          yAxisID = Option("y"),
-          fill = Option(true),
-        )
-        val incomingPax: ChartJsDataSet = ChartJsDataSet.line(
-          label = "Incoming pax",
-          data = sortedCrunchMinuteSummaries.map {
-            case (_, queuesAndMinutes) => queuesAndMinutes(queue).paxLoad
-          },
-          colour = RGBA.blue2.copy(alpha = 0.4),
-          pointRadius = Option(0),
-          yAxisID = Option("y"),
-        )
-        val desks: ChartJsDataSet = ChartJsDataSet.bar(
-          label = "Staff",
-          data = sortedCrunchMinuteSummaries.map {
-            case (_, queuesAndMinutes) => queuesAndMinutes(queue).deskRec.toDouble
-          },
-          colour = RGBA(200, 200, 200),
-          backgroundColour = Option(RGBA(200, 200, 200, 0.2)),
-          yAxisID = Option("y2"),
-        )
-        val waits: ChartJsDataSet = ChartJsDataSet.line(
-          label = "Wait times",
-          data = sortedCrunchMinuteSummaries.map {
-            case (_, queuesAndMinutes) => queuesAndMinutes(queue).waitTime.toDouble
-          },
-          colour = RGBA.red1,
-          backgroundColour = Option(RGBA.red1.copy(alpha = 0.2)),
-          pointRadius = Option(0),
-          yAxisID = Option("y3"),
-          fill = Option(true),
-        )
-        val sla: ChartJsDataSet = ChartJsDataSet.line(
-          label = "SLA",
-          data = Seq.fill(96)(props.airportConfig.slaByQueue(queue).toDouble),
-          colour = RGBA.green3,
-          pointRadius = Option(0),
-          yAxisID = Option("y3"),
-        )
-        ChartJSComponent(
-          ChartJsProps(
-            data = ChartJsData(
-              datasets = Seq(paxInQueueSet, incomingPax, desks, waits, sla),
-              labels = Option(labels),
-            ),
-            width = 300,
-            height = 25,
-            options = ChartJsOptions.withMultipleDataSets(queue.toString, suggestedMax = Map("y3" -> props.airportConfig.slaByQueue(queue) * 2), maxTicks = 96)
-          )
-        )
-      }
-
       <.div(
         floatingHeader(state.showWaitColumn),
-        props.airportConfig.queuesByTerminal(props.terminalPageTab.terminal).filterNot(_ == Transfer).map(queueGraphic).toTagMod,
+        props.airportConfig.queuesByTerminal(props.terminalPageTab.terminal).filterNot(_ == Transfer).map { queue =>
+          val dayStart = SDate(props.viewStart.getLocalLastMidnight.millisSinceEpoch)
+          val sortedCrunchMinuteSummaries: List[(Long, Map[Queue, CrunchApi.CrunchMinute])] = props.portState.crunchSummary(dayStart, 96, 15, terminal, queues).toList.sortBy(_._1)
+          val queueSla = props.airportConfig.slaByQueue(queue)
+          QueueChartComponent(QueueChartComponent.Props(queue, sortedCrunchMinuteSummaries, queueSla))
+        }.toTagMod,
         <.div(^.className := "desks-and-queues-top",
           viewTypeControls(props.featureFlags.displayWaitTimesToggle),
           StaffMissingWarningComponent(terminalStaffMinutes, props.loggedInUser, props.router, props.terminalPageTab)
@@ -312,7 +254,7 @@ object TerminalDesksAndQueues {
 
   }
 
-  val component = ScalaComponent.builder[Props]("Loader")
+  val component: Component[Props, State, Backend, CtorType.Props] = ScalaComponent.builder[Props]("Loader")
     .initialStateFromProps(p => State(showActuals = p.airportConfig.hasActualDeskStats && p.showActuals, p.terminalPageTab.viewType, showWaitColumn = !p.featureFlags.displayWaitTimesToggle))
     .renderBackend[Backend]
     .componentDidMount(_ => StickyTableHeader("[data-sticky]"))
@@ -335,6 +277,76 @@ object TerminalDesksAndQueues {
 
     override def apply(idx: Int): T = nodes(idx)
   }
+}
+
+object QueueChartComponent {
+  case class Props(queue: Queue, queueSummaries: List[(Long, Map[Queue, CrunchApi.CrunchMinute])], sla: Int)
+
+  val component: Component[Props, Unit, Unit, CtorType.Props] = ScalaComponent.builder[Props]("QueueChart")
+    .render_P { props =>
+      val labels: Seq[String] = (0 until 96).map(m => SDate("2022-08-17T23:00").addMinutes(m * 15).toHoursAndMinutes)
+      val paxInQueueSet: ChartJsDataSet = ChartJsDataSet.line(
+        label = "Pax in queue",
+        data = props.queueSummaries.map {
+          case (_, queuesAndMinutes) => queuesAndMinutes(props.queue).maybePaxInQueue.getOrElse(0).toDouble
+        },
+        colour = RGBA.blue1,
+        backgroundColour = Option(RGBA.blue1.copy(alpha = 0.2)),
+        pointRadius = Option(0),
+        yAxisID = Option("y"),
+        fill = Option(true),
+      )
+      val incomingPax: ChartJsDataSet = ChartJsDataSet.line(
+        label = "Incoming pax",
+        data = props.queueSummaries.map {
+          case (_, queuesAndMinutes) => queuesAndMinutes(props.queue).paxLoad
+        },
+        colour = RGBA.blue2.copy(alpha = 0.4),
+        pointRadius = Option(0),
+        yAxisID = Option("y"),
+      )
+      val desks: ChartJsDataSet = ChartJsDataSet.bar(
+        label = "Staff",
+        data = props.queueSummaries.map {
+          case (_, queuesAndMinutes) => queuesAndMinutes(props.queue).deskRec.toDouble
+        },
+        colour = RGBA(200, 200, 200),
+        backgroundColour = Option(RGBA(200, 200, 200, 0.2)),
+        yAxisID = Option("y2"),
+      )
+      val waits: ChartJsDataSet = ChartJsDataSet.line(
+        label = "Wait times",
+        data = props.queueSummaries.map {
+          case (_, queuesAndMinutes) => queuesAndMinutes(props.queue).waitTime.toDouble
+        },
+        colour = RGBA.red1,
+        backgroundColour = Option(RGBA.red1.copy(alpha = 0.2)),
+        pointRadius = Option(0),
+        yAxisID = Option("y3"),
+        fill = Option(true),
+      )
+      val slaDataSet: ChartJsDataSet = ChartJsDataSet.line(
+        label = "SLA",
+        data = Seq.fill(96)(props.sla.toDouble),
+        colour = RGBA.green3,
+        pointRadius = Option(0),
+        yAxisID = Option("y3"),
+      )
+      ChartJSComponent(
+        ChartJsProps(
+          data = ChartJsData(
+            datasets = Seq(paxInQueueSet, incomingPax, desks, waits, slaDataSet),
+            labels = Option(labels),
+          ),
+          width = 300,
+          height = 25,
+          options = ChartJsOptions.withMultipleDataSets(props.queue.toString, suggestedMax = Map("y3" -> props.sla * 2), maxTicks = 96)
+        )
+      )
+    }
+    .build
+
+  def apply(props: Props): Unmounted[Props, Unit, Unit] = component(props)
 
 }
 
