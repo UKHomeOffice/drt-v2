e515889cf0f6074aea7f69a21b5a1370a6ed595a
Rich Birch <ringo@albumsnaps.com>
2022-09-01 08:38:50 +0100

    DRTII-632 Finish test & move class to prod code

--- server/src/test/scala/actors/routing/ResourceControlActorSpec.scala
+++ server/src/test/scala/actors/routing/ResourceControlActorSpec.scala
@@ -1,92 +1,79 @@
 package actors.routing
 
-import actors.routing.ResourceControlActor.{ProcessNextResourceRequest, RequestFinished}
-import akka.actor.{Actor, ActorRef, Props}
+import akka.actor.{ActorRef, Props}
+import akka.pattern.ask
 import akka.testkit.TestProbe
 import services.crunch.CrunchTestLike
 
-import scala.concurrent.{ExecutionContextExecutor, Future}
-
-object ResourceControlActor {
-  case class ProcessNextResourceRequest[A](resource: A)
-
-  case class RequestFinished[A](resource: A)
-}
-
-class ResourceControlActor[RES, REQ, RESP](useResource: (RES, REQ) => Future[REQ]) extends Actor {
-  var inUse: List[RES] = List()
-  var requestQueue: Map[RES, List[(ActorRef, REQ)]] = Map()
-
-  implicit val ec: ExecutionContextExecutor = context.dispatcher
-
-  override def receive: Receive = {
-    case (resource: RES, request: REQ) =>
-      queueRequest(sender(), resource, request)
-      self ! ProcessNextResourceRequest(resource)
-
-    case ProcessNextResourceRequest(resource: RES) =>
-      if (!inUse.contains(resource)) {
-        requestQueue.get(resource).foreach {
-          case (replyTo, next) :: remainingRequests =>
-            inUse = resource :: inUse
-            requestQueue = requestQueue.updated(resource, remainingRequests)
-            useResource(resource, next).map { response =>
-              replyTo ! response
-              self ! RequestFinished(resource)
-            }
-          case Nil =>
-        }
-      }
-
-    case RequestFinished(resource: RES) =>
-      inUse = inUse.filterNot(_ == resource)
-      self ! ProcessNextResourceRequest(resource)
-  }
-
-  def queueRequest(sender: ActorRef, resource: RES, request: REQ): Unit =
-    requestQueue = requestQueue.get(resource) match {
-      case Some(list) =>
-        requestQueue.updated(resource, list :+ (sender, request))
-      case None =>
-        requestQueue.updated(resource, List((sender, request)))
-    }
-}
+import scala.concurrent.duration.DurationInt
+import scala.concurrent.{Await, Future}
 
 class ResourceControlActorSpec extends CrunchTestLike {
+  def callResource(probeRefs: Map[String, ActorRef]): (String, String) => Future[String] = (resource: String, request: String) => {
+    probeRefs.get(resource).foreach(_ ! ((resource, request)))
+    Future({
+      Thread.sleep(100)
+      probeRefs.get(resource).foreach(_ ! s"$request done")
+      resource + request
+    })
+  }
+
   "A control actor should send a request" >> {
     val probe = TestProbe()
-    val callResource: (String, String) => Future[String] = (resource: String, request: String) => {
-      probe.ref ! (resource, request)
-      Future.successful(resource + request)
-    }
-    val actor = system.actorOf(Props(new ResourceControlActor[String, String, String](callResource)))
+    val actor = system.actorOf(Props(new ResourceControlActor[String, String, String](callResource(Map("A" -> probe.ref)))))
 
-    actor ! ("A", "A1")
+    actor ! (("A", "A1"))
 
     probe.expectMsg(("A", "A1"))
+    probe.expectMsg("A1 done")
 
     success
   }
 
   "A control actor should send a request to the same resource sequentially" >> {
     val probe = TestProbe()
-    val callResource: (String, String) => Future[String] = (resource: String, request: String) => {
-      probe.ref ! (resource, request)
-      Future({
-        Thread.sleep(100)
-        probe.ref ! s"$request done"
-        resource + request
-      })
-    }
-    val actor = system.actorOf(Props(new ResourceControlActor[String, String, String](callResource)))
+    val actor = system.actorOf(Props(new ResourceControlActor[String, String, String](callResource(Map("A" -> probe.ref)))))
 
-    actor ! ("A", "A1")
-    actor ! ("A", "A2")
+    actor ! (("A", "A1"))
+    actor ! (("A", "A2"))
 
     probe.expectMsg(("A", "A1"))
     probe.expectMsg("A1 done")
     probe.expectMsg(("A", "A2"))
     probe.expectMsg("A2 done")
+
+    success
+  }
+
+  "A control actor should send a request to the same resource sequentially when there are multiple resources" >> {
+    val probeA = TestProbe()
+    val probeB = TestProbe()
+    val actor = system.actorOf(Props(new ResourceControlActor[String, String, String](
+      callResource(Map("A" -> probeA.ref, "B" -> probeB.ref))
+    )))
+
+    actor ! (("A", "A1"))
+    actor ! (("B", "B1"))
+    actor ! (("A", "A2"))
+    actor ! (("B", "B2"))
+
+    probeA.expectMsg(("A", "A1"))
+    probeA.expectMsg("A1 done")
+    probeB.expectMsg(("B", "B1"))
+    probeB.expectMsg("B1 done")
+
+    probeA.expectMsg(("A", "A2"))
+    probeA.expectMsg("A2 done")
+    probeB.expectMsg(("B", "B2"))
+    probeB.expectMsg("B2 done")
+
+    success
+  }
+
+  "A control actor should send the response back to caller" >> {
+    val actor = system.actorOf(Props(new ResourceControlActor[String, String, String](callResource(Map()))))
+
+    Await.result(actor.ask(("A", "A1")), 1.second)
 
     success
   }
