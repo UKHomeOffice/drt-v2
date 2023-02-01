a1281b9109d3df489730a3a8fe8ab54a1a288d9f
Rich Birch <ringo@albumsnaps.com>
2023-02-01 08:52:14 +0000

    DRTII-576 Add bhx wsdl to resources

--- server/src/main/scala/drt/server/feeds/legacy/bhx/BHXFeedConfig.scala
+++ server/src/main/scala/drt/server/feeds/legacy/bhx/BHXFeedConfig.scala
@@ -23,7 +23,7 @@ trait BHXFeedConfig {
         case binder: BindingProvider =>
           binder.getRequestContext.put("javax.xml.ws.client.connectionTimeout", connectionTimeout.toString)
           binder.getRequestContext.put("javax.xml.ws.client.receiveTimeout", receiveTimeout.toString)
-          if (!endPointUrl.isEmpty)
+          if (endPointUrl.nonEmpty)
             binder.getRequestContext.put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, endPointUrl)
           binder
         case flightInformationSoap => flightInformationSoap
