b147407b32dd3e74743795a649269cc60abad16b
Nilesh Gupta <nilesh.nemichandgupta@digital.homeoffice.gov.uk>
2023-02-02 10:00:49 +0000

    DRTII-1132 csrf token filter

--- e2e/cypress/e2e/monthly-staffing.cy.ts
+++ e2e/cypress/e2e/monthly-staffing.cy.ts
@@ -2,89 +2,137 @@ import {moment} from '../support/time-helpers'
 
 describe('Monthly Staffing', () => {
 
-  beforeEach(() => {
-    cy.deleteData();
-  });
+    beforeEach(() => {
+        cy.deleteData();
+    });
 
-  const firstMidnightOfThisMonth = (): moment.Moment => {
-    return moment().tz('Europe/London').startOf('month');
-  }
-
-  const firstMidnightOfNextMonth = (): moment.Moment => {
-    return firstMidnightOfThisMonth().add(1, 'M');
-  }
-
-  const midDayToday = (): moment.Moment => {
-    return moment().tz('Europe/London').startOf('day').hour(12).minute(0);
-  }
-
-  const shifts = (): object => {
-    return {
-      "shifts": [
-        { "port_code": "test", "terminal": "T1", "staff": "1", "shift_start": firstMidnightOfThisMonth().toISOString() },
-        { "port_code": "test", "terminal": "T1", "staff": "2", "shift_start": firstMidnightOfNextMonth().toISOString() }
-      ]
+    const firstMidnightOfThisMonth = (): moment.Moment => {
+        return moment().tz('Europe/London').startOf('month');
     }
-  }
 
-  Cypress.Commands.add('addShiftForToday', () => {
-    cy
-      .asABorderForcePlanningOfficer()
-      .request("POST", "/data/staff", {
-        "shifts": [
-          { "port_code": "test", "terminal": "T1", "staff": "1", "shift_start": midDayToday().toISOString() }
-        ]
-      });
-  });
+    const firstMidnightOfNextMonth = (): moment.Moment => {
+        return firstMidnightOfThisMonth().add(1, 'M');
+    }
 
-  Cypress.Commands.add('resetShifts', () => {
-    cy
-      .asABorderForcePlanningOfficer()
-      .request("POST", "/data/staff", {
-        "shifts": [
-          { "port_code": "test", "terminal": "T1", "staff": "0", "shift_start": firstMidnightOfThisMonth().toISOString() },
-          { "port_code": "test", "terminal": "T1", "staff": "0", "shift_start": firstMidnightOfNextMonth().toISOString() },
-          { "port_code": "test", "terminal": "T1", "staff": "0", "shift_start": midDayToday().toISOString() }
-        ]
-      });
-  });
+    const midDayToday = (): moment.Moment => {
+        return moment().tz('Europe/London').startOf('day').hour(12).minute(0);
+    }
 
-  const thisMonthDateString = (): string => {
-    return moment().toISOString().split("T")[0];
-  }
+    const shifts = (): object => {
+        return {
+            "shifts": [
+                {
+                    "port_code": "test",
+                    "terminal": "T1",
+                    "staff": "1",
+                    "shift_start": firstMidnightOfThisMonth().toISOString()
+                },
+                {
+                    "port_code": "test",
+                    "terminal": "T1",
+                    "staff": "2",
+                    "shift_start": firstMidnightOfNextMonth().toISOString()
+                }
+            ]
+        }
+    }
 
-  const nextMonthDateString = (): string => {
-    return moment().add(1, 'M').toISOString().split("T")[0];
-  }
-
-  describe('No staff entered warning compontent', () => {
-    // const cellToTest = ".htCore tbody :nth-child(1) :nth-child(2)";
-    it("should display a warning when there are no staff entered for current period and hide it when there are.", () => {
-      cy
-        .asABorderForcePlanningOfficer()
-        .visit('#terminal/T1/current/desksAndQueues/?timeRangeStart=0&timeRangeEnd=24')
-        .get('.staff-alert')
-        .contains("You have not entered any staff ")
-        .addShiftForToday()
-        .visit('#terminal/T1/current/desksAndQueues/?timeRangeStart=0&timeRangeEnd=24')
-        .get('.staff-alert').should('not.exist')
-        .resetShifts();
+    Cypress.Commands.add('addShiftForToday', (csrfParam) => {
+        cy
+            .request({
+                method: "POST",
+                url: "/data/staff",
+                body: {
+                    "shifts": [
+                        {
+                            "port_code": "test",
+                            "terminal": "T1",
+                            "staff": "1",
+                            "shift_start": midDayToday().toISOString()
+                        }
+                    ]
+                },
+                headers: {'Csrf-Token': csrfParam}
+            });
     });
-  });
 
-  describe('When adding staff using the monthly staff view', () => {
-    const cellToTest = ".htCore tbody :nth-child(1) :nth-child(2)";
-    it("If I enter staff for the current month those staff should still be visible if I change months and change back", () => {
-      cy
-        .asABorderForcePlanningOfficer()
-        .saveShifts(shifts())
-        .visit('#terminal/T1/staffing/15/')
-        .get(cellToTest).contains("1")
-        .visit('#terminal/T1/staffing/15/?date=' + nextMonthDateString())
-        .get(cellToTest).contains("2")
-        .visit('#terminal/T1/staffing/15/?date=' + thisMonthDateString())
-        .get(cellToTest).contains("1")
-        .resetShifts();
+    Cypress.Commands.add('resetShifts', (csrfToken) => {
+        cy.request({
+            method: "POST",
+            url: "/data/staff",
+            body: {
+                "shifts": [
+                    {
+                        "port_code": "test",
+                        "terminal": "T1",
+                        "staff": "0",
+                        "shift_start": firstMidnightOfThisMonth().toISOString()
+                    },
+                    {
+                        "port_code": "test",
+                        "terminal": "T1",
+                        "staff": "0",
+                        "shift_start": firstMidnightOfNextMonth().toISOString()
+                    },
+                    {
+                        "port_code": "test",
+                        "terminal": "T1",
+                        "staff": "0",
+                        "shift_start": midDayToday().toISOString()
+                    }
+                ]
+            },
+            headers: {'Csrf-Token': csrfToken}
+        });
     });
-  });
+
+    const thisMonthDateString = (): string => {
+        return moment().toISOString().split("T")[0];
+    }
+
+    const nextMonthDateString = (): string => {
+        return moment().add(1, 'M').toISOString().split("T")[0];
+    }
+
+    describe('No staff entered warning compontent', () => {
+        // const cellToTest = ".htCore tbody :nth-child(1) :nth-child(2)";
+        it("should display a warning when there are no staff entered for current period and hide it when there are.", () => {
+            cy.asABorderForcePlanningOfficer()
+                .request({
+                    method: 'GET',
+                    url: '/',
+                }).then((response) => {
+                const $html = Cypress.$(response.body)
+                const csrf = $html.filter('input:hidden[name="csrfToken"]').val()
+                cy.visit('#terminal/T1/current/desksAndQueues/?timeRangeStart=0&timeRangeEnd=24')
+                    .get('.staff-alert')
+                    .contains("You have not entered any staff ")
+                cy.addShiftForToday(csrf)
+                    .visit('#terminal/T1/current/desksAndQueues/?timeRangeStart=0&timeRangeEnd=24')
+                    .get('.staff-alert').should('not.exist')
+                    .resetShifts(csrf);
+            });
+        });
+
+        describe('When adding staff using the monthly staff view', () => {
+            const cellToTest = ".htCore tbody :nth-child(1) :nth-child(2)";
+            it("If I enter staff for the current month those staff should still be visible if I change months and change back", () => {
+                cy
+                    .asABorderForcePlanningOfficer()
+                    .request('/')
+                    .then((response) => {
+                        const $html = Cypress.$(response.body)
+                        const csrf = $html.filter('input:hidden[name="csrfToken"]').val()
+                        cy.saveShifts(shifts(), csrf)
+                            .visit('#terminal/T1/staffing/15/')
+                            .get(cellToTest).contains("1")
+                            .visit('#terminal/T1/staffing/15/?date=' + nextMonthDateString())
+                            .get(cellToTest).contains("2")
+                            .visit('#terminal/T1/staffing/15/?date=' + thisMonthDateString())
+                            .get(cellToTest).contains("1")
+                            .resetShifts(csrf);
+                    });
+            });
+        });
+    });
 });
--- e2e/cypress/e2e/staff-movements.cy.ts
+++ e2e/cypress/e2e/staff-movements.cy.ts
@@ -2,219 +2,246 @@ import {moment} from '../support/time-helpers'
 
 describe('Staff movements', () => {
 
-  beforeEach(function () {
-    cy.deleteData()
-      .addFlight({});
-  });
-
-  const midnightThisMorning = (): moment.Moment => {
-    return moment().tz('Europe/London').startOf('day');
-  }
-
-  const shifts = (numberOfStaff): object => {
-    return {
-      "shifts": [
-        { "port_code": "test", "terminal": "T1", "staff": String(numberOfStaff), "shift_start": midnightThisMorning().toISOString() },
-        { "port_code": "test", "terminal": "T1", "staff": String(numberOfStaff), "shift_start": midnightThisMorning().add('minute', 15).toISOString() },
-        { "port_code": "test", "terminal": "T1", "staff": String(numberOfStaff), "shift_start": midnightThisMorning().add('minute', 30).toISOString() },
-        { "port_code": "test", "terminal": "T1", "staff": String(numberOfStaff), "shift_start": midnightThisMorning().add('minute', 45).toISOString() }
-      ]
-    };
-  }
-
-  describe('When adding staff movements on the desks and queues page', () => {
-    it("Should update the available staff when 1 staff member is added for 1 hour, and record the correct reason and " +
-      "it should appear in the export", () => {
-        const movementsCSV = "Terminal,Reason,Time,Staff Change,Made by" + "\n" +
-          "T1,Other start," + midnightThisMorning().format("YYYY-MM-DD") + " 00:00,1,\"Unknown\"" + "\n" +
-          "T1,Other end," + midnightThisMorning().format("YYYY-MM-DD") + " 01:00,-1,\"Unknown\""
-        cy
-          .asABorderForcePlanningOfficer()
-          .navigateHome()
-          .navigateToMenuItem('T1')
-          .selectCurrentTab()
-          .choose24Hours()
-          .openAdjustmentDialogueForHour('add', 0)
-          .adjustMinutes(60)
-          .adjustStaffBy(1)
-          .checkStaffMovementsOnDesksAndQueuesTabAre(1)
-          .checkStaffAvailableOnDesksAndQueuesTabAre(1)
-          .findAndClick('Staff Movements')
-          .checkStaffNumbersOnMovementsTabAre(1)
-          .checkUserNameOnMovementsTab(1, "Unknown")
-          .get('#export-day-staff-movements')
-          .then((el) => {
-            const href = el.prop('href')
-            cy.request({
-              method: 'GET',
-              url: href,
-            }).then((resp) => {
-              expect(resp.body).to.equal(movementsCSV, "Staff movements Export CSV is wrong.");
-            })
-          })
-          .removeXMovements(1);
-      });
-
-    it("Should update the available staff when 1 staff member is removed for 1 hour", () => {
-      cy
-        .asABorderForcePlanningOfficer()
-        .saveShifts(shifts(2))
-        .navigateHome()
-        .navigateToMenuItem('T1')
-        .selectCurrentTab()
-        .choose24Hours()
-        .openAdjustmentDialogueForHour('remove', 0)
-        .adjustMinutes(60)
-        .adjustStaffBy(1)
-        .checkStaffMovementsOnDesksAndQueuesTabAre(-1)
-        .checkStaffAvailableOnDesksAndQueuesTabAre(1)
-        .findAndClick('Staff Movements')
-        .checkStaffNumbersOnMovementsTabAre(1)
-        .checkUserNameOnMovementsTab(1, "Unknown")
-        .removeXMovements(1);
+    beforeEach(function () {
+        cy.deleteData()
+            .addFlight({});
     });
 
-    it("Should update the available staff when 2 staff members are added for 1 hour, and record the reason", () => {
-      cy
-        .asABorderForceOfficer()
-        .navigateHome()
-        .navigateToMenuItem('T1')
-        .selectCurrentTab()
-        .choose24Hours()
-        .openAdjustmentDialogueForHour('add', 0)
-        .selectReason('Case working')
-        .selectAdditionalReason('extra case work')
-        .adjustMinutes(60)
-        .adjustStaffBy(2)
-        .checkStaffMovementsOnDesksAndQueuesTabAre(2)
-        .checkStaffAvailableOnDesksAndQueuesTabAre(2)
-        .findAndClick('Staff Movements')
-        .checkStaffNumbersOnMovementsTabAre(2)
-        .checkUserNameOnMovementsTab(1, "Unknown")
-        .checkReasonOnMovementsTab('Case working: extra case work')
-        .removeXMovements(1);
-    });
+    const midnightThisMorning = (): moment.Moment => {
+        return moment().tz('Europe/London').startOf('day');
+    }
 
-    it("BorderForce user should be able to adjust the staff movement", () => {
-      cy
-        .asABorderForceOfficer()
-        .navigateHome()
-        .navigateToMenuItem('T1')
-        .selectCurrentTab()
-        .choose24Hours()
-        .get('.staff-deployment-adjustment-container').should('exist')
-    });
+    const shifts = (numberOfStaff): object => {
+        return {
+            "shifts": [
+                {
+                    "port_code": "test",
+                    "terminal": "T1",
+                    "staff": String(numberOfStaff),
+                    "shift_start": midnightThisMorning().toISOString()
+                },
+                {
+                    "port_code": "test",
+                    "terminal": "T1",
+                    "staff": String(numberOfStaff),
+                    "shift_start": midnightThisMorning().add('minute', 15).toISOString()
+                },
+                {
+                    "port_code": "test",
+                    "terminal": "T1",
+                    "staff": String(numberOfStaff),
+                    "shift_start": midnightThisMorning().add('minute', 30).toISOString()
+                },
+                {
+                    "port_code": "test",
+                    "terminal": "T1",
+                    "staff": String(numberOfStaff),
+                    "shift_start": midnightThisMorning().add('minute', 45).toISOString()
+                }
+            ]
+        };
+    }
 
-    it("BorderForceReadOnly should not be able to adjust the staff movement", () => {
-      cy
-        .asABorderForceReadOnlyOfficer()
-        .navigateHome()
-        .navigateToMenuItem('T1')
-        .selectCurrentTab()
-        .choose24Hours()
-        .get('.staff-deployment-adjustment-container').should('not.exist')
+    describe('When adding staff movements on the desks and queues page', () => {
+        it("Should update the available staff when 1 staff member is added for 1 hour, and record the correct reason and " +
+            "it should appear in the export", () => {
+            const movementsCSV = "Terminal,Reason,Time,Staff Change,Made by" + "\n" +
+                "T1,Other start," + midnightThisMorning().format("YYYY-MM-DD") + " 00:00,1,\"Unknown\"" + "\n" +
+                "T1,Other end," + midnightThisMorning().format("YYYY-MM-DD") + " 01:00,-1,\"Unknown\""
+            cy
+                .asABorderForcePlanningOfficer()
+                .navigateHome()
+                .navigateToMenuItem('T1')
+                .selectCurrentTab()
+                .choose24Hours()
+                .openAdjustmentDialogueForHour('add', 0)
+                .adjustMinutes(60)
+                .adjustStaffBy(1)
+                .checkStaffMovementsOnDesksAndQueuesTabAre(1)
+                .checkStaffAvailableOnDesksAndQueuesTabAre(1)
+                .findAndClick('Staff Movements')
+                .checkStaffNumbersOnMovementsTabAre(1)
+                .checkUserNameOnMovementsTab(1, "Unknown")
+                .get('#export-day-staff-movements')
+                .then((el) => {
+                    const href = el.prop('href')
+                    cy.request({
+                        method: 'GET',
+                        url: href,
+                    }).then((resp) => {
+                        expect(resp.body).to.equal(movementsCSV, "Staff movements Export CSV is wrong.");
+                    })
+                })
+                .removeXMovements(1);
+        });
+
+        it("Should update the available staff when 1 staff member is removed for 1 hour", () => {
+            cy
+                .asABorderForcePlanningOfficer()
+                .request('/')
+                .then((response) => {
+                    const $html = Cypress.$(response.body)
+                    const csrf = $html.filter('input:hidden[name="csrfToken"]').val()
+                    cy.saveShifts(shifts(2), csrf)
+                        .navigateHome()
+                        .navigateToMenuItem('T1')
+                        .selectCurrentTab()
+                        .choose24Hours()
+                        .openAdjustmentDialogueForHour('remove', 0)
+                        .adjustMinutes(60)
+                        .adjustStaffBy(1)
+                        .checkStaffMovementsOnDesksAndQueuesTabAre(-1)
+                        .checkStaffAvailableOnDesksAndQueuesTabAre(1)
+                        .findAndClick('Staff Movements')
+                        .checkStaffNumbersOnMovementsTabAre(1)
+                        .checkUserNameOnMovementsTab(1, "Unknown")
+                        .removeXMovements(1);
+                })
+        });
+
+        it("Should update the available staff when 2 staff members are added for 1 hour, and record the reason", () => {
+            cy
+                .asABorderForceOfficer()
+                .navigateHome()
+                .navigateToMenuItem('T1')
+                .selectCurrentTab()
+                .choose24Hours()
+                .openAdjustmentDialogueForHour('add', 0)
+                .selectReason('Case working')
+                .selectAdditionalReason('extra case work')
+                .adjustMinutes(60)
+                .adjustStaffBy(2)
+                .checkStaffMovementsOnDesksAndQueuesTabAre(2)
+                .checkStaffAvailableOnDesksAndQueuesTabAre(2)
+                .findAndClick('Staff Movements')
+                .checkStaffNumbersOnMovementsTabAre(2)
+                .checkUserNameOnMovementsTab(1, "Unknown")
+                .checkReasonOnMovementsTab('Case working: extra case work')
+                .removeXMovements(1);
+        });
+
+        it("BorderForce user should be able to adjust the staff movement", () => {
+            cy
+                .asABorderForceOfficer()
+                .navigateHome()
+                .navigateToMenuItem('T1')
+                .selectCurrentTab()
+                .choose24Hours()
+                .get('.staff-deployment-adjustment-container').should('exist')
+        });
+
+        it("BorderForceReadOnly should not be able to adjust the staff movement", () => {
+            cy
+                .asABorderForceReadOnlyOfficer()
+                .navigateHome()
+                .navigateToMenuItem('T1')
+                .selectCurrentTab()
+                .choose24Hours()
+                .get('.staff-deployment-adjustment-container').should('not.exist')
+        });
     });
-  });
 });
 
 Cypress.Commands.add('selectReason', (reason) => {
-  cy
-    .get('.staff-adjustment--select-reason')
-    .select(reason)
-    .should('have.value', reason)
+    cy
+        .get('.staff-adjustment--select-reason')
+        .select(reason)
+        .should('have.value', reason)
 });
 
 Cypress.Commands.add('selectAdditionalReason', (reason) => {
-  cy
-    .get('.staff-adjustment--additional-info')
-    .type(reason)
+    cy
+        .get('.staff-adjustment--additional-info')
+        .type(reason)
 });
 
 Cypress.Commands.add('staffMovementsAtRow', (row) => {
-  const selector = `td.non-pcp:nth(${row * 2 + 1})`
-  cy.get(selector);
+    const selector = `td.non-pcp:nth(${row * 2 + 1})`
+    cy.get(selector);
 });
 
 Cypress.Commands.add('staffAvailableAtRow', (row) => {
-  const selector = `:nth-child(${row + 1}) > .staff-adjustments > :nth-child(1) > .deployed`;
-  cy.get(selector);
+    const selector = `:nth-child(${row + 1}) > .staff-adjustments > :nth-child(1) > .deployed`;
+    cy.get(selector);
 });
 
 Cypress.Commands.add('staffOverTheDayAtSlot', (slot) => {
-  const selector = '#available-staff tbody > :nth-child(2) > :nth-child(' + (slot + 1) + ')';
-  cy.get(selector);
+    const selector = '#available-staff tbody > :nth-child(2) > :nth-child(' + (slot + 1) + ')';
+    cy.get(selector);
 });
 
 Cypress.Commands.add('checkStaffAvailableOnDesksAndQueuesTabAre', (numStaff) => {
-  [0, 1, 2, 3].map((row) => {
-    cy.staffAvailableAtRow(row).contains(numStaff);
-  });
-  cy.staffAvailableAtRow(4, 0).contains(0);
+    [0, 1, 2, 3].map((row) => {
+        cy.staffAvailableAtRow(row).contains(numStaff);
+    });
+    cy.staffAvailableAtRow(4, 0).contains(0);
 });
 
 Cypress.Commands.add('checkStaffMovementsOnDesksAndQueuesTabAre', (numStaff) => {
-  [0, 1, 2, 3].map((row) => {
-    cy.staffMovementsAtRow(row).contains(numStaff);
-  });
-  cy.staffMovementsAtRow(4).contains(0);
+    [0, 1, 2, 3].map((row) => {
+        cy.staffMovementsAtRow(row).contains(numStaff);
+    });
+    cy.staffMovementsAtRow(4).contains(0);
 });
 
 Cypress.Commands.add('checkStaffNumbersOnMovementsTabAre', (numStaff) => {
-  cy.contains("Staff Movements").click({force: true}).then(() => {
-    [0, 1, 2, 3].map((slot) => { cy.staffOverTheDayAtSlot(slot).contains(numStaff) });
-    cy.staffOverTheDayAtSlot(4).contains("0");
-  });
-});
-
-Cypress.Commands.add('removeXMovements', (numToRemove) => {
-  cy.contains("Staff Movements").click({force: true}).then(() => {
-    for (let i = 1; i <= numToRemove; i++) {
-      cy.get('.fa-remove').first().click({force: true}).end();
-      cy.get('.fa-remove').should('have.length', numToRemove - i).end();
-    }
-  });
-});
-
-Cypress.Commands.add('checkUserNameOnMovementsTab', (numMovements, userName) => {
-  cy.get('.movement-display')
-    .should('have.length', numMovements)
-    .each(($element) => {
-      cy.wrap($element).contains(userName);
-      cy.wrap($element);
+    cy.contains("Staff Movements").click({force: true}).then(() => {
+        [0, 1, 2, 3].map((slot) => {
+            cy.staffOverTheDayAtSlot(slot).contains(numStaff)
+        });
+        cy.staffOverTheDayAtSlot(4).contains("0");
     });
 });
 
+Cypress.Commands.add('removeXMovements', (numToRemove) => {
+    cy.contains("Staff Movements").click({force: true}).then(() => {
+        for (let i = 1; i <= numToRemove; i++) {
+            cy.get('.fa-remove').first().click({force: true}).end();
+            cy.get('.fa-remove').should('have.length', numToRemove - i).end();
+        }
+    });
+});
+
+Cypress.Commands.add('checkUserNameOnMovementsTab', (numMovements, userName) => {
+    cy.get('.movement-display')
+        .should('have.length', numMovements)
+        .each(($element) => {
+            cy.wrap($element).contains(userName);
+            cy.wrap($element);
+        });
+});
+
 Cypress.Commands.add('checkReasonOnMovementsTab', (reason) => {
-  cy
-    .get('.staff-movements-list')
-    .contains(reason);
+    cy
+        .get('.staff-movements-list')
+        .contains(reason);
 });
 
 Cypress.Commands.add('openAdjustmentDialogueForHour', (addOrRemove, hour) => {
-  const buttonLabel = addOrRemove == "add" ? "+" : "-";
-  const buttonNth = addOrRemove == "add" ? 3 : 1;
+    const buttonLabel = addOrRemove == "add" ? "+" : "-";
+    const buttonNth = addOrRemove == "add" ? 3 : 1;
 
-  cy
-    .get('.staff-adjustments > :nth-child(' + (hour + 1) + ') > :nth-child(' + buttonNth + ')')
-    .contains(buttonLabel)
-    .click({force: true});
+    cy
+        .get('.staff-adjustments > :nth-child(' + (hour + 1) + ') > :nth-child(' + buttonNth + ')')
+        .contains(buttonLabel)
+        .click({force: true});
 });
 
 Cypress.Commands.add('adjustStaffBy', (numStaff) => {
-  const adjustmentSelector = numStaff > 0 ? '.staff-adjustment--adjustment-button__increase' : '.staff-adjustment--adjustment-button__decrease';
+    const adjustmentSelector = numStaff > 0 ? '.staff-adjustment--adjustment-button__increase' : '.staff-adjustment--adjustment-button__decrease';
 
-  for (let i = 1; i < numStaff; i++) {
-    cy.get(adjustmentSelector).click({force: true});
-  }
+    for (let i = 1; i < numStaff; i++) {
+        cy.get(adjustmentSelector).click({force: true});
+    }
 
-  cy
-    .get('.btn-primary.staff-adjustment--save-cancel')
-    .click({force: true});
+    cy
+        .get('.btn-primary.staff-adjustment--save-cancel')
+        .click({force: true});
 });
 
 Cypress.Commands.add('adjustMinutes', (minutes) => {
-  cy
-    .get('.staff-adjustment--select-time-length')
-    .select('' + minutes)
-    .should('have.value', '' + minutes);
+    cy
+        .get('.staff-adjustment--select-time-length')
+        .select('' + minutes)
+        .should('have.value', '' + minutes);
 });
