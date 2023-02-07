2e401de845eb257ad7f67b4b03326c6b599eaa8b
Rich Birch <ringo@albumsnaps.com>
2023-02-07 09:44:29 +0000

    DRTII-576 Test around opening STN forecast file

--- server/src/test/scala/drt/server/feeds/common/XlsExtractorUtilSpec.scala
+++ server/src/test/scala/drt/server/feeds/common/XlsExtractorUtilSpec.scala
@@ -5,7 +5,7 @@ import org.specs2.mutable.Specification
 class XlsExtractorUtilSpec extends Specification {
   "Given a sheet" >> {
     "I should be able to open it" >> {
-      val filePath = getClass.getClassLoader.getResource("StnForecast-2023-02-06.xlsx").getPath
+      val filePath = getClass.getClassLoader.getResource("STN_Forecast_Fixture.xlsx").getPath
       val workbook = XlsExtractorUtil.workbook(filePath)
       val sheet = XlsExtractorUtil.sheetMapByName("Arrivals by flight", workbook)
       sheet.getSheetName === "Arrivals by flight"
