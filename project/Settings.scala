49ae6e0d4cafc57a01a9b94ff96aa55677f3e896
Scala Steward <scala_steward@virtuslab.com>
2023-03-07 06:53:26 +0000

    Update pac4j-saml to 2.0.0

--- project/Settings.scala
+++ project/Settings.scala
@@ -65,7 +65,7 @@ object Settings {
     val log4Javascript = "1.4.15"
     val typesafeConfig = "1.3.0"
     val reactHandsontable = "3.1.2"
-    val pac4jSaml = "2.0.0-RC1"
+    val pac4jSaml = "2.0.0"
     val drtBirminghamSchema = "50"
     val drtCirium = "186"
     val playJson = "2.6.0"
