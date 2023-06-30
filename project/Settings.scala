d68f1cb7e2e58cdb33b2bcf7cd316a1b6233d555
Rich Birch <ringo@albumsnaps.com>
2023-06-30 09:51:11 +0100

    drt-lib master build & fix sbt version

--- project/Settings.scala
+++ project/Settings.scala
@@ -25,7 +25,7 @@ object Settings {
   /** Declare global dependency versions here to avoid mismatches in multi part dependencies */
   //noinspection ScalaStyle
   object versions {
-    val drtLib = "v548"
+    val drtLib = "v549"
 
     val scala = "2.13.11"
     val scalaDom = "2.0.0"
