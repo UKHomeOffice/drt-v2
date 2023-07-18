eb9ae078f68cf1e774728a37120cf6715c4449a1
Rich Birch <rich@freshcodelimited.com>
2023-07-18 11:15:22 +0100

    DRTII-1258 Remove repeated code

--- server/src/main/scala/services/exports/flights/templates/AdminExport.scala
+++ server/src/main/scala/services/exports/flights/templates/AdminExport.scala
@@ -1,7 +1,5 @@
 package services.exports.flights.templates
 
-import manifests.passengers.PassengerInfo
-import passengersplits.parsing.VoyageManifestParser.VoyageManifest
 import uk.gov.homeoffice.drt.arrivals.{ApiFlightWithSplits, ArrivalExportHeadings}
 import uk.gov.homeoffice.drt.ports.Terminals._
 import uk.gov.homeoffice.drt.ports.{FeedSource, MlFeedSource}
@@ -10,15 +8,7 @@ import uk.gov.homeoffice.drt.time.SDateLike
 
 trait AdminExport extends FlightsWithSplitsWithActualApiExport {
   override val headings: String = ArrivalExportHeadings.arrivalWithSplitsAndRawApiHeadings.replace("PCP Pax", "PCP Pax,Predicted PCP Pax")
-
-  override def rowValues(fws: ApiFlightWithSplits, maybeManifest: Option[VoyageManifest]): Seq[String] = {
-    val maybePaxSummary = maybeManifest.flatMap(PassengerInfo.manifestToFlightManifestSummary)
-
-    (flightWithSplitsToCsvRow(fws) :::
-      actualAPISplitsForFlightInHeadingOrder(fws, ArrivalExportHeadings.actualApiHeadings.split(",")).toList).map(s => s"$s") :::
-      List(s""""${nationalitiesFromSummary(maybePaxSummary)}"""", s""""${ageRangesFromSummary(maybePaxSummary)}"""")
-  }
-
+  
   override def flightWithSplitsToCsvRow(fws: ApiFlightWithSplits): List[String] = {
     val apiIsInvalid = if (fws.hasApi && !fws.hasValidApi) "Y" else ""
     val pcpPax =
