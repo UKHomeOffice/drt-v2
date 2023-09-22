94d3eaa76d33e858716f665abd86b070685f1dbc
Rich Birch <rich@freshcodelimited.com>
2023-09-22 08:55:27 +0100

    DRTII-1268 Implementation of headline figures export

--- server/src/main/scala/services/exports/StaffRequirementExports.scala
+++ server/src/main/scala/services/exports/StaffRequirementExports.scala
@@ -1,5 +1,45 @@
 package services.exports
 
-object StaffRequirementExports {
+import akka.NotUsed
+import akka.stream.scaladsl.Source
+import drt.shared.CrunchApi.CrunchMinute
+import services.LocalDateStream
+import uk.gov.homeoffice.drt.ports.Queues
+import uk.gov.homeoffice.drt.ports.Terminals.Terminal
+import uk.gov.homeoffice.drt.time.{LocalDate, SDate, UtcDate}
 
+import scala.concurrent.Future
+
+object StaffRequirementExport {
+  private val relevantMinute: (LocalDate, Seq[CrunchMinute]) => Seq[CrunchMinute] =
+    (current, minutes) => minutes.filter { m => SDate(m.minute).toLocalDate == current }
+
+  def queuesProvider(utcQueuesProvider: (UtcDate, UtcDate, Terminal) => Source[(UtcDate, Seq[CrunchMinute]), NotUsed],
+                    ): (LocalDate, LocalDate, Terminal) => Source[(LocalDate, Seq[CrunchMinute]), NotUsed] =
+    LocalDateStream(utcQueuesProvider, startBufferDays = 0, endBufferDays = 0, transformData = relevantMinute)
+
+  def toDailyHeadlines: (LocalDate, Seq[CrunchMinute]) => Seq[String] =
+    (date, minutes) => {
+      val dateFormatted = f"${date.day}%02d/${date.month}%02d"
+      val total = minutes.map(_.paxLoad).sum.toInt
+      val workLoad = minutes.map(_.workLoad).sum.toInt
+      val byQueue = paxByQueue(minutes)
+      val byQueueInOrder = Queues.queueOrder
+        .map(q => byQueue.get(q))
+        .collect {
+          case Some(pax) => pax
+        }
+      Seq(dateFormatted, total.toString) ++ byQueueInOrder.map(_.toString) ++ Seq(workLoad.toString)
+    }
+
+  private def paxByQueue(minutes: Seq[CrunchMinute]): Map[Queues.Queue, Int] =
+    minutes
+      .map(m => (m.queue, m.paxLoad))
+      .groupBy { case (queue, _) => queue }
+      .map { case (queue, paxLoads) =>
+        val pax = paxLoads.map {
+          case (_, pax) => pax
+        }.sum
+        (queue, pax.toInt)
+      }
 }
