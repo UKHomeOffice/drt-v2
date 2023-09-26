23757824a0944d8dae2d2d9dc6fd10817d777751
Rich Birch <ringo@albumsnaps.com>
2023-09-26 13:37:40 +0100

    DRTII-1268 Style tidying

--- client/src/main/scala/drt/client/components/styles/FormFieldsStyle.scala
+++ client/src/main/scala/drt/client/components/styles/FormFieldsStyle.scala
@@ -7,61 +7,6 @@ case class FormFieldsStyle(common: CommonStyle = DefaultCommonStyle) extends Sty
 
   import dsl._
 
-  val textField = style(
-    width(150.px),
-    unsafeChild("label")(
-      fontSize(1.5.rem)
-    )
-  )
-
-  val datePicker = style(
-    marginBottom(1.2.rem),
-    width(150.px),
-    padding(4.px, 0.px, 5.px),
-    unsafeChild("input")(
-      fontSize(1.5.rem)
-    )
-  )
-
-  val daySelector = style(
-    maxWidth(820.px),
-  )
-
-  val datePickerLabel = style(
-    unsafeChild("label")(
-      marginTop(1.rem),
-      fontSize(1.2.rem)
-    ),
-  )
-
-  val textFieldSmall = style(
-    width(45.px),
-    unsafeChild("label")(
-      fontSize(1.5.rem)
-    ),
-    unsafeChild("p")(
-      fontSize(1.rem)
-    )
-  )
-
-  val regularText = style(
-    fontSize :=! 1.2.rem,
-    unsafeChild("p")(
-      fontSize :=! 1.2.rem
-    )
-  )
-
-  val formHelperText = style(
-    unsafeChild("span")(
-      fontSize(1.rem),
-      padding(1.rem),
-    )
-  )
-
-  val labelWide = style(
-    width(100.pc)
-  )
-
   val simulation = style(
     padding(15.px),
     minHeight(550.px),
