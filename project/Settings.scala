f0bd097e9c69cedb014c0cd46ebb3472117c7772
Scala Steward <scala_steward@virtuslab.com>
2023-10-04 08:04:28 +0000

    Update poi, poi-ooxml to 5.2.4

--- project/Settings.scala
+++ project/Settings.scala
@@ -47,7 +47,7 @@ object Settings {
 
     val renjin = "0.9.2725"
     val csvCommons = "1.10.0"
-    val poi = "5.2.3"
+    val poi = "5.2.4"
     val pprint = "0.5.9"
     val akkaPersistenceJdbc = "5.2.0"
     val bluebus = "v95"
