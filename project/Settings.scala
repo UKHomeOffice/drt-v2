98d3f37a8bea667284bd59ddc094cef3ea6e8c8c
Rich Birch <ringo@albumsnaps.com>
2023-10-10 15:09:40 +0100

    DRTII-1292 drt-lib update

--- project/Settings.scala
+++ project/Settings.scala
@@ -25,7 +25,7 @@ object Settings {
   /** Declare global dependency versions here to avoid mismatches in multi part dependencies */
   //noinspection ScalaStyle
   object versions {
-    val drtLib = "v631"
+    val drtLib = "v632"
 
     val scala = "2.13.10"
     val scalaDom = "2.0.0"
