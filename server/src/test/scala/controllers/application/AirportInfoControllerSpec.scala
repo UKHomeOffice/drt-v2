6e3e62f987a4e63e09a78e5581df4659f7a09587
Nilesh Gupta <nilesh.nemichandgupta@digital.homeoffice.gov.uk>
2023-10-18 15:33:54 +0100

    DRTII-1319 adding test for alert and airport controller

---
+++ server/src/test/scala/controllers/application/AirportInfoControllerSpec.scala
@@ -0,0 +1,38 @@
+package controllers.application
+
+import module.DRTModule
+import org.scalatestplus.play.PlaySpec
+import play.api.test.Helpers._
+import play.api.test.{FakeRequest, Helpers}
+import uk.gov.homeoffice.drt.auth.Roles.{ArrivalsAndSplitsView, TEST}
+
+class AirportInfoControllerSpec extends PlaySpec {
+
+  "AirportInfoController" should {
+
+    "get airport info for the portCode requested" in {
+
+      val module = new DRTModule() {
+        override lazy val isTestEnvironment: Boolean = true
+      }
+
+      val drtSystemInterface = module.provideDrtSystemInterface
+
+      val controller = new AirportInfoController(Helpers.stubControllerComponents(), drtSystemInterface)
+
+      val result = controller.getAirportInfo.apply(FakeRequest(GET, "/airport-info?portCode=EDI")
+        .withHeaders("X-Auth-Roles" -> s"${TEST.name},${ArrivalsAndSplitsView.name}")
+
+      )
+
+      status(result) mustBe OK
+
+      val resultExpected =
+        s"""[[{"iata":"EDI"},{"airportName":"Edinburgh","city":"Edinburgh","country":"United Kingdom","code":"EDI"}]]"""
+          .stripMargin
+
+      contentAsString(result) must include(resultExpected)
+    }
+
+  }
+}
