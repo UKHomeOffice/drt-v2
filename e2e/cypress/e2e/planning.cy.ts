126c5b1f89a7dacd4f668fd5c71f6f27931c40e3
Rich Birch <ringo@albumsnaps.com>
2023-11-30 09:03:32 +0000

    DRTII-1344 Add end to end test for planning page display

---
+++ e2e/cypress/e2e/planning.cy.ts
@@ -0,0 +1,21 @@
+describe('Planning page', () => {
+
+  it("Clicking on the planning page should display a table", () => {
+    cy
+      .asABorderForceOfficer()
+      .navigateHome()
+      .get('.terminal-t1')
+      .click({force: true})
+      .get('#planning-tab')
+      .click({force: true})
+      .get('.tab-content').as('tabContent')
+      .contains('Headline Figures')
+      .get('@tabContent').contains('e-Gates')
+      .get('@tabContent').contains('EEA')
+      .get('@tabContent').contains('Non-EEA')
+      .get('@tabContent').contains('Total Pax')
+      .get('@tabContent').contains('Workloads')
+      .get('@tabContent').contains('Total staff required at each hour of the day')
+  })
+
+})
