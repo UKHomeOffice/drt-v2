8416377981458c9c355a5e56b13f189d43d04b4e
Nilesh Gupta <nilesh.nemichandgupta@digital.homeoffice.gov.uk>
2024-01-09 14:09:53 +0000

    DRTII-1360 disable red list dialog for a given date
    Signed-off-by: Nilesh Gupta <nilesh.nemichandgupta@digital.homeoffice.gov.uk>

--- shared/src/main/scala/drt/shared/redlist/LhrRedListDates.scala
+++ shared/src/main/scala/drt/shared/redlist/LhrRedListDates.scala
@@ -6,11 +6,18 @@ import uk.gov.homeoffice.drt.time.SDateLike
 trait LhrRedListDates {
   val t3RedListOpeningDate: MillisSinceEpoch
   val t4RedListOpeningDate: MillisSinceEpoch
+
+  val startRedListingDate: MillisSinceEpoch
+  val endRedListingDate: MillisSinceEpoch
 }
 
 case object LhrRedListDatesImpl extends LhrRedListDates {
   def dayHasPaxDiversions(day: SDateLike): Boolean = day.millisSinceEpoch >= t3RedListOpeningDate
 
+  def isRedListActive(day: SDateLike): Boolean = day.millisSinceEpoch >= startRedListingDate && day.millisSinceEpoch <= endRedListingDate
+
   override val t3RedListOpeningDate = 1622502000000L // 2021-06-01 BST
   override val t4RedListOpeningDate = 1624921200000L // 2021-06-29 BST
+  override val startRedListingDate = 1613347200000L // 2021-02-15 BST
+  override val endRedListingDate = 1639526400000L // 2021-12-15 BST
 }
