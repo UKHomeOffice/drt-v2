44d9f57b3a48de1b58a2ffc4dc017cf1527c3515
Rich Birch <ringo@albumsnaps.com>
2024-01-10 12:19:51 +0000

    DRTII-1381 update drt-lib

--- server/src/main/scala/controllers/application/SummariesController.scala
+++ server/src/main/scala/controllers/application/SummariesController.scala
@@ -9,10 +9,11 @@ import play.api.mvc._
 import uk.gov.homeoffice.drt.crunchsystem.DrtSystemInterface
 import uk.gov.homeoffice.drt.db.queries.PassengersHourlyDao
 import uk.gov.homeoffice.drt.ports.Queues.Queue
-import uk.gov.homeoffice.drt.ports.Terminals.{T2, Terminal}
+import uk.gov.homeoffice.drt.ports.Terminals.Terminal
 import uk.gov.homeoffice.drt.ports.{PortRegion, Queues}
 import uk.gov.homeoffice.drt.time.{LocalDate, SDate}
 
+import scala.concurrent.Future
 import scala.util.{Failure, Success, Try}
 
 class SummariesController @Inject()(cc: ControllerComponents, ctrl: DrtSystemInterface) extends AuthController(cc, ctrl) {
@@ -52,24 +53,19 @@ class SummariesController @Inject()(cc: ControllerComponents, ctrl: DrtSystemInt
                            request: Request[AnyContent],
                            maybeTerminal: Option[Terminal],
                           ): Result = {
-    val dailyBreakdown = request.getQueryString("granularity").contains("daily")
+    val csvRowsStream = streamForGranularity(maybeTerminal, request.getQueryString("granularity"))
 
-    val stream =
-      if (dailyBreakdown) dailyStream(maybeTerminal)
-      else totalsStream(maybeTerminal)
-
-    exportDateRange(startLocalDateString, endLocalDateString, maybeTerminal, stream)
+    exportDateRange(startLocalDateString, endLocalDateString, maybeTerminal, csvRowsStream)
   }
 
   private def exportDateRange(startLocalDateString: String,
                               endLocalDateString: String,
                               maybeTerminal: Option[Terminal],
-                              stream: (LocalDate, LocalDate) => Source[String, NotUsed]): Result =
+                              csvRows: (LocalDate, LocalDate) => Source[String, NotUsed]): Result =
     (LocalDate.parse(startLocalDateString), LocalDate.parse(endLocalDateString)) match {
       case (Some(start), Some(end)) =>
-        val csvStream = stream(start, end)
         val fileName = makeFileName("passengers", maybeTerminal, start, end, airportConfig.portCode)
-        Try(sourceToCsvResponse(csvStream, fileName)) match {
+        Try(sourceToCsvResponse(csvRows(start, end), fileName)) match {
           case Success(value) => value
           case Failure(t) =>
             log.error(s"Failed to get CSV export${t.getMessage}")
@@ -79,59 +75,63 @@ class SummariesController @Inject()(cc: ControllerComponents, ctrl: DrtSystemInt
         BadRequest("Invalid date format for start or end date")
     }
 
-  private def dailyStream(maybeTerminal: Option[Terminal]): (LocalDate, LocalDate) => Source[String, NotUsed] =
+  private def streamForGranularity(maybeTerminal: Option[Terminal], granularity: Option[String]): (LocalDate, LocalDate) => Source[String, NotUsed] =
     (start, end) => {
       val portCode = airportConfig.portCode
       val regionName = PortRegion.fromPort(portCode).name
       val portCodeStr = portCode.toString
       val maybeTerminalName = maybeTerminal.map(_.toString)
-      val queueTotalsQueryForDate = PassengersHourlyDao.queueTotalsForPortAndDate(ctrl.airportConfig.portCode.iata, maybeTerminal.map(_.toString))
-      Queues.queueOrder.filter(q => airportConfig.queuesByTerminal.get(T2).exists(_.contains(q)))
-      Source(DateRange(start, end))
-        .mapAsync(1) { date =>
-          ctrl.db.run(queueTotalsQueryForDate(date))
-            .map { queueCounts =>
-              val totalPcpPax = queueCounts.values.sum
-              val queueCells = Queues.queueOrder
-                .map(queue => queueCounts.getOrElse(queue, 0).toString)
-                .mkString(",")
-              maybeTerminalName match {
-                case Some(terminalName) =>
-                  s"${date.toISOString},$regionName,$portCodeStr,$terminalName,$totalPcpPax,$queueCells\n"
-                case None =>
-                  s"${date.toISOString},$regionName,$portCodeStr,$totalPcpPax,$queueCells\n"
-              }
-            }
-        }
+      val queueTotals = PassengersHourlyDao.queueTotalsForPortAndDate(ctrl.airportConfig.portCode.iata, maybeTerminal.map(_.toString))
+      val queueTotalsQueryForDate: LocalDate => Future[Map[Queue, Int]] = date => ctrl.db.run(queueTotals(date))
+      if (granularity.contains("daily"))
+        dailyStream(start, end, regionName, portCodeStr, maybeTerminalName, queueTotalsQueryForDate)
+      else
+        totalsStream(start, end, regionName, portCodeStr, maybeTerminalName, queueTotalsQueryForDate)
     }
 
-  private def totalsStream(maybeTerminal: Option[Terminal]): (LocalDate, LocalDate) => Source[String, NotUsed] =
-    (start, end) => {
-      val portCode = airportConfig.portCode
-      val regionName = PortRegion.fromPort(portCode).name
-      val portCodeStr = portCode.toString
-      val maybeTerminalName = maybeTerminal.map(_.toString)
-      val queueTotalsQueryForDate = PassengersHourlyDao.queueTotalsForPortAndDate(ctrl.airportConfig.portCode.iata, maybeTerminal.map(_.toString))
-      Source(DateRange(start, end))
-        .mapAsync(1)(date => ctrl.db.run(queueTotalsQueryForDate(date)))
-        .fold(Map[Queue, Int]()) {
-          case (acc, queueCounts) =>
-            acc ++ queueCounts.map {
-              case (queue, count) =>
-                queue -> (acc.getOrElse(queue, 0) + count)
-            }
-        }
-        .map { queueCounts =>
-          val totalPcpPax = queueCounts.values.sum
-          val queueCells = Queues.queueOrder
-            .map(queue => queueCounts.getOrElse(queue, 0).toString)
-            .mkString(",")
-          maybeTerminalName match {
-            case Some(terminalName) =>
-              s"$regionName,$portCodeStr,$terminalName,$totalPcpPax,$queueCells\n"
-            case None =>
-              s"$regionName,$portCodeStr,$totalPcpPax,$queueCells\n"
+  private def dailyStream(start: LocalDate,
+                          end: LocalDate,
+                          regionName: String,
+                          portCodeStr: String,
+                          maybeTerminalName: Option[String],
+                          queueTotalsQueryForDate: LocalDate => Future[Map[Queue, Int]]): Source[String, NotUsed] =
+    Source(DateRange(start, end))
+      .mapAsync(1) { date =>
+        queueTotalsQueryForDate(date)
+          .map { queueCounts =>
+            s"${date.toISOString}," + passengersCsvRow(regionName, portCodeStr, maybeTerminalName, queueCounts)
           }
-        }
+      }
+
+  private def totalsStream(start: LocalDate,
+                           end: LocalDate,
+                           regionName: String,
+                           portCodeStr: String,
+                           maybeTerminalName: Option[String],
+                           queueTotalsQueryForDate: LocalDate => Future[Map[Queue, Int]]): Source[String, NotUsed] =
+    Source(DateRange(start, end))
+      .mapAsync(1)(date => queueTotalsQueryForDate(date))
+      .fold(Map[Queue, Int]()) {
+        case (acc, queueCounts) =>
+          acc ++ queueCounts.map {
+            case (queue, count) =>
+              queue -> (acc.getOrElse(queue, 0) + count)
+          }
+      }
+      .map { queueCounts =>
+        passengersCsvRow(regionName, portCodeStr, maybeTerminalName, queueCounts)
+      }
+
+  private def passengersCsvRow(regionName: String, portCodeStr: String, maybeTerminalName: Option[String], queueCounts: Map[Queue, Int]) = {
+    val totalPcpPax = queueCounts.values.sum
+    val queueCells = Queues.queueOrder
+      .map(queue => queueCounts.getOrElse(queue, 0).toString)
+      .mkString(",")
+    maybeTerminalName match {
+      case Some(terminalName) =>
+        s"$regionName,$portCodeStr,$terminalName,$totalPcpPax,$queueCells\n"
+      case None =>
+        s"$regionName,$portCodeStr,$totalPcpPax,$queueCells\n"
     }
+  }
 }
