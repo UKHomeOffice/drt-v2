2f09be1ce639b3fa85cd1a9efad2272880f0128d
Rich Birch <ringo@albumsnaps.com>
2024-01-11 11:07:59 +0000

    DRTII-1384 Start adding json response to pax summary end poinits

--- server/src/main/scala/controllers/application/SummariesController.scala
+++ server/src/main/scala/controllers/application/SummariesController.scala
@@ -5,7 +5,7 @@ import akka.NotUsed
 import akka.stream.scaladsl.Source
 import com.google.inject.Inject
 import controllers.application.exports.CsvFileStreaming.{makeFileName, sourceToCsvResponse}
-import play.api.mvc._
+import play.api.mvc.{request, _}
 import uk.gov.homeoffice.drt.crunchsystem.DrtSystemInterface
 import uk.gov.homeoffice.drt.db.queries.PassengersHourlyDao
 import uk.gov.homeoffice.drt.ports.Queues.Queue
@@ -15,6 +15,7 @@ import uk.gov.homeoffice.drt.time.{LocalDate, SDate}
 
 import scala.concurrent.Future
 import scala.util.{Failure, Success, Try}
+
 
 class SummariesController @Inject()(cc: ControllerComponents, ctrl: DrtSystemInterface) extends AuthController(cc, ctrl) {
   def populatePassengersForDate(localDateStr: String): Action[AnyContent] = {
@@ -38,34 +39,25 @@ class SummariesController @Inject()(cc: ControllerComponents, ctrl: DrtSystemInt
       request =>
         val terminal = Terminal(terminalName)
         val maybeTerminal = Option(terminal)
-        exportStream(startLocalDateString, endLocalDateString, request, maybeTerminal)
+        exportPassengersCsv(startLocalDateString, endLocalDateString, request, maybeTerminal)
     }
 
   def exportPassengersByPortForDateRangeApi(startLocalDateString: String, endLocalDateString: String): Action[AnyContent] =
     Action {
-      request =>
-        val maybeTerminal = None
-        exportStream(startLocalDateString, endLocalDateString, request, maybeTerminal)
+      request => exportPassengersCsv(startLocalDateString, endLocalDateString, request, maybeTerminal = None)
     }
 
-  private def exportStream(startLocalDateString: String,
-                           endLocalDateString: String,
-                           request: Request[AnyContent],
-                           maybeTerminal: Option[Terminal],
-                          ): Result = {
-    val csvRowsStream = streamForGranularity(maybeTerminal, request.getQueryString("granularity"))
-
-    exportDateRange(startLocalDateString, endLocalDateString, maybeTerminal, csvRowsStream)
-  }
-
-  private def exportDateRange(startLocalDateString: String,
-                              endLocalDateString: String,
-                              maybeTerminal: Option[Terminal],
-                              csvRows: (LocalDate, LocalDate) => Source[String, NotUsed]): Result =
+  private def exportPassengersCsv(startLocalDateString: String,
+                                  endLocalDateString: String,
+                                  request: Request[AnyContent],
+                                  maybeTerminal: Option[Terminal],
+                                 ): Result =
     (LocalDate.parse(startLocalDateString), LocalDate.parse(endLocalDateString)) match {
       case (Some(start), Some(end)) =>
         val fileName = makeFileName("passengers", maybeTerminal, start, end, airportConfig.portCode)
-        Try(sourceToCsvResponse(csvRows(start, end), fileName)) match {
+        val csvRowsStream = streamForGranularity(maybeTerminal, request.getQueryString("granularity"))
+
+        Try(sourceToCsvResponse(csvRowsStream(start, end), fileName)) match {
           case Success(value) => value
           case Failure(t) =>
             log.error(s"Failed to get CSV export${t.getMessage}")
@@ -75,7 +67,46 @@ class SummariesController @Inject()(cc: ControllerComponents, ctrl: DrtSystemInt
         BadRequest("Invalid date format for start or end date")
     }
 
-  private def streamForGranularity(maybeTerminal: Option[Terminal], granularity: Option[String]): (LocalDate, LocalDate) => Source[String, NotUsed] =
+  private def exportPassengersJson(startLocalDateString: String,
+                                   endLocalDateString: String,
+                                   request: Request[AnyContent],
+                                   maybeTerminal: Option[Terminal],
+                                  ): Result =
+    (LocalDate.parse(startLocalDateString), LocalDate.parse(endLocalDateString)) match {
+      case (Some(start), Some(end)) =>
+        val contentType = request.headers.get("Content-Type").getOrElse("application/json")
+        val maybeGranularity = request.getQueryString("granularity")
+        val csvRowsStream = streamForGranularity(maybeTerminal, maybeGranularity, contentType)
+
+//        Try(sourceToCsvResponse(csvRowsStream(start, end), fileName)) match {
+//          case Success(value) => value
+//          case Failure(t) =>
+//            log.error(s"Failed to get CSV export${t.getMessage}")
+//            BadRequest("Failed to get CSV export")
+//        }
+      case _ =>
+        BadRequest("Invalid date format for start or end date")
+    }
+
+
+  private def exportDateRange(startLocalDateString: String,
+                              endLocalDateString: String,
+                              maybeTerminal: Option[Terminal],
+                              toCsvResponse: (String, LocalDate, LocalDate) => Result): Result =
+    (LocalDate.parse(startLocalDateString), LocalDate.parse(endLocalDateString)) match {
+      case (Some(start), Some(end)) =>
+        val fileName = makeFileName("passengers", maybeTerminal, start, end, airportConfig.portCode)
+        Try(toCsvResponse(fileName, start, end)) match {
+          case Success(value) => value
+          case Failure(t) =>
+            log.error(s"Failed to get CSV export${t.getMessage}")
+            BadRequest("Failed to get CSV export")
+        }
+      case _ =>
+        BadRequest("Invalid date format for start or end date")
+    }
+
+  private def streamForGranularity(maybeTerminal: Option[Terminal], granularity: Option[String], contentType: String): (LocalDate, LocalDate) => Source[String, NotUsed] =
     (start, end) => {
       val portCode = airportConfig.portCode
       val regionName = PortRegion.fromPort(portCode).name
@@ -94,7 +125,9 @@ class SummariesController @Inject()(cc: ControllerComponents, ctrl: DrtSystemInt
                           regionName: String,
                           portCodeStr: String,
                           maybeTerminalName: Option[String],
-                          queueTotalsQueryForDate: LocalDate => Future[Map[Queue, Int]]): Source[String, NotUsed] =
+                          queueTotalsQueryForDate: LocalDate => Future[Map[Queue, Int]],
+                          contentType: String,
+                         ): Source[String, NotUsed] =
     Source(DateRange(start, end))
       .mapAsync(1) { date =>
         queueTotalsQueryForDate(date)
@@ -122,7 +155,20 @@ class SummariesController @Inject()(cc: ControllerComponents, ctrl: DrtSystemInt
         passengersCsvRow(regionName, portCodeStr, maybeTerminalName, queueCounts)
       }
 
-  private def passengersCsvRow(regionName: String, portCodeStr: String, maybeTerminalName: Option[String], queueCounts: Map[Queue, Int]) = {
+  private def passengersCsvRow(regionName: String, portCodeStr: String, maybeTerminalName: Option[String], queueCounts: Map[Queue, Int]): String = {
+    val totalPcpPax = queueCounts.values.sum
+    val queueCells = Queues.queueOrder
+      .map(queue => queueCounts.getOrElse(queue, 0).toString)
+      .mkString(",")
+    maybeTerminalName match {
+      case Some(terminalName) =>
+        s"$regionName,$portCodeStr,$terminalName,$totalPcpPax,$queueCells\n"
+      case None =>
+        s"$regionName,$portCodeStr,$totalPcpPax,$queueCells\n"
+    }
+  }
+
+  private def passengersJson(regionName: String, portCodeStr: String, maybeTerminalName: Option[String], queueCounts: Map[Queue, Int]) = {
     val totalPcpPax = queueCounts.values.sum
     val queueCells = Queues.queueOrder
       .map(queue => queueCounts.getOrElse(queue, 0).toString)
