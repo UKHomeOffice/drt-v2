9a9925f561bd2f4b56e3fe544b16cd4bb46b8661
Rich Birch <ringo@albumsnaps.com>
2024-04-03 15:27:34 +0100

    DRTII-1000 drt-lib master build

--- server/src/main/scala/services/arrivals/EdiArrivalsTerminalAdjustments.scala
+++ server/src/main/scala/services/arrivals/EdiArrivalsTerminalAdjustments.scala
@@ -8,8 +8,8 @@ object EdiArrivalsTerminalAdjustments extends ArrivalsAdjustmentsLike {
   val log: Logger = LoggerFactory.getLogger(getClass)
 
   override def adjust(arrival: Arrival): Arrival = {
-      val a1BaggageBelt = Seq("1", "2", "3").contains(arrival.BaggageReclaimId.getOrElse(""))
-      val correctedTerminal = if (a1BaggageBelt) A1 else A2
-      arrival.copy(Terminal = correctedTerminal)
+    val a1BaggageBelt = Seq("1", "2", "3").contains(arrival.BaggageReclaimId.getOrElse(""))
+    val correctedTerminal = if (a1BaggageBelt) A1 else A2
+    arrival.copy(Terminal = correctedTerminal)
   }
 }
