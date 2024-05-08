c055b7bed686504bf7c70a2db108f3e40fd77806
Nilesh Gupta <nilesh.nemichandgupta@digital.homeoffice.gov.uk>
2024-05-08 13:26:43 +0100

    moving pax before country code for flag
    Signed-off-by: Nilesh Gupta <nilesh.nemichandgupta@digital.homeoffice.gov.uk>

--- client/src/main/scala/drt/client/components/FlightTableRow.scala
+++ client/src/main/scala/drt/client/components/FlightTableRow.scala
@@ -230,7 +230,7 @@ object FlightTableRow {
           .map { country =>
             val pax = summary.nationalities.find(n => n._1.code == country.threeLetterCode).map(_._2).getOrElse(0)
             if (pax > 0) Option(MuiChip(
-              label = VdomNode(s"${country.threeLetterCode} ($pax)"),
+              label = VdomNode(s"($pax) ${country.threeLetterCode}"),
               sx = SxProps(Map(
                 "color" -> "#FFFFFF",
                 "backgroundColor" -> "#316CCC",
