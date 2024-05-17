09c03b3f6bc9c2a2ca2a56e5d52d2587cf6d17e1
Nilesh Gupta <nilesh.nemichandgupta@digital.homeoffice.gov.uk>
2024-05-17 14:33:41 +0100

    using X-Forward- instead of keycloak-gatekeeper

--- server/src/main/scala/uk/gov/homeoffice/drt/routes/UserRoleProviderLike.scala
+++ server/src/main/scala/uk/gov/homeoffice/drt/routes/UserRoleProviderLike.scala
@@ -12,7 +12,8 @@ trait UserRoleProviderLike {
 
   val userService: UserTableLike
 
-  def userRolesFromHeader(headers: Headers): Set[Role] = headers.get("X-Auth-Roles").map(_.split(",").flatMap(Roles.parse).toSet).getOrElse(Set.empty[Role])
+  def userRolesFromHeader(headers: Headers): Set[Role] = headers.get("X-Forwarded-Groups").map(_.split(",")).map(_.map(_.split(":").last))
+      .map(_.flatMap(Roles.parse).toSet).getOrElse(Set.empty[Role])
 
   def getRoles(config: Configuration, headers: Headers, session: Session): Set[Role]
 
@@ -21,12 +22,12 @@ trait UserRoleProviderLike {
     val baseRoles = Set()
     val roles: Set[Role] =
       getRoles(config, headers, session) ++ baseRoles
-    val email = headers.get("X-Auth-Email").getOrElse("Unknown")
+    val email = headers.get("X-Forwarded-Email").getOrElse("Unknown")
 
     LoggedInUser(
       email = email,
-      userName = headers.get("X-Auth-Username").getOrElse(email),
-      id = headers.get("X-Auth-Userid").getOrElse(email),
+      userName = headers.get("X-Forwarded-Preferred-Username").getOrElse(email),
+      id = headers.get("X-Forwarded-Preferred-Username").getOrElse(email),
       roles = roles)
   }
 }
