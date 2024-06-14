a8edf58dd508f1e79fb08857c1e0b8f30045eb07
Rich Birch <ringo@albumsnaps.com>
2024-06-14 08:11:45 +0100

    DRTII-1508 Turn off proxy buffering for streamed downloads

--- server/src/main/scala/controllers/application/exports/CsvFileStreaming.scala
+++ server/src/main/scala/controllers/application/exports/CsvFileStreaming.scala
@@ -29,8 +29,13 @@ object CsvFileStreaming {
       case s if s.nonEmpty => writeable.transform(s)
     }
 
+    val headers = Map(
+      "Content-Type" -> mimeType,
+      "X-Accel-Buffering" -> "no",
+    ) ++ maybeFileName.map(fn => "Content-Disposition" -> s"attachment; filename=$fn")
+    
     Result(
-      header = ResponseHeader(200, Map("Content-Type" -> mimeType) ++ maybeFileName.map(fn => "Content-Disposition" -> s"attachment; filename=$fn")),
+      header = ResponseHeader(200, headers),
       body = HttpEntity.Streamed(byteStringStream, None, writeable.contentType)
     )
   }
