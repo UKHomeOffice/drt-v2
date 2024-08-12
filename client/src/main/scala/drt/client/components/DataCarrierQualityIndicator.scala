ede911096eea598d1c11de6c10108685b38b6eb3
Nilesh Gupta <nilesh.nemichandgupta@digital.homeoffice.gov.uk>
2024-08-12 12:18:18 +0100

    using tooltip for indicator
    Signed-off-by: Nilesh Gupta <nilesh.nemichandgupta@digital.homeoffice.gov.uk>

--- client/src/main/scala/drt/client/components/DataCarrierQualityIndicator.scala
+++ client/src/main/scala/drt/client/components/DataCarrierQualityIndicator.scala
@@ -7,25 +7,45 @@ import japgolly.scalajs.react.component.Scala.{Component, Unmounted}
 import japgolly.scalajs.react.vdom.html_<^.{<, ^, _}
 import japgolly.scalajs.react.{CtorType, _}
 import uk.gov.homeoffice.drt.ports.Terminals.Terminal
-
-import scala.scalajs.js
+import io.kinoplan.scalajs.react.material.ui.core.MuiTooltip
+import io.kinoplan.scalajs.react.material.ui.core.system.{SxProps, ThemeProvider}
 
 object DataCarrierQualityIndicator {
   case class Props(dq: FlightComponents.DataQuality, terminal: Terminal, classPrefix: String)
 
-  case class State(open: Boolean)
+  case class State(showTooltip: Boolean)
 
-  val component: Component[Props, State, Unit, CtorType.Props] = ScalaComponent.builder[Props]("ArrivalTableRow")
+  val component: Component[Props, State, Unit, CtorType.Props] = ScalaComponent.builder[Props]("TableRow")
     .initialState(State(false))
-    .renderPS { case (_, props, _) =>
-      ThemeProvider(DrtTheme.tooltipTheme)(
-        <.div(^.className := s"data-quality data-quality__${props.dq.`type`} pax-rag-${props.dq.`type`}",
-          <.dfn(^.className := "data-quality-indicator",
-              ^.title := props.dq.maybeTooltip.getOrElse(""),
-              ^.onPointerEnter --> Callback {
-                GoogleEventTracker.sendEvent(props.terminal.toString, "Info button click", "Data quality", props.dq.text)
-              },
-              props.dq.text)))
+    .renderPS { case (scope, props, state) =>
+      <.div(
+        ThemeProvider(DrtTheme.tooltipTheme)(
+          <.dfn(^.className := "data-quality-indicator question-hover",
+            props.dq.text,
+            props.dq.maybeTooltip.map { tt =>
+              <.span(
+                ^.className := "data-quality__more-info",
+                MuiTooltip(
+                  title = tt,
+                  placement = "bottom-end",
+                  onOpen = (_: ReactEventFromHtml) => Callback {
+                    GoogleEventTracker.sendEvent(props.terminal.toString, "Info button click", "Data quality", props.dq.text)
+                  },
+                  open = state.showTooltip,
+                  onClose = (_: ReactEventFromHtml) => scope.modState(s => s.copy(showTooltip = false)),
+                  sx = SxProps(Map("fontSize" -> "inherit")),
+                )
+              )
+            },
+            ^.onClick ==> {
+              e => {
+                e.preventDefault()
+                scope.modState(s => s.copy(showTooltip = !s.showTooltip))
+              }
+            },
+          )),
+        ^.className := s"data-quality data-quality__${props.dq.`type`} ${props.classPrefix}-${props.dq.`type`}",
+      )
     }
     .build
 
