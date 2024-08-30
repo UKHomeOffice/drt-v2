6f1070b987e3fa454b130ec931d16beea6d84883
Rich Birch <ringo@albumsnaps.com>
2024-08-30 17:13:55 +0100

    Fix min staff service type

--- server/src/main/scala/module/DrtModule.scala
+++ server/src/main/scala/module/DrtModule.scala
@@ -119,7 +119,7 @@ class DrtModule extends AbstractModule with AkkaGuiceSupport {
   def provideGovNotifyEmail: GovNotifyEmail = new GovNotifyEmail(drtParameters.govNotifyApiKey)
 
   @Provides
-  def minimumStaffingService: MinimumStaffingServiceImpl = MinimumStaffingServiceImpl(
+  def minimumStaffingService: MinimumStaffingService = MinimumStaffingServiceImpl(
     portCode = airportConfig.portCode,
     now = now,
     forecastMaxDays = drtParameters.forecastMaxDays,
