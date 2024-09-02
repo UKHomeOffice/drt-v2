b67c748d2d07aa5adc0f210636b968dead0a4969
Nilesh Gupta <nilesh.nemichandgupta@digital.homeoffice.gov.uk>
2024-09-02 14:50:39 +0100

    Adding updated version for drt react
    Signed-off-by: Nilesh Gupta <nilesh.nemichandgupta@digital.homeoffice.gov.uk>

--- project/Settings.scala
+++ project/Settings.scala
@@ -99,7 +99,7 @@ object Settings {
     "react-markdown" -> "9.0.1",
     "@types/react-dom" -> react,
     "css-loader" -> "6.7.2",
-    "@drt/drt-react"-> "https://github.com/UKHomeOffice/drt-react.git#85f28a4b21c1926f74d7d2fa8c0890fa109eda62"
+    "@drt/drt-react"-> "https://github.com/UKHomeOffice/drt-react.git#1d5ebae15f422746958d8a4018136fee1ac4c653"
   )
 
   /** Dependencies only used by the JS project (note the use of %%% instead of %%) */
