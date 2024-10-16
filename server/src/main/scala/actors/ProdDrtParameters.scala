dd509bf60968fa9e68989a8321eeb3076e9e4730
Nilesh Gupta <guptanilesh13@gmail.com>
2024-10-16 11:21:39 +0100

    feature flag for port to enable staffing changes
    Signed-off-by: Nilesh Gupta <guptanilesh13@gmail.com>

--- server/src/main/scala/actors/ProdDrtParameters.scala
+++ server/src/main/scala/actors/ProdDrtParameters.scala
@@ -8,6 +8,7 @@ import uk.gov.homeoffice.drt.time.{SDate, SDateLike}
 
 import java.nio.file.{Files, Paths}
 import scala.concurrent.duration._
+import scala.util.Try
 
 trait DrtParameters {
   val gateWalkTimesFilePath: Option[String]
@@ -76,7 +77,7 @@ object DrtParameters {
   }
 }
 
-case class ProdDrtParameters@Inject()(config: Configuration) extends DrtParameters {
+case class ProdDrtParameters @Inject()(config: Configuration) extends DrtParameters {
   val log: Logger = LoggerFactory.getLogger(getClass)
 
   override val gateWalkTimesFilePath: Option[String] =
@@ -139,6 +140,8 @@ case class ProdDrtParameters@Inject()(config: Configuration) extends DrtParamete
 
   override val isTestEnvironment: Boolean = config.getOptional[String]("env").getOrElse("prod") == "test"
 
-  override val enableStaffPlanningChange: Boolean = config.get[Boolean]("feature-flags.enable-staff-planning-change")
-
+  override val enableStaffPlanningChange: Boolean = Try(
+    config.get[String]("feature-flags.enable-ports-staff-planning-change").split(",")
+      .map(_.toUpperCase).contains(config.get[String]("portcode").toUpperCase)
+  ).getOrElse(false)
 }
