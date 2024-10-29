1c6774bbe182678fa1be0254ddb1bd15b20c55a5
Nilesh Gupta <guptanilesh13@gmail.com>
2024-10-29 09:34:12 +0000

    Adding test for almost 18k shiftsassignment
    Signed-off-by: Nilesh Gupta <guptanilesh13@gmail.com>

--- server/src/test/scala/actors/persistent/staffing/SplitUtilSpec.scala
+++ server/src/test/scala/actors/persistent/staffing/SplitUtilSpec.scala
@@ -36,29 +36,6 @@ class SplitUtilSpec extends Specification {
       result.toSet mustEqual expectedResult
     }
 
-  "update shift if shift exists before" >> {
-
-    val existingShiftStartTime = SDate(s"2017-01-01T07:00").millisSinceEpoch
-    val existingShiftEndTime = SDate(s"2017-01-01T8:00").millisSinceEpoch
-
-    val startTime = SDate(s"2017-01-01T07:00").millisSinceEpoch
-    val endTime = SDate(s"2017-01-01T9:00").millisSinceEpoch
-    val expectedResult = Set(
-      StaffAssignment("Morning", T1, SDate("2017-01-01T07:00:00Z").millisSinceEpoch, SDate("2017-01-01T07:14:00Z").millisSinceEpoch, 10, None),
-      StaffAssignment("Morning", T1, SDate("2017-01-01T07:15:00Z").millisSinceEpoch, SDate("2017-01-01T07:29:00Z").millisSinceEpoch, 10, None),
-      StaffAssignment("Morning", T1, SDate("2017-01-01T07:30:00Z").millisSinceEpoch, SDate("2017-01-01T07:44:00Z").millisSinceEpoch, 10, None),
-      StaffAssignment("Morning", T1, SDate("2017-01-01T07:45:00Z").millisSinceEpoch, SDate("2017-01-01T07:59:00Z").millisSinceEpoch, 10, None),
-      StaffAssignment("Morning", T1, SDate("2017-01-01T08:00:00Z").millisSinceEpoch, SDate("2017-01-01T08:14:00Z").millisSinceEpoch, 10, None),
-      StaffAssignment("Morning", T1, SDate("2017-01-01T08:15:00Z").millisSinceEpoch, SDate("2017-01-01T08:29:00Z").millisSinceEpoch, 10, None),
-      StaffAssignment("Morning", T1, SDate("2017-01-01T08:30:00Z").millisSinceEpoch, SDate("2017-01-01T08:44:00Z").millisSinceEpoch, 10, None),
-      StaffAssignment("Morning", T1, SDate("2017-01-01T08:45:00Z").millisSinceEpoch, SDate("2017-01-01T08:59:00Z").millisSinceEpoch, 10, None))
-    val result: Seq[StaffAssignmentLike] = SplitUtil.applyUpdatedShifts(
-      Seq(StaffAssignment("Morning", T1, existingShiftStartTime, existingShiftEndTime, 5, None)),
-      Seq(StaffAssignment("Morning", T1, startTime, endTime, 10, None)))
-    result.toSet mustEqual expectedResult
-  }
-
-
     "update shift if shift exists before" >> {
 
       val existingShiftStartTime = SDate(s"2017-01-01T07:00").millisSinceEpoch
@@ -81,6 +58,31 @@ class SplitUtilSpec extends Specification {
       result.toSet mustEqual expectedResult
     }
 
-}
+    "should not take too long to apply update to 6 months of 15 minute existing shifts" >> {
+      val existingShifts = ShiftAssignments(
+        (0 until 96 * 180).map { slot =>
+          StaffAssignment(
+            "Morning",
+            T1,
+            SDate(s"2017-01-01T07:00").addMinutes(15 * slot).millisSinceEpoch,
+            SDate(s"2017-01-01T07:00").addMinutes((15 * slot) + 14).millisSinceEpoch,
+            5,
+            None)
+        }
+      )
+
+      val updateShifts = Seq(
+        StaffAssignment("Morning", T1, SDate(s"2017-01-01T12:00").millisSinceEpoch, SDate(s"2017-01-01T18:59").millisSinceEpoch, 10, None)
+      )
+
+
+      val start = System.currentTimeMillis()
+      SplitUtil.applyUpdatedShifts(existingShifts.assignments, updateShifts)
+      val end = System.currentTimeMillis()
+      println(s"Time taken: ${end - start}")
+      (end - start) must be_<(100L)
+    }
+
+  }
 
 }
