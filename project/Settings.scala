ea417e7d01ce169898841585f751ce3fd6cb182f
Nilesh Gupta <guptanilesh13@gmail.com>
2024-11-11 08:45:38 +0000

    update drt react component
    Signed-off-by: Nilesh Gupta <guptanilesh13@gmail.com>

--- project/Settings.scala
+++ project/Settings.scala
@@ -99,7 +99,7 @@ object Settings {
     "react-markdown" -> "9.0.1",
     "@types/react-dom" -> react,
     "css-loader" -> "6.7.2",
-    "@drt/drt-react"-> "https://github.com/UKHomeOffice/drt-react.git#23eec837a3ef9e8d2b289bac54f610ae353398d7",
+    "@drt/drt-react"-> "https://github.com/UKHomeOffice/drt-react.git#a61beb47561b84e5ff43214eb406f9bafc81db15",
   )
 
   /** Dependencies only used by the JS project (note the use of %%% instead of %%) */
