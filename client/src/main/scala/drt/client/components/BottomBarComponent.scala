2901c27c2ac30f65cd5087ea5b939acdb4ad69b3
Nilesh Gupta <guptanilesh13@gmail.com>
2024-11-27 09:19:03 +0000

    adding url as separate param to feedbackUrl
    Signed-off-by: Nilesh Gupta <guptanilesh13@gmail.com>

--- client/src/main/scala/drt/client/components/BottomBarComponent.scala
+++ client/src/main/scala/drt/client/components/BottomBarComponent.scala
@@ -12,14 +12,16 @@ trait BottomBarProps extends js.Object {
   var email: String = js.native
   var onClickAccessibilityStatement: js.Function0[Unit] = js.native
   var url: String = js.native
+  var feedbackUrl: String = js.native
 }
 
 object BottomBarProps {
   def apply(teamEmail: String, onClickAccessibilityStatement: js.Function0[Unit], feedbackUrl: String): BottomBarProps = {
     val p = (new js.Object).asInstanceOf[BottomBarProps]
+    p.url = "#accessibility/"
     p.email = teamEmail
     p.onClickAccessibilityStatement = onClickAccessibilityStatement
-    p.url = feedbackUrl
+    p.feedbackUrl = feedbackUrl
     p
   }
 }
