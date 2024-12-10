ea1548f5f3be7c606f264697931f29e3a1bacec7
Nilesh Gupta <guptanilesh13@gmail.com>
2024-12-10 10:09:12 +0000

    update drt react with update style
    Signed-off-by: Nilesh Gupta <guptanilesh13@gmail.com>

--- project/Settings.scala
+++ project/Settings.scala
@@ -99,7 +99,7 @@ object Settings {
     "react-markdown" -> "9.0.1",
     "@types/react-dom" -> react,
     "css-loader" -> "6.7.2",
-    "@drt/drt-react"-> "https://github.com/UKHomeOffice/drt-react.git#eb2cb016b95547f4076a3959774ed49e286b7cd8",
+    "@drt/drt-react"-> "https://github.com/UKHomeOffice/drt-react.git#6da3e0fe541c2e23ff04d335406d10a717120ab9",
   )
 
   /** Dependencies only used by the JS project (note the use of %%% instead of %%) */
