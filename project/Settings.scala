d75d54219cc3f299e8fda231dd2ff75356c6efab
Rich Birch <ringo@albumsnaps.com>
2025-01-21 13:28:41 +0000

    DRTII-1692 Upgrades

--- project/Settings.scala
+++ project/Settings.scala
@@ -25,7 +25,7 @@ object Settings {
   /** Declare global dependency versions here to avoid mismatches in multi part dependencies */
   //noinspection ScalaStyle
   object versions {
-    val drtLib = "v202501016_1"
+    val drtLib = "v1061"
 
     val scala = "2.13.12"
     val scalaDom = "2.8.0"
@@ -42,7 +42,7 @@ object Settings {
     val specs2 = "4.20.3"
     val react = "18.2.0"
 
-    val bootstrap = "3.3.6"
+    val bootstrap = "5.3.3"
 
     val renjin = "0.9.2725"
     val csvCommons = "1.13.0"
@@ -71,7 +71,7 @@ object Settings {
     val mockito = "5.15.2"
     val rtVersion = "4.0.2"
     val jakartaXmlWsApi = "4.0.1"
-    val scalatestplusPlay = "7.0.0"
+    val scalatestplusPlay = "7.0.1"
     val nettyAll = "4.1.116.Final"
     val jwtCore = "9.4.5"
   }
@@ -137,6 +137,7 @@ object Settings {
     "com.lihaoyi" %%% "upickle" % uPickle,
     "uk.gov.homeoffice" %%% "drt-lib" % drtLib exclude("org.apache.spark", "spark-mllib_2.13"),
     "io.suzaku" %%% "boopickle" % booPickle,
+    "org.webjars" % "webjars-locator" % "0.52",
   ))
 
   /** Dependencies only used by the JVM project */
@@ -202,7 +203,11 @@ object Settings {
     "org.mockito" % "mockito-core" % mockito % Test,
     "org.scalatestplus.play" %% "scalatestplus-play" % scalatestplusPlay % Test,
     "org.scalatestplus" %% "mockito-3-4" % "3.2.10.0" % Test,
+
+    "org.webjars" % "font-awesome" % "6.7.1" % Provided,
     "org.webjars" % "bootstrap" % bootstrap % Provided,
+    "org.webjars" %% "webjars-play" % "3.0.2",
+    "org.webjars" % "webjars-locator" % "0.52",
 
     "io.netty" % "netty-all" % nettyAll,
 
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -5,17 +5,18 @@ ThisBuild / libraryDependencySchemes += "org.scala-lang.modules" %% "scala-xml" 
 
 addSbtPlugin("org.scoverage" % "sbt-scoverage" % "2.0.9")
 
-addSbtPlugin("org.portable-scala" % "sbt-scalajs-crossproject" % "1.3.1")
+addSbtPlugin("org.portable-scala" % "sbt-scalajs-crossproject" % "1.3.2")
 
 addSbtPlugin("org.scala-js" % "sbt-scalajs" % "1.18.1")
 
 addSbtPlugin("ch.epfl.scala" % "sbt-web-scalajs-bundler" % "0.21.1")
 
-addSbtPlugin("com.typesafe.sbt" % "sbt-less" % "1.1.2")
+//addSbtPlugin("com.typesafe.sbt" % "sbt-less" % "1.1.2")
+addSbtPlugin("com.github.sbt" % "sbt-less" % "2.0.0")
 
 addSbtPlugin("com.github.sbt" % "sbt-native-packager" % "1.9.9")
 
-addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.8.19")
+addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.9.6")
 
 addSbtPlugin("com.typesafe.sbt" % "sbt-digest" % "1.1.4")
 
@@ -24,5 +25,7 @@ addSbtPlugin("com.eed3si9n" % "sbt-buildinfo" % "0.11.0")
 addSbtPlugin("com.sksamuel.scapegoat" %% "sbt-scapegoat" % "1.2.2")
 
 addSbtPlugin("net.vonbuchholtz" % "sbt-dependency-check" % "5.1.0")
+
+addSbtPlugin("com.github.sbt" % "sbt-js-engine" % "1.3.9")
 
 addDependencyTreePlugin
