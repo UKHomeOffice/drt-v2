8a2bb9d336e7760c41fbd97071de0a8d72fcdb56
Nilesh Gupta <guptanilesh13@gmail.com>
2025-01-31 11:45:51 +0000

    using drt-version 1.6.5
    Signed-off-by: Nilesh Gupta <guptanilesh13@gmail.com>

--- project/Settings.scala
+++ project/Settings.scala
@@ -99,7 +99,7 @@ object Settings {
     "react-markdown" -> "9.0.1",
     "@types/react-dom" -> react,
     "css-loader" -> "6.7.2",
-    "@drt/drt-react"-> "https://github.com/UKHomeOffice/drt-react.git#8235c2363908927890da92d58fe6cc4269c8752b",
+    "@drt/drt-react"-> "https://github.com/UKHomeOffice/drt-react.git#1.6.5",
   )
 
   /** Dependencies only used by the JS project (note the use of %%% instead of %%) */
