0639ba6fec27d725db0aa7aa1d38f7290f6fefbf
Rich Birch <ringo@albumsnaps.com>
2025-02-20 16:30:33 +0000

    Finally got drt-react updated!

--- project/Settings.scala
+++ project/Settings.scala
@@ -103,7 +103,7 @@ object Settings {
     "react-markdown" -> "9.0.1",
     "@types/react-dom" -> react,
     "css-loader" -> "6.7.2",
-    "@drt/drt-react"-> "https://github.com/UKHomeOffice/drt-react.git#1.6.7",
+    "@drt/drt-react"-> "UKHomeOffice/drt-react#1.6.8",
   )
 
   /** Dependencies only used by the JS project (note the use of %%% instead of %%) */
