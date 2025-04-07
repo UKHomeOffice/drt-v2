24eb8793d09405a182e4135f444390e1c9c62758
Rich Birch <ringo@albumsnaps.com>
2025-04-07 12:34:03 +0100

    DRTII-1773 Fix timezone issue with terminal dash tests

--- e2e/cypress/e2e/port-dashboard.cy.ts
+++ e2e/cypress/e2e/port-dashboard.cy.ts
@@ -1,4 +1,4 @@
-import { todayAtUtcString as todayAtString } from '../support/time-helpers'
+import { todayAsLocalString } from '../support/time-helpers'
 import { currentTimeString as currentTimeString } from '../support/time-helpers'
 
 describe('Port dashboard', () => {
@@ -12,7 +12,7 @@ describe('Port dashboard', () => {
     .addFlight({
       "ICAO": "TS0124",
       "IATA": "TS0124",
-      "SchDT": todayAtString(10, 35),
+      "SchDT": todayAsLocalString(10, 35),
       "ActChoxDT": currentTimeString(),
       "ActPax": 0,
       "Status": "Cancelled"
@@ -20,7 +20,7 @@ describe('Port dashboard', () => {
     .addFlight({
       "ICAO": "TS0122",
       "IATA": "TS0122",
-      "SchDT": todayAtString(10, 40),
+      "SchDT": todayAsLocalString(10, 40),
       "ActChoxDT": currentTimeString(),
       "ActPax": 300
     })
