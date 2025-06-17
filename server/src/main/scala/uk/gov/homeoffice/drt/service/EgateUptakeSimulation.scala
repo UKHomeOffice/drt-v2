bc22c284b721ba40d8ac4794715bcc6d066e4d18
Rich Birch <ringo@albumsnaps.com>
2025-06-17 14:17:14 +0100

    DRTII-1864 Construct functions to produce stats

--- server/src/main/scala/uk/gov/homeoffice/drt/service/EgateUptakeSimulation.scala
+++ server/src/main/scala/uk/gov/homeoffice/drt/service/EgateUptakeSimulation.scala
@@ -1,10 +1,10 @@
 package uk.gov.homeoffice.drt.service
 
-import org.apache.pekko.http.impl.engine.rendering.RenderSupport.ChunkTransformer.flow.mapAsync
 import org.apache.pekko.stream.Materializer
 import org.apache.pekko.stream.scaladsl.{Sink, Source}
 import uk.gov.homeoffice.drt.arrivals.{ApiFlightWithSplits, UniqueArrival}
 import uk.gov.homeoffice.drt.models.ManifestLike
+import uk.gov.homeoffice.drt.ports.Queues.{EGate, Queue}
 import uk.gov.homeoffice.drt.ports.Terminals.Terminal
 import uk.gov.homeoffice.drt.time.{DateRange, LocalDate, SDate, UtcDate}
 
@@ -40,9 +40,54 @@ object EgateUptakeSimulation {
           case (date, terminal, flightsWithMaybeManifests) =>
             bxEgatePercentageForDateAndTerminal(date, terminal).map(bxEgatePercentage => (date, terminal, flightsWithMaybeManifests, bxEgatePercentage))
         }
-        .mapAsync(1) { case (date, terminal, flightsWithMaybeManifests, bxEgatePercentage) =>
-          
-        }
+      //        .mapAsync(1) { case (date, terminal, flightsWithMaybeManifests, bxEgatePercentage) =>
+      //
+      //        }
       eventualDailyStats.runWith(Sink.seq)
+    }
+
+  def bxVersusDrtEgatePercentageForDate(bxEgatePercentageForDateAndTerminal: (UtcDate, Terminal) => Future[Double],
+                                        drtEgatePercentageForDateAndTerminal: (UtcDate, Terminal) => Future[Double],
+                                       )
+                                       (implicit ec: ExecutionContext): (UtcDate, Terminal) => Future[(Double, Double)] =
+    (date, terminal) => {
+      for {
+        bxEgatePercentage <- bxEgatePercentageForDateAndTerminal(date, terminal)
+        drtEgatePercentage <- drtEgatePercentageForDateAndTerminal(date, terminal)
+      } yield (bxEgatePercentage, drtEgatePercentage)
+    }
+
+  def drtEgatePercentageForDateAndTerminal(flightsWithManifestsForDateAndTerminal: (UtcDate, Terminal) => Future[Seq[(ApiFlightWithSplits, Option[ManifestLike])]],
+                                           egateAndDeskPaxForFlight: (ApiFlightWithSplits, Option[ManifestLike], Double) => (Int, Int),
+                                          )
+                                          (implicit ec: ExecutionContext): (UtcDate, Terminal, Double) => Future[Double] =
+    (date, terminal, egateUptake) => {
+      flightsWithManifestsForDateAndTerminal(date, terminal)
+        .map { flightsWithManifests =>
+          val (egatePax, deskPax) = flightsWithManifests.map { case (flight, manifest) =>
+            egateAndDeskPaxForFlight(flight, manifest, egateUptake)
+          }.foldLeft((0, 0)) { case ((egateAcc, deskAcc), (egate, desk)) => (egateAcc + egate, deskAcc + desk) }
+          if (egatePax + deskPax > 0) {
+            (egatePax.toDouble / (egatePax + deskPax)) * 100.0
+          } else {
+            0.0
+          }
+        }
+    }
+
+  def bxEgatePercentageForDateAndTerminal(bxQueueTotalsForPortAndDate: (UtcDate, Terminal) => Future[Map[Queue, Int]],
+                                         )
+                                         (implicit ec: ExecutionContext): (UtcDate, Terminal) => Future[Double] =
+    (date, terminal) => {
+      bxQueueTotalsForPortAndDate(date, terminal)
+        .map { queueTotals =>
+          val egatePax = queueTotals.getOrElse(EGate, 0)
+          val totalPax = queueTotals.values.sum
+          if (totalPax > 0) {
+            (egatePax.toDouble / totalPax) * 100.0
+          } else {
+            0.0
+          }
+        }
     }
 }
