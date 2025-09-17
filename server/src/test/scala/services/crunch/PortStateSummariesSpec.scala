aed12a8053a69c882351569008789d0f71d84b47
Rich Birch <rich@freshcodelimited.com>
2025-09-17 12:45:16 +0100

    DRTII-1982 uncomment tests

--- server/src/test/scala/services/crunch/PortStateSummariesSpec.scala
+++ server/src/test/scala/services/crunch/PortStateSummariesSpec.scala
@@ -5,80 +5,83 @@ import drt.shared._
 import org.specs2.mutable.Specification
 import uk.gov.homeoffice.drt.arrivals.{ApiFlightWithSplits, UniqueArrival}
 import uk.gov.homeoffice.drt.models.{CrunchMinute, TQM}
+import uk.gov.homeoffice.drt.ports.Queues
+import uk.gov.homeoffice.drt.ports.Queues.{EeaDesk, Queue}
 import uk.gov.homeoffice.drt.ports.Terminals.T1
 import uk.gov.homeoffice.drt.time.SDate
+import uk.gov.homeoffice.drt.time.TimeZoneHelper.{europeLondonTimeZone, utcTimeZone}
 
 import scala.collection.immutable.SortedMap
 
 class PortStateSummariesSpec extends Specification {
-//  "Given a port state with crunch minutes for 2 queues over 30 minutes " +
-//    "When I ask for a 4 period summary of 15 minutes each period " +
-//    "Then I should see crunch minutes for all periods with the sum of any loads, and the max of any desks or wait times" >> {
-//    val queues: List[Queues.Queue] = List(Queues.EeaDesk, Queues.EGate)
-//    val cmsList = for {
-//      queue <- queues
-//      minute <- 0 to 29
-//    } yield {
-//      CrunchMinute(T1, queue, minute.toLong * 60000, minute.toDouble, minute.toDouble, minute, minute, Option(minute), Option(minute), Option(minute), Option(minute))
-//    }
-//
-//    val cmsMap = SortedMap[TQM, CrunchMinute]() ++ cmsList.map(cm => (TQM(cm), cm)).toMap
-//    val portState = PortState(SortedMap[UniqueArrival, ApiFlightWithSplits](), cmsMap, SortedMap[TM, StaffMinute]())
-//
-//    val periods = 4
-//    val periodSize = 15
-//    val summary: Map[MillisSinceEpoch, Map[Queue, CrunchMinute]] = portState.crunchSummary(SDate(0L), periods, periodSize, T1, queues)
-//
-//    val expected = Map(
-//      0L -> Map(
-//        Queues.EeaDesk -> CrunchMinute(T1, Queues.EeaDesk, 0, 105, 105, 14, 14, Option(14), Option(14), Option(14), Option(14)),
-//        Queues.EGate -> CrunchMinute(T1, Queues.EGate, 0, 105, 105, 14, 14, Option(14), Option(14), Option(14), Option(14))
-//      ),
-//      15L * 60000 -> Map(
-//        Queues.EeaDesk -> CrunchMinute(T1, Queues.EeaDesk, 15 * 60000, 330, 330, 29, 29, Option(29), Option(29), Option(29), Option(29)),
-//        Queues.EGate -> CrunchMinute(T1, Queues.EGate, 15 * 60000, 330, 330, 29, 29, Option(29), Option(29), Option(29), Option(29))
-//      ),
-//      30L * 60000 -> Map(
-//        Queues.EeaDesk -> CrunchMinute(T1, Queues.EeaDesk, 30 * 60000, 0, 0, 0, 0, None, None, None, None),
-//        Queues.EGate -> CrunchMinute(T1, Queues.EGate, 30 * 60000, 0, 0, 0, 0, None, None, None, None)
-//      ),
-//      45L * 60000 -> Map(
-//        Queues.EeaDesk -> CrunchMinute(T1, Queues.EeaDesk, 45 * 60000, 0, 0, 0, 0, None, None, None, None),
-//        Queues.EGate -> CrunchMinute(T1, Queues.EGate, 45 * 60000, 0, 0, 0, 0, None, None, None, None)
-//      )
-//    )
-//
-//    summary === expected
-//  }
-//
-//  "Given a port state with crunch minutes with no deployed or actual desks & wait times " +
-//    "When I ask for a summary  " +
-//    "Then I should see crunch minutes with None for all the deployed and actual desks and wait times values" >> {
-//    val terminal = T1
-//    val queues: List[Queue] = List(Queues.EeaDesk, Queues.EGate)
-//    val cmsList = for {
-//      queue <- queues
-//      minute <- 0 to 14
-//    } yield {
-//      CrunchMinute(terminal, queue, minute.toLong * 60000, minute.toDouble, minute.toDouble, minute, minute, None, None, None, None)
-//    }
-//
-//    val cmsMap = SortedMap[TQM, CrunchMinute]() ++ cmsList.map(cm => (TQM(cm), cm)).toMap
-//    val portState = PortState(SortedMap[UniqueArrival, ApiFlightWithSplits](), cmsMap, SortedMap[TM, StaffMinute]())
-//
-//    val periods = 1
-//    val periodSize = 15
-//    val summary: Map[MillisSinceEpoch, Map[Queue, CrunchMinute]] = portState.crunchSummary(SDate(0L), periods, periodSize, terminal, queues)
-//
-//    val expected = Map(
-//      0L -> Map(
-//        Queues.EeaDesk -> CrunchMinute(terminal, Queues.EeaDesk, 0, 105, 105, 14, 14, None, None, None, None),
-//        Queues.EGate -> CrunchMinute(terminal, Queues.EGate, 0, 105, 105, 14, 14, None, None, None, None)
-//      )
-//    )
-//
-//    summary === expected
-//  }
+  "Given a port state with crunch minutes for 2 queues over 30 minutes " +
+    "When I ask for a 4 period summary of 15 minutes each period " +
+    "Then I should see crunch minutes for all periods with the sum of any loads, and the max of any desks or wait times" >> {
+    val queues: List[Queues.Queue] = List(Queues.EeaDesk, Queues.EGate)
+    val cmsList = for {
+      queue <- queues
+      minute <- 0 to 29
+    } yield {
+      CrunchMinute(T1, queue, minute.toLong * 60000, minute.toDouble, minute.toDouble, minute, minute, Option(minute), Option(minute), Option(minute), Option(minute))
+    }
+
+    val cmsMap = SortedMap[TQM, CrunchMinute]() ++ cmsList.map(cm => (TQM(cm), cm)).toMap
+    val portState = PortState(SortedMap[UniqueArrival, ApiFlightWithSplits](), cmsMap, SortedMap[TM, StaffMinute]())
+
+    val periods = 4
+    val periodSize = 15
+    val summary: Map[MillisSinceEpoch, Map[Queue, CrunchMinute]] = portState.crunchSummary(SDate(0L), periods, periodSize, T1, queues)
+
+    val expected = Map(
+      0L -> Map(
+        Queues.EeaDesk -> CrunchMinute(T1, Queues.EeaDesk, 0, 105, 105, 14, 14, Option(14), Option(14), Option(14), Option(14)),
+        Queues.EGate -> CrunchMinute(T1, Queues.EGate, 0, 105, 105, 14, 14, Option(14), Option(14), Option(14), Option(14))
+      ),
+      15L * 60000 -> Map(
+        Queues.EeaDesk -> CrunchMinute(T1, Queues.EeaDesk, 15 * 60000, 330, 330, 29, 29, Option(29), Option(29), Option(29), Option(29)),
+        Queues.EGate -> CrunchMinute(T1, Queues.EGate, 15 * 60000, 330, 330, 29, 29, Option(29), Option(29), Option(29), Option(29))
+      ),
+      30L * 60000 -> Map(
+        Queues.EeaDesk -> CrunchMinute(T1, Queues.EeaDesk, 30 * 60000, 0, 0, 0, 0, None, None, None, None),
+        Queues.EGate -> CrunchMinute(T1, Queues.EGate, 30 * 60000, 0, 0, 0, 0, None, None, None, None)
+      ),
+      45L * 60000 -> Map(
+        Queues.EeaDesk -> CrunchMinute(T1, Queues.EeaDesk, 45 * 60000, 0, 0, 0, 0, None, None, None, None),
+        Queues.EGate -> CrunchMinute(T1, Queues.EGate, 45 * 60000, 0, 0, 0, 0, None, None, None, None)
+      )
+    )
+
+    summary === expected
+  }
+
+  "Given a port state with crunch minutes with no deployed or actual desks & wait times " +
+    "When I ask for a summary  " +
+    "Then I should see crunch minutes with None for all the deployed and actual desks and wait times values" >> {
+    val terminal = T1
+    val queues: List[Queue] = List(Queues.EeaDesk, Queues.EGate)
+    val cmsList = for {
+      queue <- queues
+      minute <- 0 to 14
+    } yield {
+      CrunchMinute(terminal, queue, minute.toLong * 60000, minute.toDouble, minute.toDouble, minute, minute, None, None, None, None)
+    }
+
+    val cmsMap = SortedMap[TQM, CrunchMinute]() ++ cmsList.map(cm => (TQM(cm), cm)).toMap
+    val portState = PortState(SortedMap[UniqueArrival, ApiFlightWithSplits](), cmsMap, SortedMap[TM, StaffMinute]())
+
+    val periods = 1
+    val periodSize = 15
+    val summary: Map[MillisSinceEpoch, Map[Queue, CrunchMinute]] = portState.crunchSummary(SDate(0L), periods, periodSize, terminal, queues)
+
+    val expected = Map(
+      0L -> Map(
+        Queues.EeaDesk -> CrunchMinute(terminal, Queues.EeaDesk, 0, 105, 105, 14, 14, None, None, None, None),
+        Queues.EGate -> CrunchMinute(terminal, Queues.EGate, 0, 105, 105, 14, 14, None, None, None, None)
+      )
+    )
+
+    summary === expected
+  }
 
   "Given a port state with staff minutes for 2 queues over 30 minutes " +
     "When I ask for a 4 period summary of 15 minutes each period " +
@@ -103,39 +106,39 @@ class PortStateSummariesSpec extends Specification {
     summary === expected
   }
 
-//  "PortState " should {
-//    "correctly calculate UTC time days when creating a summary spanning a clock change" in {
-//      val summaries = PortState.empty.dailyCrunchSummary(SDate("2022-03-27", utcTimeZone), 7, T1, (_, _, _) => Seq(EeaDesk))
-//      val days = summaries.keys.toList.sorted.map(SDate(_))
-//
-//      val expected = List(
-//        SDate(2022, 3, 27, 0, 0, utcTimeZone),
-//        SDate(2022, 3, 28, 0, 0, utcTimeZone),
-//        SDate(2022, 3, 29, 0, 0, utcTimeZone),
-//        SDate(2022, 3, 30, 0, 0, utcTimeZone),
-//        SDate(2022, 3, 31, 0, 0, utcTimeZone),
-//        SDate(2022, 4, 1, 0, 0, utcTimeZone),
-//        SDate(2022, 4, 2, 0, 0, utcTimeZone),
-//      )
-//
-//      days === expected
-//    }
-//
-//    "correctly calculate local time days when creating a summary spanning a clock change" in {
-//      val summaries = PortState.empty.dailyCrunchSummary(SDate("2022-03-27", europeLondonTimeZone), 7, T1, (_, _, _) => Seq(EeaDesk))
-//      val days = summaries.keys.toList.sorted.map(SDate(_))
-//
-//      val expected = List(
-//        SDate(2022, 3, 27, 0, 0, utcTimeZone),
-//        SDate(2022, 3, 27, 23, 0, utcTimeZone),
-//        SDate(2022, 3, 28, 23, 0, utcTimeZone),
-//        SDate(2022, 3, 29, 23, 0, utcTimeZone),
-//        SDate(2022, 3, 30, 23, 0, utcTimeZone),
-//        SDate(2022, 3, 31, 23, 0, utcTimeZone),
-//        SDate(2022, 4, 1, 23, 0, utcTimeZone),
-//      )
-//
-//      days === expected
-//    }
-//  }
+  "PortState " should {
+    "correctly calculate UTC time days when creating a summary spanning a clock change" in {
+      val summaries = PortState.empty.dailyCrunchSummary(SDate("2022-03-27", utcTimeZone), 7, T1, (_, _, _) => Seq(EeaDesk))
+      val days = summaries.keys.toList.sorted.map(SDate(_))
+
+      val expected = List(
+        SDate(2022, 3, 27, 0, 0, utcTimeZone),
+        SDate(2022, 3, 28, 0, 0, utcTimeZone),
+        SDate(2022, 3, 29, 0, 0, utcTimeZone),
+        SDate(2022, 3, 30, 0, 0, utcTimeZone),
+        SDate(2022, 3, 31, 0, 0, utcTimeZone),
+        SDate(2022, 4, 1, 0, 0, utcTimeZone),
+        SDate(2022, 4, 2, 0, 0, utcTimeZone),
+      )
+
+      days === expected
+    }
+
+    "correctly calculate local time days when creating a summary spanning a clock change" in {
+      val summaries = PortState.empty.dailyCrunchSummary(SDate("2022-03-27", europeLondonTimeZone), 7, T1, (_, _, _) => Seq(EeaDesk))
+      val days = summaries.keys.toList.sorted.map(SDate(_))
+
+      val expected = List(
+        SDate(2022, 3, 27, 0, 0, utcTimeZone),
+        SDate(2022, 3, 27, 23, 0, utcTimeZone),
+        SDate(2022, 3, 28, 23, 0, utcTimeZone),
+        SDate(2022, 3, 29, 23, 0, utcTimeZone),
+        SDate(2022, 3, 30, 23, 0, utcTimeZone),
+        SDate(2022, 3, 31, 23, 0, utcTimeZone),
+        SDate(2022, 4, 1, 23, 0, utcTimeZone),
+      )
+
+      days === expected
+    }
+  }
 }
