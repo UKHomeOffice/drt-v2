b21c1c7bece604b75e5aaaa3807dcd46e1a3aba8
Rich Birch <ringo@albumsnaps.com>
2025-10-16 17:52:56 +0100

    DRTII-2008 Move cap update function to immediately after flights live view is updated

--- server/src/main/scala/uk/gov/homeoffice/drt/crunchsystem/DrtSystemInterface.scala
+++ server/src/main/scala/uk/gov/homeoffice/drt/crunchsystem/DrtSystemInterface.scala
@@ -1,24 +1,27 @@
 package uk.gov.homeoffice.drt.crunchsystem
 
 import actors._
+import actors.routing.FeedArrivalsRouterActor
 import com.typesafe.config.ConfigFactory
 import controllers._
 import manifests.ManifestLookupLike
 import manifests.queues.SplitsCalculator
-import org.apache.pekko.NotUsed
+import org.apache.pekko.{Done, NotUsed}
 import org.apache.pekko.actor.ActorSystem
+import org.apache.pekko.pattern.ask
 import org.apache.pekko.stream.Materializer
-import org.apache.pekko.stream.scaladsl.Source
+import org.apache.pekko.stream.scaladsl.{Sink, Source}
 import org.apache.pekko.util.Timeout
 import play.api.Configuration
 import play.api.mvc.{Headers, Session}
 import queueus.{AdjustmentsNoop, ChildEGateAdjustments, QueueAdjustments, TerminalQueueAllocator}
 import services.crunch.CrunchSystem.paxTypeQueueAllocator
-import services.liveviews.{FlightsLiveView, QueuesLiveView}
+import services.liveviews.{FlightsLiveView, PassengersLiveView, QueuesLiveView}
 import slickdb.AkkaDbTables
 import uk.gov.homeoffice.drt.AppEnvironment
 import uk.gov.homeoffice.drt.AppEnvironment.AppEnvironment
-import uk.gov.homeoffice.drt.arrivals.{ApiFlightWithSplits, UniqueArrival}
+import uk.gov.homeoffice.drt.actor.state.ArrivalsState
+import uk.gov.homeoffice.drt.arrivals.{ApiFlightWithSplits, FeedArrival, UniqueArrival}
 import uk.gov.homeoffice.drt.auth.Roles
 import uk.gov.homeoffice.drt.auth.Roles.Role
 import uk.gov.homeoffice.drt.db.AggregatedDbTables
@@ -77,12 +80,6 @@ trait DrtSystemInterface extends UserRoleProviderLike
   lazy val flightsForPcpDateRange: (DateLike, DateLike, Seq[Terminal]) => Source[(UtcDate, Seq[ApiFlightWithSplits]), NotUsed] =
     flightDao.flightsForPcpDateRange(airportConfig.portCode, paxFeedSourceOrder, aggregatedDb.run)
 
-  lazy val updateFlightsLiveView: (Iterable[ApiFlightWithSplits], Iterable[UniqueArrival]) => Future[Unit] = {
-    val doUpdate = FlightsLiveView.updateFlightsLiveView(flightDao, aggregatedDb, airportConfig.portCode)
-    (updates, removals) =>
-      doUpdate(updates, removals)
-  }
-
   def getRoles(config: Configuration, headers: Headers, session: Session): Set[Role] = {
     if (params.isSuperUserMode) {
       Roles.availableRoles
@@ -117,6 +114,44 @@ trait DrtSystemInterface extends UserRoleProviderLike
 
     val paxQueueAllocator = paxTypeQueueAllocator(airportConfig.hasTransfer, TerminalQueueAllocator(airportConfig.terminalPaxTypeQueueAllocation))
     SplitsCalculator(paxQueueAllocator, airportConfig.terminalPaxSplits, queueAdjustments)
+  }
+
+  val flightsForDate: UtcDate => Future[Seq[ApiFlightWithSplits]] = {
+    val getFlights = FlightDao().getForUtcDate(airportConfig.portCode)
+    (d: UtcDate) => aggregatedDb.run(getFlights(d))
+  }
+
+  private lazy val aclArrivalsForDate: UtcDate => Future[ArrivalsState] = feedService.activeFeedActorsWithPrimary.find(_._1 == AclFeedSource) match {
+    case Some((_, _, _, actor)) =>
+      (date: UtcDate) =>
+        actor.ask(FeedArrivalsRouterActor.GetStateForDateRange(date, date))
+          .mapTo[Source[(UtcDate, Seq[FeedArrival]), NotUsed]]
+          .flatMap(_.runWith(Sink.fold(Seq[FeedArrival]())((acc, next) => acc ++ next._2)))
+          .map(f => ArrivalsState.empty(AclFeedSource) ++ f.map(_.toArrival(AclFeedSource)))
+    case None =>
+      (_: UtcDate) => Future.successful(ArrivalsState.empty(AclFeedSource))
+  }
+
+  private lazy val uniqueFlightsForDate = PassengersLiveView.uniqueFlightsForDate(
+    flights = flightsForDate,
+    baseArrivals = aclArrivalsForDate,
+    paxFeedSourceOrder = feedService.paxFeedSourceOrder,
+  )
+
+  private lazy val getCapacities = PassengersLiveView.capacityForDate(uniqueFlightsForDate)
+  private lazy val persistCapacity = PassengersLiveView.persistCapacityForDate(aggregatedDb, airportConfig.portCode)
+
+  lazy val updateAndPersistCapacity: UtcDate => Future[Done] =
+    PassengersLiveView.updateAndPersistCapacityForDate(getCapacities, persistCapacity)
+
+  lazy val updateFlightsLiveView: (Iterable[ApiFlightWithSplits], Iterable[UniqueArrival]) => Future[Unit] = {
+    val doUpdate = FlightsLiveView.updateFlightsLiveView(flightDao, aggregatedDb, airportConfig.portCode)
+    (updates, removals) =>
+      doUpdate(updates, removals)
+        .flatMap { _ =>
+          val datesAffected = updates.map(f => SDate(f.apiFlight.Scheduled).toUtcDate).toSet ++ removals.map(r => SDate(r.scheduled).toUtcDate).toSet
+          Future.sequence(datesAffected.map(updateAndPersistCapacity)).map(_ => Done)
+        }
   }
 
   lazy val applicationService: ApplicationService = ApplicationService(
