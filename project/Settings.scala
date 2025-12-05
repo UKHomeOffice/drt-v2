d573868ba754b08aafa259c8d7c244e86da37b24
Rich Birch <ringo@albumsnaps.com>
2025-12-05 12:00:58 +0000

    upgrade netty

--- project/Settings.scala
+++ project/Settings.scala
@@ -74,7 +74,7 @@ object Settings {
     val rtVersion = "4.0.3"
     val jakartaXmlWsApi = "4.0.2"
     val scalatestplusPlay = "7.0.2"
-    val nettyAll = "4.1.128.Final"
+    val nettyAll = "4.2.0.Final"
     val jwtCore = "11.0.3"
     val logback = "1.5.21"
     val logbackContrib = "0.1.5"
