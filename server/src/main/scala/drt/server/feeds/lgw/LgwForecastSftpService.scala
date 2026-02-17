d1627afa2100d6c16c1357fe92487b2eab0c86df
slawomirbedraHO <259381811+slawomirbedraHO@users.noreply.github.com>
2026-02-17 16:46:53 +0000

    DRTII-2068 | LGW forecast filename update. (#2314)
    * DRTII-2068 | LGW forecast filename update.
    
    Signed-off-by: slawekb <259381811+slawomirbedraHO@users.noreply.github.com>
    
    * DRTII-2068 | Commented out the Slack step temporarily.
    
    Signed-off-by: slawekb <259381811+slawomirbedraHO@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: slawekb <259381811+slawomirbedraHO@users.noreply.github.com>

--- server/src/main/scala/drt/server/feeds/lgw/LgwForecastSftpService.scala
+++ server/src/main/scala/drt/server/feeds/lgw/LgwForecastSftpService.scala
@@ -6,10 +6,11 @@ import net.schmizz.sshj.sftp.SFTPClient
 import net.schmizz.sshj.transport.verification.PromiscuousVerifier
 import net.schmizz.sshj.xfer.InMemoryDestFile
 import org.slf4j.{Logger, LoggerFactory}
-import uk.gov.homeoffice.drt.time.SDate
+import uk.gov.homeoffice.drt.time.{SDate, SDateLike}
 
 import java.io.{ByteArrayOutputStream, OutputStream}
 import scala.jdk.CollectionConverters.CollectionHasAsScala
+import scala.util.matching.Regex
 import scala.util.{Failure, Success, Try}
 
 
@@ -38,14 +39,14 @@ case class LgwForecastSftpService(ftpServer: String, port: Int, username: String
         ssh.close()
         maybeContent
       case Failure(t) =>
-        log.error(s"Failed to connect to sftp server: ${t.getMessage}")
+        log.error(s"[LgwForecastSftpService][latestContent] Failed to connect to sftp server: ${t.getMessage}")
         ssh.close()
         None
     }
   }
 
   private def contentForFile(sftp: SFTPClient, fileName: String): Option[String] = {
-    log.info(s"Fetching file '$fileName' from sftp server: $ftpServer:$port")
+    log.info(s"[LgwForecastSftpService][contentForFile] Fetching file '$fileName' from sftp server: $ftpServer:$port")
 
     val outputStream: ByteArrayOutputStream = new ByteArrayOutputStream()
 
@@ -60,30 +61,56 @@ case class LgwForecastSftpService(ftpServer: String, port: Int, username: String
     Try(sftp.get(fileName, output)) match {
       case Success(_) => Some(outputStream.toString())
       case Failure(e) =>
-        log.error(s"Failed to get file '$fileName' from sftp server: ${e.getMessage}")
+        log.error(s"[LgwForecastSftpService][contentForFile] Failed to get file '$fileName' from sftp server: ${e.getMessage}")
         None
     }
   }
 
-  private def latestFileName(sftp: SFTPClient): Option[String] = {
-    log.info(s"Fetching latest file name from sftp server: $ftpServer:$port")
+  private[lgw] def latestFileName(sftp: SFTPClient): Option[String] = {
+    log.info(s"[LgwForecastSftpService][latestFileName] Fetching latest file name from sftp server: $ftpServer:$port")
 
-    sftp
-      .ls(pathPrefix).asScala
-      .filter(_.getName.contains("-LGWArrForecast.csv"))
-      .map { f =>
-        Try(f.getName.split("-", 4)).map {
+    val legacyFileName: String = "-LGWArrForecast.csv"
+    val newFilePattern: Regex = """LGWARRFORECAST-(\d{4})-(\d{2})-(\d{2})\.csv""".r
+
+    val files = sftp.ls(pathPrefix).asScala.toList
+
+    val newFiles = files.flatMap { f =>
+      f.getName match {
+        case newFilePattern(year, month, day) =>
+          parseDate(year, month, day, f.getName).map(date => (date, f.getPath))
+        case _ =>
+          log.warn(s"[LgwForecastSftpService][latestFileName] Filename did not match pattern: ${f.getName}")
+          None
+      }
+    }
+
+    val legacyFiles = files.flatMap { f =>
+      if (f.getName.contains(legacyFileName)) {
+        Try(f.getName.split("-", 4)).toOption.flatMap {
           case Array(year, month, day, _) =>
-            (SDate(year.toInt, month.toInt, day.toInt), f.getPath)
+            parseDate(year, month, day, f.getName).map(date => (date, f.getPath))
+          case _ =>
+            log.warn(s"[LgwForecastSftpService][latestFileName] Legacy filename parsing failed: ${f.getName}")
+            None
         }
-      }
-      .collect {
-        case Success(someDate) => someDate
-      }
-      .toList
-      .sortBy(_._1.millisSinceEpoch)
-      .reverse
-      .headOption
+      } else None
+    }
+
+    newFiles.sortBy(_._1.millisSinceEpoch).reverse.headOption
+      .orElse(legacyFiles.sortBy(_._1.millisSinceEpoch).reverse.headOption)
       .map(_._2)
+      .orElse {
+        log.warn(s"[LgwForecastSftpService][latestFileName] No suitable forecast file found in $pathPrefix on $ftpServer:$port")
+        None
+      }
+  }
+
+  private def parseDate(year: String, month: String, day: String, fileName: String): Option[SDateLike] = {
+    Try(SDate(year.toInt, month.toInt, day.toInt)) match {
+      case Success(date) => Some(date)
+      case Failure(e) =>
+        log.warn(s"[LgwForecastSftpService][parseDate] Date parsing failed for file '$fileName': ${e.getMessage}")
+        None
+    }
   }
 }
