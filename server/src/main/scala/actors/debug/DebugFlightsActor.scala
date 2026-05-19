82c7e86d98dd45a1015aa54d6f56b0ced470b6a7
slawekb <259381811+slawomirbedraHO@users.noreply.github.com>
2026-05-19 16:01:42 +0100

    DRTII-2065 | Add flight trace endpoint and enhance debug functionality.
    Signed-off-by: slawekb <259381811+slawomirbedraHO@users.noreply.github.com>

--- server/src/main/scala/actors/debug/DebugFlightsActor.scala
+++ server/src/main/scala/actors/debug/DebugFlightsActor.scala
@@ -12,6 +12,7 @@ import uk.gov.homeoffice.drt.protobuf.messages.CrunchState.{
   FlightsWithSplitsDiffMessage
 }
 import uk.gov.homeoffice.drt.protobuf.messages.FlightsMessage.FlightsDiffMessage
+import scala.util.Try
 
 case class DebugState(lastSnapshot: Option[GeneratedMessage], messages: List[GeneratedMessage])
 
@@ -19,42 +20,101 @@ case class MessageQuery(numberOfMessages: Int)
 
 case class MessageResponse(messages: List[GeneratedMessage])
 
-class DebugFlightsActor(lookupId: String, maybePointInTime: Option[MillisSinceEpoch] = None) extends RecoveryActorLike {
+case object DebugStatsQuery
+
+case class DebugStatsResponse(
+    messages: List[GeneratedMessage],
+    totalRecovered: Int,
+    supportedTypeCount: Int,
+    includedMessageCount: Int,
+    unhandledTypeCounts: Map[String, Int],
+    snapshotClass: Option[String]
+)
+
+class DebugFlightsActor(lookupId: String, pointInTime: Option[MillisSinceEpoch] = None) extends RecoveryActorLike {
+
+  override val maybePointInTime: Option[MillisSinceEpoch] = pointInTime
 
   override val log: Logger = LoggerFactory.getLogger(getClass)
+
+  private val feedDiffMessageClasses: Set[String] = Set(
+    "uk.gov.homeoffice.drt.protobuf.messages.FeedArrivalsMessage.LiveFeedArrivalsDiffMessage",
+    "uk.gov.homeoffice.drt.protobuf.messages.FeedArrivalsMessage.ForecastFeedArrivalsDiffMessage"
+  )
 
   var snapshot: Option[GeneratedMessage] = None
 
   var messages: List[GeneratedMessage] = List()
 
+  var recoveredCount: Int = 0
+  var supportedTypeCount: Int = 0
+  var unhandledTypeCounts: Map[String, Int] = Map.empty
+
+  private def incrementUnhandled(other: Any): Unit = {
+    val className = other.getClass.getName
+    val nextCount = unhandledTypeCounts.getOrElse(className, 0) + 1
+    unhandledTypeCounts = unhandledTypeCounts + (className -> nextCount)
+  }
+
+  private def markRecovered(supported: Boolean): Unit = {
+    recoveredCount += 1
+    if (supported) supportedTypeCount += 1
+  }
+
+  private def addMessageIfInScope(message: GeneratedMessage, maybeCreatedAt: Option[Long]): Unit = {
+    if (includeByPointInTime(maybeCreatedAt)) {
+      messages = message :: messages
+    }
+  }
+
+  private def createdAtFromMessage(message: GeneratedMessage): Option[Long] = {
+    val methodValue = message.getClass.getMethods
+      .find(_.getName == "createdAt")
+      .flatMap { method =>
+        Try(method.invoke(message)).toOption
+      }
+
+    methodValue match {
+      case Some(v: java.lang.Long) => Option(v.longValue())
+      case Some(v: Long)           => Option(v)
+      case Some(v: Option[_])      => v.collect { case l: Long => l }
+      case Some(v: Some[_])        => v.collect { case l: Long => l }
+      case _                       => None
+    }
+  }
+
+  private def includeByPointInTime(maybeCreatedAt: Option[Long]): Boolean =
+    maybeCreatedAt.getOrElse(Long.MinValue) < maybePointInTime.getOrElse(Long.MaxValue)
+
   override def processRecoveryMessage: PartialFunction[Any, Unit] = {
 
     case CrunchDiffMessage(Some(createdAt), _, removals, updates, _, _, _, _)
         if removals.nonEmpty || updates.nonEmpty =>
-
-      if (createdAt < maybePointInTime.getOrElse(Long.MaxValue)) {
-        messages = FlightsWithSplitsDiffMessage(Option(createdAt), removals, updates) :: messages
-      }
+      markRecovered(supported = true)
+      addMessageIfInScope(FlightsWithSplitsDiffMessage(Option(createdAt), removals, updates), Option(createdAt))
 
     case FlightsDiffMessage(Some(createdAt), removals, updates, _) =>
+      markRecovered(supported = true)
 
-      if (createdAt < maybePointInTime.getOrElse(Long.MaxValue)) {
-        val fws = FlightsWithSplitsDiffMessage(
-          Option(createdAt),
-          removals,
-          updates.map(f => FlightWithSplitsMessage(Option(f), Seq()))
-        )
-        messages = fws :: messages
-      }
+      val fws = FlightsWithSplitsDiffMessage(
+        Option(createdAt),
+        removals,
+        updates.map(f => FlightWithSplitsMessage(Option(f), Seq()))
+      )
+      addMessageIfInScope(fws, Option(createdAt))
 
     case m @ FlightsWithSplitsDiffMessage(createdAt, _, _) =>
+      markRecovered(supported = true)
+      addMessageIfInScope(m, createdAt)
 
-      if (createdAt.getOrElse(Long.MinValue) < maybePointInTime.getOrElse(Long.MaxValue)) {
-        messages = m :: messages
-      }
+    case m: GeneratedMessage if feedDiffMessageClasses.contains(m.getClass.getName) =>
+      markRecovered(supported = true)
+      addMessageIfInScope(m, createdAtFromMessage(m))
 
     case other =>
-      log.info(s"Not handling ${other.getClass}")
+      markRecovered(supported = false)
+      incrementUnhandled(other)
+      log.debug(s"Not handling ${other.getClass}")
   }
 
   override def processSnapshotMessage: PartialFunction[Any, Unit] = {
@@ -69,6 +129,15 @@ class DebugFlightsActor(lookupId: String, maybePointInTime: Option[MillisSinceEp
       sender() ! DebugState(snapshot, messages)
     case MessageQuery(num) =>
       sender() ! MessageResponse(messages.take(num))
+    case DebugStatsQuery =>
+      sender() ! DebugStatsResponse(
+        messages = messages,
+        totalRecovered = recoveredCount,
+        supportedTypeCount = supportedTypeCount,
+        includedMessageCount = messages.size,
+        unhandledTypeCounts = unhandledTypeCounts,
+        snapshotClass = snapshot.map(_.getClass.getName)
+      )
   }
 
   override def persistenceId: String = lookupId
@@ -78,7 +147,7 @@ class DebugFlightsActor(lookupId: String, maybePointInTime: Option[MillisSinceEp
       Recovery(SnapshotSelectionCriteria(Long.MaxValue, maxTimestamp = Long.MaxValue, 0L, 0L))
     case Some(pointInTime) =>
       val criteria = SnapshotSelectionCriteria(maxTimestamp = pointInTime)
-      Recovery(fromSnapshot = criteria, replayMax = 1000)
+      Recovery(fromSnapshot = criteria)
   }
 
   override val maybeSnapshotInterval: Option[Int] = Option(1000)
--- server/src/main/scala/controllers/application/DebugController.scala
+++ server/src/main/scala/controllers/application/DebugController.scala
@@ -7,20 +7,311 @@ import actors.persistent.arrivals.{
   PortForecastArrivalsActor,
   PortLiveArrivalsActor
 }
+import actors.debug.{ DebugFlightsActor, DebugStatsQuery, DebugStatsResponse, MessageQuery, MessageResponse }
+import actors.persistent.arrivals.{
+  AclForecastArrivalsActor,
+  CiriumLiveArrivalsActor,
+  PortForecastArrivalsActor,
+  PortLiveArrivalsActor
+}
 import org.apache.pekko.actor.Props
 import org.apache.pekko.pattern.ask
 import com.google.inject.Inject
 import play.api.mvc.{ Action, AnyContent, ControllerComponents }
+import play.api.libs.json.{ JsObject, Json }
+import play.api.mvc.{ Action, AnyContent, ControllerComponents }
+import scalapb.GeneratedMessage
 import services.ActorTree
 import uk.gov.homeoffice.drt.auth.Roles.Debug
 import uk.gov.homeoffice.drt.crunchsystem.DrtSystemInterface
 import uk.gov.homeoffice.drt.ports.ApiFeedSource
 import uk.gov.homeoffice.drt.protobuf.messages.CrunchState.FlightsWithSplitsDiffMessage
-import uk.gov.homeoffice.drt.time.SDate
+import uk.gov.homeoffice.drt.time.{ SDate, SDateLike }
 
-import scala.collection.SortedMap
+import java.net.URLEncoder
+import java.nio.charset.StandardCharsets
+import scala.concurrent.Future
+import scala.util.matching.Regex
+import scala.util.Try
 
 class DebugController @Inject() (cc: ControllerComponents, ctrl: DrtSystemInterface) extends AuthController(cc, ctrl) {
+
+  private val liveFeedArrivalsDiffClass =
+    "uk.gov.homeoffice.drt.protobuf.messages.FeedArrivalsMessage.LiveFeedArrivalsDiffMessage"
+  private val forecastFeedArrivalsDiffClass =
+    "uk.gov.homeoffice.drt.protobuf.messages.FeedArrivalsMessage.ForecastFeedArrivalsDiffMessage"
+  private val feedDiffMessageClasses: Set[String] = Set(
+    liveFeedArrivalsDiffClass,
+    forecastFeedArrivalsDiffClass
+  )
+
+  private def encodePathSegment(value: String): String =
+    URLEncoder.encode(value, StandardCharsets.UTF_8.toString).replace("+", "%20")
+
+  private def traceActorIdsForPit(pit: SDateLike): Seq[(String, String)] = {
+    val day = f"${pit.getFullYear}-${pit.getMonth}%02d-${pit.getDate}%02d"
+
+    val staticActorIds = Seq(
+      "ACL (status actor)" -> AclForecastArrivalsActor.persistenceId,
+      "Port Forecast (status actor)" -> PortForecastArrivalsActor.persistenceId,
+      "Cirium Live (status actor)" -> CiriumLiveArrivalsActor.persistenceId,
+      "Port Live (status actor)" -> PortLiveArrivalsActor.persistenceId,
+      "Crunch State" -> "crunch-state",
+      "Flight State" -> "flight-state"
+    )
+
+    val dayActorIds = Seq(
+      "Manifests (day)" -> s"manifests-$day",
+      "Live Feed Arrivals (all terminals)" -> s"live-feed-arrivals--$day",
+      "Live Base Feed Arrivals (all terminals)" -> s"live-base-feed-arrivals--$day",
+      "Terminal Flights (all terminals)" -> s"terminal-flights--$day"
+    )
+
+    val terminalActorIds = airportConfig.terminalsForDate(pit.toLocalDate).toSeq.sortBy(_.toString).flatMap { t =>
+      val terminal = t.toString.toLowerCase
+      Seq(
+        s"Terminal Flights ${t.toString}" -> s"terminal-flights-$terminal-$day",
+        s"ACL Feed Arrivals ${t.toString}" -> s"acl-feed-arrivals-$terminal-$day",
+        s"Forecast Feed Arrivals ${t.toString}" -> s"forecast-feed-arrivals-$terminal-$day",
+        s"Live Feed Arrivals ${t.toString}" -> s"live-feed-arrivals-$terminal-$day"
+      )
+    }
+
+    (staticActorIds ++ dayActorIds ++ terminalActorIds).distinct
+  }
+
+  private def flightsUiActorIdsForPit(pit: SDateLike): Seq[(String, String)] = {
+    val day = f"${pit.getFullYear}-${pit.getMonth}%02d-${pit.getDate}%02d"
+
+    val dayActorIds = Seq(
+      "Manifests (day)" -> s"manifests-$day",
+      "Live Feed Arrivals (all terminals)" -> s"live-feed-arrivals--$day",
+      "Live Base Feed Arrivals (all terminals)" -> s"live-base-feed-arrivals--$day",
+      "Terminal Flights (all terminals)" -> s"terminal-flights--$day"
+    )
+
+    val terminalActorIds = airportConfig.terminalsForDate(pit.toLocalDate).toSeq.sortBy(_.toString).flatMap { t =>
+      val terminal = t.toString.toLowerCase
+      Seq(
+        s"Terminal Flights ${t.toString}" -> s"terminal-flights-$terminal-$day",
+        s"ACL Feed Arrivals ${t.toString}" -> s"acl-feed-arrivals-$terminal-$day",
+        s"Forecast Feed Arrivals ${t.toString}" -> s"forecast-feed-arrivals-$terminal-$day",
+        s"Live Feed Arrivals ${t.toString}" -> s"live-feed-arrivals-$terminal-$day"
+      )
+    }
+
+    (dayActorIds ++ terminalActorIds).distinct
+  }
+
+  private case class ActorTraceDiagnostics(
+      persistenceId: String,
+      messages: Seq[GeneratedMessage],
+      totalRecovered: Int,
+      supportedTypeCount: Int,
+      includedMessageCount: Int,
+      unhandledTypeCounts: Map[String, Int]
+  )
+
+  private def fetchFlightDiffMessagesWithDiagnostics(
+      persistenceId: String,
+      pit: SDateLike,
+      messages: Int
+  ): Future[ActorTraceDiagnostics] = {
+    val actor = actorSystem.actorOf(Props(new DebugFlightsActor(persistenceId, Option(pit.millisSinceEpoch))))
+    (actor ? DebugStatsQuery)
+      .mapTo[DebugStatsResponse]
+      .map { response =>
+        ActorTraceDiagnostics(
+          persistenceId = persistenceId,
+          messages = response.messages.take(messages),
+          totalRecovered = response.totalRecovered,
+          supportedTypeCount = response.supportedTypeCount,
+          includedMessageCount = response.includedMessageCount,
+          unhandledTypeCounts = response.unhandledTypeCounts
+        )
+      }
+      .andThen { case _ => actorSystem.stop(actor) }
+  }
+
+  private val createdAtRegex: Regex = "(?i)createdAt:\\s*(\\d+)".r
+  private val terminalRegex: Regex = "(?i)terminal:\\s*\"([^\"]+)\"".r
+  private val statusRegex: Regex = "(?i)status:\\s*\"([^\"]+)\"".r
+  private val gateRegex: Regex = "(?i)gate:\\s*\"([^\"]*)\"".r
+  private val standRegex: Regex = "(?i)stand:\\s*\"([^\"]*)\"".r
+  private val estimatedRegex: Regex = "(?i)estimated:\\s*(\\d+)".r
+  private val actualChoxRegex: Regex = "(?i)actualChox:\\s*(\\d+)".r
+  private val carrierFromFlightCodeRegex: Regex = """^([A-Z]{2,3})(\d+)""".r
+  private val protoCarrierRegex: Regex = "(?i)carrier(?:Code)?:\\s*\"([A-Z0-9]{2,3})\"".r
+  private val protoVoyageRegex: Regex = """(?i)(?:voyageNumber|number):\s*(\d+)""".r
+
+  private def firstMatch(regex: Regex, text: String): Option[String] =
+    regex.findFirstMatchIn(text).map(_.group(1))
+
+  private def flightCodeParts(flightCodeUpper: String): (Option[String], Option[String]) =
+    flightCodeUpper match {
+      case carrierFromFlightCodeRegex(carrier, voyage) => Option(carrier) -> Option(voyage)
+      case _                                           => None -> None
+    }
+
+  private def scheduledMatchTokens(millis: Long): Seq[String] = {
+    val seconds = millis / 1000
+    val dayToken = SDate(millis).toISOString.take(10)
+    Seq(millis.toString, seconds.toString, dayToken)
+  }
+
+  private def feedDiffEventsFromMessage(
+      message: GeneratedMessage,
+      persistenceId: String,
+      flightCodeUpper: String,
+      maybeCarrierCode: Option[String],
+      maybeVoyageNumber: Option[String],
+      maybeScheduledMillis: Option[Long]
+  ): Seq[(Long, String, JsObject)] = {
+    val className = message.getClass.getName
+    val isFeedDiff = feedDiffMessageClasses.contains(className)
+
+    if (!isFeedDiff) Seq.empty
+    else {
+      val proto = message.toProtoString
+      val protoUpper = proto.toUpperCase
+
+      val containsFlightCode = protoUpper.contains(flightCodeUpper)
+      val carrierMatches = maybeCarrierCode.exists { carrier =>
+        protoCarrierRegex.findAllMatchIn(protoUpper).exists(_.group(1).toUpperCase == carrier)
+      }
+      val voyageMatches = maybeVoyageNumber.exists { voyage =>
+        protoVoyageRegex.findAllMatchIn(proto).exists(_.group(1) == voyage)
+      }
+      val containsCarrierAndVoyage = carrierMatches && voyageMatches
+
+      val matchesScheduled = maybeScheduledMillis.forall { millis =>
+        scheduledMatchTokens(millis).exists(token => proto.contains(token))
+      }
+
+      val matchedBy = Seq(
+        Option.when(containsFlightCode)("iata"),
+        Option.when(containsCarrierAndVoyage)("carrier+voyage"),
+        Option.when(matchesScheduled)("scheduled-token")
+      ).flatten
+
+      if ((containsFlightCode || containsCarrierAndVoyage) && matchesScheduled) {
+        val createdAtMillis = firstMatch(createdAtRegex, proto).flatMap(v => Try(v.toLong).toOption).getOrElse(0L)
+        val terminal = firstMatch(terminalRegex, proto).getOrElse("")
+        val status = firstMatch(statusRegex, proto).getOrElse("")
+        val gate = firstMatch(gateRegex, proto).getOrElse("")
+        val stand = firstMatch(standRegex, proto).getOrElse("")
+        val estimated =
+          firstMatch(estimatedRegex, proto).flatMap(v => Try(v.toLong).toOption).map(v => SDate(v).toISOString)
+        val actualChox =
+          firstMatch(actualChoxRegex, proto).flatMap(v => Try(v.toLong).toOption).map(v => SDate(v).toISOString)
+
+        Seq((
+          createdAtMillis,
+          persistenceId,
+          Json.obj(
+            "eventType" -> "feed-diff-update",
+            "createdAt" -> SDate(createdAtMillis).toISOString,
+            "persistenceId" -> persistenceId,
+            "flightCode" -> flightCodeUpper,
+            "terminal" -> terminal,
+            "status" -> status,
+            "gate" -> gate,
+            "stand" -> stand,
+            "estimated" -> estimated,
+            "actualChox" -> actualChox,
+            "rawMessageClass" -> className,
+            "matchedBy" -> matchedBy
+          )
+        ))
+      } else {
+        Seq.empty
+      }
+    }
+  }
+
+  private def maybeScheduledMillisFromRequest(request: play.api.mvc.Request[AnyContent]): Option[Long] =
+    request.getQueryString("scheduled")
+      .flatMap(s => SDate.tryParseString(s).toOption.map(_.millisSinceEpoch))
+      .orElse(request.getQueryString("scheduledMillis").flatMap(v => Try(v.toLong).toOption))
+
+  private def scheduledMatches(maybeScheduledMillis: Option[Long], scheduledMillis: Long): Boolean =
+    maybeScheduledMillis.forall(_ == scheduledMillis)
+
+  private def flightNumberFromCode(flightCodeUpper: String): String =
+    """\d+""".r.findFirstIn(flightCodeUpper).getOrElse("")
+
+  private def optionToString(maybeValue: Option[Any]): String =
+    maybeValue.map(_.toString).getOrElse("")
+
+  private def traceEventsFromFlightsDiff(
+      diff: FlightsWithSplitsDiffMessage,
+      persistenceId: String,
+      flightCodeUpper: String,
+      flightNumberOnly: String,
+      maybeScheduledMillis: Option[Long]
+  ): Seq[(Long, String, JsObject)] = {
+    val createdAt = diff.createdAt.getOrElse(0L)
+
+    val updates: Seq[(Long, String, JsObject)] = diff.updates.collect {
+      case update
+          if update.getFlight.getIATA.toUpperCase == flightCodeUpper &&
+            scheduledMatches(maybeScheduledMillis, update.getFlight.getScheduled) =>
+        (
+          createdAt,
+          persistenceId,
+          Json.obj(
+            "eventType" -> "update",
+            "createdAt" -> SDate(createdAt).toISOString,
+            "persistenceId" -> persistenceId,
+            "flightCode" -> update.getFlight.getIATA,
+            "terminal" -> update.getFlight.getTerminal,
+            "scheduled" -> SDate(update.getFlight.getScheduled).toISOString,
+            "status" -> optionToString(update.getFlight.status),
+            "gate" -> optionToString(update.getFlight.gate),
+            "stand" -> optionToString(update.getFlight.stand),
+            "estimated" -> update.getFlight.estimated.map(e => SDate(e).toISOString),
+            "actualChox" -> update.getFlight.actualChox.map(c => SDate(c).toISOString)
+          )
+        )
+    }
+
+    val removals: Seq[(Long, String, JsObject)] = diff.removals.collect {
+      case removal
+          if flightNumberOnly.nonEmpty && removal.getNumber.toString == flightNumberOnly &&
+            scheduledMatches(maybeScheduledMillis, removal.getScheduled) =>
+        (
+          createdAt,
+          persistenceId,
+          Json.obj(
+            "eventType" -> "removal",
+            "createdAt" -> SDate(createdAt).toISOString,
+            "persistenceId" -> persistenceId,
+            "flightNumber" -> removal.getNumber,
+            "terminal" -> removal.getTerminalName,
+            "scheduled" -> SDate(removal.getScheduled).toISOString
+          )
+        )
+    }
+
+    updates ++ removals
+  }
+
+  private def actorDiagnosticsJson(
+      actorData: Seq[ActorTraceDiagnostics],
+      actorMatchCounts: Map[String, Int]
+  ): Seq[JsObject] =
+    actorData.map { data =>
+      val matchedEvents: Int = actorMatchCounts.getOrElse(data.persistenceId, 0)
+      Json.obj(
+        "persistenceId" -> data.persistenceId,
+        "matchedEvents" -> matchedEvents,
+        "decodedDiffMessages" -> data.messages.collect { case _: FlightsWithSplitsDiffMessage => 1 }.size,
+        "supportedTypeCount" -> data.supportedTypeCount,
+        "totalRecovered" -> data.totalRecovered,
+        "includedMessageCount" -> data.includedMessageCount,
+        "unhandledTypeCounts" -> data.unhandledTypeCounts
+      )
+    }
 
   def getActorTree: Action[AnyContent] = authByRole(Debug) {
     Action { _ =>
@@ -35,25 +326,16 @@ class DebugController @Inject() (cc: ControllerComponents, ctrl: DrtSystemInterf
   ): Action[AnyContent] = authByRole(Debug) {
     Action.async { _ =>
       val pit = SDate(dateString)
-      val persistenceIds = SortedMap(
-        "ACL" -> AclForecastArrivalsActor.persistenceId,
-        "Port Forecast" -> PortForecastArrivalsActor.persistenceId,
-        "Cirium Live" -> CiriumLiveArrivalsActor.persistenceId,
-        "Port Live" -> PortLiveArrivalsActor.persistenceId,
-        "Crunch State" -> "crunch-state",
-        "Flight State" -> "flight-state"
-      ) ++ airportConfig.terminalsForDate(SDate.now().toLocalDate).map(t => {
-        "Terminal Day Flight (for snapshot day)" ->
-          f"terminal-flights-${t.toString.toLowerCase}-${pit.getFullYear}-${pit.getMonth}%02d-${pit.getDate}%02d"
-      })
+      val persistenceIds = flightsUiActorIdsForPit(pit)
 
-      if (persistenceIds.keys.exists(_ == persistenceId)) throw new Exception("Invalid actor")
+      if (persistenceId.trim.isEmpty)
+        throw new Exception("Invalid actor")
 
       val actor = actorSystem.actorOf(Props(new DebugFlightsActor(persistenceId, Option(pit.millisSinceEpoch))))
 
       val actorSelection = persistenceIds.map {
         case (feed, id) =>
-          s"<a href='/debug/flights/$id/${pit.toISOString}/$messages'>$feed</a></br>"
+          s"<a href='/debug/flights/${encodePathSegment(id)}/${encodePathSegment(pit.toISOString)}/$messages'>$feed</a></br>"
       }.mkString("\n")
 
       val timeNavigation =
@@ -78,7 +360,7 @@ class DebugController @Inject() (cc: ControllerComponents, ctrl: DrtSystemInterf
       (actor ? MessageQuery(messages)).map {
         case m: MessageResponse =>
 
-          val debugHtml = m.messages.map {
+          val debugHtml = m.messages.collect {
             case f: FlightsWithSplitsDiffMessage =>
 
               val heading = s"<h3>Created at: ${f.createdAt.map(SDate(_).toISOString).getOrElse("Missing")}</h3>"
@@ -134,7 +416,7 @@ class DebugController @Inject() (cc: ControllerComponents, ctrl: DrtSystemInterf
               } else "No Updates </br>"
               val removals = if (f.removals.nonEmpty) {
 
-                "<table border='1' cellpadding='5' cellspacing='0'><tr>" +
+                "<table border='1' cellpadding='5' cellspacing='0'>" +
                   "<tr>" +
                   s"<td colspan='14'>Removals: </td>" +
                   "</tr>" +
@@ -158,4 +440,67 @@ class DebugController @Inject() (cc: ControllerComponents, ctrl: DrtSystemInterf
       }
     }
   }
+
+  def getFlightTraceAtTime(flightCode: String, dateString: String, messages: Int): Action[AnyContent] =
+    authByRole(Debug) {
+      Action.async { request =>
+        val pit = SDate(dateString)
+        val actorIds = traceActorIdsForPit(pit).map(_._2).distinct
+        val flightCodeUpper = flightCode.toUpperCase
+        val (maybeCarrierCode, maybeVoyageNumber) = flightCodeParts(flightCodeUpper)
+        val maybeScheduledMillis = maybeScheduledMillisFromRequest(request)
+        val flightNumberOnly = flightNumberFromCode(flightCodeUpper)
+
+        Future.sequence(actorIds.map(id => fetchFlightDiffMessagesWithDiagnostics(id, pit, messages))).map {
+          actorData =>
+            val events: Seq[(Long, String, JsObject)] = actorData.flatMap { data =>
+              data.messages.flatMap {
+                case diff: FlightsWithSplitsDiffMessage =>
+                  traceEventsFromFlightsDiff(
+                    diff = diff,
+                    persistenceId = data.persistenceId,
+                    flightCodeUpper = flightCodeUpper,
+                    flightNumberOnly = flightNumberOnly,
+                    maybeScheduledMillis = maybeScheduledMillis
+                  )
+
+                case message: GeneratedMessage =>
+                  feedDiffEventsFromMessage(
+                    message = message,
+                    persistenceId = data.persistenceId,
+                    flightCodeUpper = flightCodeUpper,
+                    maybeCarrierCode = maybeCarrierCode,
+                    maybeVoyageNumber = maybeVoyageNumber,
+                    maybeScheduledMillis = maybeScheduledMillis
+                  )
+              }
+            }
+
+            val sortedEvents: Seq[JsObject] = events.sortBy(_._1).map(_._3)
+            val actorMatchCounts: Map[String, Int] = events
+              .map(_._2)
+              .groupBy(identity)
+              .view
+              .mapValues(_.size)
+              .toMap
+            val actorsWithMatches = actorMatchCounts.keys.toSeq.sorted
+            val actorsWithNoMatches = actorIds.diff(actorsWithMatches)
+
+            val actorDiagnostics: Seq[JsObject] = actorDiagnosticsJson(actorData, actorMatchCounts)
+
+            Ok(Json.obj(
+              "flightCode" -> flightCodeUpper,
+              "pointInTime" -> pit.toISOString,
+              "scheduledFilterMillis" -> maybeScheduledMillis,
+              "messagesPerActor" -> messages,
+              "actorsChecked" -> actorIds,
+              "actorsWithMatches" -> actorsWithMatches,
+              "actorsWithNoMatches" -> actorsWithNoMatches,
+              "eventCount" -> sortedEvents.size,
+              "events" -> sortedEvents,
+              "actorDiagnostics" -> actorDiagnostics
+            ))
+        }
+      }
+    }
 }
