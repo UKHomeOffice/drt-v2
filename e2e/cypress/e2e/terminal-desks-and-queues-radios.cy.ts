4b7fdc2370c4fb22871ac54c0cc4e27d7e4ff2e8
slawekb <259381811+slawomirbedraHO@users.noreply.github.com>
2026-07-31 12:36:09 +0100

    DRTII-1830 | Another e2e fix.

--- e2e/cypress/e2e/terminal-desks-and-queues-radios.cy.ts
+++ e2e/cypress/e2e/terminal-desks-and-queues-radios.cy.ts
@@ -1,5 +1,14 @@
 describe('Terminal desks and queues radios', () => {
 
+  const radioSelectors = {
+    deskTypeRecommended: '#show-recs, input[type="radio"][name="deskType"][value="recommended"]',
+    deskTypeDeployments: '#show-deps, input[type="radio"][name="deskType"][value="deployments"]',
+    displayTypeTable: '#display-table, input[type="radio"][name="displayType"][value="table"]',
+    displayTypeCharts: '#display-charts, input[type="radio"][name="displayType"][value="charts"]',
+    displayIntervalQuarterly: '#display-quaterly-interval, input[type="radio"][name="displayInterval"][value="quarterly"]',
+    displayIntervalHourly: '#display-hourly-interval, input[type="radio"][name="displayInterval"][value="hourly"]',
+  }
+
   beforeEach(() => {
     cy.deleteData('')
       .addFlight({}, '')
@@ -22,42 +31,43 @@ describe('Terminal desks and queues radios', () => {
     cy.contains('.view-controls-label', 'View').should('be.visible')
     cy.contains('.view-controls-label', 'Time interval').should('be.visible')
 
-    cy.get('#show-recs').should('exist')
-    cy.get('#show-deps').should('exist')
-    cy.get('#display-table').should('exist')
-    cy.get('#display-charts').should('exist')
-    cy.get('#display-quaterly-interval').should('exist')
-    cy.get('#display-hourly-interval').should('exist')
+    cy.get(radioSelectors.deskTypeRecommended).should('exist')
+    cy.get(radioSelectors.deskTypeDeployments).should('exist')
+    cy.get(radioSelectors.displayTypeTable).should('exist')
+    cy.get(radioSelectors.displayTypeCharts).should('exist')
+    cy.get(radioSelectors.displayIntervalQuarterly).should('exist')
+    cy.get(radioSelectors.displayIntervalHourly).should('exist')
 
-    cy.get('#show-recs').check({force: true})
-    cy.get('#show-recs').should('be.checked')
+    cy.get(radioSelectors.deskTypeRecommended).first().check({force: true})
+    cy.get(radioSelectors.deskTypeRecommended).first().should('be.checked')
     cy.location('hash', {timeout: 10000}).should('include', 'viewType=ideal')
 
-    cy.get('#show-deps').check({force: true})
-    cy.get('#show-deps').should('be.checked')
+    cy.get(radioSelectors.deskTypeDeployments).first().check({force: true})
+    cy.get(radioSelectors.deskTypeDeployments).first().should('be.checked')
     cy.location('hash', {timeout: 10000}).should('include', 'viewType=deployments')
 
-    cy.get('#display-charts').check({force: true})
-    cy.get('#display-charts').should('be.checked')
+    cy.get(radioSelectors.displayTypeCharts).first().check({force: true})
+    cy.get(radioSelectors.displayTypeCharts).first().should('be.checked')
     cy.location('hash', {timeout: 10000}).should('include', 'displayType=charts')
     cy.get('table.user-desk-recs').should('not.exist')
     cy.get('.chart-container').should('be.visible')
 
-    cy.get('#display-table').check({force: true})
-    cy.get('#display-table').should('be.checked')
+    cy.get(radioSelectors.displayTypeTable).first().check({force: true})
+    cy.get(radioSelectors.displayTypeTable).first().should('be.checked')
     cy.location('hash', {timeout: 10000}).should('include', 'displayType=table')
     cy.get('table.user-desk-recs', {timeout: 10000}).should('be.visible')
 
-    cy.get('#display-quaterly-interval').check({force: true})
-    cy.get('#display-quaterly-interval').should('be.checked')
+    cy.get(radioSelectors.displayIntervalQuarterly).first().check({force: true})
+    cy.get(radioSelectors.displayIntervalQuarterly).first().should('be.checked')
     cy.get('table.user-desk-recs tbody tr', {timeout: 10000}).should('have.length', 96)
 
-    cy.get('#display-hourly-interval').check({force: true})
-    cy.get('#display-hourly-interval').should('be.checked')
-    cy.get('#display-quaterly-interval').should('not.be.checked')
+    cy.get(radioSelectors.displayIntervalHourly).first().check({force: true})
+    cy.get(radioSelectors.displayIntervalHourly).first().should('be.checked')
+    cy.get(radioSelectors.displayIntervalQuarterly).first().should('not.be.checked')
     cy.get('table.user-desk-recs tbody tr', {timeout: 10000}).should('have.length', 24)
   })
 })
+
 
 
 
