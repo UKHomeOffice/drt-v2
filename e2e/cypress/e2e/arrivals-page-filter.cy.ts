55ef979bcc3d57f61f7214835dd1917ebb3f6e2e
James Collins <james.collins@digital.homeoffice.gov.uk>
2026-09-17 17:27:31 +0100

    DRTII-1831 - Change select to match GDS

--- e2e/cypress/e2e/arrivals-page-filter.cy.ts
+++ e2e/cypress/e2e/arrivals-page-filter.cy.ts
@@ -4,49 +4,49 @@ import moment from "moment-timezone";
 
 describe('Arrivals page filter', () => {
 
-  beforeEach(function () {
-    cy.deleteData("");
-  });
+    beforeEach(function () {
+        cy.deleteData("");
+    });
 
-  it('Filters flights by any relevant time range intersecting the selected range', () => {
-    const flightTime: moment.Moment = todayAtUtc(16, 55);
-    const scheduledHour = flightTime.tz('Europe/London').format('HH');
-    const oneHourFromScheduled = (parseInt(scheduledHour) + 1)+':00'
+    it('Filters flights by any relevant time range intersecting the selected range', () => {
+        const flightTime: moment.Moment = todayAtUtc(16, 55);
+        const scheduledHour = flightTime.tz('Europe/London').format('HH');
+        const oneHourFromScheduled = (parseInt(scheduledHour) + 1) + ':00'
+        const twoHoursFromScheduled = (parseInt(scheduledHour) + 2) + ':00'
 
-    const todayYYYYMMDD = flightTime.format('YYYY-MM-DD');
+        cy.addFlight(
+            {
+                SchDT: todayAsLocalString(16, 55),
+                EstDT: todayAsLocalString(16, 5),
+                EstChoxDT: todayAsLocalString(16, 11),
+                ActDT: todayAsLocalString(16, 7),
+                ActChoxDT: todayAsLocalString(16, 45),
+                ActPax: 300,
+            },
+            ''
+        )
+            .asABorderForceOfficer()
+            .waitForFlightToAppear('TS0123')
+            .get('.arrival-datetime-pax-search')
+            .then(() => {
+                cy.get('#from-date').should('be.disabled')
+                    .get('#to-date').should('be.disabled')
 
-    cy.addFlight(
-      {
-        SchDT: todayAsLocalString(16, 55),
-        EstDT: todayAsLocalString(16, 5),
-        EstChoxDT: todayAsLocalString(16, 11),
-        ActDT: todayAsLocalString(16, 7),
-        ActChoxDT: todayAsLocalString(16, 45),
-        ActPax: 300,
-      },
-      ''
-    )
-      .asABorderForceOfficer()
-      .waitForFlightToAppear('TS0123')
-      .get('.arrival-datetime-pax-search')
-      .then(() => {
-        cy.contains('button', 'Custom')
-          .should('be.visible')
-          .click({ force: true })
-          .then(() => {
-            cy.wait(1000)
-            .get('div[role="combobox"]').eq(0).click()
-            .get(`[data-cy="select-start-time-option-${todayYYYYMMDD}_00:00"]`).click()
-            .get('div[role="combobox"]').eq(1).click()
-            .get(`[data-cy="select-end-time-option-${todayYYYYMMDD}_02:00"]`).click()
-            .get('#arrivals > div').contains('No flights to display')
-            .get('div[role="combobox"]').eq(0).click()
-            .get(`li[data-cy="select-start-time-option-${todayYYYYMMDD}_${scheduledHour}:00"]`).click()
-            .get('.arrivals__table__flight-code').contains('TS0123')
-            .get('div[role="combobox"]').eq(0).click()
-            .get(`li[data-cy="select-start-time-option-${todayYYYYMMDD}_${oneHourFromScheduled}"]`).click()
-            .get('.arrivals__table__flight-code').contains('TS0123')
-          });
-      });
-  });
+                cy.contains('button', 'Custom')
+                    .should('be.visible')
+                    .click({ force: true })
+                    .then(() => {
+                        cy.wait(1000)
+                            .get('#from-date').should('be.enabled').select('00:00')
+                            .get('#to-date').select('02:00 (+2 hours)')
+                            .get('#arrivals > div').contains('No flights to display')
+                            .get('#from-date').select(`${scheduledHour}:00`)
+                            .get('#to-date option:selected').should('have.text', `${oneHourFromScheduled} (+1 hours)`)
+                            .get('.arrivals__table__flight-code').contains('TS0123')
+                            .get('#from-date').select(oneHourFromScheduled)
+                            .get('#to-date option:selected').should('have.text', `${twoHoursFromScheduled} (+1 hours)`)
+                            .get('.arrivals__table__flight-code').contains('TS0123')
+                    });
+            });
+    });
 });
